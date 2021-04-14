b695eca715fa34f03ee973e63f3ac4393335b396
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-04-14 16:15:53 -0600

    Add files via upload

---
+++ script/c1.lua
@@ -0,0 +1,135 @@
+--Blue-Eyes White Dragon (DM)
+function c1.initial_effect(c)
+	--pendulum summon
+	aux.EnablePendulumAttribute(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--Fusion Attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetDescription(aux.Stringid(1,0))
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetTarget(c1.target)
+	e2:SetOperation(c1.operation)
+	c:RegisterEffect(e2)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(1,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetRange(LOCATION_PZONE)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetTarget(c1.sptg)
+	e3:SetOperation(c1.spop)
+	c:RegisterEffect(e3)
+	--Return
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(1,2))
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTarget(c1.rtg)
+	e4:SetOperation(c1.rop)
+	c:RegisterEffect(e4)
+end
+function c1.filter(c)
+	return c:IsFaceup() and c:IsType(TYPE_FUSION) and c:GetAttackedCount()==0 and c:GetFlagEffect(1)==0
+end
+function c1.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(c1.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.SetChainLimit(aux.FALSE)
+end
+function c1.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g=Duel.SelectMatchingCard(tp,c1.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	if g:GetCount()>0 then
+		local tc=g:GetFirst()
+		if Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)>0 then
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+			local g2=Duel.SelectMatchingCard(tp,nil,tp,0,LOCATION_MZONE,1,1,nil)
+			Duel.CalculateDamage(tc,g2:GetFirst())
+		else
+			Duel.Damage(1-tp,tc:GetAttack(),REASON_BATTLE)
+		end
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		tc:RegisterFlagEffect(1,RESET_EVENT+0x1ff0000+RESET_PHASE+PHASE_END,0,0)
+	end
+end
+function c1.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) and not e:GetHandler():IsStatus(STATUS_CHAINING) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c1.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_CANNOT_BE_FUSION_MATERIAL)
+		e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		e2:SetValue(1)
+		c:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_UNRELEASABLE_SUM)
+		e3:SetValue(1)
+		e3:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e3,true)
+		local e4=e3:Clone()
+		e4:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+		c:RegisterEffect(e4,true)
+		local e6=Effect.CreateEffect(c)
+		e6:SetType(EFFECT_TYPE_SINGLE)
+		e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e6:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+		e6:SetValue(1)
+		e6:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e6)
+		local e7=Effect.CreateEffect(c)
+		e7:SetType(EFFECT_TYPE_SINGLE)
+		e7:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+		e7:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+		e7:SetValue(1)
+		e7:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e7)
+	end
+end
+function c1.rtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc1=Duel.GetFieldCard(e:GetHandlerPlayer(),LOCATION_SZONE,6)
+	local tc2=Duel.GetFieldCard(e:GetHandlerPlayer(),LOCATION_SZONE,7)
+	if chk==0 then return e:GetHandler():CheckActivateEffect(false,false,false)~=nil and (not tc1 or not tc2) 
+		and not e:GetHandler():IsStatus(STATUS_CHAINING) end
+end
+function c1.rop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	local tpe=c:GetType()
+	local te=c:GetActivateEffect()
+	local tg=te:GetTarget()
+	local co=te:GetCost()
+	local op=te:GetOperation()
+	e:SetCategory(te:GetCategory())
+	e:SetProperty(te:GetProperty())
+	Duel.ClearTargetCard()
+	Duel.Hint(HINT_CARD,0,c:GetCode())
+	c:CreateEffectRelation(te)
+	if co then co(te,tp,eg,ep,ev,re,r,rp,1) end
+	if tg then tg(te,tp,eg,ep,ev,re,r,rp,1) end
+	Duel.BreakEffect()
+	if op then op(te,tp,eg,ep,ev,re,r,rp) end
+	c:ReleaseEffectRelation(te)
+end
