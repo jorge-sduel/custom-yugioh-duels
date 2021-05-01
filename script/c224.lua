6ff3d56584dee71f8bdbacb211285550d4b0f6e8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-04-30 21:46:03 -0600

    Update c224.lua

--- script/c224.lua
+++ script/c224.lua
@@ -1,15 +1,31 @@
---Borrel xyz link
-function c224.initial_effect(c)
+
+--Borrel xyz link
+function c224.initial_effect(c)
 	--xyz summon
 	c:EnableReviveLimit()
+	c224.xyz_filter=function(mc,ignoretoken,xyz,tp) return mc and mc:IsType(TYPE_LINK,xyz,SUMMON_TYPE_XYZ,tp) and (not mc:IsType(TYPE_TOKEN) or ignoretoken) end
+	c224.xyz_parameters={c224.xyz_filter,nil,2,nil,nil,2}
+	s.minxyzct=ct
+	s.maxxyzct=maxct
+	local chk1=Effect.CreateEffect(c)
+	chk1:SetType(EFFECT_TYPE_SINGLE)
+	chk1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+	chk1:SetCode(946)
+	chk1:SetCondition(Xyz.Condition(aux.FilterBoolFunctionEx(Card.IsType,TYPE_LINK),nil,2,2))
+	chk1:SetTarget(Xyz.Target(aux.FilterBoolFunctionEx(Card.IsType,TYPE_LINK),nil,2,2))
+	chk1:SetOperation(c224.xyzop)
+	c:RegisterEffect(chk1)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetDescription(1073)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(c224.xyzcon)
+	e1:SetCondition(Xyz.Condition(aux.FilterBoolFunctionEx(Card.IsType,TYPE_LINK),nil,2,2))
+	e1:SetTarget(Xyz.Target(aux.FilterBoolFunctionEx(Card.IsType,TYPE_LINK),nil,2,2))
 	e1:SetOperation(c224.xyzop)
 	e1:SetValue(SUMMON_TYPE_XYZ)
+	e1:SetLabelObject(chk1)
 	c:RegisterEffect(e1)
 	--cannot be target
 	local e2=Effect.CreateEffect(c)
@@ -29,7 +45,8 @@ function c224.initial_effect(c)
 	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e3:SetHintTiming(TIMING_DAMAGE_STEP+TIMING_END_PHASE)
 
-	e3:SetCountLimit(1)
+	e3:SetCountLimit(1)
+
 	e3:SetTarget(c224.atktg)
 	e3:SetOperation(c224.atkop)
 	c:RegisterEffect(e3)
@@ -39,40 +56,25 @@ e4:SetDescription(aux.Stringid(224,0))
 	e4:SetTarget(c224.target)
 	e4:SetOperation(c224.activate)
 	c:RegisterEffect(e4,false,REGISTER_FLAG_DETACH_XMAT)
-end
-function c224.ovfilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_LINK) and c:IsLink(4)
-end	
-function c224.xyzcon(e,c,og)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	return Duel.IsExistingMatchingCard(c224.ovfilter,tp,LOCATION_MZONE,0,2,nil)
 end
-function c224.xyzop(e,tp,eg,ep,ev,re,r,rp,c,og)
-	if chk==0 then return Duel.IsExistingMatchingCard(c224.ovfilter,tp,LOCATION_MZONE,0,2,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local g1=Duel.SelectMatchingCard(tp,c224.ovfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
-	local g2=Duel.SelectMatchingCard(tp,c224.ovfilter,tp,LOCATION_MZONE,0,1,1,g1:GetFirst())
-	og=Group.CreateGroup()
-	og:Merge(g1)
-	og:Merge(g2)
-	if g1:GetCount()>0 then
-		local mg1=g1:GetFirst():GetOverlayGroup()
-		if mg1:GetCount()~=0 then
-			og:Merge(mg1)
-			Duel.Overlay(g2:GetFirst(),mg1)
-		end
-		Duel.Overlay(g2:GetFirst(),g1)
-		local mg2=g2:GetFirst():GetOverlayGroup()
-		if mg2:GetCount()~=0 then
-			og:Merge(mg2)
-			Duel.Overlay(c,mg2)
-		end
-		c:SetMaterial(og)
-		Duel.Overlay(c,g2:GetFirst())	
+function c224.xyzop(e,tp,eg,ep,ev,re,r,rp,c,og,min,max)
+	local g=e:GetLabelObject()
+	if not g then return end
+	local remg=g:Filter(Card.IsHasEffect,nil,511002116)
+	remg:ForEach(function(c) c:RegisterFlagEffect(511002115,RESET_EVENT+RESETS_STANDARD,0,0) end)
+	g:Remove(Card.IsHasEffect,nil,511002116)
+	g:Remove(Card.IsHasEffect,nil,511002115)
+	local sg=Group.CreateGroup()
+	for tc in aux.Next(g) do
+		local sg1=tc:GetOverlayGroup()
+		sg:Merge(sg1)
 	end
-end
+	Duel.Overlay(c,sg)
+	c:SetMaterial(g)
+	Duel.Overlay(c,g:Filter(function(c) return c:GetEquipTarget() end,nil))
+	Duel.Overlay(c,g)
+	g:DeleteGroup()
+end
 function c224.efilter1(e,re,rp)
 	return re:IsActiveType(TYPE_MONSTER)
 end
