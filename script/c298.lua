0cfb53b53f954b4bf14d176ff183eed5c6683ebc
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-01 22:59:10 -0600

    Update c298.lua

--- script/c298.lua
+++ script/c298.lua
@@ -178,6 +178,46 @@ function c298.initial_effect(c)
 	e22:SetTarget(c298.sptg)
 	e22:SetOperation(c298.spop)
 	c:RegisterEffect(e22)
+  --effect gain
+	local e23=Effect.CreateEffect(c)
+	e23:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e23:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e23:SetRange(LOCATION_EXTRA)
+	e23:SetOperation(c298.negop)
+	local e24=Effect.CreateEffect(c)
+	e24:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e24:SetRange(LOCATION_MZONE)
+	e24:SetTargetRange(LOCATION_EXTRA,0)
+	e24:SetTarget(c298.eftg)
+	e24:SetLabelObject(e23)
+	c:RegisterEffect(e24)
+end
+function c298.eftg(e,c)
+	return c:IsType(TYPE_XYZ)
+end
+function s.target(e,tp,eg,ev,ep,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil,e:GetHandler()) and e:GetHandler():IsCanBeSpecialSummoned(e,SUMMON_TYPE_SPECIAL,tp,true,false) end
+	local tg=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil,e:GetHandler())
+	Duel.SetTargetCard(tg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.operation(e,tp,eg,ev,ep,re,r,rp)
+	local c=e:GetHandler()
+	local tg=Duel.GetTargetCards(e)
+	if c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SPECIAL,tp,true,false) and tg and #tg>0 then
+		local mg=tg:Clone()
+		local tc=tg:GetFirst()
+		while tc do
+			if tc:GetOverlayCount()~=0 then Duel.SendtoGrave(tc:GetOverlayGroup(),REASON_RULE) end
+			tc=tg:GetNext()
+		end
+		c:SetMaterial(mg)
+		Duel.Overlay(c,mg)
+		Duel.SpecialSummon(c,SUMMON_TYPE_SPECIAL,tp,tp,true,false,POS_FACEUP)
+	end
+end
+function s.filter1(c)
+	return c:IsAbleToHand() and c:IsCode(78509901)
 end
 function c298.tgval(e,c)
 	return c:IsType(TYPE_XYZ)
