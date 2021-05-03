997f640f9ee0229c718529924144fb155a86bc9c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-03 13:18:49 -0600

    Update c325.lua

--- script/c325.lua
+++ script/c325.lua
@@ -1,41 +1,40 @@
 --Synchro difraction
-local s,id=GetID()
-function s.initial_effect(c)
+function c325.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)
+	e1:SetTarget(c325.target)
+	e1:SetOperation(c325.activate)
 	c:RegisterEffect(e1)
 end
-function s.cfilter(c,e,tp,g,maxc)
+function c325.cfilter(c,e,tp,g,maxc)
 	local tmax=maxc
 	if c:GetSequence()<5 then tmax=tmax-1 end
 	if tmax<=0 then return end
 	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsAbleToExtraAsCost()
 		and g:CheckWithSumEqual(Card.GetLevel,c:GetLevel(),1,maxc)
 end
-function s.spfilter(c,e,tp)
+function c325.spfilter(c,e,tp)
 	return c:IsType(TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function c325.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	local maxc=ft+1
-	if maxc>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then maxc=1 end
+	if maxc>1 then maxc=1 end
 	if chk==0 then
 		if e:GetLabel()==0 then return false end
 		e:SetLabel(0)
-		local spg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
-		return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp,spg,maxc)
+		local spg=Duel.GetMatchingGroup(c325.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
+		return Duel.IsExistingMatchingCard(c325.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp,spg,maxc)
 	end
 	e:SetLabel(0)
-	local spg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
+	local spg=Duel.GetMatchingGroup(c325.spfilter,tp,LOCATION_EXTRA,0,nil,e,tp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local cg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp,spg,maxc)
+	local cg=Duel.SelectMatchingCard(tp,c325.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp,spg,maxc)
 	local lv=cg:GetFirst():GetLevel()
 	Duel.SendtoDeck(cg,nil,0,REASON_COST)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
