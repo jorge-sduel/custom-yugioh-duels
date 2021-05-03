d90ef3d4bd55d822c100ded39064212aeecc5d55
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-03 13:09:28 -0600

    Update c325.lua

--- script/c325.lua
+++ script/c325.lua
@@ -4,99 +4,52 @@ function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.spfilter(c,e,tp,mc)
-	return c:IsType(TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,true)
+function s.splimit(e,c,tp,sumtp,sumpos)
+	return sumtp&SUMMON_TYPE_SYNCHRO==SUMMON_TYPE_SYNCHRO
 end
-function s.rfilter(c,mc)
-	local mlv=mc:GetLevel(c)
-	if mlv==mc:GetLevel() then return false end
-	local lv=c:GetLevel()
-	return lv==(mlv&0xffff) or lv==(mlv>>16)
+function s.cfilter(c,e,tp,g,maxc)
+	local tmax=maxc
+	if c:GetSequence()<5 then tmax=tmax-1 end
+	if tmax<=0 then return end
+	return c:IsFaceup() and c:IsType(TYPE_SYNCHRO) and c:IsAbleToExtraAsCost()
+		and g:CheckWithSumEqual(Card.GetLevel,c:GetLevel(),1,maxc)
 end
-function s.filter(c,e,tp)
-	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,c,e,tp,c)
+function s.spfilter(c,e,tp)
+	return c:IsType(TYPE_SYNCHRO) and c:IsCanBeEffectTarget(e) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if c:IsLocation(LOCATION_MZONE) then ft=ft+1 end
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	return sg:IsExists(c325.rfilter,1,nil,c) or sg:CheckWithSumEqual(Card.GetLevel,c:GetLevel(),1,ft)
-end
-function s.mfilter(c)
-	return c:HasLevel() and c:IsType(TYPE_SYNCHRO) and c:IsAbleToGrave()
-end
-function s.mzfilter(c,tp)
-	return c:IsLocation(LOCATION_MZONE) and c:IsControler(tp) and c:GetSequence()<5
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local maxc=ft+1
+	if maxc>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then maxc=1 end
 	if chk==0 then
-		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-		if ft<0 then return false end
-		local mg=Duel.GetSynchroMaterial(tp)
-		if ft>0 then
-			local mg2=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_MZONE,0,nil)
-			mg:Merge(mg2)
-		else
-			mg=mg:Filter(s.mzfilter,nil,tp)
-		end
-		return mg:IsExists(s.filter,2,nil,e,tp)
+		if e:GetLabel()==0 then return false end
+		e:SetLabel(0)
+		local spg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+		return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp,spg,maxc)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+	e:SetLabel(0)
+	local spg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_GRAVE,0,nil,e,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local cg=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp,spg,maxc)
+	local lv=cg:GetFirst():GetLevel()
+	Duel.SendtoDeck(cg,nil,0,REASON_COST)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sg=spg:SelectWithSumEqual(tp,Card.GetLevel,lv,1,maxc)
+	Duel.SetTargetCard(sg)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,sg,#sg,0,0)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
-	if ft<0 then return end
-	local mg=Duel.GetSynchroMaterial(tp)
-	if ft>0 then
-		local mg2=Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_MZONE,0,nil)
-		mg:Merge(mg2)
-	else
-		mg=mg:Filter(Card.IsLocation,nil,LOCATION_MZONE)
-	end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-	local mat=mg:FilterSelect(tp,s.filter,1,1,nil,e,tp)
-	local mc=mat:GetFirst()
-	if not mc then return end
-	local sg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_EXTRA,0,mc,e,tp,mc)
-	if mc:IsLocation(LOCATION_MZONE) then ft=ft+1 end
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	local b1=sg:IsExists(s.rfilter,1,nil,mc)
-	local b2=sg:CheckWithSumEqual(Card.GetLevel,mc:GetLevel(),1,ft)
-	if b1 and (not b2 or Duel.SelectYesNo(tp,aux.Stringid(id,0))) then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:FilterSelect(tp,s.rfilter,1,1,nil,mc)
-		local tc=tg:GetFirst()
-		tc:SetMaterial(mat)
-		if not mc:IsLocation(LOCATION_EXTRA) then
-			Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_SYNCHRO)
-		else
-			Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_SYNCHRO)
-		end
-		Duel.BreakEffect()
-		Duel.SpecialSummon(tc,SUMMON_TYPE_SYNCHRO,tp,tp,false,true,POS_FACEUP)
-		tc:CompleteProcedure()
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local tg=sg:SelectWithSumEqual(tp,Card.GetLevel,mc:GetLevel(),1,ft)
-		local tc=tg:GetFirst()
-		for tc in aux.Next(tg) do
-			tc:SetMaterial(mat)
-		end
-		if not mc:IsLocation(LOCATION_MZONE) then
-			Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_SYNCHRO)
-		else
-			Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_SYNCHRO)
-		end
-		Duel.BreakEffect()
-		tc=tg:GetFirst()
-		for tc in aux.Next(tg) do
-			Duel.SpecialSummonStep(tc,SUMMON_TYPE_SYNCHRO,tp,tp,false,true,POS_FACEUP)
-			tc:CompleteProcedure()
-		end
-		Duel.SpecialSummonComplete()
-	end
+	local g=Duel.GetTargetCards(e)
+	local ct=#g
+	if ct==0 or (ct>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT))
+		or ct>Duel.GetLocationCount(tp,LOCATION_MZONE) then return end
+	Duel.SpecialSummon(g,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
 end
