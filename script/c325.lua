5f6579199ae54625196309ed0b8ab6653edea4de
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-03 16:02:12 -0600

    Update c325.lua

--- script/c325.lua
+++ script/c325.lua
@@ -46,6 +46,4 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if ct==0 or (ct>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT))
 		or ct>Duel.GetLocationCount(tp,LOCATION_MZONE) return end
 	Duel.SpecialSummon(g,SUMMON_TYPE_SYNCHRO,tp,tp,false,false,POS_FACEUP)
-		g:GetFirst():CompleteProcedure()
-	end
 end
