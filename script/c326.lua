057ae351599a50545bbc0e1df5462e1a726019a8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-08 17:45:02 -0600

    Add files via upload

---
+++ script/c326.lua
@@ -0,0 +1,147 @@
+--Armagedon knight
+function c326.initial_effect(c)
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,309,310)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(309,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c326.target)
+	e1:SetOperation(c326.operation)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(310,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(c326.target)
+	e2:SetOperation(c326.operation)
+	c:RegisterEffect(e2)
+	--special summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(310,0))
+	e3:SetCategory(CATEGORY_DISABLE)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCost(c326.cost)
+	e3:SetTarget(c326.target2)
+	e3:SetOperation(c326.operation2)
+	c:RegisterEffect(e3)
+	--Return
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(51,2))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetRange(LOCATION_EXTRA)
+	e4:SetCost(c326.cost1)
+	e4:SetOperation(c326.rop)
+	c:RegisterEffect(e4)
+	--special summon
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(309,6))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCost(c326.cost)
+	e5:SetTarget(c326.target3)
+	e5:SetOperation(c326.operation3)
+	c:RegisterEffect(e5)
+end
+function c326.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c326.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.PayLPCost(tp,1000)
+end
+function c326.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+	if chk==0 then return ft>2 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,327,0,TYPES_TOKEN,0,0,1,RACE_DRAGON,0) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,0)
+end
+function c326.operation(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft>1 and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) 
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,327,0,TYPES_TOKEN,0,0,1,RACE_DRAGON,0) then
+		for i=1,2 do
+			local token=Duel.CreateToken(tp,327)
+			Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
+function c326.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.disfilter1,tp,0,LOCATION_MZONE+LOCATION_SZONE+LOCATION_FZONE+LOCATION_PZONE,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(aux.disfilter1,tp,0,LOCATION_MZONE+LOCATION_SZONE+LOCATION_FZONE+LOCATION_PZONE,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
+end
+function c326.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(aux.disfilter1,tp,0,LOCATION_MZONE+LOCATION_SZONE+LOCATION_FZONE+LOCATION_PZONE,nil)
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+		if tc:IsType(TYPE_TRAPMONSTER) then
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+			tc:RegisterEffect(e3)
+		end
+	end
+end
+function c326.rop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+end
+function c326.target3(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,326)==0 end
+end
+function c326.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.RegisterFlagEffect(tp,326,RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,nil,aux.Stringid(326,2))
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_ADJUST)
+	e1:SetOperation(c326.operation)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function c326.filter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:GetFlagEffect(326)==0
+end
+function c326.operation3(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(c326.filter,tp,LOCATION_GRAVE,0,nil)
+	for tc in aux.Next(g) do
+		local te=tc:GetActivateEffect()
+		if te then
+			local e1=te:Clone()
+			e1:SetRange(LOCATION_GRAVE)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e1)
+		end
+		tc:RegisterFlagEffect(326,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	end
+end
