356396d026afb9e5167271ebcb31bbe2af49a1e7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-16 13:08:26 -0600

    Update c82353467.lua

--- script/c82353467.lua
+++ script/c82353467.lua
@@ -8,7 +8,7 @@ function c82353467.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTarget(c82353467.target)
+	e1:SetCondition(c82353467.condition)
 	e1:SetOperation(c82353467.operation)
 	e1:SetCountLimit(1)
 	c:RegisterEffect(e1)
@@ -20,36 +20,10 @@ function c82353467.condition(e,tp,eg,ep,ev,re,r,rp)
 			e:GetHandler():IsPosition(POS_FACEUP_ATTACK)
 end
 
---Negate Attack target
-function c82353467.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ng=Group.CreateGroup()
-	local dg=Group.CreateGroup()
-	for i=1,ev do
-		local te=Duel.GetChainInfo(i,CHAININFO_TRIGGERING_EFFECT)
-		local tc=te:GetHandler()
-		if te:IsHasType(EFFECT_TYPE_ACTIVATE) or te:IsActiveType(TYPE_MONSTER) then
-			ng:AddCard(tc)
-			if tc:IsRelateToEffect(te) then
-				dg:AddCard(tc)
-			end
-		end
-	end
-	if chk==0 then return #ng>0 end
-	Duel.SetTargetCard(dg)
-	Duel.SetOperationInfo(0,CATEGORY_NEGATE,ng,#ng,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,#dg,0,0)
-end
-
 --Negate Attack operation
 function c82353467.operation(e,tp,eg,ep,ev,re,r,rp)
-	local dg=Group.CreateGroup()
-	for i=1,ev do
-		local te=Duel.GetChainInfo(i,CHAININFO_TRIGGERING_EFFECT)
-		local tc=te:GetHandler()
-		if (te:IsHasType(EFFECT_TYPE_ACTIVATE) or te:IsActiveType(TYPE_MONSTER)) and Duel.NegateActivation(i) 
-			and tc:IsRelateToEffect(e) and tc:IsRelateToEffect(te) then
-			dg:AddCard(tc)
-		end
-	end
-	Duel.Destroy(dg,REASON_EFFECT)
+	local c = e:GetHandler()
+	if not c:IsRelateToEffect(e) or not c:IsPosition(POS_FACEUP_ATTACK) then return end
+	Duel.ChangePosition(c,POS_FACEUP_DEFENCE,POS_FACEDOWN_DEFENCE,0,0)
+	Duel.NegateAttack()
 end
