8c1ab4491fccf41a551da46539d6828a50e55af6
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-16 09:34:50 -0600

    Update c272.lua

--- script/c272.lua
+++ script/c272.lua
@@ -76,7 +76,9 @@ function c272.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c272.recop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-end
+			e:GetHandler():SetTurnCounter(0)
+end
+
 function c272.discon(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer()
 end
