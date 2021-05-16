514bd8e507f4cf43cf6de1b59c7beba3211e8ce1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-16 09:52:13 -0600

    Update c277.lua

--- script/c277.lua
+++ script/c277.lua
@@ -94,7 +94,7 @@ function c277.disop(e,tp,eg,ep,ev,re,r,rp)
 end
 function c277.spcon(e,c)
 	if c==nil then return true end
-	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and c:GetTurnCounter()>=e:GetHandler():GetLevel()
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and c:GetTurnCounter()>=e:GetHandler():GetLevel() and  c:IsPublic()
 end
 function c277.condition(e,tp,eg,ep,ev,re,r,rp)
 	local bc=e:GetHandler():GetBattleTarget()
