e78c22b6e860496d29ee444db317587a7c7d8ea4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-16 09:36:38 -0600

    Update c280.lua

--- script/c280.lua
+++ script/c280.lua
@@ -94,7 +94,9 @@ function c280.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c280.recop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-end
+			e:GetHandler():SetTurnCounter(0)
+end
+
 function c280.discon(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer()
 end
