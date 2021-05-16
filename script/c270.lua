1117cab12d786f48ea43ffee5a23a35ec1299aa4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-16 09:55:51 -0600

    Update c270.lua

--- script/c270.lua
+++ script/c270.lua
@@ -91,7 +91,7 @@ function c270.disop(e,tp,eg,ep,ev,re,r,rp)
 end
 function c270.spcon(e,c)
 	if c==nil then return true end
-	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and c:GetTurnCounter()>=e:GetHandler():GetLevel()
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and c:GetTurnCounter()>=e:GetHandler():GetLevel() and  c:IsPublic()
 end
 function c270.cost1(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
