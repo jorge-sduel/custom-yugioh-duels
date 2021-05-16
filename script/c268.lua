db78f641c3b11616d80a5d38155b72f374a07dc5
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-16 09:29:00 -0600

    Update c268.lua

--- script/c268.lua
+++ script/c268.lua
@@ -75,7 +75,9 @@ function c268.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function c268.recop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-end
+	e:GetHandler():SetTurnCounter(0)
+end
+
 function c268.discon(e,tp,eg,ep,ev,re,r,rp)
 	return tp~=Duel.GetTurnPlayer()
 end
@@ -91,7 +93,7 @@ function c268.disop(e,tp,eg,ep,ev,re,r,rp)
 end
 function c268.spcon(e,c)
 	if c==nil then return true end
-	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and c:GetTurnCounter()>=e:GetHandler():GetLevel()
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0 and c:GetTurnCounter()>=e:GetHandler():GetLevel() and  c:IsPublic() 
 end
 function c268.thcon(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
