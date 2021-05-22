7e4da1bd2591aacac762841c786f2783c7c3694a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-22 15:27:12 -0600

    Update c329.lua

--- script/c329.lua
+++ script/c329.lua
@@ -2,15 +2,14 @@
 function c329.initial_effect(c)
 	c:EnableCounterPermit(0x1106)
 	c:SetCounterLimit(0x1106,1)
-	--atk
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_SET_ATTACK)
-	e2:SetCondition(c329.condition)
-	e2:SetValue(c329.atkval)
-	c:RegisterEffect(e2)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e1:SetCondition(c329.con)
+	e1:SetOperation(c329.op)
+	e1:SetRange(LOCATION_MZONE)
+	c:RegisterEffect(e1)
 	--add counter
 	local e3=Effect.CreateEffect(c)
 e3:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
@@ -21,9 +20,36 @@ e3:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGN
 	e3:SetOperation(c329.ctop)
 	c:RegisterEffect(e3)
 end
-function c329.condtion(e)
-	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL
-		and Duel.GetAttacker()==e:GetHandler() and Duel.GetAttackTarget()~=nil
+function c329.con(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if not d then return false end
+	local g=Group.FromCards(a,d)
+	return g:IsExists(c45.filter,1,nil,1)
+end
+function c329.filter(c,ct)
+	return c:GetCounter(0x1106)==ct and c:IsFaceup() and c:IsCode(329)
+end
+function c329.op(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	if not d then return end
+	local g=Group.FromCards(a,d)
+	g=g:Filter(c329.filter,nil,1)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		if tc==a then
+			e1:SetValue(d:GetAttack())
+		else
+			e1:SetValue(a:GetAttack())
+		end
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE)
+		tc:RegisterEffect(e1)
+		tc=g:GetNext()
+	end
 end
 function c329.atkval(e,c)
 	return Duel.GetAttackTarget():GetAttack()
