95ca98e95d433b9d8761f34df32508b786348929
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-22 14:55:28 -0600

    Create c329.lua

--- script/c329.lua
+++ script/c329.lua
@@ -7,7 +7,8 @@ function c329.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e2:SetCode(EFFECT_SET_ATTACK)
+
 	e2:SetValue(c329.atkval)
 	c:RegisterEffect(e2)
 	--add counter
