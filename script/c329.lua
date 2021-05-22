d80695a1f407aa5f5e29c628841f62e82a891db7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-22 15:39:29 -0600

    Create c329.lua

--- script/c329.lua
+++ script/c329.lua
@@ -2,13 +2,13 @@
 function c329.initial_effect(c)
 	c:EnableCounterPermit(0x1106)
 	c:SetCounterLimit(0x1106,1)
-	--
+	--atk
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e1:SetCondition(c329.con)
-	e1:SetOperation(c329.op)
-	e1:SetRange(LOCATION_MZONE)
+	e1:SetDescription(aux.Stringid(26593852,0))
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BATTLE_START)
+	e1:SetTarget(c329.atktg)
+	e1:SetOperation(c329.atkop)
 	c:RegisterEffect(e1)
 	--add counter
 	local e3=Effect.CreateEffect(c)
@@ -19,40 +19,6 @@ e3:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGN
 	e3:SetCode(EVENT_PHASE_START+PHASE_END)
 	e3:SetOperation(c329.ctop)
 	c:RegisterEffect(e3)
-end
-function c329.con(e,tp,eg,ep,ev,re,r,rp)
-	local a=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
-	if not d then return false end
-	local g=Group.FromCards(a,d)
-	return g:IsExists(c45.filter,1,nil,1)
-end
-function c329.filter(c,ct)
-	return c:IsFaceup() and c:IsCode(329)
-end
-function c329.op(e,tp,eg,ep,ev,re,r,rp)
-	local a=Duel.GetAttacker()
-	local d=Duel.GetAttackTarget()
-	if not d then return end
-	local g=Group.FromCards(a,d)
-	g=g:Filter(c329.filter,nil,1)
-	local tc=g:GetFirst()
-	while tc do
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		if tc==a then
-			e1:SetValue(d:GetAttack())
-		else
-			e1:SetValue(a:GetAttack())
-		end
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE)
-		tc:RegisterEffect(e1)
-		tc=g:GetNext()
-	end
-end
-function c329.atkval(e,c)
-	return Duel.GetAttackTarget():GetAttack()
 end
 function c329.ctop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsPlayerAffectedByEffect(tp,100100090) then return end
