298530a5a1b5a50bddb36eb32e664deacf69fc85
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-22 16:19:34 -0600

    Create c329.lua

--- script/c329.lua
+++ script/c329.lua
@@ -2,13 +2,6 @@
 function c329.initial_effect(c)
 	c:EnableCounterPermit(0x1106)
 	c:SetCounterLimit(0x1106,1)
-	--atk
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(26593852,0))
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_BATTLE_START)
-	e1:SetOperation(c329.atkop)
-	c:RegisterEffect(e1)
 	--add counter
 	local e3=Effect.CreateEffect(c)
 e3:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
