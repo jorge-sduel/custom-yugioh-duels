3229e702a5690ccefa97ea9540ed5af16a2b6ce1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-22 15:50:21 -0600

    Update c329.lua

--- script/c329.lua
+++ script/c329.lua
@@ -24,3 +24,22 @@ function c329.ctop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsPlayerAffectedByEffect(tp,100100090) then return end
 	e:GetHandler():AddCounter(0x1106,1)
 end
+function c329.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local tc=Duel.GetAttacker()
+	if tc==c then tc=Duel.GetAttackTarget() end
+	if chk==0 then return tc and tc:IsFaceup() end
+end
+function c329.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetAttacker()
+	if tc==c then tc=Duel.GetAttackTarget() end
+	if tc:IsRelateToBattle() and c:IsRelateToBattle() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
+		e1:SetValue(tc:GetAttack())
+		c:RegisterEffect(e1)
+	end
+end
