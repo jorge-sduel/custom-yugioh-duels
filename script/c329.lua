97d1b76f746d688dcea252ad7f1a2c6da9f2149a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-22 16:28:48 -0600

    Create c329.lua

--- script/c329.lua
+++ script/c329.lua
@@ -2,7 +2,16 @@
 function c329.initial_effect(c)
 	c:EnableCounterPermit(0x1106)
 	c:SetCounterLimit(0x1106,1)
-	--add counter
+	--atk up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(c329.atkcon)
+	e1:SetValue(c329.atkval)
+	c:RegisterEffect(e1)
+--
 	local e3=Effect.CreateEffect(c)
 e3:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
@@ -12,18 +21,14 @@ e3:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_IGN
 	e3:SetOperation(c329.ctop)
 	c:RegisterEffect(e3)
 end
+function c329.atkcon(e)
+	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL
+		and e:GetHandler()==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil
+end
+function c329.atkval(e,c)
+	return Duel.GetAttackTarget():GetAttack()
+end
 function c329.ctop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.IsPlayerAffectedByEffect(tp,100100090) then return end
 	e:GetHandler():AddCounter(0x1106,1)
-end
-function 329.atkop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetAttackTarget()
-	if tc:IsRelateToBattle() then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_DAMAGE)
-		e1:SetValue(tc:GetAttack())
-		c:RegisterEffect(e1)
-	end
 end
