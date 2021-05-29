2ecdec1aaa25ebadef585bbb2cf3ddf3fa12fb64
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-05-29 07:29:33 -0600

    Update c12340324.lua

--- script/c12340324.lua
+++ script/c12340324.lua
@@ -127,7 +127,7 @@ function c12340324.spop(e,tp,eg,ep,ev,re,r,rp)
 	local e4=Effect.CreateEffect(e:GetHandler())
 		e4:SetType(EFFECT_TYPE_SINGLE)
 		e4:SetCode(EFFECT_SET_DEFENSE)
-		e4:SetValue(1000)
+		e4:SetValue(g*1000)
 		e4:SetReset(RESET_EVENT+0x1fe0000)
 		tc:RegisterEffect(e4)
 end
