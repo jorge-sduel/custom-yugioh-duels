240f9cd891e8cb7203222e690d0ca05a857eb479
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-06-06 09:14:07 -0600

    Add files via upload

---
+++ script/c136.lua
@@ -0,0 +1,46 @@
+--mirror 2
+function c136.initial_effect(c)
+	Pendulum.AddProcedure(c)
+	--lower
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(136,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCost(c136.cost)
+	e1:SetTarget(c136.sptg1)
+	e1:SetOperation(c136.spop1)
+	c:RegisterEffect(e1)
+end
+function c136.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c136.spfilter1(c,e,tp,ac)
+	return c:IsCode(137)
+end
+function c136.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c136.spfilter1,tp,LOCATION_DECK,0,1,nil,e,tp,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c136.spop1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c136.spfilter1,tp,LOCATION_DECK,0,1,1,nil,e,tp,c)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local at=Duel.GetAttacker()
+		if at:IsAttackable() and not at:IsImmuneToEffect(e) and Duel.ChangeAttackTarget(tc) then
+			Duel.BreakEffect()
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e3:SetReset(RESET_EVENT+0x1fe0000)
+			e3:SetValue(math.ceil(at:GetAttack()/2))
+			at:RegisterEffect(e3)
+		end
+	end
+end
---
+++ script/c137.lua
@@ -0,0 +1,70 @@
+--mirror 3
+function c137.initial_effect(c)
+	Pendulum.AddProcedure(c)
+	--negate attack
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(137,0))
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetHintTiming(0,TIMING_BATTLE_PHASE)
+	e1:SetCost(c137.cost)
+	e1:SetTarget(c137.atktg)
+	e1:SetOperation(c137.atkop)
+	c:RegisterEffect(e1)
+  --target
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(137,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetHintTiming(0,TIMING_BATTLE_PHASE)
+	e2:SetCost(c137.cost)
+	e2:SetTarget(c137.target)
+	e2:SetOperation(c137.activate)
+	c:RegisterEffect(e2)
+end
+function c137.filter(c)
+	return c:IsFaceup() and c:IsAttackBelow(1000)
+end
+function c137.con(e,c)
+	if c==nil then return true end
+	return Duel.IsExistingMatchingCard(c137.filter,c:GetControler(),0,LOCATION_MZONE,1,nil)
+end
+function c137.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c137.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function c137.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CANNOT_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,2)
+		tc:RegisterEffect(e1)
+	end
+end
+function c137.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(nil,tp,LOCATION_MZONE,0,1,Duel.GetAttackTarget()) end
+end
+function c137.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.SelectMatchingCard(tp,nil,tp,LOCATION_MZONE,0,1,1,Duel.GetAttackTarget())
+	if g:GetCount()>0 then
+		local a=Duel.GetAttacker()
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+		e1:SetValue(a:GetAttack())
+		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_DAMAGE_CAL)
+		a:RegisterEffect(e1)
+		Duel.HintSelection(g)
+		Duel.ChangeAttackTarget(g:GetFirst())
+	end
+end
