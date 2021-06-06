ea43b14c5471af560b57a09a68c66d49861ce065
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-06-06 10:58:11 -0600

    Update c136.lua

--- script/c136.lua
+++ script/c136.lua
@@ -17,30 +17,31 @@ function c136.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsReleasable() end
 	Duel.Release(e:GetHandler(),REASON_COST)
 end
-function c136.spfilter1(c,e,tp,ac)
-	return c:IsCode(137)
+function c136.spfilter(c,e,tp)
+	return c:IsCode(137) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c136.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+function c316.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c136.spfilter1,tp,LOCATION_DECK,0,1,nil,e,tp,e:GetHandler()) end
+		and Duel.IsExistingMatchingCard(c136.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
 end
-function c136.spop1(e,tp,eg,ep,ev,re,r,rp)
+function c136.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c136.spfilter1,tp,LOCATION_DECK,0,1,1,nil,e,tp,c)
+	local g=Duel.SelectMatchingCard(tp,c136.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
-		local at=Duel.GetAttacker()
-		if at:IsAttackable() and not at:IsImmuneToEffect(e) and Duel.ChangeAttackTarget(tc) then
+		local a=Duel.GetAttacker()
+		local ag=a:GetAttackableTarget()
+		if a:CanAttack() and not a:IsImmuneToEffect(e) and ag:IsContains(tc) then
 			Duel.BreakEffect()
-			local e3=Effect.CreateEffect(c)
-			e3:SetType(EFFECT_TYPE_SINGLE)
-			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
-			e3:SetReset(RESET_EVENT+0x1fe0000)
-			e3:SetValue(math.ceil(at:GetAttack()/2))
-			at:RegisterEffect(e3)
+			Duel.ChangeAttackTarget(tc)
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e1:SetValue(math.ceil(a:GetAttack()/2))
+			a:RegisterEffect(e1)
 		end
 	end
-end
+end
