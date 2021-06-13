dd42e30bde31ee38baf564ab797ed7feb07ce95d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-06-13 07:35:11 -0600

    Add files via upload

---
+++ c135.lua
@@ -0,0 +1,46 @@
+--mirror 1
+function c135.initial_effect(c)
+	Pendulum.AddProcedure(c)
+	--lower
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(135,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCost(c135.cost)
+	e1:SetTarget(c135.sptg1)
+	e1:SetOperation(c135.spop1)
+	c:RegisterEffect(e1)
+end
+function c135.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function c135.spfilter1(c,e,tp,ac)
+	return c:IsCode(136)
+end
+function c135.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c135.spfilter1,tp,LOCATION_DECK,0,1,nil,e,tp,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function c135.spop1(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,c135.spfilter1,tp,LOCATION_DECK,0,1,1,nil,e,tp,c)
+	local tc=g:GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)~=0 then
+		local at=Duel.GetAttacker()
+		if at:IsAttackable() and not at:IsImmuneToEffect(e) and Duel.ChangeAttackTarget(tc) then
+			Duel.BreakEffect()
+			local e3=Effect.CreateEffect(c)
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_SET_ATTACK_FINAL)
+			e3:SetReset(RESET_EVENT+0x1fe0000)
+			e3:SetValue(math.ceil(at:GetAttack()-1000))
+			at:RegisterEffect(e3)
+		end
+	end
+end
