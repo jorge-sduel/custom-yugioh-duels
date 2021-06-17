19d07c707e17f69416f626bead31ab7cb19903ac
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-06-17 17:07:48 -0600

    Update c333.lua

--- script/c333.lua
+++ script/c333.lua
@@ -55,19 +55,14 @@ function s.penfilter(c,e,tp,lscale,rscale)
 		and ((lv>p:GetRightScale() and lv<5) or (lv<p:GetLeftScale() and lv>0) or (Rk>p:GetRightScale() and Rk<5) or (Rk<p:GetLeftScale() and Rk>0) or c:IsHasEffect(511004423) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_PENDULUM,tp,true,false)
 		and not c:IsForbidden() and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_PENDULUM,tp,false,true))
 end
-function c333.penop(e,tp,eg,ep,ev,re,r,rp,c,og)
+function s.penop(e,tp,eg,ep,ev,re,r,rp,c,og)
 	local ft=Duel.GetLocationCountFromEx(tp)
-	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
-	ft=math.min(ft,aux.CheckSummonGate(tp) or ft)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,Pendulum.Filter,tp,LOCATION_EXTRA,0,Duel.IsSummonCancelable() and 0 or 1,ft,nil,e,tp,lscale,rscale)
-	if g then
-		sg:Merge(g)
-	end
-	if #sg>0 then
-		Duel.Hint(HINT_CARD,0,id)
-		if not inchain then
-			Duel.RegisterFlagEffect(tp,10000000,RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
-		end
+	local g=Duel.SelectMatchingCard(tp,s.penfilter,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,ft,nil,e,tp,lscale,rscale)
+	og:Merge(g)
+	local tc=og:GetFirst()
+	if og:GetCount()>0 then
+		og:KeepAlive()
+		tc=og:GetNext()
 	end
 end
