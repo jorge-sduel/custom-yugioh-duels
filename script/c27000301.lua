42f49c402153df403a947c15dc7d4bf33f5fcfd8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-06-18 21:57:42 -0600

    Update c27000301.lua

--- script/c27000301.lua
+++ script/c27000301.lua
@@ -65,16 +65,7 @@ function c27000301.initial_effect(c)
 		e5:SetTargetRange(1,0)
 		e5:SetValue(c27000301.REFCon)
 	c:RegisterEffect(e5)
-	--Duel.AddCustomActivityCounter(27000301,ACTIVITY_SPSUMMON,c27000301.counterfilter)
 end
--- [Global Check: Special Summon Check]
---function c27000301.counterfilter(c)
---	if not (c:IsSetCard(0xbf) or c:IsSetCard(0xc0)) then
---		return false
---	else
---		return true
---	end
---end
  -- {Pendulum Summon Limit: Charmers & Familiar-Possessed}
  function c27000301.PENDLimit(e,c,sump,sumtype,sumpos,targetp)
 	if c:IsSetCard(0xbf) or c:IsSetCard(0xc0) then return false end
