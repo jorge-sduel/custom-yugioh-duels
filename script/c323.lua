533707e8bf434c010fc7adbd3469724a1c8e9106
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-06-30 13:31:33 -0600

    Update c323.lua

--- script/c323.lua
+++ script/c323.lua
@@ -1,4 +1,4 @@
---last draw
+--Master Rule 3.5
 local s,id=GetID()
 function s.initial_effect(c)
 	aux.EnableExtraRules(c,s,s.WorldStart)
@@ -7,11 +7,6 @@ function s.WorldStart()
 	local e3=Effect.GlobalEffect()
 	e3:SetType(EFFECT_TYPE_FIELD)
 	e3:SetCode(EFFECT_BECOME_LINKED_ZONE)
-	e3:SetValue(0x17)
+	e3:SetValue(0xffffff)
 	Duel.RegisterEffect(e3,0)
-	local e4=Effect.GlobalEffect()
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetCode(EFFECT_BECOME_LINKED_ZONE)
-	e4:SetValue(0x08)
-	Duel.RegisterEffect(e4,0)
 end
