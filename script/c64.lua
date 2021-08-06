9bc40dbd9afdd9efb519643c510a219f34323cda
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-08-05 20:46:31 -0600

    Update c64.lua

--- script/c64.lua
+++ script/c64.lua
@@ -3,16 +3,6 @@ function c64.initial_effect(c)
 	--dark synchro summon
 	c:EnableReviveLimit()
 Synchro.AddDarkSynchroProcedure(c,Synchro.NonTunerEx(Card.IsType,TYPE_SYNCHRO),aux.FilterBoolFunctionEx(Card.IsType,TYPE_SYNCHRO),1)
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SPSUMMON_PROC)
-	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(c64.syncon)
-	e1:SetOperation(c64.synop)
-	e1:SetValue(SUMMON_TYPE_SYNCHRO)
-	c:RegisterEffect(e1)
-
  --battle indes
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
@@ -38,53 +28,6 @@ Synchro.AddDarkSynchroProcedure(c,Synchro.NonTunerEx(Card.IsType,TYPE_SYNCHRO),a
 	e6:SetCode(EFFECT_ADD_SETCODE)
 	e6:SetValue(0x601)
 	c:RegisterEffect(e6)
-end
-function c64.matfilter1(c,syncard)
-	return c:IsType(TYPE_TUNER) and c:IsSetCard(0x600) and c:IsType(TYPE_SYNCHRO) and c:IsFaceup() and c:IsCanBeSynchroMaterial(syncard)
-end
-function c64.matfilter2(c,syncard)	
-	return c:IsFaceup() and c:IsCanBeSynchroMaterial(syncard) and c:IsType(TYPE_SYNCHRO) and not c:IsType(TYPE_TUNER)
-end
-function c64.synfilter1(c,lv,g1,g2)
-	local tlv=c:GetLevel()	
-	if c:GetFlagEffect(64)==0 then	
-	return g1:IsExists(c64.synfilter3,1,nil,lv+tlv)
-	else
-	return g1:IsExists(c64.synfilter3,1,nil,lv-tlv)
-	end	
-end
-function c64.synfilter3(c,lv)
-	return c:GetLevel()==lv
-end
-function c64.syncon(e,c,tuner)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<-1 then return false end
-	local g1=Duel.GetMatchingGroup(c64.matfilter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-	local g2=Duel.GetMatchingGroup(c64.matfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-	local lv=c:GetLevel()
-	return g2:IsExists(c64.synfilter1,1,nil,lv,g1,g2)
-end
-function c64.synop(e,tp,eg,ep,ev,re,r,rp,c,tuner)
-	local g=Group.CreateGroup()
-	local g1=Duel.GetMatchingGroup(c64.matfilter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-	local g2=Duel.GetMatchingGroup(c64.matfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-	local lv=c:GetLevel()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		local m3=g2:FilterSelect(tp,c64.synfilter1,1,1,nil,lv,g1,g2)
-		local mt1=m3:GetFirst()
-		g:AddCard(mt1)
-		local lv1=mt1:GetLevel()
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		if mt1:GetFlagEffect(64)==0 then	
-		local t1=g1:FilterSelect(tp,c64.synfilter3,1,1,nil,lv+lv1)
-		g:Merge(t1)
-		else 
-		local t1=g1:FilterSelect(tp,c64.synfilter3,1,1,nil,lv-lv1)
-		g:Merge(t1)
-		end			
-	c:SetMaterial(g)
-	Duel.SendtoGrave(g,REASON_MATERIAL+REASON_SYNCHRO)
 end
 function c64.valcon(e,re,r,rp)
 	return bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0
