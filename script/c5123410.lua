de33e9ee3aead3103c5f9ba1ac7a14af53fcc3bb
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-08-17 14:30:04 -0600

    Create c5123410.lua

--- script/c5123410.lua
+++ script/c5123410.lua
@@ -1,10 +1,10 @@
---Battle Fusion (Anime)
+--Battle level
 local s,id=GetID()
 function s.initial_effect(c)
 	--ATK Up
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetDescription(aux.Stringid(432254340,1))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_ATKCHANGE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetHintTiming(TIMING_DAMAGE_STEP)
