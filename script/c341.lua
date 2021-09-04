4d8e4e09ba5666f6c0bd8d15fabfed8d8c939732
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-09-04 15:33:20 -0600

    Update c341.lua

--- script/c341.lua
+++ script/c341.lua
@@ -54,7 +54,7 @@ function s.reccost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:GetHandler():RegisterEffect(e1)
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x52) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x52) and not c:IsCode(id) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
