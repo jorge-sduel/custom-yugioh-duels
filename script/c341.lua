97096e23104b7ed5bfe44323d42f5e0d195a390c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-09-04 15:27:31 -0600

    Update c341.lua

--- script/c341.lua
+++ script/c341.lua
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 	--spsummon guardian in hand
 	local e4=Effect.CreateEffect(c)
-	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetDescription(aux.Stringid(id,0))
 	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e4:SetType(EFFECT_TYPE_IGNITION)
 	e4:SetRange(LOCATION_HAND)
