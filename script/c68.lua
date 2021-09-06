26ef79222eafbfa46819982eabd8b83657d228e5
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-09-06 12:09:07 -0600

    Update c68.lua

--- script/c68.lua
+++ script/c68.lua
@@ -261,11 +261,11 @@ if not tc then break end
 							ft2=ft2-1
 						end
 						ft=ft-1
-		Duel.SpecialSummonStep(tc,SUMMON_TYPE_PENDULUM,tp,tp,true,true,POS_FACEUP)
 					end
 				end
 				if #sg>0 then
 					if not inchain then
+		Duel.SpecialSummonStep(tc,SUMMON_TYPE_PENDULUM,tp,tp,true,true,POS_FACEUP)
 						Duel.RegisterFlagEffect(tp,10000000,RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
 					end
 					Duel.HintSelection(Group.FromCards(c))
