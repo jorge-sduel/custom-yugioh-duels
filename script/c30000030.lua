4c8c07ec89a4ee0b317396e857ddfb2a8c940bf3
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-09-09 09:12:21 -0600

    Add files via upload

---
+++ script/c30000030.lua
@@ -0,0 +1,102 @@
+--clear hellfiend
+local s,id=GetID()
+function s.initial_effect(c)
+Fusion.AddProcMixN(c,true,true,s.ffilter,3)
+	--remove att
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetCode(EFFECT_REMOVE_ATTRIBUTE)
+	e1:SetValue(0x7f)
+	c:RegisterEffect(e1)
+	--base attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.atkval)
+	c:RegisterEffect(e2)
+	--cannot target
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetValue(aux.tgoval)
+	c:RegisterEffect(e3)
+	--indes
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(s.indval)
+	c:RegisterEffect(e4)
+	--negate
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(67000071,0))
+	e5:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_CHAINING)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1)
+	e5:SetCondition(s.negcon)
+	e5:SetCost(s.cost)
+	e5:SetTarget(s.negtg)
+	e5:SetOperation(s.negop)
+	c:RegisterEffect(e5)
+end
+function s.ffilter(c,fc,sumtype,sp,sub,mg,sg)
+	return (c:IsSetCard(0x306,fc,sumtype,sp)) and (not sg or sg:FilterCount(aux.TRUE,c)==0 or not sg:IsExists(Card.IsCode,1,c,c:GetCode(),fc,sumtype,sp))
+end
+function s.indval(e,re,tp)
+	return tp~=e:GetHandlerPlayer()
+end
+function s.atkval(e,c)
+	return s.attcount(e:GetHandler():GetControler(),0,LOCATION_MZONE)*300
+end
+
+--att count function
+function s.attfilter(c,att)
+	return c:IsFaceup() and c:IsAttribute(att)
+end
+function s.attcount(tp,loc1,loc2)
+	local att=0
+	if Duel.IsExistingMatchingCard(s.attfilter,tp,loc1,loc2,1,nil,ATTRIBUTE_LIGHT) then att=att+1 end
+	if Duel.IsExistingMatchingCard(s.attfilter,tp,loc1,loc2,1,nil,ATTRIBUTE_DARK) then att=att+1 end
+	if Duel.IsExistingMatchingCard(s.attfilter,tp,loc1,loc2,1,nil,ATTRIBUTE_WATER) then att=att+1 end
+	if Duel.IsExistingMatchingCard(s.attfilter,tp,loc1,loc2,1,nil,ATTRIBUTE_FIRE) then att=att+1 end
+	if Duel.IsExistingMatchingCard(s.attfilter,tp,loc1,loc2,1,nil,ATTRIBUTE_EARTH) then att=att+1 end
+	if Duel.IsExistingMatchingCard(s.attfilter,tp,loc1,loc2,1,nil,ATTRIBUTE_WIND) then att=att+1 end
+	if Duel.IsExistingMatchingCard(s.attfilter,tp,loc1,loc2,1,nil,ATTRIBUTE_DEVINE) then att=att+1 end
+	return att
+end
+function s.spfilter(c)
+	return c:IsSetCard(0x306) and c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if ep==tp or c:IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+	return (re:IsActiveType(TYPE_MONSTER) or re:IsHasType(EFFECT_TYPE_ACTIVATE)) and Duel.IsChainNegatable(ev)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
