fb8dd784ff35650cc9b6ab06c9f26431af0b7847
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-09-11 16:36:44 -0600

    Update c232.lua

--- script/c232.lua
+++ script/c232.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--xyz summon
 	c:EnableReviveLimit()
-		s.xyz_filter=function(mc,ignoretoken,xyz,tp) return mc and (not f or f(mc,xyz,SUMMON_TYPE_XYZ|MATERIAL_XYZ,tp)) and ((not 7 or mc:IsXyzLevel(c,7) or mc:c:GetFlagEffect(511000189)==7) and (not mc:IsType(TYPE_TOKEN) or ignoretoken) end
+		s.xyz_filter=function(mc,ignoretoken,xyz,tp) return mc and (not f or f(mc,xyz,SUMMON_TYPE_XYZ|MATERIAL_XYZ,tp)) and (not 7 or mc:IsXyzLevel(c,7) and (not mc:IsType(TYPE_TOKEN) or ignoretoken) end
 		s.xyz_parameters={s.xyz_filter,nil,2,nil,nil,2}
 		s.minxyzct=ct
 		s.maxxyzct=maxct
