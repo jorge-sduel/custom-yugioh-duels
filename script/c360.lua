a556d7797a0bf4fa14a5b43417b01e0e0aa5fec9
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-09-17 08:02:55 -0600

    Add files via upload

---
+++ script/c360.lua
@@ -0,0 +1,59 @@
+--baby spider
+--fixed by MLD
+local s,id=GetID()
+function s.initial_effect(c)
+	--lvup
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(81974607,0))
+	e1:SetCategory(CATEGORY_LVCHANGE)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.lvtg)
+	e1:SetOperation(s.lvop)
+	c:RegisterEffect(e1)
+	--Lv
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetOperation(s.tnop)
+	c:RegisterEffect(e2)
+end
+function s.tnop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(5)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e1)
+	end
+end
+function s.lvcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function s.lvfilter(c)
+	return c:IsFaceup() and c:GetLevel()>0
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.lvfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local lv=c:GetLevel()
+	local g=Duel.GetMatchingGroup(s.lvfilter,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+Duel.Release(e:GetHandler(),REASON_COST)
+		tc=g:GetNext()
+	end
+end
