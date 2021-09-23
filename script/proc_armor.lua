bd47b953bba827285ec49b6b495f3ba9c0bd9103
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-09-23 13:44:02 -0600

    Add files via upload

--- script/proc_armor.lua
+++ script/proc_armor.lua
@@ -1,131 +1,143 @@
-ARMOR_IMPORTED			= true
-CATEGORY_ATTACH_ARMOR	= 0x20000000
-REASON_ARMORIZING		= 0x40000000
-SUMMON_TYPE_ARMORIZING	= 0x40
-EFFECT_FLAG_ARMOR		= 0x20000000
-HINTMSG_AMATERIAL       = 602
-HINTMSG_REMOVEARMOR     = 603
-HINTMSG_REMOVEARMORFROM = 604
-HINTMSG_ARMORTARGET     = 605
-HINTMSG_ATTACHARMOR     = 606
-if not aux.ArmorProcedure then
-	aux.ArmorProcedure = {}
-	Armor = aux.ArmorProcedure
-end
-if not Armor then
-	Armor = aux.ArmorProcedure
-end
+ARMOR_SET_CODE		   = 0x700
+CATEGORY_ATTACH_ARMOR  = 0x20000000
+REASON_ARMORIZING	   = 0x40000000
+SUMMON_TYPE_ARMORIZING = 0x40
+FLAG_ARMOR_RESOLVED	   = 0x8000000
+ARMOR_IMPORTED         = true
 --[[
 add at the start of the script to add Armor/Armorizing procedure
-if not ARMOR_IMPORTED then dofile Duel.LoadScript("proc_armor.lua") end
+if not ARMOR_IMPORTED then dofile Duel.LoadScript("script\proc_armor.lua") end
 condition if Armorizing summoned
     return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+SUMMON_TYPE_ARMORIZING
 ]]
---attach armor function
-function Armor.AttachCheck(ar,c)
-	return ar.IsArmor and c:IsFaceup() and not c:IsType(TYPE_XYZ) and (ar.AttachFilter == nil or ar.AttachFilter(c))
-end
-function Armor.Attach(c,ar)
-	Duel.Overlay(c,ar)
-end
 --add procedure to armor cards
-function Armor.AddProcedure(c)
+function Auxiliary.AttachArmor(c,ar)
+	if Duel.Overlay(c,ar)~=0 then
+		c:RegisterFlagEffect(FLAG_ARMOR_RESOLVED,RESET_CHAIN,0,1)
+	end
+end
+function Auxiliary.AddArmorProcedure(c,f,efftype,category,property,zone,hint1,hint2,con,cost,tg)
+	--Attach
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(1183)
-	e1:SetCategory(CATEGORY_ATTACH_ARMOR)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetDescription(aux.Stringid(c:GetCode(),0))
+	if efftype then
+		e1:SetType(efftype)
+	--elseif c:IsType(TYPE_SPELL) then
+		--e1:SetType(EFFECT_TYPE_ACTIVATE)
+	else
+		e1:SetType(EFFECT_TYPE_IGNITION)
+	end
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetTarget(Armor.Target)
-	e1:SetOperation(Armor.Operation)
+	if zone then
+		e1:SetRange(zone)
+	else--if not c:IsType(TYPE_SPELL) then
+		e1:SetRange(LOCATION_HAND)
+	end
+	if hint1 or hint2 then
+		if hint1==hint2 then
+			e1:SetHintTiming(hint1)
+		elseif hint1 and not hint2 then
+			e1:SetHintTiming(hint1,0)
+		elseif hint2 and not hint1 then
+			e1:SetHintTiming(0,hint2)
+		else
+			e1:SetHintTiming(hint1,hint2)
+		end
+	end
+	if category then
+		e1:SetCategory(CATEGORY_ATTACH_ARMOR+category)
+	else
+		e1:SetCategory(CATEGORY_ATTACH_ARMOR)
+	end
+	if property then
+		e1:SetProperty(EFFECT_FLAG_CARD_TARGET+property)
+	else
+		e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	end
+	if con then e1:SetCondition(con) end
+	if cost then e1:SetCost(cost) end
+	e1:SetTarget(Auxiliary.ArmorTarget(tg,f))
+	e1:SetOperation(Auxiliary.ArmorOperation)
 	c:RegisterEffect(e1)
 end
-function Armor.Filter(c,e,tp)
-	return not c:IsType(TYPE_XYZ) and c:IsFaceup()
-		and (e:GetHandler().AttachFilter == nil or e:GetHandler().AttachFilter(c))
+function Auxiliary.ArmorFilter(c,f,e,tp,tg,eg,ep,ev,re,r,rp)
+	return not c:IsType(TYPE_XYZ) and c:IsControler(tp) and c:IsFaceup() and (not f or f(c,e,tp)) and (not tg or tg(e,tp,eg,ep,ev,re,r,rp,c,0))
 end
-function Armor.Target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and Armor.Filter(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(Armor.Filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ARMORTARGET)
-	local g=Duel.SelectTarget(tp,Armor.Filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_ATTACH_ARMOR,g,1,0,0)
+function Auxiliary.ArmorTarget(tg,f)
+	return	function(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+				if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() and Auxiliary.ArmorFilter(chkc,f,e,tp) end
+				if chk==0 then return Duel.IsExistingTarget(Auxiliary.ArmorFilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,f,e,tp,tg,eg,ep,ev,re,r,rp) end
+				--	and e:GetHandler():GetFlagEffect(c)==0 end
+				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+				local g=Duel.SelectTarget(tp,Auxiliary.ArmorFilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,f,e,tp)
+				Duel.SetOperationInfo(0,CATEGORY_ATTACH_ARMOR,g,1,0,0)
+				if tg then tg(e,tp,eg,ep,ev,re,r,rp,g:GetFirst(),1) end
+			end
 end
-function Armor.Operation(e,tp,eg,ep,ev,re,r,rp)
+function Auxiliary.ArmorOperation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
 		if c:IsType(TYPE_SPELL+TYPE_TRAP) then c:CancelToGrave() end
-		Armor.Attach(tc,Group.FromCards(c))
+		Auxiliary.AttachArmor(tc,Group.FromCards(c))
 	end
 end
-function Armor.Condition(e)
+function Auxiliary.ArmorCondition(e)
 	return not e:GetHandler():IsType(TYPE_XYZ)
 end
 
 --Armorizing Summon
-if not aux.ArmorizingProcedure then
-	aux.ArmorizingProcedure = {}
-	Armorizing = aux.ArmorizingProcedure
-end
-if not Armorizing then
-	Armorizing = aux.ArmorizingProcedure
-end
-function Armorizing.AddProcedure(c,f1,min,f2)
+function Auxiliary.AddArmorizingProcedure(c,f1,armor)
+	if c.armorizing_filter==nil then
+		local code=c:GetOriginalCode()
+		local mt=_G["c" .. code]
+		mt.minarmorizingct=ct
+		mt.maxarmorizingct=maxct
+		mt.armorizing_type=1
+	end
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetDescription(1184)
+	--e1:SetDescription("Armorizing Summon")
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(Armorizing.Condition(f1,min,f2))
-	e1:SetTarget(Armorizing.Target(f1,min,f2))
-	e1:SetOperation(Armorizing.Operation)
+	e1:SetCondition(Auxiliary.ArmorizingCondition(f1,armor))
+	e1:SetTarget(Auxiliary.ArmorizingTarget(f1,armor))
+	e1:SetOperation(Auxiliary.ArmorizingOperation)
 	e1:SetValue(SUMMON_TYPE_ARMORIZING)
 	c:RegisterEffect(e1)
 end
 
-function Armorizing.ArmorFilter(c,sc,tp,f1,min,f2)
+function Auxiliary.ArmorizingMatFilter(c,f1,sc,tp,armor)
 	local g=Group.CreateGroup()
 	g:AddCard(c)
-	return c:IsFaceup() and not c:IsType(TYPE_XYZ)
-		and c:GetOverlayCount()>=min
-		and (not f2  or c:GetOverlayGroup():IsExists(f2,min,nil,e,tp))
+	return c:IsFaceup() and not c:IsType(TYPE_XYZ) and c:GetOverlayCount()>=armor
 		and (not f1 or f1(c,sc,SUMMON_TYPE_SPECIAL,tp))
         and Duel.GetLocationCountFromEx(tp,tp,g,sc)>0
 end
-function Armorizing.MatFilter(c,sc,tp,f1,min,f2)
-	local g=Group.CreateGroup()
-	g:AddCard(c)
-	return c:IsFaceup() and not c:IsType(TYPE_XYZ)
-		and c:GetOverlayCount()>=min
-		and (not f2 or c:GetOverlayGroup():IsExists(f2,min,nil,e,tp))
-		and (not f1 or f1(c,sc,SUMMON_TYPE_SPECIAL,tp))
-        and Duel.GetLocationCountFromEx(tp,tp,g,sc)>0
+function Auxiliary.ArmorizingCheck(sg,e,tp,mg,f1,sc,armor)
+	return sg:IsExists(Auxiliary.ArmorizingMatFilter,1,nil,f1,sc,tp,armor)
 end
-function Armorizing.Check(sg,e,tp,mg,sc,f1,min,f2)
-	return sg:IsExists(Armorizing.MatFilter,1,nil,sc,tp,f1,min,f2)
-end
-function Armorizing.Condition(f1,min,f2)
+function Auxiliary.ArmorizingCondition(f1,armor)
 	return	function(e,c)
 				if c==nil then return true end
 				if c:IsType(TYPE_PENDULUM) and c:IsFaceup() then return false end
 				local tp=c:GetControler()
 				
-				return Duel.IsExistingMatchingCard(Armorizing.MatFilter,tp,LOCATION_MZONE,0,1,nil,c,tp,f1,min,f2)
-				--local g=Duel.GetMatchingGroup(Armorizing.MatFilter,tp,LOCATION_MZONE,0,nil,f1,c,tp,armor)
-				--return aux.SelectUnselectGroup(g,e,tp,1,1,Armorizing.Check,0,c)
+				return Duel.IsExistingMatchingCard(Auxiliary.ArmorizingMatFilter,tp,LOCATION_MZONE,0,1,nil,f1,c,tp,armor)
+				--local g=Duel.GetMatchingGroup(Auxiliary.ArmorizingMatFilter,tp,LOCATION_MZONE,0,nil,f1,c,tp,armor)
+				--return aux.SelectUnselectGroup(g,e,tp,1,1,Auxiliary.ArmorizingCheck,0,f1,c,armor)
             end
 end
-function Armorizing.Target(f1,min,f2)
+function Auxiliary.ArmorizingTarget(f1,armor)
 	return	function(e,tp,eg,ep,ev,re,r,rp,chk)
                 local c=e:GetHandler()
 				local tp=e:GetHandlerPlayer()
                 
-                Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_AMATERIAL)
-				local rg=Duel.GetMatchingGroup(Armorizing.MatFilter,tp,LOCATION_MZONE,0,nil,c,tp,f1,min,f2)
-				local mg=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_SELECT,nil,nil,true,c)
+                --Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SELECT)
+				--local mg=Duel.SelectMatchingCard(tp,Auxiliary.ArmorizingMatFilter,tp,LOCATION_MZONE,0,1,1,nil,f1,c,tp,armor)
+				local rg=Duel.GetMatchingGroup(Auxiliary.ArmorizingMatFilter,tp,LOCATION_MZONE,0,nil,f1,c,tp,armor)
+				local mg=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_SELECT,nil,nil,true,f1,c,armor)
                 
 				if #mg>0 then
 					local sg=mg:GetFirst():GetOverlayGroup()
@@ -139,21 +151,21 @@ function Armorizing.Target(f1,min,f2)
 				end
             end
 end
-Armorizing.Send=0
-function Armorizing.Operation(e,tp,eg,ep,ev,re,r,rp,c,smat,mg)
+Auxiliary.ArmorizingSend=0
+function Auxiliary.ArmorizingOperation(e,tp,eg,ep,ev,re,r,rp,c,smat,mg)
 	local g=e:GetLabelObject()
 	c:SetMaterial(g)
-	if Armorizing.Send==1 then
+	if Auxiliary.ArmorizingSend==1 then
 		Duel.SendtoGrave(g,REASON_MATERIAL+REASON_ARMORIZING+REASON_RETURN)
-	elseif Armorizing.Send==2 then
+	elseif Auxiliary.ArmorizingSend==2 then
 		Duel.Remove(g,POS_FACEUP,REASON_MATERIAL+REASON_ARMORIZING)
-	elseif Armorizing.Send==3 then
+	elseif Auxiliary.ArmorizingSend==3 then
 		Duel.Remove(g,POS_FACEDOWN,REASON_MATERIAL+REASON_ARMORIZING)
-	elseif Armorizing.Send==4 then
+	elseif Auxiliary.ArmorizingSend==4 then
 		Duel.SendtoHand(g,nil,REASON_MATERIAL+REASON_ARMORIZING)
-	elseif Armorizing.Send==5 then
+	elseif Auxiliary.ArmorizingSend==5 then
 		Duel.SendtoDeck(g,nil,2,REASON_MATERIAL+REASON_ARMORIZING)
-	elseif Armorizing.Send==6 then
+	elseif Auxiliary.ArmorizingSend==6 then
 		Duel.Destroy(g,REASON_MATERIAL+REASON_ARMORIZING)
 	else
 		Duel.SendtoGrave(g,REASON_MATERIAL+REASON_ARMORIZING)
