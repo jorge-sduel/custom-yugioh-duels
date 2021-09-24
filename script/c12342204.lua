50de6b09b4e174d831bb7850d0c13fd25b69ffba
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-09-23 21:39:44 -0600

    Update c12342204.lua

--- script/c12342204.lua
+++ script/c12342204.lua
@@ -2,8 +2,8 @@
 --Scripted by Secuter
 local s,id=GetID()
 if not ARMOR_IMPORTED then Duel.LoadScript("proc_armor.lua") end
-s.armor_Atk=300
-s.armor_Def=0
+s.armor_atk=300
+s.armor_def=0
 s.is_armor=true
 function s.initial_effect(c)
 	--Armor
@@ -14,7 +14,7 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_XMATERIAL)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetCondition(aux.ArmorCondition)
-	e1:SetValue(s.ArmorAtk)
+	e1:SetValue(s.armor_atk)
 	c:RegisterEffect(e1)
 	--sp
 	local e2=Effect.CreateEffect(c)
