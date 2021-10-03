a367f1c5e85301922ca9957ac8d832a4af64cc54
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-03 09:03:11 -0600

    Add files via upload

---
+++ script/c364.lua
@@ -0,0 +1,69 @@
+--Tribute Ticket
+local s,id=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--Def
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SET_DEFENSE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(s.tg)
+	e1:SetValue(0)
+	c:RegisterEffect(e1)
+	--xyz
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(511000189)
+	e2:SetValue(7)
+	e2:SetRange(LOCATION_PZONE)
+	c:RegisterEffect(e2)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(511001225)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(32086564,0))
+	e6:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e6:SetCode(EVENT_BATTLED)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCondition(s.atkcon)
+	e6:SetOperation(s.atkop)
+	c:RegisterEffect(e6)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return a:IsControler(tp) and a:IsOnField() and d and d:IsDefensePos()
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local tc=Duel.GetAttacker()
+	if tc and tc:IsFaceup() and tc:IsRelateToBattle() then
+		local maxv=0
+		local eff={tc:GetCardEffect(EFFECT_EXTRA_ATTACK)}
+		for _,te in ipairs(eff) do
+			local val=te:GetValue()
+			if type(val)=='function' then
+				maxv=math.max(val(te,tc),maxv)
+			else
+				maxv=math.max(val,maxv)
+			end
+		end
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_EXTRA_ATTACK)
+		e2:SetValue(maxv+1)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+		tc:RegisterEffect(e2)
+	end
+end
+function s.tg(e,c)
+	return c:IsType(TYPE_MONSTER)
+end
+
