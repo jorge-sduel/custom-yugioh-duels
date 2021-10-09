247aff650be1ec4dfb3e9851906e62457a5ab042
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-09 12:33:22 -0600

    Update c20200202.lua

--- script/c20200202.lua
+++ script/c20200202.lua
@@ -38,8 +38,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local ct=g:GetClassCount(Card.GetCode)	
 	local g1=Duel.GetDecktopGroup(tp,ct)
 	local g2=Duel.GetDecktopGroup(1-tp,ct)
-		Duel.SendtoGrave(g1,nil,REASON_EFFECT)
-		Duel.SendtoGrave(g2,nil,REASON_EFFECT)
+		Duel.SendtoGrave(g1,REASON_EFFECT)
+		Duel.SendtoGrave(g2,REASON_EFFECT)
 	
 	
 	local g=Duel.GetOperatedGroup()
