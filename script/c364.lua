50fbd543eb601086e86726acecb7b605a401722a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-08 20:19:06 -0600

    Update c364.lua

--- script/c364.lua
+++ script/c364.lua
@@ -3,15 +3,6 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
-	--Def
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SET_DEFENSE)
-	e1:SetRange(LOCATION_PZONE)
-	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	e1:SetTarget(s.tg)
-	e1:SetValue(0)
-	c:RegisterEffect(e1)
 	--xyz
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
