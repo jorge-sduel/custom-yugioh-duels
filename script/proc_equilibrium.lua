af9b8b3bfd411be4e655fbce2b4fb7fa8bb58f3d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-09 17:59:06 -0600

    Update proc_equilibrium.lua

--- script/proc_equilibrium.lua
+++ script/proc_equilibrium.lua
@@ -159,7 +159,7 @@ function Equilibrium.attachcond()
 	if not tc1 or not tc2 then return false end
 	return tc1:GetLeftScale()==tc2:GetRightScale()
 end
-function Equilibrium.cfilter()
+function Equilibrium.cfilter(c,e)
 	return c:IsType(TYPE_PENDULUM)
 end
 function Equilibrium.attachop()
