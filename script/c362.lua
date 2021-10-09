bddea1a855bda1d3a4abb6ffea5a305a20279429
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-08 20:43:09 -0600

    Update c362.lua

--- script/c362.lua
+++ script/c362.lua
@@ -47,20 +47,46 @@ function s.initial_effect(c)
 	c:RegisterEffect(e7)
 end
 s.pendulum_level=7
-function s.con2(e,tp,eg,ep,ev,re,r,rp)
-	return aux.damcon1(e,tp,eg,ep,ev,re,r,rp)
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
+	if ex then return true end
+	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
+	if not ex then return false end
+	if cp~=PLAYER_ALL then return Duel.IsPlayerAffectedByEffect(cp,EFFECT_REVERSE_RECOVER)
+	else return Duel.IsPlayerAffectedByEffect(0,EFFECT_REVERSE_RECOVER)
+		or Duel.IsPlayerAffectedByEffect(1,EFFECT_REVERSE_RECOVER)
+	end
 end
-function s.op2(e,tp,eg,ep,ev,re,r,rp,val,r,rc)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
+	e1:SetTargetRange(1,1)
 	e1:SetLabel(cid)
 	e1:SetValue(s.refcon)
 	e1:SetReset(RESET_CHAIN)
 	Duel.RegisterEffect(e1,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	if #g>0 then
+		Duel.HintSelection(g)
+		if Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT) then
+			local tc=g:GetFirst()
+			local atk=tc:GetTextAttack()
+			if atk<0 then atk=0 end
+		end
+	end
+end
+function s.filter(c)
+	return c:IsFaceup() and c:IsAbleToDeck()
+end
+function s.refcon(e,re,val,r,rp,rc)
+	local cc=Duel.GetCurrentChain()
+	if cc==0 or (r&REASON_EFFECT)==0 then return end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	if cid==e:GetLabel() then return 0
+	else return val end
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
