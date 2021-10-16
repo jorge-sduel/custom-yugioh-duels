10c86fb92e468c529c2738dd1c3f49c2858f6c91
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-16 13:46:09 -0600

    Update c376.lua

--- script/c376.lua
+++ script/c376.lua
@@ -16,6 +16,10 @@ end
 function s.cfilter(c)
 	return c:IsType(TYPE_FUSION) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_PENDULUM) and c:IsType(TYPE_MONSTER)
 end
+function s.cfilter(c)
+	return (c:IsSetCard(0x10f2) or c:IsSetCard(0x2073) or c:IsSetCard(0x2017) or c:IsSetCard(0x1046)) and c:IsType(TYPE_MONSTER)
+		and c:IsAbleToRemoveAsCost()
+end
 function s.rescon(checkfunc)
 	return function(sg,e,tp,mg)
 		if not sg:CheckDifferentProperty(checkfunc) then return false,true end
@@ -23,30 +27,25 @@ function s.rescon(checkfunc)
 	end
 end
 function s.hnfilter(c,e,tp,sg)
-	return c:IsCode(13331639) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO) and (c:IsType(TYPE_PENDULUM) or c:IsType(TYPE_LINK)))) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+	return c:IsCode(13331639) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,true,true) and c:CheckFusionMaterial()
 			and Duel.GetLocationCountFromEx(tp,tp,sg and (sg+e:GetHandler()) or nil,c)>0
 end
 function s.hncost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local mg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,c)
-	local checkfunc=aux.PropertyTableFilter(Card.GetType,TYPE_FUSION,TYPE_XYZ,TYPE_SYNCHRO,TYPE_PENDULUM)
-	if chk==0 then return c:IsAbleToRemoveAsCost() and aux.SelectUnselectGroup(mg,e,tp,4,4,s.rescon(checkfunc),0) end
-	local sg=aux.SelectUnselectGroup(mg,e,tp,4,4,s.rescon(checkfunc),1,tp,HINTMSG_REMOVE,s.rescon(checkfunc))
-	Duel.Overlay(sg,c)
+	local mg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE+LOCATION_SZONE+LOCATION_PZONE+LOCATION_OVERLAY+LOCATION_DECK+LOCATION_EXTRA,0,c)
+	local checkfunc=aux.PropertyTableFilter(Card.GetSetCard,0x10f2,0x2073,0x2017,0x1046)
+	if chk==0 then return c:IsAbleToRemoveAsCost() and aux.SelectUnselectGroup(mg,e,tp,4,4,c328.rescon(checkfunc),0) end
+	local sg=aux.SelectUnselectGroup(mg,e,tp,4,4,s.rescon(checkfunc),1,tp,HINTMSG_REMOVE,c328.rescon(checkfunc))+c
+	Duel.Remove(sg,POS_FACEUP,REASON_COST)
 end
 function s.hntg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.hnop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.hnfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 	if #g>0 then
-		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
-		local mg1=e:GetHandler():GetOverlayGroup()
-			Duel.Overlay(g,mg1)
-    Duel.Overlay(g,c)
-    c:SetMaterial(c)
+		Duel.SpecialSummon(g,SUMMON_TYPE_FUSION,tp,tp,true,true,POS_FACEUP)
 	end
 end
