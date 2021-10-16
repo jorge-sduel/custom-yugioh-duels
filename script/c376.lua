eb03dc865dd4ee2f8a1f11e3e93f4dc311f85017
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-16 15:43:28 -0600

    Update c376.lua

--- script/c376.lua
+++ script/c376.lua
@@ -35,55 +35,13 @@ function s.hncost(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.Overlay(c,sg)
 end
 function s.hntg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) and Duel.GetLocationCount(tp,LOCATION_MZONE,0)>0
-		and Duel.IsExistingTarget(s.hnfilter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,nil,e,tp)end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,s.hnfilter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,LOCATION_EXTRA+LOCATION_GRAVE)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.hnop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE,0)<=0 and not c:IsRelateToEffect(e) then return end
-	if tc and tc:IsRelateToEffect(e) and Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)>0 then
-		if c:IsRelateToEffect(e) then
-			c:CancelToGrave()
-			Duel.Overlay(tc,c)
-			local e1=Effect.CreateEffect(tc)
-			e1:SetDescription(aux.Stringid(51960178,0))
-			e1:SetCategory(CATEGORY_DRAW)
-			e1:SetType(EFFECT_TYPE_IGNITION)
-			e1:SetRange(LOCATION_MZONE)
-			e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-			e1:SetCost(s.drcost)
-			e1:SetTarget(s.drtg)
-			e1:SetOperation(s.drop)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			e1:SetLabelObject(c)
-			tc:RegisterEffect(e1)
-			if not tc:IsType(TYPE_EFFECT) then
-				local e2=Effect.CreateEffect(c)
-				e2:SetType(EFFECT_TYPE_SINGLE)
-				e2:SetCode(EFFECT_ADD_TYPE)
-				e2:SetValue(TYPE_EFFECT)
-				e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-				tc:RegisterEffect(e2,true)
-			end
-		end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.hnfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
 	end
-end
-function s.drcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local tc=e:GetLabelObject()
-	if chk==0 then return tc and e:GetHandler():GetOverlayGroup():IsContains(tc) end
-	Duel.SendtoGrave(tc,REASON_COST)
-end
-function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
-	Duel.SetTargetPlayer(tp)
-	Duel.SetTargetParam(2)
-	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
-end
-function s.drop(e,tp,eg,ep,ev,re,r,rp,chk)
-	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
-	Duel.Draw(p,d,REASON_EFFECT)
 end
