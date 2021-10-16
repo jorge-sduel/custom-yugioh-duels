ed43529f603694669fa808e7adf86bcb853bc665
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-16 14:29:31 -0600

    Update c376.lua

--- script/c376.lua
+++ script/c376.lua
@@ -46,7 +46,6 @@ function s.hnop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.hnfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 		local mg1=c:GetOverlayGroup()
-	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
 		c:CancelToGrave()
 		Duel.Overlay(g,mg1)
