f1a07960f4cb5df2ab2654de4d298a08503d269c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-17 10:36:13 -0600

    Update c376.lua

--- script/c376.lua
+++ script/c376.lua
@@ -8,13 +8,44 @@ function s.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-        e1:SetCost(s.hncost)
 	c:RegisterEffect(e1)
 	--remain field
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_REMAIN_FIELD)
 	c:RegisterEffect(e3)
+	--special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_SPSUMMON_PROC)
+	e4:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e4:SetRange(LOCATION_EXTRA)
+	e4:SetCondition(s.conditionov2)
+	e4:SetOperation(s.hncost2)
+	e4:SetValue(SUMMON_TYPE_XYZ)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetTargetRange(LOCATION_EXTRA,0)
+	e5:SetTarget(s.eftg2)
+	e5:SetLabelObject(e4)
+	c:RegisterEffect(e5)
+	--special summon
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetCode(EFFECT_SPSUMMON_PROC)
+	e6:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e6:SetRange(LOCATION_EXTRA)
+	e6:SetCondition(s.conditionov3)
+	e6:SetOperation(s.hncost3)
+	e6:SetValue(SUMMON_TYPE_XYZ)
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_GRANT)
+	e7:SetRange(LOCATION_SZONE)
+	e7:SetTargetRange(LOCATION_EXTRA,0)
+	e7:SetTarget(s.eftg3)
+	e7:SetLabelObject(e6)
+	c:RegisterEffect(e7)
 	--special summon
 	local e23=Effect.CreateEffect(c)
 	e23:SetType(EFFECT_TYPE_FIELD)
@@ -42,7 +73,7 @@ function s.rescon(checkfunc)
 	end
 end
 function s.hnfilter(c,e,tp,sg)
-	return c:IsCode(13331639) or c:IsCode(12341305) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true) and c:CheckFusionMaterial()
+	return c:IsCode(13331639) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true) and c:CheckFusionMaterial()
 			and Duel.GetLocationCountFromEx(tp,tp,sg and (sg+e:GetHandler()) or nil,c)>0
 end
 function s.hncost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -68,7 +99,13 @@ function s.hnop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.eftg(e,c)
-	return c:IsCode(13331639) or c:IsCode(12341305) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+	return c:IsCode(13331639) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.eftg2(e,c)
+	return c:IsCode(12341305) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO) and c:IsType(TYPE_LINK)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.eftg3(e,c)
+	return c:IsCode(0000000) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO) and c:IsType(TYPE_RITUAL)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
 end
 function s.hspfilter(c)
 	return c:IsCode(id)
@@ -76,13 +113,59 @@ end
 function s.conditionov(e,c)
 	if c==nil then return true end
 	return Duel.GetLocationCount(e:GetHandlerPlayer(),LOCATION_SZONE)+Duel.GetLocationCountFromEx(e:GetHandlerPlayer(),e:GetHandlerPlayer(),nil,c)>0
-        and Duel.IsExistingMatchingCard(s.hspfilter,e:GetHandlerPlayer(),LOCATION_SZONE,0,1,nil)
+        and Duel.IsExistingMatchingCard(s.cspfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
 end
-function s.operationov(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-	local g=Duel.SelectMatchingCard(tp,s.hspfilter,tp,LOCATION_SZONE,0,1,1,nil)
-		local mg1=g:GetFirst():GetOverlayGroup()
-			Duel.Overlay(c,mg1)
-    Duel.Overlay(c,g)
-    c:SetMaterial(g)
+function s.conditionov2(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(e:GetHandlerPlayer(),LOCATION_SZONE)+Duel.GetLocationCountFromEx(e:GetHandlerPlayer(),e:GetHandlerPlayer(),nil,c)>0
+        and Duel.IsExistingMatchingCard(s.cspfilter2,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.cfilter2(c)
+	return (c:IsType(TYPE_FUSION) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_PENDULUM) or c:IsType(TYPE_LINK)) and c:IsType(TYPE_MONSTER)
+end
+function s.rescon2(checkfunc)
+	return function(sg,e,tp,mg)
+		if not sg:CheckDifferentProperty(checkfunc) then return false,true end
+		return Duel.IsExistingMatchingCard(s.hnfilter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,sg)
+	end
+end
+function s.hnfilter2(c,e,tp,sg)
+	return c:IsCode(12341305) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO) and c:IsType(TYPE_LINK)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true) and c:CheckFusionMaterial()
+			and Duel.GetLocationCountFromEx(tp,tp,sg and (sg+e:GetHandler()) or nil,c)>0
+end
+function s.hncost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local mg=Duel.GetMatchingGroup(s.cfilter2,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE+LOCATION_SZONE+LOCATION_PZONE+LOCATION_OVERLAY+LOCATION_DECK+LOCATION_EXTRA,0,c)
+	local checkfunc=aux.PropertyTableFilter(Card.GetSetCard,0x10f2,0x2073,0x2017,0x1046)
+	if chk==0 then return aux.SelectUnselectGroup(mg,e,tp,4,4,s.rescon(checkfunc),0) end
+	local sg=aux.SelectUnselectGroup(mg,e,tp,4,4,s.rescon(checkfunc),1,tp,HINTMSG_REMOVE,s.rescon(checkfunc))
+	Duel.Overlay(c,sg)
+	c:CancelToGrave()
+end
+function s.conditionov3(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(e:GetHandlerPlayer(),LOCATION_SZONE)+Duel.GetLocationCountFromEx(e:GetHandlerPlayer(),e:GetHandlerPlayer(),nil,c)>0
+        and Duel.IsExistingMatchingCard(s.cspfilter2,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
+function s.cfilter3(c)
+	return (c:IsType(TYPE_FUSION) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_PENDULUM) or c:IsType(TYPE_RITUAL)) and c:IsType(TYPE_MONSTER)
+end
+function s.rescon3(checkfunc)
+	return function(sg,e,tp,mg)
+		if not sg:CheckDifferentProperty(checkfunc) then return false,true end
+		return Duel.IsExistingMatchingCard(s.hnfilter3,tp,LOCATION_EXTRA,0,1,nil,e,tp,sg)
+	end
+end
+function s.hnfilter3(c,e,tp,sg)
+	return c:IsCode(0000) or (c:IsType(TYPE_FUSION) and c:IsType(TYPE_XYZ) and c:IsType(TYPE_SYNCHRO) and c:IsType(TYPE_LINK)) and c:IsCanBeSpecialSummoned(e,0,tp,true,true) and c:CheckFusionMaterial()
+			and Duel.GetLocationCountFromEx(tp,tp,sg and (sg+e:GetHandler()) or nil,c)>0
+end
+function s.hncost3(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local mg=Duel.GetMatchingGroup(s.cfilter3,tp,LOCATION_HAND+LOCATION_MZONE+LOCATION_GRAVE+LOCATION_SZONE+LOCATION_PZONE+LOCATION_OVERLAY+LOCATION_DECK+LOCATION_EXTRA,0,c)
+	local checkfunc=aux.PropertyTableFilter(Card.GetSetCard,0x10f2,0x2073,0x2017,0x1046)
+	if chk==0 then return aux.SelectUnselectGroup(mg,e,tp,4,4,s.rescon(checkfunc),0) end
+	local sg=aux.SelectUnselectGroup(mg,e,tp,4,4,s.rescon(checkfunc),1,tp,HINTMSG_REMOVE,s.rescon(checkfunc))
+	Duel.Overlay(c,sg)
+	c:CancelToGrave()
 end
