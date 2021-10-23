3ef83219eb020438b68dfbd7e58c590b4d579c03
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-23 13:53:57 -0600

    Add files via upload

---
+++ script/c378.lua
@@ -0,0 +1,71 @@
+--SS fus darkroze
+function c378.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcFun2(c,c378.ffilter,aux.FilterBoolFunction(Card.IsSetCard,0x1115),true)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(378,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(c378.target)
+	e1:SetOperation(c378.operation)
+	c:RegisterEffect(e1)
+	--Special summon
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(4796100,0))
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e7:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e7:SetCode(EVENT_DESTROYED)
+	e7:SetTarget(c378.sptg)
+	e7:SetOperation(c378.spop)
+	c:RegisterEffect(e7)
+end
+function c378.ffilter(c)
+	return c:IsLinkAbove(2) and c:IsSetCard(0x1115)
+end
+function c378.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsType,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler(),TYPE_MONSTER) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,Card.IsType,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,TYPE_MONSTER)
+end
+function c378.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_SET_ATTACK)
+		e1:SetValue(1500)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_FIELD)
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetTargetRange(LOCATION_MZONE,0)
+		e2:SetValue(1500)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	  Duel.RegisterEffect(e2,tp)
+	end
+end
+function c378.spfilter(c,e,tp)
+	return c:IsSetCard(0x1115) and not c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c378.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c378.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(c378.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,c378.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function c378.spop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
