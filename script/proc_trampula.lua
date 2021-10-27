d4ff12a3701e32b06cedfb4d9a757205982d861f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-27 08:31:00 -0600

    Update proc_trampula.lua

--- script/proc_trampula.lua
+++ script/proc_trampula.lua
@@ -143,6 +143,20 @@ if not tc then break end
 							end
 						end
 						if tc:IsLocation(LOCATION_HAND) then
+							ft1=ft1-1
+						else
+							ft2=ft2-1
+						end
+						ft=ft-1
+					end
+				end
+				if #sg>0 then
+					if not inchain then
+						Duel.RegisterFlagEffect(tp,10000000,RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
+					end
+					Duel.HintSelection(Group.FromCards(c))
+					Duel.HintSelection(Group.FromCards(rpz))
+	end
 		Duel.SpecialSummonStep(tc,SUMMON_TYPE_PENDULUM,tp,tp,true,true,POS_FACEUP)
 		local e1=Effect.CreateEffect(c)
 			e1:SetCode(EFFECT_ADD_TYPE)
@@ -158,20 +172,6 @@ if not tc then break end
 			e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
 			e2:SetValue(TYPE_TRAP)
 			tc:RegisterEffect(e2)
-							ft1=ft1-1
-						else
-							ft2=ft2-1
-						end
-						ft=ft-1
-					end
-				end
-				if #sg>0 then
-					if not inchain then
-						Duel.RegisterFlagEffect(tp,10000000,RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
-					end
-					Duel.HintSelection(Group.FromCards(c))
-					Duel.HintSelection(Group.FromCards(rpz))
-	end
 	Duel.SpecialSummonComplete()
 	if lp>0 then
 		Duel.BreakEffect()
