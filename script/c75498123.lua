87c1611407d70ab8e13cf2ed2f61ad6cd15616b7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-28 17:39:28 -0600

    Update c75498123.lua

--- script/c75498123.lua
+++ script/c75498123.lua
@@ -3,32 +3,39 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function s.xyzfilter(c,mg)
-	return c:IsXyzSummonable(nil,mg)
+function s.filter1(c)
+	return c:IsFaceup() and c:IsLevelAbove(0)
 end
-function s.Lvfilter(c)
-	return c:IsType(TYPE_MONSTER)
+function s.filter2(c)
+	return c:IsFaceup() and c:HasLevel()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter1(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.filter1,tp,LOCATION_MZONE,0,1,1,nil)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local mg1=Duel.GetMatchingGroup(s.Lvfilter,tp,LOCATION_GRAVE+LOCATION_MZONE,0,nil)
-	local mg=Duel.GetMatchingGroup(Card.IsType,tp,LOCATION_GRAVE+LOCATION_MZONE,0,nil,TYPE_MONSTER)
-	local e2=Effect.CreateEffect(e:GetHandler())
-	e2:SetType(EFFECT_TYPE_FIELD)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e2:SetCode(EFFECT_XYZ_LEVEL)
-	e2:SetTargetRange(LOCATION_MZONE+LOCATION_GRAVE,0)
-	e2:SetValue(mg1:GetLevel())
-	Duel.RegisterEffect(e2)
-	local xyzg=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_EXTRA,0,nil,mg)
-	if #xyzg>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local xyz=xyzg:Select(tp,1,1,nil)
-		Duel.XyzSummon(tp,xyz,nil,mg,99,99)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_MZONE+LOCATION_GRAVE,0,tc)
+		local lc=g:GetFirst()
+		local lv=tc:GetLevel()
+		for lc in aux.Next(g) do
+			local e1=Effect.CreateEffect(e:GetHandler())
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_XYZ_LEVEL)
+			e1:SetValue(lv)
+			e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			lc:RegisterEffect(e1)
+		end
 	end
 end
