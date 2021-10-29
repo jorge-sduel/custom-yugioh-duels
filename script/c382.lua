1a4a9c554272c129348b038c07ebc84bacc0342a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-29 13:18:33 -0600

    Update c382.lua

--- script/c382.lua
+++ script/c382.lua
@@ -53,6 +53,16 @@ function s.initial_effect(c)
 	e5:SetTarget(s.edtg)
 	e5:SetOperation(s.edop)
 	c:RegisterEffect(e5)
+	local e6=Effect.CreateEffect(c)
+	e6:SetCategory(CATEGORY_ATKCHANGE)
+	e6:SetDescription(aux.Stringid(id,4))
+	e6:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetRange(LOCATION_EXTRA)
+	e6:SetCost(aux.bfgcost)
+	e6:SetTarget(s.tg)
+	e6:SetOperation(s.op)
+	c:RegisterEffect(e6)
 end
 function s.matfilter(c,scard,sumtype,tp)
 	return c:IsType(TYPE_PENDULUM,scard,sumtype,tp) and c:IsType(TYPE_SYNCHRO,scard,sumtype,tp)
@@ -184,4 +194,37 @@ function s.damval(e,re,val,r,rp,rc)
 	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
 	if cid~=e:GetLabel() then return val end
 	return 0
+end
+function s.filter3(c)
+	return c:IsFaceup() and c:GetAttack()~=c:GetBaseAttack()
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(s.filter3,tp,LOCATION_MZONE,0,1,nil) 
+		and Duel.IsExistingTarget(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local g1=Duel.SelectTarget(tp,s.filter3,tp,LOCATION_MZONE,0,1,1,nil)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(34016756,0))
+	local g2=Duel.SelectTarget(tp,Card.IsFaceup,tp,0,LOCATION_MZONE,1,1,nil)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local hc=e:GetLabelObject()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tc=g:GetFirst()
+	if tc==hc then tc=g:GetNext() end
+	if hc:IsFaceup() and hc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		local atk=0
+		if hc:GetBaseAttack()>hc:GetAttack() then
+			atk=hc:GetBaseAttack()-hc:GetAttack()
+		else
+			atk=hc:GetAttack()-hc:GetBaseAttack()
+		end
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(-atk)
+		tc:RegisterEffect(e1)
+	end
 end
