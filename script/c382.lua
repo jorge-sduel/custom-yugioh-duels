a9e4eb3855df8094b679c681728c1cf74528c8cd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-10-29 11:59:05 -0600

    Update c382.lua

--- script/c382.lua
+++ script/c382.lua
@@ -31,15 +31,28 @@ function s.initial_effect(c)
 	e3:SetTarget(s.destg)
 	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
-	--damage
+	--attack
 	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_DESTROY)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
 	e4:SetDescription(aux.Stringid(id,2))
-	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
 	e4:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-	e4:SetCountLimit(1,id)
-	e4:SetCondition(s.damcon)
-	e4:SetOperation(s.surop)
+	e4:SetRange(LOCATION_HAND)
+	e4:SetCountLimit(1)
+	e4:SetCondition(s.dmcon)
+	e4:SetTarget(s.dmtg)
+	e4:SetOperation(s.dmop)
+	e4:SetHintTiming(TIMING_DAMAGE_CAL) --credit's to keddy
 	c:RegisterEffect(e4)
+	--effect damage
+	local e5=e4:Clone()
+	e5:SetDescription(aux.Stringid(id,3))
+	e5:SetCode(EVENT_CHAINING)
+	e5:SetCondition(s.edcon)
+	e5:SetTarget(s.edtg)
+	e5:SetOperation(s.edop)
+	c:RegisterEffect(e5)
 end
 function s.matfilter(c,scard,sumtype,tp)
 	return c:IsType(TYPE_PENDULUM,scard,sumtype,tp) and c:IsType(TYPE_SYNCHRO,scard,sumtype,tp)
@@ -107,23 +120,68 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.desfilter,tp,0,LOCATION_ONFIELD,nil)
 	Duel.Destroy(g,REASON_EFFECT)
 end
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	return Duel.GetBattleDamage(tp)>=Duel.GetLP(tp)
+function s.dmcon(e,tp,eg,ev,ep,re,r,rp)
+	local a=Duel.GetAttacker()
+	local lp=Duel.GetLP(tp)
+	e:SetLabelObject(a)
+	return Duel.GetBattleDamage(tp)>=lp
 end
-function s.damcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return c:CheckRemoveOverlayCard(tp,1,REASON_COST) end
-	c:RemoveOverlayCard(tp,1,1,REASON_COST)
+function s.dmtg(e,tp,eg,ev,ep,re,r,rp,chk)
+	a=e:GetLabelObject()
+	if chk==0 then return a:IsDestructable(e) end
+	Duel.SetTargetCard(a)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,e:GetLabelObject(),1,0,0)
 end
-function s.surop(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e1:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e1:SetOperation(s.damopx)
-	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
-	Duel.RegisterEffect(e1,tp)
-	Duel.SetLP(tp,100,REASON_EFFECT)
-end
-function s.damopx(e,tp,eg,ep,ev,re,r,rp)
+function s.dmop(e,tp,eg,ev,ep,re,r,rp)
+	local dam0=Duel.GetBattleDamage(tp)
 	Duel.ChangeBattleDamage(tp,0)
+	local tc=Duel.GetFirstTarget()
+	local dam1=Duel.GetBattleDamage(tp)
+	if dam0>0 and dam1==0 and tc:IsRelateToBattle() and tc:IsRelateToBattle() then
+		Duel.Destroy(tc,REASON_EFFECT)
+	end
+end
+function s.edcon(e,tp,eg,ev,ep,re,r,rp)
+	local e1=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_DAMAGE)
+	local e2=Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
+	local rd=e1 and not e2
+	local rr=not e1 and e2
+	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
+	if ex and (cp==tp or cp==PLAYER_ALL) and not rd 
+		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and Duel.GetLP(tp)<=cv then 
+		return true 
+	end
+	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
+	return ex and (cp==tp or cp==PLAYER_ALL) and rr 
+		and not Duel.IsPlayerAffectedByEffect(tp,EFFECT_NO_EFFECT_DAMAGE) and Duel.GetLP(tp)<=cv
+end
+function s.edtg(e,tp,eg,ev,ep,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsDestructable() and re:GetHandler():IsRelateToEffect(re) then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.edop(e,tp,eg,ev,ep,re,r,rp)
+	local c=e:GetHandler()
+	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(1,0)
+	e1:SetLabel(cid)
+	e1:SetValue(s.damval)
+	e1:SetReset(RESET_CHAIN)
+	Duel.RegisterEffect(e1,tp)
+	if re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(eg,REASON_EFFECT)
+	end
+end
+function s.damval(e,re,val,r,rp,rc)
+	local cc=Duel.GetCurrentChain()
+	if cc==0 or (r&REASON_EFFECT)==0 then return val end
+	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
+	if cid~=e:GetLabel() then return val end
+	return 0
 end
