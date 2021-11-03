f16784bb418ee6f774f9ec2ec20a06b6429bedff
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-03 15:48:12 -0600

    Update c386.lua

--- script/c386.lua
+++ script/c386.lua
@@ -4,32 +4,27 @@ function s.initial_effect(c)
 	--summon
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(92001300,0))
-	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_SUMMON_PROC)
-	e2:SetRange(LOCATION_HAND)
-	e2:SetTarget(s.target)
-	e2:SetOperation(s.activate)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
+	e2:SetCondition(s.sumcon)
+	e2:SetOperation(s.sumop)
 	c:RegisterEffect(e2)
 	local e3=e2:Clone()
 	e3:SetCode(EFFECT_SET_PROC)
 	c:RegisterEffect(e3)
 end
-function s.filter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsCanChangePosition()
+function s.sumcon(e,c,minc)
+	if c==nil then return true end
+	local mi,ma=c:GetTributeRequirement()
+	if mi<minc then mi=minc end
+	if ma<mi then return false end
+	e:SetLabel(mi)
+	return ma>0 and Duel.IsExistingMatchingCard(Card.IsCanChangePosition,tp,LOCATION_MZONE,LOCATION_MZONE,mi,nil) and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,2,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,2,2,nil)
-	Duel.SetOperationInfo(0,CATEGORY_POSITION,g,2,0,0)
-end
-function s.chkfilter(c,e)
-	return c:IsRelateToEffect(e) and c:IsLocation(LOCATION_MZONE) and c:IsFaceup()
-end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS):Filter(s.chkfilter,nil,e)
-	if #g>0 then
-		Duel.ChangePosition(sg,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
-	end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp,c)
+	local sg=e:GetLabelObject()
+	local g=Duel.SelectTarget(tp,Card.IsCanChangePosition,tp,LOCATION_MZONE,LOCATION_MZONE,sg,sg,nil)
+Duel.ChangePosition(g,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
 end
