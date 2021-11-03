95802d5df5dfc1c3890359fbaa55825315d35718
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-03 13:57:25 -0600

    Update c386.lua

--- script/c386.lua
+++ script/c386.lua
@@ -1,55 +1,29 @@
 --Lava Spider
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetCode(EFFECT_SUMMON_PROC)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCondition(s.otcon)
-	e1:SetTarget(aux.FieldSummonProcTg(s.ottg,s.sumtg))
-	e1:SetOperation(s.otop)
-	e1:SetCountLimit(1)
-	e1:SetValue(SUMMON_TYPE_TRIBUTE)
-	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_SET_PROC)
+	--summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(92001300,0))
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_SUMMON_PROC)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(s.sumcon)
+	e2:SetOperation(s.sumop)
 	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_SET_PROC)
+	c:RegisterEffect(e3)
 end
-function s.rmfilter(c)
-	return c:IsType(TYPE_MONSTER) and c:IsCanChangePosition()
-end
-function s.otcon(e,c,minc)
+function s.sumcon(e,c,minc)
 	if c==nil then return true end
-	local tp=c:GetControler()
-	return minc<=2 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
-		and Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil)
-end
-function s.ottg(e,c)
 	local mi,ma=c:GetTributeRequirement()
-	return mi<=2 and ma>=2
+	if mi<minc then mi=minc end
+	if ma<mi then return false end
+	e:SetLabel(mi)
+	return ma>0 and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,mi,nil,e,tp) and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 end
-function s.sumtg(e,tp,eg,ep,ev,re,r,rp,c)
-	local mg1=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	local mg2=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
-	::restart::
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g1=mg1:Select(tp,1,1,true,nil)
-	if not g1 then return false end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local tc=mg2:SelectUnselect(g1,tp,false,false,2,2)
-	if mg2:IsContains(tc) then
-		g1:AddCard(tc)
-		g1:KeepAlive()
-		e:SetLabelObject(g1)
-		return true
-	end
-	goto restart
-end
-function s.otop(e,tp,eg,ep,ev,re,r,rp,c)
-	local sg=e:GetLabelObject()
-	if not sg then return end
-Duel.ChangePosition(sg,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
-	sg:DeleteGroup()
+function s.sumop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,min,min,nil)
+	local tc=g:GetFirst()
+Duel.ChangePosition(tc,POS_FACEUP_DEFENSE,POS_FACEDOWN_DEFENSE,POS_FACEUP_ATTACK,POS_FACEUP_ATTACK)
 end
