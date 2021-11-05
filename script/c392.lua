e8994bdef8e34a1a4616e0ba4557356e48c8dfed
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-04 21:27:36 -0600

    Add files via upload

---
+++ script/c392.lua
@@ -0,0 +1,29 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,6,2)
+	c:EnableReviveLimit()
+	--chain attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(64,0))
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_DAMAGE_STEP_END)
+	e3:SetCost(s.cost)
+	e3:SetOperation(s.atop)
+	c:RegisterEffect(e3,false,REGISTER_FLAG_DETACH_XMAT)
+	--direct attack
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e4)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.atop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToBattle() then return end
+	Duel.ChainAttack()
+end
