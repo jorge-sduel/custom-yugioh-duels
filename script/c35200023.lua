49da05792ba28ae7d7810f19f270dd434235b64d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-05 08:22:15 -0600

    Update c35200023.lua

--- script/c35200023.lua
+++ script/c35200023.lua
@@ -71,7 +71,7 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsReason(REASON_DESTROY)
 end
 function s.spfilter(c,e,tp)
-	return c:IsCode(60800381) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsCode(60800381) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
