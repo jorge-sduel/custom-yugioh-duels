fb885e0b0c9be68f97dc5f2534190f3b01412cd8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-05 10:42:44 -0600

    Update c35200023.lua

--- script/c35200023.lua
+++ script/c35200023.lua
@@ -84,7 +84,7 @@ end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
-		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+		Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)
 	end
 end
 function s.efilter(e,te)
