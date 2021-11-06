f80e64fa5cc20afd11d33c1df48df3a688ee3ef2
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-06 07:40:52 -0600

    Add files via upload

---
+++ script/c393.lua
@@ -0,0 +1,55 @@
+--Titanic Combat Unit TCU-3000
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,s.xyzfilter,nil,3,nil,nil,nil,nil,false)
+	--cannot negate summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e1)
+	--Attribute change
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.attval)
+	c:RegisterEffect(e2)
+	--destroy replace
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.reptg)
+	e3:SetValue(s.repval)
+	c:RegisterEffect(e3)
+end
+function s.xyzfilter(c,xyz,sumtype,tp)
+	return c:IsRace(RACE_DINOSAUR)
+end
+function s.attval(e,c)
+	local att=0
+	local og=e:GetHandler():GetOverlayGroup()
+	for tc in aux.Next(og) do
+		att=att|tc:GetAttack()
+	end
+	return att
+end
+function s.repfilter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_ONFIELD)
+		and c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local g=c:GetLinkedGroup()
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) and c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
+		return true
+	else return false end
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
