227ee64f814ac456ea99fb64b24ecc4b5c931952
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-13 11:57:31 -0600

    Update c9999996.lua

--- script/c9999996.lua
+++ script/c9999996.lua
@@ -2,6 +2,7 @@
 function c9999996.initial_effect(c)
 	c:SetUniqueOnField(1,0,9999996)
 	c:EnableReviveLimit()
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsType,TYPE_SYNCHRO),2,2,Synchro.NonTuner(Card.IsType,TYPE_SYNCHRO),1,1)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
@@ -13,15 +14,6 @@ function c9999996.initial_effect(c)
 	e2:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_FIELD)
-	e3:SetCode(EFFECT_SPSUMMON_PROC)
-	e3:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e3:SetRange(LOCATION_EXTRA)
-	e3:SetCondition(c9999996.syncon)
-	e3:SetOperation(c9999996.synop)
-	e3:SetValue(SUMMON_TYPE_SYNCHRO)
-	c:RegisterEffect(e3)
 	--summon success
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
@@ -103,136 +95,6 @@ function c9999996.initial_effect(c)
 end
 function c9999996.splimit(e,se,sp,st)
 	return bit.band(st,SUMMON_TYPE_SYNCHRO)==SUMMON_TYPE_SYNCHRO
-end
-function c9999996.matfilter1(c,syncard)
-	return c:IsType(TYPE_TUNER) and c:IsType(TYPE_SYNCHRO) and (c:IsLocation(LOCATION_HAND) or c:IsFaceup()) and c:IsCanBeSynchroMaterial(syncard)
-end
-function c9999996.matfilter2(c,syncard)
-	return c:IsFaceup() and not c:IsType(TYPE_TUNER) and c:IsType(TYPE_SYNCHRO) and c:IsCanBeSynchroMaterial(syncard)
-end
-function c9999996.synfilter1(c,syncard,lv,g1,g2,g3)
-	local tlv=c:GetSynchroLevel(syncard)
-	if lv-tlv<=0 then return false end
-	local f1=c.tuner_filter
-	if c:IsHasEffect(55863245) then
-		return g3:IsExists(c9999996.synfilter2,1,c,syncard,lv-tlv,g2,f1,c)
-	else
-		return g1:IsExists(c9999996.synfilter2,1,c,syncard,lv-tlv,g2,f1,c)
-	end
-end
-function c9999996.synfilter2(c,syncard,lv,g2,f1,tuner1)
-	local tlv=c:GetSynchroLevel(syncard)
-	if lv-tlv<=0 then return false end
-	local f2=c.tuner_filter
-	if f1 and not f1(c) then return false end
-	if f2 and not f2(tuner1) then return false end
-	return g2:IsExists(c9999996.synfilter3,1,nil,syncard,lv-tlv,f1,f2)
-end
-function c9999996.synfilter3(c,syncard,lv,f1,f2)
-	local mlv=c:GetSynchroLevel(syncard)
-	local lv1=bit.band(mlv,0xffff)
-	local lv2=bit.rshift(mlv,16)
-	return (lv1==lv or lv2==lv) and (not f1 or f1(c)) and (not f2 or f2(c))
-end
-function c9999996.syncon(e,c,tuner,mg)
-	if c==nil then return true end
-	local tp=c:GetControler()
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<-2 then return false end
-	local g1=nil
-	local g2=nil
-	local g3=nil
-	if mg then
-		g1=mg:Filter(c9999996.matfilter1,nil,c)
-		g2=mg:Filter(c9999996.matfilter2,nil,c)
-		g3=mg:Filter(c9999996.matfilter1,nil,c)
-	else
-		g1=Duel.GetMatchingGroup(c9999996.matfilter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-		g2=Duel.GetMatchingGroup(c9999996.matfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-		g3=Duel.GetMatchingGroup(c9999996.matfilter1,tp,LOCATION_MZONE+LOCATION_HAND,LOCATION_MZONE,nil,c)
-	end
-	local pe=Duel.IsPlayerAffectedByEffect(tp,EFFECT_MUST_BE_SMATERIAL)
-	local lv=c:GetLevel()
-	if tuner then
-		local tlv=tuner:GetSynchroLevel(c)
-		if lv-tlv<=0 then return false end
-		local f1=tuner.tuner_filter
-		if not pe then
-			return g1:IsExists(c9999996.synfilter2,1,tuner,c,lv-tlv,g2,f1,tuner)
-		else
-			return c9999996.synfilter2(pe:GetOwner(),c,lv-tlv,g2,f1,tuner)
-		end
-	end
-	if not pe then
-		return g1:IsExists(c9999996.synfilter1,1,nil,c,lv,g1,g2,g3)
-	else
-		return c9999996.synfilter1(pe:GetOwner(),c,lv,g1,g2)
-	end
-end
-function c9999996.synop(e,tp,eg,ep,ev,re,r,rp,c,tuner,mg)
-	local g=Group.CreateGroup()
-	local g1=nil
-	local g2=nil
-	local g3=nil
-	if mg then
-		g1=mg:Filter(c9999996.matfilter1,nil,c)
-		g2=mg:Filter(c9999996.matfilter2,nil,c)
-		g3=mg:Filter(c9999996.matfilter1,nil,c)
-	else
-		g1=Duel.GetMatchingGroup(c9999996.matfilter1,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-		g2=Duel.GetMatchingGroup(c9999996.matfilter2,tp,LOCATION_MZONE,LOCATION_MZONE,nil,c)
-		g3=Duel.GetMatchingGroup(c9999996.matfilter1,tp,LOCATION_MZONE+LOCATION_HAND,LOCATION_MZONE,nil,c)
-	end
-	local pe=Duel.IsPlayerAffectedByEffect(tp,EFFECT_MUST_BE_SMATERIAL)
-	local lv=c:GetLevel()
-	if tuner then
-		g:AddCard(tuner)
-		local lv1=tuner:GetSynchroLevel(c)
-		local f1=tuner.tuner_filter
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		local tuner2=nil
-		if not pe then
-			local t2=g1:FilterSelect(tp,c9999996.synfilter2,1,1,tuner,c,lv-lv1,g2,f1,tuner)
-			tuner2=t2:GetFirst()
-		else
-			tuner2=pe:GetOwner()
-			Group.FromCards(tuner2):Select(tp,1,1,nil)
-		end
-		g:AddCard(tuner2)
-		local lv2=tuner2:GetSynchroLevel(c)
-		local f2=tuner2.tuner_filter
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		local m3=g2:FilterSelect(tp,c9999996.synfilter3,1,1,nil,c,lv-lv1-lv2,f1,f2)
-		g:Merge(m3)
-	else
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		local tuner1=nil
-		if not pe then
-			local t1=g1:FilterSelect(tp,c9999996.synfilter1,1,1,nil,c,lv,g1,g2,g3)
-			tuner1=t1:GetFirst()
-		else
-			tuner1=pe:GetOwner()
-			Group.FromCards(tuner1):Select(tp,1,1,nil)
-		end
-		g:AddCard(tuner1)
-		local lv1=tuner1:GetSynchroLevel(c)
-		local f1=tuner1.tuner_filter
-		local t2=nil
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		if tuner1:IsHasEffect(55863245) then
-			t2=g3:FilterSelect(tp,c9999996.synfilter2,1,1,tuner1,c,lv-lv1,g2,f1,tuner1)
-		else
-			t2=g1:FilterSelect(tp,c9999996.synfilter2,1,1,tuner1,c,lv-lv1,g2,f1,tuner1)
-		end
-		local tuner2=t2:GetFirst()
-		g:AddCard(tuner2)
-		local lv2=tuner2:GetSynchroLevel(c)
-		local f2=tuner2.tuner_filter
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-		local m3=g2:FilterSelect(tp,c9999996.synfilter3,1,1,nil,c,lv-lv1-lv2,f1,f2)
-		g:Merge(m3)
-	end
-	c:SetMaterial(g)
-	Duel.SendtoGrave(g,REASON_MATERIAL+REASON_SYNCHRO)
 end
 function c9999996.sumsuc(e,tp,eg,ep,ev,re,r,rp)
 	if e:GetHandler():GetSummonType()~=SUMMON_TYPE_SYNCHRO then return end
