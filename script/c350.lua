2f2d3839d114043acd4c89ff5ba3dddcdac17897
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-13 22:13:41 -0600

    Update c350.lua

--- script/c350.lua
+++ script/c350.lua
@@ -14,18 +14,18 @@ function s.initial_effect(c)
 	a2:SetCode(EFFECT_UPDATE_DEFENSE)
 	a2:SetValue(300)
 	c:RegisterEffect(a2)
-	--atk up
-	local a3=Effect.CreateEffect(c)
-	a3:SetDescription(aux.Stringid(id,2))
-	a3:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
-	a3:SetType(EFFECT_TYPE_XMATERIAL+EFFECT_TYPE_TRIGGER_F)
-	a3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	a3:SetCode(EFFECT_CANNOT_ACTIVATE)
-	a3:SetRange(LOCATION_OVERLAY)
-	a3:SetTargetRange(0,1)
-	a3:SetValue(s.aclimit)
-	a3:SetCondition(s.actcon)
-	c:RegisterEffect(a3)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(aux.dxmcostgen(1,1,nil))
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	e1:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	c:RegisterEffect(e1)
 end
 function s.efcon(e,tp,eg,ep,ev,re,r,rp)
 	return r==REASON_XYZ
@@ -80,4 +80,41 @@ function s.aclimit(e,re,tp)
 end
 function s.actcon(e)
 	return Duel.GetAttacker()==e:GetHandler()
+end
+function s.spfilter(c,e,tp,mc,pg)
+	return c:IsType(TYPE_XYZ) and not c:IsRank(e:GetHandler():GetRank()+1) and Duel.GetLocationCountFromEx(tp,tp,mc,c)>0
+		and mc:IsCanBeXyzMaterial(c,tp) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
+end
+	--Activation legality
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local c=e:GetHandler()
+		local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
+		return (#pg<=0 or (#pg==1 and pg:IsContains(c)))
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,e:GetHandler(),pg) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+	--Add 1 "Double or Nothing!", then Xyz summon 1 "Utopia" Xyz monster by using this card, and if you do, double its ATK
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local pg=aux.GetMustBeMaterialGroup(tp,Group.FromCards(c),tp,nil,nil,REASON_XYZ)
+	if not (c:IsFaceup() and c:IsRelateToEffect(e) and c:IsControler(tp) and not c:IsImmuneToEffect(e)) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local sc=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,c,pg):GetFirst()
+	if not sc then return end
+	Duel.BreakEffect()
+	sc:SetMaterial(c)
+	Duel.Overlay(sc,c)
+	if Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)==0 then return end
+	sc:CompleteProcedure()
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetValue(s.aclimit)
+	e2:SetCondition(s.actcon)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	sc:RegisterEffect(e2,true)
 end
