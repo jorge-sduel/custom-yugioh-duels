b81636fcbf4c6189f2fe2d065baaf478e05e37f9
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-17 15:31:14 -0600

    Update c396.lua

--- script/c396.lua
+++ script/c396.lua
@@ -6,45 +6,29 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
-	e1:SetCondition(s.con2)
 	e1:SetOperation(s.op2)
 	c:RegisterEffect(e1)
 end
 function s.spfilter1(c,e,tp)
 	return c:IsSetCard(0x1115) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.con2(e,tp,eg,ep,ev,re,r,rp)
-	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
-	if ex then return true end
-	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
-	if not ex then return false end
-	if cp~=PLAYER_ALL then return Duel.IsPlayerAffectedByEffect(cp,EFFECT_REVERSE_RECOVER)
-	else return Duel.IsPlayerAffectedByEffect(0,EFFECT_REVERSE_RECOVER)
-		or Duel.IsPlayerAffectedByEffect(1,EFFECT_REVERSE_RECOVER)
-	end
-end
-function s.op2(e,tp,eg,ep,ev,re,r,rp)
-	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,1)
-	e1:SetLabel(cid)
-	e1:SetValue(s.refcon)
-	e1:SetReset(RESET_CHAIN)
+	e1:SetTargetRange(1,0)
+	e1:SetValue(s.damval)
+	e1:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(e1,tp)
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e2,tp)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 end
-function s.chdmgcon(e,re,val,r,rp,rc)
-	local cc=Duel.GetCurrentChain()
-	if cc==0 or (r&REASON_EFFECT)==0 then return end
-	local cid=Duel.GetChainInfo(0,CHAININFO_CHAIN_ID)
-	if cid==e:GetLabel() then 
-		if e:GetLabelObject():GetLabel()==0 then
-			e:GetLabelObject():SetLabel(val)
-		end
-		return 0
+function s.damval(e,re,val,r,rp,rc)
+	if (r&REASON_EFFECT)~=0 then return 1500
 	else return val end
 end
