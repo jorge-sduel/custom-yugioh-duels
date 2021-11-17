4bae7debc2e51f1caeceaa2a4e92c280365b880d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-17 15:14:46 -0600

    Update c396.lua

--- script/c396.lua
+++ script/c396.lua
@@ -6,50 +6,33 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_CHAINING)
-	e1:SetCondition(s.condition)
-	e1:SetOperation(s.activate)
+	e1:SetCondition(s.con2)
+	e1:SetOperation(s.op2)
 	c:RegisterEffect(e1)
-end
-function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
-	if cv then e:SetLabel(cv) end
-	if ex and (cp==tp or cp==PLAYER_ALL) then return true end
-	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
-	if cv then e:SetLabel(cv) end
-	return ex and (cp==tp or cp==PLAYER_ALL) and Duel.IsPlayerAffectedByEffect(tp,EFFECT_REVERSE_RECOVER)
 end
 function s.spfilter1(c,e,tp)
 	return c:IsSetCard(0x1115) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetCode(EFFECT_FORCE_MZONE)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e1:SetTargetRange(1,0)
-		e1:SetValue(96)
-		Duel.RegisterEffect(e1,tp)
-		local res=Duel.GetLocationCountFromEx(tp,tp,e:GetHandler())>0
-			and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_EXTRA,0,1,nil,e,tp)
-		e1:Reset()
-		return res
+function s.con2(e,tp,eg,ep,ev,re,r,rp)
+	local ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_DAMAGE)
+	if ex then return true end
+	ex,cg,ct,cp,cv=Duel.GetOperationInfo(ev,CATEGORY_RECOVER)
+	if not ex then return false end
+	if cp~=PLAYER_ALL then return Duel.IsPlayerAffectedByEffect(cp,EFFECT_REVERSE_RECOVER)
+	else return Duel.IsPlayerAffectedByEffect(0,EFFECT_REVERSE_RECOVER)
+		or Duel.IsPlayerAffectedByEffect(1,EFFECT_REVERSE_RECOVER)
 	end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp,val,r,rc)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local c=e:GetHandler()
+function s.op2(e,tp,eg,ep,ev,re,r,rp)
 	local cid=Duel.GetChainInfo(ev,CHAININFO_CHAIN_ID)
-	local e1=Effect.CreateEffect(c)
+	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CHANGE_DAMAGE)
 	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetTargetRange(1,0)
+	e1:SetTargetRange(1,1)
 	e1:SetLabel(cid)
-	e1:SetValue(s.chdmgcon)
+	e1:SetValue(s.refcon)
 	e1:SetReset(RESET_CHAIN)
-	e1:SetLabelObject(e)
 	Duel.RegisterEffect(e1,tp)
 		local g=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
