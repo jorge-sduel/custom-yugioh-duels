24ea5cd254dafdb6e1a5f8ff888bb17d3669d99e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-20 10:22:01 -0600

    Update c30000029.lua

--- script/c30000029.lua
+++ script/c30000029.lua
@@ -12,23 +12,21 @@ function s.initial_effect(c)
 end
 s.listed_series={0x306}
 function s.filter(c)
-	return c:IsSetCard(0x306) and c:IsAbleToHand()
-end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_DECK,0,1,nil) end
+	return c:IsFaceup()
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetMatchingGroup(Card,Isfaceup(),tp,0,LOCATION_MZONE,nil)
-	if #g>0 then
-		local tc=g:GetFirst()
-		local at=AnnounceAttribute(tp,1,ATTRIBUTE_ALL)
-		for tc in aux.Next(g) do
-			local e1=Effect.CreateEffect(e:GetHandler())
-			e1:SetType(EFFECT_TYPE_SINGLE)
-			e1:SetCode(EFFECT_UPDATE_ATTACK)
-			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-			e1:SetValue(at)
-			tc:RegisterEffect(e1)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local sg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+	local tc=sg:GetFirst()
+	while tc do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetRange(LOCATION_MZONE)
+		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+		e1:SetValue(ATTRIBUTE_DARK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		tc=sg:GetNext()
 		end
 	end
 end
