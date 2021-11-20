241c6d6c72b31b06549c3e1c7f92fff86b1192f9
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-20 11:05:45 -0600

    Update c30000029.lua

--- script/c30000029.lua
+++ script/c30000029.lua
@@ -6,7 +6,6 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(s.negtg)
 	e1:SetOperation(s.negop)
 	c:RegisterEffect(e1)
 end
