f1706e7dd9bc64f15dbcf8bca26ecde2106cee78
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-11-20 10:41:46 -0600

    Update c30000029.lua

--- script/c30000029.lua
+++ script/c30000029.lua
@@ -6,28 +6,35 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetOperation(s.activate)
+	e1:SetTarget(s.negtg)
+	e1:SetOperation(s.negop)
 	c:RegisterEffect(e1)
 end
 s.listed_series={0x306}
 function s.filter(c)
-	return c:IsFaceup()
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.filter2(c)
+	return c:IsFaceup() and c:IsSetCard(0x306)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
-	end
 end
-function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local sg=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
-	local tc=sg:GetFirst()
-	while tc do
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(aux.FilterFaceupFunction(s.filter),tp,0,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetRange(LOCATION_MZONE)
 		e1:SetCode(EFFECT_CHANGE_ATTRIBUTE)
 		e1:SetValue(ATTRIBUTE_DARK)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g2=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g2>0 then
+		Duel.SendtoHand(g2,REASON_EFFECT)
 	end
 end
