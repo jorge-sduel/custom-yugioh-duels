3ed6b4a51106aa4226e16aa8aec585271e02e299
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-12-02 11:58:35 -0600

    Create c210777127.lua

---
+++ script/c210777127.lua
@@ -0,0 +1,39 @@
+--Destruction Sword Requisition Fusion
+--designed and scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	c:RegisterEffect(Fusion.CreateSummonEff(c,nil,s.matfilter,s.fextra))
+	--to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SEARCH+CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_TO_GRAVE)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_names={78193831}
+s.listed_series={0xd6,0xd7}
+function s.matfilter(c)
+	return c:IsSetCard(0xd6) or c:IsSetCard(0xd7) or c:IsRace(RACE_DRAGON) or c:IsRace(RACE_WARRIOR)
+end
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(aux.FilterFaceupFunction(s.matfilter),tp,0,LOCATION_ONFIELD,nil)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND+LOCATION_DECK)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToHand() end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,e:GetHandler(),1,tp,LOCATION_GRAVE)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsAbleToHand() then
+		Duel.SendtoHand(c,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,c)
+	end
+end
