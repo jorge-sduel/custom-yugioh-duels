e1a0f88992cff102e902b9a8244adc9ec3459428
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2021-12-16 14:11:30 -0600

    Update c95.lua

--- script/c95.lua
+++ script/c95.lua
@@ -152,161 +152,82 @@ function c95.descon(e,tp,eg,ep,ev,re,r,rp)
 end
 
 function c95.otcon4(e,c)
-
 	if c==nil then return true end
-
 	local g=Duel.GetTributeGroup(c)
-
 	return Duel.GetTributeCount(c)>=4 and g:IsExists(Card.IsFaceup,4,nil)
-
 end
-
 function c95.otop4(e,tp,eg,ep,ev,re,r,rp,c)
-
 	local g=Duel.GetTributeGroup(c)
-
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-
 	local sg=g:FilterSelect(tp,Card.IsFaceup,4,4,nil)
-
 	c:SetMaterial(sg)
-
 	Duel.Release(sg, REASON_SUMMON+REASON_MATERIAL)
-
 	--shuffle
-
 	local e4=Effect.CreateEffect(c)
-
 	e4:SetCategory(CATEGORY_TODECK)
-
 	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-
 	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-
 	e4:SetCode(EVENT_SUMMON_SUCCESS)
-
 	e4:SetCondition(c95.descon4)
-
 	e4:SetTarget(c95.target4)
-
 	e4:SetOperation(c95.activate4)
-
 	c:RegisterEffect(e4)
-
 end
-
 function c95.descon4(e,tp,eg,ep,ev,re,r,rp)
-
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_ADVANCE
-
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_TRIBUTE
 end
-
 function c95.target4(e,tp,eg,ep,ev,re,r,rp,chk)
-
 	if chk==0 then return Duel.GetFieldGroupCount(tp,0,LOCATION_HAND+LOCATION_ONFIELD)>0 end
-
 end
-
 function c95.activate4(e,tp,eg,ep,ev,re,r,rp)
-
 	local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND+LOCATION_ONFIELD)
-
 	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
-
 end
-
 function c95.otcon5(e,c)
-
 	if c==nil then return true end
-
 	local g=Duel.GetTributeGroup(c)
-
 	return Duel.GetTributeCount(c)>=5 and g:IsExists(Card.IsFaceup,5,nil)
-
 end
-
 function c95.otop5(e,tp,eg,ep,ev,re,r,rp,c)
-
 	local g=Duel.GetTributeGroup(c)
-
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RELEASE)
-
 	local sg=g:FilterSelect(tp,Card.IsFaceup,5,5,nil)
-
 	c:SetMaterial(sg)
-
 	Duel.Release(sg, REASON_SUMMON+REASON_MATERIAL)
-
 	--atkup
-
 	local e5=Effect.CreateEffect(c)
-
 	e5:SetType(EFFECT_TYPE_SINGLE)
-
 	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-
 	e5:SetRange(LOCATION_MZONE)
-
 	e5:SetCode(EFFECT_SET_ATTACK_FINAL)
-
 	e5:SetValue(c95.atkval)
-
 	c:RegisterEffect(e5)
-
 	--direct attack
-
 	local e6=Effect.CreateEffect(c)
-
 	e6:SetType(EFFECT_TYPE_SINGLE)
-
 	e6:SetCode(EFFECT_DIRECT_ATTACK)
-
 	c:RegisterEffect(e6)
-
 	--draw
-
 	local e7=Effect.CreateEffect(c)
-
 	e7:SetCategory(CATEGORY_DRAW)
-
 	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-
 	e7:SetProperty(EFFECT_FLAG_CARD_TARGET)
-
 	e7:SetCode(EVENT_SUMMON_SUCCESS)
-
 	e7:SetCondition(c95.descon)
-
 	e7:SetTarget(c95.target)
-
 	e7:SetOperation(c95.activate)
-
 	c:RegisterEffect(e7)
-
 	--shuffle
-
 	local e8=Effect.CreateEffect(c)
-
 	e8:SetCategory(CATEGORY_TODECK)
-
 	e8:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-
 	e8:SetProperty(EFFECT_FLAG_CARD_TARGET)
-
 	e8:SetCode(EVENT_SUMMON_SUCCESS)
-
 	e8:SetCondition(c95.descon4)
-
 	e8:SetTarget(c95.target4)
-
 	e8:SetOperation(c95.activate4)
-
 	c:RegisterEffect(e8)
-
 end
-
 function c95.atktg(e,c)
-
 	return c~=e:GetHandler()
-
 end
