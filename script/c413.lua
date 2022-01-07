d30db1e246eeab70401f3cab0aa5b9aad4bbf8a1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-01-07 16:56:45 -0600

    Update c413.lua

--- script/c413.lua
+++ script/c413.lua
@@ -74,7 +74,7 @@ function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.filter(c,e,tp)
-	return c:IsSetCard(0x86) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsLevel(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_DECK) and s.filter(chkc,e,tp) end
