b96b280569b09bc5d577aa8e9a4d9f3813bca41a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-01-19 13:05:18 -0600

    Add files via upload

---
+++ script/c424.lua
@@ -0,0 +1,31 @@
+--ミラーフォース・ドラゴン
+--Mirror Force Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,68540058,11082056)
+	--Special Summon Condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e1)
+	--atk up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.atkcon)
+	e2:SetValue(s.atkval)
+	c:RegisterEffect(e2)
+end
+s.material_trap=44095762
+function s.atkcon(e)
+	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL
+		and e:GetHandler()==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil
+end
+function s.atkval(e,c)
+	return Duel.GetAttackTarget():GetAttack()/2
+end
