0faef96efae3485ef77c634ffa155216277ddc8e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-01-21 09:33:19 -0600

    Add files via upload

---
+++ script/c426.lua
@@ -0,0 +1,90 @@
+--Percy's Hunting Ground
+--scripted by Naim
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_PREDRAW)
+	e1:SetRange(0x5f)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+	--Cannot leave the field due to effects.
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetTargetRange(LOCATION_FZONE,0)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetValue(s.filter)
+	Duel.RegisterEffect(e2,0)
+	--prevents ctivating/setting new fields
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_SSET)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(1,0)
+	e3:SetTarget(s.setlimit)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTargetRange(1,0)
+	e4:SetValue(s.actlimit)
+	c:RegisterEffect(e4)
+	--add spell/set trap from the banlist
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetRange(LOCATION_FZONE)
+	e5:SetTargetRange(LOCATION_FZONE,LOCATION_FZONE)
+	e5:SetProperty(EFFECT_FLAG_BOTH_SIDE)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	--e5:SetCountLimit(1,EFFECT_COUNT_CODE_SINGLE)
+	--e5:SetCountLimit(1,id)
+	e5:SetTarget(s.nametg)
+	e5:SetOperation(s.nameop)
+	c:RegisterEffect(e5)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	Duel.MoveToField(c,tp,tp,LOCATION_FZONE,POS_FACEUP,true)
+end
+function s.filter(e,te,c)
+	if not te then return false end
+	local tc=te:GetOwner()
+	return (te:IsActiveType(TYPE_MONSTER) and c~=tc
+		or (te:IsHasCategory(CATEGORY_TOHAND+CATEGORY_DESTROY+CATEGORY_REMOVE+CATEGORY_TODECK+CATEGORY_RELEASE+CATEGORY_TOGRAVE+CATEGORY_FUSION_SUMMON)
+		and te:IsActiveType(TYPE_SPELL+TYPE_TRAP+TYPE_MONSTER)))
+end
+function s.setlimit(e,c,tp)
+	return c:IsType(TYPE_FIELD)
+end
+function s.actlimit(e,re,tp)
+	return re:IsActiveType(TYPE_FIELD) and re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.cond1(e,tp,eg,ep,ev,re,r,rp)
+	--return Duel.GetFlagEffect(e:GetHandlerPlayer(),id)==0 and not e:GetHandler():IsStatus(STATUS_CHAINING)
+	return e:GetHandler():GetFlagEffect(id+ep)==0
+end
+function s.nametg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local code=e:GetHandler():GetCode()
+	--"Gadget" monster, except this card's current name
+	s.announce_filter={0x48,OPCODE_ISSETCARD,code,OPCODE_ISCODE,OPCODE_NOT,OPCODE_AND,TYPE_MONSTER,OPCODE_ISTYPE,OPCODE_AND}
+	local 		ac=Duel.GetRandomNumber(1,s.announce_filter)
+	Duel.SetTargetParam(ac)
+	Duel.SetOperationInfo(0,CATEGORY_ANNOUNCE,nil,0,tp,ANNOUNCE_CARD_FILTER)
+end
+function s.nameop(e,tp,eg,ep,ev,re,r,rp)
+	local ac=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+token=Duel.CreateToken(tp,ac)
+				Duel.SendtoHand(ac,tp,REASON_RULE)
+	end
+end
