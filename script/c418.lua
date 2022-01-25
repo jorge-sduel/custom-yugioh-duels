a002e11a5092868ecf7cd17bc1194184057d18e8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-01-25 16:49:43 -0600

    Update c418.lua

--- script/c418.lua
+++ script/c418.lua
@@ -35,7 +35,27 @@ function s.initial_effect(c)
 	e5:SetTargetRange(1,0)
 	e5:SetCondition(s.damcon)
 	e5:SetValue(s.val)
-	c:RegisterEffect(e5)
+	c:RegisterEffect(e5) 
+	--Halve damage
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCode(EFFECT_CHANGE_DAMAGE)
+	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e6:SetTargetRange(1,0)
+	e6:SetCondition(s.damcon2)
+	e6:SetValue(s.val2)
+	c:RegisterEffect(e6)
+	--Halve damage
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_FIELD)
+	e7:SetRange(LOCATION_GRAVE)
+	e7:SetCode(EFFECT_CHANGE_DAMAGE)
+	e7:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e7:SetTargetRange(1,0)
+	e7:SetCondition(s.damcon2)
+	e7:SetValue(s.val2)
+	c:RegisterEffect(e7)
 end
 s.listed_series={0x115}
 function s.indct(e,re,r,rp)
@@ -49,4 +69,10 @@ function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.val(e,re,dam,r,rp,rc)
 	return math.floor(dam-1500)
+end
+function s.damcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetBattleDamage(tp)<=1500 or Duel.GetEffectDamage(tp)<=1500
+end
+function s.val2(e,re,dam,r,rp,rc)
+	return math.floor(0)
 end
