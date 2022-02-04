f6941fde73432b496d0d97812b62036b40e1d75a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-02-04 14:04:56 -0600

    Add files via upload

--- script/c429.lua
+++ script/c429.lua
@@ -1,50 +1,73 @@
---閃刀姫-カイナ
---Sky Striker Ace - Khamuy
---Scripted by AlphaKretin
-
+--ノヴァ・ポータル
+--Nova Portal
+--scripted by Larry126
 local s,id=GetID()
 function s.initial_effect(c)
-	--Can only be special summoned once per turn
-	c:SetSPSummonOnce(id)
-	--Must be properly summoned before reviving
-	c:EnableReviveLimit()
-	--Link summon procedure
-	Link.AddProcedure(c,s.matfilter,1,1)
-	--indes
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_NEGATE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCondition(s.discon)
+	e1:SetTarget(s.distg)
+	e1:SetOperation(s.disop)
+	c:RegisterEffect(e1)
+	--Back to hand
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e2:SetDescription(aux.Stringid(65518099,0))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_PZONE)
 	e2:SetCountLimit(1)
-	e2:SetValue(s.indct)
-	c:RegisterEffect(e2)
-	--Halve damage
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCode(EFFECT_CHANGE_DAMAGE)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e4:SetTargetRange(1,0)
-	e4:SetValue(s.val)
-	c:RegisterEffect(e4)
-	--Halve damage
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_FIELD)
-	e5:SetRange(LOCATION_GRAVE)
-	e5:SetCode(EFFECT_CHANGE_DAMAGE)
-	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e5:SetTargetRange(1,0)
-	e5:SetValue(s.val)
-	c:RegisterEffect(e5)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)	
 end
-s.listed_series={0x115}
-function s.indct(e,re,r,rp)
-	return (r&REASON_BATTLE+REASON_EFFECT)~=0
+function s.discon(e,tp,eg,ep,ev,re,r,rp)
+	return rp~=tp and re:IsActiveType(TYPE_MONSTER) and Duel.IsChainNegatable(ev)
 end
-function s.matfilter(c,scard,sumtype,tp)
-	return c:IsSetCard(0x1115,scard,sumtype,tp) and not c:IsAttribute(ATTRIBUTE_EARTH,scard,sumtype,tp)
+function s.distg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_PZONE,0,2,nil,id) end
+	local dg=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_PZONE,0,nil,id)
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,1-tp,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,2,tp,LOCATION_PZONE)
 end
-function s.val(e,re,dam,r,rp,rc)
-	return math.floor(dam-1500)
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local dg=Duel.GetMatchingGroup(Card.IsCode,tp,LOCATION_PZONE,0,nil,id)
+	if Duel.Destroy(#dg==2 and dg or dg:Select(tp,2,2,nil),REASON_EFFECT)==2 and Duel.NegateActivation(ev) then
+		local turnp=Duel.GetTurnPlayer()
+		Duel.SkipPhase(turnp,PHASE_DRAW,RESET_PHASE+PHASE_END,1)
+		Duel.SkipPhase(turnp,PHASE_STANDBY,RESET_PHASE+PHASE_END,1)
+		Duel.SkipPhase(turnp,PHASE_MAIN1,RESET_PHASE+PHASE_END,1)
+		Duel.SkipPhase(turnp,PHASE_BATTLE,RESET_PHASE+PHASE_END,1,1)
+		Duel.SkipPhase(turnp,PHASE_MAIN2,RESET_PHASE+PHASE_END,1)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetCode(EFFECT_CANNOT_BP)
+		e1:SetTargetRange(1,0)
+		e1:SetReset(RESET_PHASE+PHASE_END)
+		Duel.RegisterEffect(e1,turnp)
+	end
+end
+function s.thfilter(c)
+	return (c:IsLevel(10) or c:IsLevel(1)) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
