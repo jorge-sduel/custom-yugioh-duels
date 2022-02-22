153d95b438723c3515ab2daefb4e3ab7bdf76f47
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-02-21 21:58:48 -0600

    Update c433004.lua

--- script/c433004.lua
+++ script/c433004.lua
@@ -62,6 +62,15 @@ Timeleap.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_EART
 	e5:SetTarget(cid.destg)
 	e5:SetOperation(cid.desop)
 	c:RegisterEffect(e5)
+	--Destroy replace
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e6:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e6:SetCode(EFFECT_DESTROY_REPLACE)
+	e6:SetCondition(Timeleap.Future)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetTarget(cid.desreptg)
+	c:RegisterEffect(e6)
 end
 function cid.TimeCost(e,c)
 	if c==nil then return true end
@@ -193,4 +202,18 @@ function cid.desop(e,tp,eg,ep,ev,re,r,rp)
 	if not tc then return end
 	if tc==c then tc=Duel.GetAttackTarget() end
 	if tc:IsRelateToBattle() then Duel.Destroy(tc,REASON_EFFECT) end
+end
+function cid.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsReason(REASON_BATTLE) and c:IsFaceup() and c:GetDefense()>=1000 end
+	if Duel.SelectYesNo(tp,aux.Stringid(40945356,0)) then
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetProperty(EFFECT_FLAG_COPY_INHERIT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e2:SetCode(EFFECT_UPDATE_DEFENSE)
+		e2:SetValue(-1000)
+		c:RegisterEffect(e2)
+		return true
+	else return false end
 end
