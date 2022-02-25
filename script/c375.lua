e009caf9d690eb4fc6a4fc8dcfe08638569f4018
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-02-25 16:43:40 -0600

    Update c375.lua

--- script/c375.lua
+++ script/c375.lua
@@ -58,9 +58,8 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.GetFieldGroup(tp,LOCATION_PZONE,LOCATION_PZONE)
 	Duel.SendtoExtraP(g,tp,REASON_EFFECT)
-	Duel.Overlay(c,g)
-	g:CancelToGrave()
 Duel.MoveToField(e:GetHandler(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	Duel.Overlay(c,g)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
