5606b49d4d6cb7d6328464fd42092b80d5acf3ea
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-02-25 11:01:27 -0600

    Update c375.lua

--- script/c375.lua
+++ script/c375.lua
@@ -63,7 +63,7 @@ function s.targetdes(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activatedes(e,tp,eg,ep,ev,re,r,rp)
 	local sg=Duel.GetMatchingGroup(s.cfilter,tp,LOCATION_PZONE,0,e:GetHandler())
-	Duel.SendtoExtraP(sg,tp,REASON_EFFECT)
+	Duel.Destroy(sg,REASON_EFFECT)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
