1c3c0447bf1d5533cd45e59bc994d9b281e08a83
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-09 15:29:34 -0600

    Update c425.lua

--- script/c425.lua
+++ script/c425.lua
@@ -2,10 +2,9 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Fusion.CreateSummonEff(c,nil,aux.FALSE,s.fextra,Fusion.ShuffleMaterial)
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsType,TYPE_MONSTER),aux.FALSE,s.fextra,Fusion.ShuffleMaterial)
 	c:RegisterEffect(e1)
 end
-s.listed_series={0x3008}
 function s.fextra(e,tp,mg)
 	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup,Card.IsAbleToDeck),tp,LOCATION_ONFIELD+LOCATION_HAND,0,nil)
 end
