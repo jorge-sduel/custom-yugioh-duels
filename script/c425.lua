58802c313472fc13b317954abf74231d84496d42
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-09 15:24:54 -0600

    Add files via upload

---
+++ script/c425.lua
@@ -0,0 +1,11 @@
+--SS fusion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,nil,aux.FALSE,s.fextra,Fusion.ShuffleMaterial)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x3008}
+function s.fextra(e,tp,mg)
+	return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsFaceup,Card.IsAbleToDeck),tp,LOCATION_ONFIEL+LOCATION_HAND,0,nil)
+end
