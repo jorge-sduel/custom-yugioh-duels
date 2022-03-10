994e0358ba205a31a7e05933c73322517ff19309
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-10 12:59:52 -0600

    Update c8017341.lua

--- script/c8017341.lua
+++ script/c8017341.lua
@@ -117,10 +117,10 @@ function cid.setop(e,tp,eg,ep,ev,re,r,rp)
 			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEDOWN)
 			local rc=Duel.SelectMatchingCard(tp,Card.IsFacedown,tp,LOCATION_SZONE,LOCATION_SZONE,1,1,e:GetHandler())
 			local rct=rc:GetFirst()
-			if #rc>0 then
+			if #rct>0 then
 				Duel.HintSelection(rc)
-				Duel.ConfirmCards(1-tp,rc)
-				if tc:IsType(TYPE_TRAP) then
+				Duel.ConfirmCards(1-tp,rct)
+				if rct:IsType(TYPE_TRAP) then
 					local actcon=rct:GetActivateEffect():GetCondition()
 					if actcon(rct:GetActivateEffect(),tp,eg,ep,ev,re,r,rp) then
 						if rct:GetActivateEffect():GetCost() then
