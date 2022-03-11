0fe51a9dc5b71346375b1345bd85997460a4e0e6
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-11 15:04:30 -0600

    Update c8017348.lua

--- script/c8017348.lua
+++ script/c8017348.lua
@@ -179,10 +179,11 @@ function cid.scop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local g=Duel.SelectMatchingCard(tp,cid.scfilter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
-		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		if Duel.ConfirmCards(1-tp,g) then
 			Duel.ConfirmCards(1-tp,g)
 			if Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
 				Duel.Destroy(c,REASON_EFFECT)
+Duel.SendtoHand(g,nil,REASON_EFFECT)
 			end
 		end
 	end
