e8588f87515d226bb0f17e99c0bc08e77ec13933
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-12 21:45:57 -0600

    Update c16000032.lua

--- script/c16000032.lua
+++ script/c16000032.lua
@@ -5,13 +5,5 @@ cid.IsEvolute=true
 if not EVOLUTE_IMPORTED then Duel.LoadScript("proc_evolute.lua") end
 function cid.initial_effect(c)
 	c:EnableReviveLimit()
-	Evolute.AddProcedure(c,cid.matfilter,2,2,cid.rcheck) 
-end
-function cid.matfilter(c,ec,tp)
-   return c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsRace(RACE_MACHINE)
-end
-function cid.rcheck(g,lc,sumtype,tp)
-	return g:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_LIGHT)
-		and g:IsExists(Card.IsRace,1,nil,RACE_MACHINE)
-end
+	Evolute.AddProcedure(c,nil,2,2) 
 end
