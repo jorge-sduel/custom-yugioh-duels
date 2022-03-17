e5afd582d6d1b7a7f9899aac1013c6776467145a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-17 08:58:12 -0600

    Update c857.lua

--- script/c857.lua
+++ script/c857.lua
@@ -1,7 +1,7 @@
 --Keyboard Star-vader, Bismuth
 function c857.initial_effect(c)
 	--pendulum summon
-	aux.AddPendulumProcedure(c)
+	Pendulum.AddProcedure(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
