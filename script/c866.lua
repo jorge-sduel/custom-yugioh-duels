4e93b5c434a74675c3d5eedf134bc1f9e7aae00e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-17 09:44:40 -0600

    Update c866.lua

--- script/c866.lua
+++ script/c866.lua
@@ -1,8 +1,29 @@
 --Gravity Collapse Dragon
 function c866.initial_effect(c)
 	--synchro summon
-	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTunerEx(nil),1,99)
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
+	--Lock
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(866,0))
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(c866.condition)
+	e1:SetTarget(c866.target)
+	e1:SetOperation(c866.operation)
+	c:RegisterEffect(e1)
+	--ATK up
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(866,1))
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(c866.atkcon)
+	e2:SetValue(100)
+	c:RegisterEffect(e2)
 end
 function c866.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
