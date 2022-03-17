07343637356cd1515b4da7896f87abb6c190dd47
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-17 09:58:00 -0600

    Update c857.lua

--- script/c857.lua
+++ script/c857.lua
@@ -2,9 +2,4 @@
 function c857.initial_effect(c)
 	--pendulum summon
 	Pendulum.AddProcedure(c)
-	--Activate
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	c:RegisterEffect(e1)
 end
