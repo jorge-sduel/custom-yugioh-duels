308caa2ea67debd0f5b4081e6378de28b24fd009
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-17 09:35:18 -0600

    Update c854.lua

--- script/c854.lua
+++ script/c854.lua
@@ -1,7 +1,7 @@
 --Star-vader, Nebula Lord Dragon
 function c854.initial_effect(c)
 	--synchro summon
-	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),Synchro.NonTunerEx(Card.IsSetCard,0x5DC),1,99)
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),1,1,Synchro.NonTunerEx(Card.IsSetCard,0x5DC),1,99)
 	c:EnableReviveLimit()
 	--cannot be destroyed
 	local e1=Effect.CreateEffect(c)
