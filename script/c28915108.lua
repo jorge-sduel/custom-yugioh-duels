e400b0c1b019b135689a408ca73e1d7c6448bc68
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-18 12:32:12 -0600

    Update c28915108.lua

--- script/c28915108.lua
+++ script/c28915108.lua
@@ -1,13 +1,6 @@
 --Corona Spell
 local ref,id=GetID()
 function ref.initial_effect(c)
-	--Cannot Summon
-	c:EnableReviveLimit()
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
-	c:RegisterEffect(e0)
 	--Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
