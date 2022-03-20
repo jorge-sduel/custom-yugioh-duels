7920bdb88a840c013bfb5de6eb2e50517dd6a946
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-20 14:03:52 -0600

    Update c16000820.lua

--- script/c16000820.lua
+++ script/c16000820.lua
@@ -197,9 +197,9 @@ function c16000820.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c16000820.destarget(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_PZONE) and cid.desfilter(chkc) and chkc~=e:GetHandler() end
+	if chkc then return chkc:IsLocation(LOCATION_PZONE) and c16000820.desfilter(chkc) and chkc~=e:GetHandler() end
 	if chk==0 then return Duel.IsExistingTarget(cid.filter,tp,LOCATION_PZONE,0,1,e:GetHandler()) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,cid.desfilter,tp,LOCATION_PZONE,0,1,1,e:GetHandler())
+	local g=Duel.SelectTarget(tp,c16000820.desfilter,tp,LOCATION_PZONE,0,1,1,e:GetHandler())
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
