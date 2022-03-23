1b4e357dfe14fcd943e7117540954b8a2c801ec0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-23 13:31:50 -0600

    Update proc_reverse_pendulum.lua

--- script/proc_reverse_pendulum.lua
+++ script/proc_reverse_pendulum.lua
@@ -1,12 +1,12 @@
-if not aux.PendulumProcedure then
-	aux.PendulumProcedure = {}
-	Pendulum = aux.PendulumProcedure
+if not aux.ReversePendulumProcedure then
+	aux.ReversePendulumProcedure = {}
+	ReversePendulum = aux.ReversePendulumProcedure
 end
-if not Pendulum then
-	Pendulum = aux.PendulumProcedure
+if not ReversePendulum then
+	ReversePendulum = aux.ReversePendulumProcedure
 end
---add procedure to Pendulum monster, also allows registeration of activation effect
-Pendulum.AddProcedure = aux.FunctionWithNamedArgs(
+--add procedure to ReversePendulum monster, also allows registeration of activation effect
+ReversePendulum.AddProcedure = aux.FunctionWithNamedArgs(
 function(c,reg,desc)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -18,8 +18,8 @@ function(c,reg,desc)
 	e1:SetCode(EFFECT_SPSUMMON_PROC_G)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
 	e1:SetRange(LOCATION_PZONE)
-	e1:SetCondition(Pendulum.Condition())
-	e1:SetOperation(Pendulum.Operation())
+	e1:SetCondition(RPendulum.Condition())
+	e1:SetOperation(RPendulum.Operation())
 	e1:SetValue(SUMMON_TYPE_PENDULUM)
 	c:RegisterEffect(e1)
 	--register by default
@@ -32,7 +32,7 @@ function(c,reg,desc)
 		c:RegisterEffect(e2)
 	end
 end,"handler","register","desc")
-function Pendulum.Filter(c,e,tp,lscale,rscale,lvchk)
+function RPendulum.Filter(c,e,tp,lscale,rscale,lvchk)
 	if lscale>rscale then lscale,rscale=rscale,lscale end
 	local lv=0
 	if c.pendulum_level then
@@ -41,10 +41,10 @@ function Pendulum.Filter(c,e,tp,lscale,rscale,lvchk)
 		lv=c:GetLevel()
 	end
 	return (c:IsLocation(LOCATION_HAND) or (c:IsFaceup() and c:IsType(TYPE_PENDULUM)))
-		and (lvchk or (lv>lscale and lv<rscale) or c:IsHasEffect(511004423)) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_PENDULUM,tp,false,false)
+		and (lvchk or (lv<lscale and lv>rscale) or c:IsHasEffect(511004423)) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_PENDULUM,tp,false,false)
 		and not c:IsForbidden()
 end
-function Pendulum.Condition()
+function RPendulum.Condition()
 	return	function(e,c,ischain,re,rp)
 				if c==nil then return true end
 				local tp=c:GetControler()
@@ -62,10 +62,10 @@ function Pendulum.Condition()
 				else
 					g=Duel.GetFieldGroup(tp,loc,0)
 				end
-				return g:IsExists(Pendulum.Filter,1,nil,e,tp,lscale,rscale,c:IsHasEffect(511007000) and rpz:IsHasEffect(511007000))
+				return g:IsExists(RPendulum.Filter,1,nil,e,tp,lscale,rscale,c:IsHasEffect(511007000) and rpz:IsHasEffect(511007000))
 			end
 end
-function Pendulum.Operation()
+function RPendulum.Operation()
 	return	function(e,tp,eg,ep,ev,re,r,rp,c,sg,inchain)
 				local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
 				local lscale=c:GetLeftScale()
@@ -83,9 +83,9 @@ function Pendulum.Operation()
 				if ft2>0 then loc=loc+LOCATION_EXTRA end
 				local tg=nil
 				if og then
-					tg=og:Filter(Card.IsLocation,nil,loc):Filter(Pendulum.Filter,nil,e,tp,lscale,rscale,c:IsHasEffect(511007000) and rpz:IsHasEffect(511007000))
+					tg=og:Filter(Card.IsLocation,nil,loc):Filter(RPendulum.Filter,nil,e,tp,lscale,rscale,c:IsHasEffect(511007000) and rpz:IsHasEffect(511007000))
 				else
-					tg=Duel.GetMatchingGroup(Pendulum.Filter,tp,loc,0,nil,e,tp,lscale,rscale,c:IsHasEffect(511007000) and rpz:IsHasEffect(511007000))
+					tg=Duel.GetMatchingGroup(RPendulum.Filter,tp,loc,0,nil,e,tp,lscale,rscale,c:IsHasEffect(511007000) and rpz:IsHasEffect(511007000))
 				end
 				ft1=math.min(ft1,tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND))
 				ft2=math.min(ft2,tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA))
@@ -115,7 +115,7 @@ function Pendulum.Operation()
 					else
 						sg:AddCard(tc)
 						if c:IsHasEffect(511007000)~=nil or rpz:IsHasEffect(511007000)~=nil then
-							if not Pendulum.Filter(tc,e,tp,lscale,rscale) then
+							if not RPendulum.Filter(tc,e,tp,lscale,rscale) then
 								local pg=sg:Filter(aux.TRUE,tc)
 								local ct0,ct3,ct4=#pg,pg:FilterCount(Card.IsLocation,nil,LOCATION_HAND),pg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
 								sg:Sub(pg)
@@ -123,7 +123,7 @@ function Pendulum.Operation()
 								ft2=ft2+ct4
 								ft=ft+ct0
 							else
-								local pg=sg:Filter(aux.NOT(Pendulum.Filter),nil,e,tp,lscale,rscale)
+								local pg=sg:Filter(aux.NOT(RPendulum.Filter),nil,e,tp,lscale,rscale)
 								sg:Sub(pg)
 								if #pg>0 then
 									if pg:GetFirst():IsLocation(LOCATION_HAND) then
