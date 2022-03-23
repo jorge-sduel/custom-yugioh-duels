a587a97402f2359a4f7efc38c1355d3188992540
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-23 14:37:51 -0600

    Update c18917001.lua

--- script/c18917001.lua
+++ script/c18917001.lua
@@ -109,7 +109,7 @@ end
 function c18917001.tuneop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		local e1=Effect.CreateEffect(c)
+		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_ADD_TYPE)
 		e1:SetValue(TYPE_TUNER)
