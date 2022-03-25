ab9caf3a9ae59487b7d8be8457445ce97fb1ed4b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-25 14:25:56 -0600

    Update c52401237.lua

--- script/c52401237.lua
+++ script/c52401237.lua
@@ -1,16 +1,16 @@
 --Cosmicburst Dragon
 local cid,id=GetID()
-cid.IsEquilibrium=true
+cid.IsBigbang=true
 if not BIGBANG_IMPORTED then Duel.LoadScript("proc_bigbang.lua") end
 function cid.initial_effect(c)
 	c:EnableReviveLimit()
 	Bigbang.AddProcedure(c,nil,2,99)
 	--equip
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(EFFECT_MATERIAL_CHECK)
-	e0:SetValue(cid.matcheck)
-	c:RegisterEffect(e0)
+	--local e0=Effect.CreateEffect(c)
+	--e0:SetType(EFFECT_TYPE_SINGLE)
+	--e0:SetCode(EFFECT_MATERIAL_CHECK)
+	--e0:SetValue(cid.matcheck)
+	--c:RegisterEffect(e0)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_EQUIP)
@@ -76,7 +76,7 @@ function cid.matcheck(e,c)
 	end
 end
 function cid.con1(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_SPECIAL)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_BIGBANG)
 end
 function cid.tg1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() end
