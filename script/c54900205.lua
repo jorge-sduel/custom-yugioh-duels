284e1171b56b6aa408c51c1e425275bc77fc04c1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-26 07:57:30 -0600

    Update c54900205.lua

--- script/c54900205.lua
+++ script/c54900205.lua
@@ -91,7 +91,7 @@ end
 function cid.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(cid.cfilter,tp,LOCATION_GRAVE,0,1,nil,tp) and e:GetHandler():IsAbleToRemoveAsCost() end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,cid.cfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp)
+	local g=Duel.SelectMatchingCard(tp,cid.cfilter,tp,LOCATION_GRAVE,0,1,1,nil,tp,c)
 	Duel.Remove(g+e:GetHandler(),POS_FACEUP,REASON_COST)
 	e:SetLabel(g:GetFirst():GetLevel())
 end
@@ -99,7 +99,7 @@ function cid.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil,e:GetHandler())
 end
 function cid.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local lv=e:GetLabel()
