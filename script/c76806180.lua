e46e2cf6413bf1cb2881e2fe5b09a64de61eafe5
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-26 12:16:28 -0600

    Update c76806180.lua

--- script/c76806180.lua
+++ script/c76806180.lua
@@ -50,8 +50,7 @@ function cid.spop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP) end
 end
 function cid.desfilter(c,tp)
-	return c:GetEquipTarget():IsBigbang
-end
+	return c:GetEquipTarget():Is Bigbang end
 function cid.tg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(cid.desfilter,tp,LOCATION_SZONE,LOCATION_SZONE,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
