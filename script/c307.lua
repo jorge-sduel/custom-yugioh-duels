62966b99cc00d7896f3492ab4d446a4f7123170c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-28 14:26:52 -0600

    Update c307.lua

--- script/c307.lua
+++ script/c307.lua
@@ -120,10 +120,9 @@ function s.scop(e,tp,eg,ep,ev,re,r,rp)
 		local sc=sg:GetFirst()
 		Synchro.Send=1
 		Duel.SynchroSummon(tp,sc,nil,mg)
-if not c:IsLocation(LOCATION_PZONE) then
+if not e:GetHandler():IsLocation(LOCATION_PZONE) then
 		Duel.SendtoExtraP(c,tp,REASON_EFFECT)
 Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
-		Duel.RaiseEvent(c,EVENT_SSET,e,REASON_EFFECT,tp,tp,0)
 	end
 end
 function s.regop(e,tp,eg,ep,ev,re,r,rp)
