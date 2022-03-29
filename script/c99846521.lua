35169c2f41f0e8347ab94e467fe563ac5e6f190b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-28 22:02:33 -0600

    Update c99846521.lua

--- script/c99846521.lua
+++ script/c99846521.lua
@@ -2,7 +2,7 @@
 local cid,id=GetID()
 function cid.initial_effect(c)
 	--synchro summon
-	Synchro.AddProcedure(c,nil,1,1,aux.NonTuner(nil),1,99)
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
 	--destroy
 	local e1=Effect.CreateEffect(c)
