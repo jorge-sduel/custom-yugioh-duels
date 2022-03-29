a0ef8d21b48c9a62e60404f7ca8cdce88a7f58c4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-03-28 21:59:10 -0600

    Update c99846521.lua

--- script/c99846521.lua
+++ script/c99846521.lua
@@ -1,15 +1,6 @@
 --Black Rose Pendulum Dragon
-local function getID()
-	local str=string.match(debug.getinfo(2,'S')['source'],"c%d+%.lua")
-	str=string.sub(str,1,string.len(str)-4)
-	local cod=_G[str]
-	local id=tonumber(string.sub(str,2))
-	return id,cod
-end
-local id,cid=getID()
-function cid.initial_effect(c)
 	--synchro summon
-	aux.AddSynchroProcedure(c,nil,aux.NonTuner(nil),1)
+	Synchro.AddProcedure(c,nil,1,1,aux.NonTuner(nil),1,99)
 	c:EnableReviveLimit()
 	--destroy
 	local e1=Effect.CreateEffect(c)
