69910a396d0d9f47f23be936650ef563147edb11
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-07 15:52:12 -0600

    Update c989705312.lua

--- script/c989705312.lua
+++ script/c989705312.lua
@@ -62,7 +62,7 @@ function c989705312.initial_effect(c)
 --
         local e8=Effect.CreateEffect(c)
 	e8:SetType(EFFECT_TYPE_SINGLE)
-	e8:SetCode(EFFECT_RANK_LEVEL)
+	e8:SetCode(EFFECT_LEVEL_RANK)
 	c:RegisterEffect(e8)
 end
 function c989705312.matfilter1(c)
