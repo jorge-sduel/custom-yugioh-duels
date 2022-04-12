7ed2a5e05edf59eb579e29d49418453b674ad86a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-12 13:56:11 -0600

    Update c979812053.lua

--- script/c979812053.lua
+++ script/c979812053.lua
@@ -120,13 +120,13 @@ function c979812053.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,11,nil)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
 end
 function c979812053.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) and Duel.Destroy(tc,REASON_EFFECT) then
-		Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+		Duel.Equip(tp,tc,e:GetHandler())
 	end
 end
 function c979812053.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
