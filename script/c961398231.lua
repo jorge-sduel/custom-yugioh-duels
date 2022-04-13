a8135fcf6b82dbb2b65d9515b105e3743e0dd0bc
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-13 13:48:43 -0600

    Update c961398231.lua

--- script/c961398231.lua
+++ script/c961398231.lua
@@ -38,6 +38,25 @@ function c961398231.initial_effect(c)
 	e3:SetOperation(c961398231.thop)
 	c:RegisterEffect(e3)
 --
+--spsummon
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetOperation(c961398231.spr)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(961398231,0))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetRange(LOCATION_GRAVE)
+	e5:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e5:SetCondition(c961398231.spcon)
+	e5:SetCost(c961398231.spcost)
+	e5:SetTarget(c961398231.sptg)
+	e5:SetOperation(c961398231.spop)
+	c:RegisterEffect(e5)
+--
         local e8=Effect.CreateEffect(c)
 	e8:SetType(EFFECT_TYPE_SINGLE)
 	e8:SetCode(EFFECT_LEVEL_RANK)
@@ -104,5 +123,32 @@ function c961398231.thop(e,tp,eg,ep,ev,re,r,rp)
 	if g:GetCount()>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.spr(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsReason(REASON_DESTROY) and c:IsPreviousLocation(LOCATION_ONFIELD)
+		and c:IsPreviousPosition(POS_FACEUP) then
+		c:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,2)
+	end
+end
+function c961398231.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:GetTurnID()~=Duel.GetTurnCount() and c:GetFlagEffect(961398231)>0
+end
+function c961398231.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD,nil)
+end
+function c961398231.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	e:GetHandler():ResetFlagEffect(id)
+end
+function c961398231.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)
 	end
 end
