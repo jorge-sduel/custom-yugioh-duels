0ed19183879b95a3cc957a61a1601025d1b5af4f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-14 17:17:28 -0600

    Update c989235101.lua

--- script/c989235101.lua
+++ script/c989235101.lua
@@ -9,7 +9,7 @@ function c989235101.initial_effect(c)
 	r1:SetRange(LOCATION_HAND)
 	r1:SetCondition(c989235101.runcon)
 	r1:SetOperation(c989235101.runop)
-	r1:SetValue(0x4f000000)
+	--r1:SetValue(0x4f000000)
 	c:RegisterEffect(r1)
 	--cannot special summon
 	--local e1=Effect.CreateEffect(c)
