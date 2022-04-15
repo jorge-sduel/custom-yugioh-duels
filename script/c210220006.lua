194d273c56efc9224f13e475bf1610618d56eb49
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-14 19:54:38 -0600

    Update c210220006.lua

--- script/c210220006.lua
+++ script/c210220006.lua
@@ -11,6 +11,14 @@ function card.initial_effect(c)
 	e1:SetOperation(card.activate)
 	c:RegisterEffect(e1)
 	--Effect to be added, other card doesn't exist in this batch
+	--change lv
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(card.tg)
+	e2:SetOperation(card.op)
+	c:RegisterEffect(e2)
 end
 function card.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():IsAbleToGraveAsCost() end
@@ -38,4 +46,25 @@ end
 function card.damop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_CARD,0,90162951)
 	Duel.Damage(1-tp,500,REASON_EFFECT)
+end
+function card.filter(c,lv)
+	return c:IsFaceup() and c:GetLevel()~=lv
+end
+function card.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local lv=e:GetHandler():GetLevel()
+	if chk==0 then return Duel.IsExistingMatchingCard(card.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,lv) end
+end
+function card.op(e,tp,eg,ep,ev,re,r,rp)
+	local lv=e:GetHandler():GetLevel()
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local tc=Duel.SelectMatchingCard(tp,card.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,lv):GetFirst()
+	if c:IsFaceup() and c:IsRelateToEffect(e) and tc then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(tc:GetLevel())
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e1)
+	end
 end
