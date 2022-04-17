4e39b55a7b95f62ad672dd21d15505205bc5dffe
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-17 14:15:01 -0600

    Update c249000634.lua

--- script/c249000634.lua
+++ script/c249000634.lua
@@ -153,8 +153,7 @@ function c249000634.spfilter(c,e,tp)
 	return c:IsCode(249001055) and c:IsCanBeSpecialSummoned(e,0,tp,true,true) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0
 end
 function c249000634.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return aux.MustMaterialCheck(nil,tp,EFFECT_MUST_BE_SMATERIAL)
-		and Duel.IsExistingMatchingCard(c249000634.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(c249000634.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function c249000634.spop(e,tp,eg,ep,ev,re,r,rp)
