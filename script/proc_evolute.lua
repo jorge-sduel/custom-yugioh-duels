0a5067a7f711988c203ed9031149e82424387e99
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-21 08:46:11 -0600

    Update proc_evolute.lua

--- script/proc_evolute.lua
+++ script/proc_evolute.lua
@@ -251,7 +251,7 @@ function Evolute.addct(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0x88)
 end
 function Evolute.addc(e,tp,eg,ep,ev,re,r,rp)
-	e:GetHandler():AddCounter(0x1109,1)
+	e:GetHandler():AddCounter(0x1,1)
 end
 --function Evolute.addc(e,tp,eg,ep,ev,re,r,rp)
 	--if e:GetHandler():IsRelateToEffect(e) then
