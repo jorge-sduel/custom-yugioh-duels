86ce112ab35b9721644ab58035dca4b96a934071
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-21 11:33:43 -0600

    Update proc_evolute.lua

--- script/proc_evolute.lua
+++ script/proc_evolute.lua
@@ -244,7 +244,6 @@ function Evolute.Operation(f,minc,maxc,specialchk,opp,loc,send)
 				end
 				g:DeleteGroup()
 				aux.DeleteExtraMaterialGroups(emt)
-	e:GetHandler():AddCounter(0x111f,e:GetHandler():GetLevel())
 			end
 end
 function Evolute.addct(e,tp,eg,ep,ev,re,r,rp,chk)
