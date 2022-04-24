319786e4b96ce48f55f7281e34db1922be77ebf4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-24 14:57:40 -0600

    Update c39212.lua

--- script/c39212.lua
+++ script/c39212.lua
@@ -35,24 +35,23 @@ function scard.filter2(c,tcode,e,tp)
 		return c:IsSetCard(0x104f) and c.assault_mode and c.assault_mode==tcode and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
 end
 function scard.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if chk==0 then
 		if e:GetLabel()~=1 then return false end
 		e:SetLabel(0)
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
-			and Duel.CheckReleaseGroup(tp,scard.filter1,1,nil,e,tp)
+		return ft>-1 and Duel.CheckReleaseGroupCost(tp,scard.filter1,1,false,nil,nil,e,tp,ft)
 	end
-	local rg=Duel.SelectReleaseGroup(tp,scard.filter1,1,1,nil,e,tp)
-	e:SetLabel(rg:GetFirst():GetCode())
+	local rg=Duel.SelectReleaseGroupCost(tp,scard.filter1,1,1,false,nil,nil,e,tp,ft)
+	Duel.SetTargetParam(rg:GetFirst():GetCode())
 	Duel.Release(rg,REASON_COST)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE+LOCATION_HAND+LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function scard.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local code=e:GetLabel()
-	local tcode=scard.list[code]
-	local tg=Duel.SelectMatchingCard(tp,scard.filter2,tp,LOCATION_GRAVE+LOCATION_HAND+LOCATION_DECK,0,1,1,nil,tcode,e,tp)
-	local tc=tg:GetFirst()
-	if tc and Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP)>0 then
+	local code=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,scard.filter2,tp,LOCATION_DECK,0,1,1,nil,code,e,tp):GetFirst()
+	if tc and Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP_ATTACK)>0 then
 		tc:CompleteProcedure()
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_FIELD)
@@ -61,5 +60,8 @@ function scard.activate(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_PHASE+PHASE_END)
 		e1:SetTargetRange(1,0)
 		Duel.RegisterEffect(e1,tp)
+	end
+end
+
 	end
 end
