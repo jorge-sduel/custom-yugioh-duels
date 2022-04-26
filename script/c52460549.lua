0c2aae7ff03320b213ba97bf0932eb151677becd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-04-26 07:56:52 -0600

    Update c52460549.lua

--- script/c52460549.lua
+++ script/c52460549.lua
@@ -1,37 +1,63 @@
 --created by Meedogh, coded by Lyris
-local cid,id=GetID()
-function cid.initial_effect(c)
+local s,id=GetID()
+if not BIGBANG_IMPORTED then Duel.LoadScript("proc_bigbang.lua") end
+function s.initial_effect(c)
+	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_SPECIAL_SUMMON)
-	e1:SetTarget(cid.tg)
-	e1:SetOperation(cid.op)
+	e1:SetLabel(0)
+	e1:SetCountLimit(1,id)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function cid.cfilter(c,e,tp)
-	return c:IsFaceup() and c:IsDestructable() and Duel.IsExistingMatchingCard(cid.filter,tp,LOCATION_EXTRA,0,1,c,c,e,tp)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	e:SetLabel(100)
+	if chk==0 then return true end
 end
-function cid.filter(c,mc,e,tp)
-	return c.IsBigbang and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_BIGBANG,tp,false,false) and Duel.GetLocationCountFromEx(tp,tp,mc)
+function s.cfilter(c,e,tp,ft)
+	local atk=c:GetAttack()
+	local eff4064256={Duel.GetPlayerEffect(tp,4064256)}
+	for _,te in ipairs(eff4064256) do
+		local val=te:GetValue()
+		if val and val(te,c,e,0) then rc=val(te,c,e,1) end
+	end
+	local eff12644061={Duel.GetPlayerEffect(tp,12644061)}
+	for _,te in ipairs(eff12644061) do
+		local val=te:GetValue()
+		if val and val(te,c,e,0) then att=val(te,c,e,1) end
+	end
+	return c:GetOriginalType()&TYPE_MONSTER~=0 and atk>0 and c:IsFaceup() and c:IsAbleToGraveAsCost() and (ft>0 or c:GetSequence()<5)
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,atk+800,e,tp)
 end
-function cid.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(cid.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil,e,tp) end
-	local g=Duel.GetMatchingGroup(cid.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+function s.spfilter(c,atk,e,tp)
+	return c.IsBigbang and c:IsAttack(atk) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_BIGBANG,tp,false,false)
 end
-function cid.op(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectMatchingCard(tp,cid.cfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then
+		if e:GetLabel()~=100 then return false end
+		e:SetLabel(0)
+		return ft>-1 and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp,ft)
+	end
+	e:SetLabel(0)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp,ft)
+	local tc=g:GetFirst()
+	Duel.SendtoGrave(tc,REASON_COST)
+	Duel.SetTargetCard(tc)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local tc=Duel.GetFirstTarget()
+	if not tc or not tc:IsLocation(LOCATION_GRAVE) or not tc:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,tc:GetAttack()+800,e,tp)
 	if #g>0 then
-		Duel.HintSelection(g)
-		if Duel.Destroy(g,REASON_EFFECT)==0 then return end
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-		local sg=Duel.SelectMatchingCard(tp,cid.filter,tp,LOCATION_EXTRA,0,1,g,g:GetFirst(),e,tp)
-		if #sg>0 then
-			Duel.BreakEffect()
-			Duel.SpecialSummon(sg,SUMMON_TYPE_BIGBANG,tp,tp,false,false,POS_FACEUP)
-		end
+		Duel.SpecialSummon(g,SUMMON_TYPE_BIGBANG,tp,tp,false,false,POS_FACEUP)
 	end
 end
