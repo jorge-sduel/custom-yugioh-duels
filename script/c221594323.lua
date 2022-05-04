ceba03b7a12c5c5429478724e962b2a264aa8d48
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-04 14:33:49 -0600

    Update c221594323.lua

--- script/c221594323.lua
+++ script/c221594323.lua
@@ -98,12 +98,11 @@ end
 function cid.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
-		local val=e:GetLabel()+1000
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
-		e1:SetValue(val)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(e:GetLabel()+1000)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
 		c:RegisterEffect(e1)
 	end
