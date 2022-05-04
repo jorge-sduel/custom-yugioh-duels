954009bea46b4ed5ffa6ed316f81ae572628712f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-04 14:41:35 -0600

    Update c221594323.lua

--- script/c221594323.lua
+++ script/c221594323.lua
@@ -104,7 +104,7 @@ function cid.op(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(val+1000)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
 		c:RegisterEffect(e1)
 	end
 end
