ae2cf4cfe0d9b92797fac966c2c6339fb4e3a71b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-04 14:27:50 -0600

    Update c221594323.lua

--- script/c221594323.lua
+++ script/c221594323.lua
@@ -99,7 +99,7 @@ function cid.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsRelateToEffect(e) and c:IsFaceup() then
 		local val=e:GetLabel()+1000
-		local e1=Effect.CreateEffect(e:GetHandler())
+		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(val)
