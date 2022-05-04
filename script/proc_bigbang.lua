746a8bb6f9a835bec39b61f6c1499f6374b8cd57
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-04 12:34:12 -0600

    Update proc_bigbang.lua

--- script/proc_bigbang.lua
+++ script/proc_bigbang.lua
@@ -47,7 +47,7 @@ function Bigbang.AddProcedure(c,f,min,max,specialchk,opp,loc,send)
 	--scale
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_CHANGE_LINK)
+	e3:SetCode(EFFECT_CHANGE_LEVEL)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CANNOT_DISABLE)
 	--e3:SetRange(LOCATION_MZONE)
 	e3:SetValue(Bigbang.Level)
