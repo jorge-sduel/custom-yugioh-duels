952dd4d336d4ce9952264248d5d40314937002ba
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-07 12:18:12 -0600

    Update c52401238.lua

--- script/c52401238.lua
+++ script/c52401238.lua
@@ -14,6 +14,5 @@ end
 function cid.lcheck(c,tp)
  --local rc=e:GetHandler()
  --local tp=e:GetControler()
-	return c:IsCode(52401237) and c:IsAttackBelow(3500)
---Duel.GetLP(e:GetHandlerPlayer()))
+	return c:IsCode(52401237) and c:IsAttackBelow(3500-Duel.GetLP(e:GetHandlerPlayer())
 end
