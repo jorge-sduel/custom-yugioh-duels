1658a3834c8937340d3326c89a47bb910faee336
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-07 12:01:33 -0600

    Update c52401238.lua

--- script/c52401238.lua
+++ script/c52401238.lua
@@ -11,7 +11,7 @@ c:AddSetcodesRule(id,false,0xbb109)
 	--e1:SetCode(52401238)
 	--c:RegisterEffect(e1)
 end
-function cid.lcheck(e,c)
+function cid.lcheck(e)
  --Local tc=e:GetHandler()
  --Local tp=e:GetControler()
 	return c:IsCode(52401237) and c:IsAttackAbove(3500-Duel.GetLP(e:GetHandlerPlayer()))
