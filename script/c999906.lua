9117d24f4b9299855f5f6be45221ee195bc950f5
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-08 21:40:11 -0600

    Update c999906.lua

--- script/c999906.lua
+++ script/c999906.lua
@@ -28,6 +28,6 @@ function c999906.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c999906.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
-		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENCE)
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
 	end
 end
