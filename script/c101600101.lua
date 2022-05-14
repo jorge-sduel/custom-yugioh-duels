769b44685344e89439e86f8d2cde9f6bf439cafd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-13 21:31:52 -0600

    Update c101600101.lua

--- script/c101600101.lua
+++ script/c101600101.lua
@@ -41,7 +41,7 @@ function c101600101.filter1(c,e,tp,code)
 	local lv=c:GetLevel()
 	--if code then
  return --c:IsCode(code) and 
-c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false) else
+--c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false) else
 	return c:IsType(TYPE_SYNCHRO) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_SYNCHRO,tp,false,false) and c:IsSetCard(0xcd01)
 		and Duel.IsExistingMatchingCard(c101600101.filter2,tp,LOCATION_GRAVE,0,1,nil,tp,lv)
 	end
