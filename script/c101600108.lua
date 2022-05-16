ede7dfb391eae18f4fbc79f9a17a56d7355b5ce8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-16 13:18:36 -0600

    Update c101600108.lua

--- script/c101600108.lua
+++ script/c101600108.lua
@@ -16,12 +16,12 @@ function c101600108.initial_effect(c)
 	c:RegisterEffect(e2)
 	--synchro custom
 	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_SINGLE)
-	e4:SetCode(EFFECT_SYNCHRO_MATERIAL_CUSTOM)
-	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e4:SetTarget(c101600108.syntg)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTarget(c101600108.sctg)
 	e4:SetValue(1)
-	e4:SetOperation(c101600108.synop)
+	e4:SetOperation(c101600108.scop)
 	c:RegisterEffect(e4)
 end
 function c101600108.filter(c)
@@ -39,71 +39,29 @@ function c101600108.op(e,tp,eg,ep,ev,re,r,rp)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
-function c101600108.synfilter1(c,syncard,tuner,f)
-    return c:IsFaceup() and c:IsCanBeSynchroMaterial(syncard,tuner) and (f==nil or f(c))
+function c101600108.scfilter1(c,tp,mc)
+	return Duel.IsExistingMatchingCard(c101600108.scfilter2,tp,LOCATION_GRAVE,0,1,nil,mc,c,tp)
 end
-function c101600108.synfilter2(c,syncard,tuner,f)
-    return c:IsNotTuner() and c:IsCanBeSynchroMaterial(syncard,tuner) and (f==nil or f(c))
+function c101600108.scfilter2(c,mc,sc,tp)
+	local mg=Group.FromCards(c,mc)
+	return c:IsCanBeSynchroMaterial(sc) and sc:IsSynchroSummonable(nil,mg)
+		and Duel.GetLocationCountFromEx(tp,tp,mg,sc)>0
 end
-function c101600108.syncheck(c,g,mg,tp,lv,syncard,minc,maxc)
-    g:AddCard(c)
-    local ct=g:GetCount()
-    local res=c101600108.syngoal(g,tp,lv,syncard,minc,ct)
-        or (ct<maxc and mg:IsExists(c101600108.syncheck,1,g,g,mg,tp,lv,syncard,minc,maxc))
-    g:RemoveCard(c)
-    return res
+function c101600108.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(c101600108.scfilter1,tp,LOCATION_EXTRA,0,1,nil,tp,c) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function c101600108.syngoal(g,tp,lv,syncard,minc,ct)
-    return ct>=minc
-        and g:CheckWithSumEqual(Card.GetSynchroLevel,lv,ct,ct,syncard)
-        and Duel.GetLocationCountFromEx(tp,tp,g,syncard)>0
-end
-function c101600108.syntg(e,syncard,f,min,max)
-    local minc=min+1
-    local maxc=max+1
-    local c=e:GetHandler()
-    local tp=syncard:GetControler()
-    local lv=syncard:GetLevel()
-    if lv<=c:GetLevel() then return false end
-    local g=Group.FromCards(c)
-    local mg=Duel.GetMatchingGroup(c101600108.synfilter1,syncard:GetControler(),LOCATION_MZONE,LOCATION_MZONE,c,syncard,c,f)
-    if (syncard:GetLevel()==7 or syncard:GetLevel()==8) and syncard:IsRace(RACE_DRAGON) then
-        local exg=Duel.GetMatchingGroup(c101600108.synfilter2,syncard:GetControler(),LOCATION_GRAVE,0,c,syncard,c,f)
-        mg:Merge(exg)
-    end
-    return mg:IsExists(c101600108.syncheck,1,g,g,mg,tp,lv,syncard,minc,maxc)
-end
-function c101600108.synop(e,tp,eg,ep,ev,re,r,rp,syncard,f,min,max)
-    local minc=min+1
-    local maxc=max+1
-    local c=e:GetHandler()
-    local lv=syncard:GetLevel()
-    local g=Group.FromCards(c)
-    local mg=Duel.GetMatchingGroup(c101600108.synfilter1,syncard:GetControler(),LOCATION_MZONE,LOCATION_MZONE,c,syncard,c,f)
-    if (syncard:GetLevel()==7 or syncard:GetLevel()==8) and syncard:IsRace(RACE_DRAGON) then
-        local exg=Duel.GetMatchingGroup(c101600108.synfilter2,syncard:GetControler(),LOCATION_GRAVE,0,c,syncard,c,f)
-        mg:Merge(exg)
-    end
-    for i=1,maxc do
-        local cg=mg:Filter(c101600108.syncheck,g,g,mg,tp,lv,syncard,minc,maxc)
-        if cg:GetCount()==0 then break end
-        local minct=1
-        if c101600108.syngoal(g,tp,lv,syncard,minc,i) then
-            if not Duel.SelectYesNo(tp,210) then break end
-            minct=0
-        end
-        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
-        local sg=cg:Select(tp,minct,1,nil)
-        if sg:GetCount()==0 then break end
-        g:Merge(sg)
-    end
-	local ban=g:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
-    if ban then
-		for i=1,ban:GetCount() do
-			bann=ban:GetFirst()
-			g:RemoveCard(bann)
-		end
+function c101600108.scop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(c101600108.scfilter1,tp,LOCATION_EXTRA,0,nil,tp,c)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
+		local sc=g:Select(tp,1,1,nil):GetFirst()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SMATERIAL)
+		local mg=Duel.SelectMatchingCard(tp,c101600108.scfilter2,tp,LOCATION_GRAVE,0,1,1,nil,c,sc,tp)
+		mg:AddCard(c)
+		Duel.SynchroSummon(tp,sc,nil,mg)
 	end
-	Duel.SetSynchroMaterial(g)
-	if ban and Duel.SendtoDeck(ban,nil,0,REASON_EFFECT+REASON_MATERIAL)~=0 then Duel.ShuffleDeck(tp) end
 end
