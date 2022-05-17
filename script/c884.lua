f613ba2f4d767603b65f992fbf76b81aa480493b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-17 12:42:25 -0600

    Update c884.lua

--- script/c884.lua
+++ script/c884.lua
@@ -43,12 +43,12 @@ function c884.thop1(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function c884.thtg2(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(c884.thfilter2,tp,LOCATION_ONFIEL+LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+	if chk==0 then return Duel.IsExistingMatchingCard(c884.thfilter2,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_ONFIELD+LOCATION_GRAVE)
 end
 function c884.thop1(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,c884.thfilter2,tp,LOCATION_ONFIEL+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,c884.thfilter2,tp,LOCATION_ONFIELD+LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
