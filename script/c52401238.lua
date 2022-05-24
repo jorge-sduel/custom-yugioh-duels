6952e7429dd98e61414721109b71c35909067a85
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-05-23 21:56:12 -0600

    Update c52401238.lua

--- script/c52401238.lua
+++ script/c52401238.lua
@@ -19,5 +19,5 @@ function cid.excon(e)
 	return Duel.GetLP(e:GetHandlerPlayer())<=6000
 end
 function cid.excon2(e,g,lc,sumtype,tp)
-	return g:IsExists(Card.IsAttackBelow,1,nil,Duel.GetLP(e:GetHandlerPlayer())+e:GetAttack())
+	return g:IsExists(Card.IsAttackBelow,1,nil,Duel.GetLP(tp)+e:GetAttack())
 end
