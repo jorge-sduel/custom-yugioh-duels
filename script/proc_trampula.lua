a2d4f491cde52f56a72d562cb59faefeb88106ce
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-01 10:04:46 -0600

    Update proc_trampula.lua

--- script/proc_trampula.lua
+++ script/proc_trampula.lua
@@ -170,7 +170,6 @@ if not tc then break end
 					end
 					Duel.HintSelection(Group.FromCards(c))
 					Duel.HintSelection(Group.FromCards(rpz))
-	end
 	Duel.SpecialSummonStep(tc,SUMMON_TYPE_PENDULUM,tp,tp,true,true,POS_FACEUP)
 		local e1=Effect.CreateEffect(c)
 			e1:SetCode(EFFECT_ADD_TYPE)
@@ -186,6 +185,7 @@ if not tc then break end
 			e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
 			e2:SetValue(TYPE_TRAP)
 			tc:RegisterEffect(e2)
+	end
 	Duel.SpecialSummonComplete()
 	if lp>0 then
 		Duel.BreakEffect()
