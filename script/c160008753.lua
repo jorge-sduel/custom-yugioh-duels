fe368eebf1361d650c79b08d4ac1442de094ade6
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-01 12:56:32 -0600

    Update c160008753.lua

--- script/c160008753.lua
+++ script/c160008753.lua
@@ -1,7 +1,13 @@
 --Queen of Bone Dragon
-function c160008753.initial_effect(c)
+local cid,id=GetID()
+function cid.initial_effect(c)
+cid.IsEvolute=true
+if not EVOLUTE_IMPORTED then Duel.LoadScript("proc_evolute.lua") end
+	--c:EnableCounterPermit(0x88)
 	c:EnableReviveLimit()
-	  --E summon
-	aux.AddOrigEvoluteType(c)
-   aux.AddEvoluteProc(c,nil,6,aux.TRUE,aux.TRUE,1,99)
+	Evolute.AddProcedure(c,nil,2,99,cid.rcheck)
+end
+function cid.rcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_DARK)
+		and (g:IsExists(Card.IsRace,1,nil,RACE_ZOMBIE) or g:IsExists(Card.IsRace,1,nil,RACE_DRAGON))
 end
