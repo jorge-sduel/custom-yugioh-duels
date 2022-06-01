159e4fbf524cc7082e72565f39ecb89f502b3b2c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-01 10:19:54 -0600

    Update proc_trampula.lua

--- script/proc_trampula.lua
+++ script/proc_trampula.lua
@@ -87,89 +87,67 @@ function Trampula.Condition()
 			end
 end
 function Trampula.Operation()
-	return	function(e,tp,eg,ep,ev,re,r,rp,c,sg,inchain)
-				local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
-				local lscale=c:GetLeftScale()
-				local rscale=rpz:GetRightScale()
-				local ft1=Duel.GetLocationCount(tp,LOCATION_MZONE)
-				local ft2=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_PENDULUM)
-				local ft=Duel.GetUsableMZoneCount(tp)
-				if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then
-					if ft1>0 then ft1=1 end
-					if ft2>0 then ft2=1 end
-					ft=1
-				end
-				local loc=0
-				if ft1>0 then loc=loc+LOCATION_HAND end
-				if ft2>0 then loc=loc+LOCATION_EXTRA end
-				local tg=nil
-				if og then
-					tg=og:Filter(Card.IsLocation,nil,loc):Filter(Trampula.Filter,nil,e,tp,lscale,rscale,c:IsHasEffect(511007000) and rpz:IsHasEffect(511007000))
-				else
-					tg=Duel.GetMatchingGroup(Trampula.Filter,tp,loc,0,nil,e,tp,lscale,rscale,c:IsHasEffect(511007000) and rpz:IsHasEffect(511007000))
-				end
-				ft1=math.min(ft1,tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND))
-				ft2=math.min(ft2,tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA))
-				ft2=math.min(ft2,aux.CheckSummonGate(tp) or ft2)
-				while true do
-					local ct1=tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND)
-					local ct2=tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
-					local ct=ft
-					if ct1>ft1 then ct=math.min(ct,ft1) end
-					if ct2>ft2 then ct=math.min(ct,ft2) end
-					local loc=0
-					if ft1>0 then loc=loc+LOCATION_HAND end
-					if ft2>0 then loc=loc+LOCATION_EXTRA end
-					local g=tg:Filter(Card.IsLocation,sg,loc)
-					if #g==0 or ft==0 then break end
-					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-					local tc=Group.SelectUnselect(g,sg,tp,#sg>0,Duel.IsSummonCancelable())
---if not tc then break end
-					--if sg:IsContains(tc) then
-						--sg:RemoveCard(tc)
-						if tc:IsLocation(LOCATION_HAND) then
-							ft1=ft1+1
-						else
-							ft2=ft2+1
-						end
-						ft=ft+1
-					else
-						sg:AddCard(tc)
-						if c:IsHasEffect(511007000)~=nil or rpz:IsHasEffect(511007000)~=nil then
-							if not Trampula.Filter(tc,e,tp,lscale,rscale) then
-								local pg=sg:Filter(aux.TRUE,tc)
-								local ct0,ct3,ct4=#pg,pg:FilterCount(Card.IsLocation,nil,LOCATION_HAND),pg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
-								sg:Sub(pg)
-								ft1=ft1+ct3
-								ft2=ft2+ct4
-								ft=ft+ct0
-							else
-								local pg=sg:Filter(aux.NOT(Trampula.Filter),nil,e,tp,lscale,rscale)
-								sg:Sub(pg)
-								if #pg>0 then
-									if pg:GetFirst():IsLocation(LOCATION_HAND) then
-										ft1=ft1+1
-									else
-										ft2=ft2+1
-									end
-									ft=ft+1
-								end
-							end
-						end
-						if tc:IsLocation(LOCATION_HAND) then
-							ft1=ft1-1
-						else
-							ft2=ft2-1
-						end
-						ft=ft-1
-					end
-				end
-				if #sg>0 then
-					if not inchain then
-						Duel.RegisterFlagEffect(tp,10000000,RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
-					end
-					Duel.HintSelection(Group.FromCards(c))
-					Duel.HintSelection(Group.FromCards(rpz))
+	local rpz=Duel.GetFieldCard(tp,LOCATION_PZONE,1)
+	local lscale=c:GetLeftScale()
+	local rscale=rpz:GetRightScale()
+	if lscale>rscale then lscale,rscale=rscale,lscale end
+	local ft1=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ft2=Duel.GetLocationCountFromEx(tp)
+	local ft=Duel.GetUsableMZoneCount(tp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then
+		if ft1>0 then ft1=1 end
+		if ft2>0 then ft2=1 end
+		ft=1
+	end
+	local loc=0
+	if ft1>0 then loc=loc+LOCATION_HAND end
+	if ft2>0 then loc=loc+LOCATION_EXTRA end
+	local tg=nil
+	if og then
+		tg=og:Filter(Card.IsLocation,nil,loc):Filter(Trampula.Filter,nil,e,tp,lscale,rscale)
+	else
+		tg=Duel.GetMatchingGroup(Trampula.Filter,tp,loc,0,nil,e,tp,lscale,rscale)
+	end
+	ft1=math.min(ft1,tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND))
+	ft2=math.min(ft2,tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA))
+	ft2=math.min(ft2,aux.CheckSummonGate(tp) or ft2)
+	while true do
+		local ct1=tg:FilterCount(Card.IsLocation,nil,LOCATION_HAND)
+		local ct2=tg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
+		local ct=ft
+		if ct1>ft1 then ct=math.min(ct,ft1) end
+		if ct2>ft2 then ct=math.min(ct,ft2) end
+		local loc=0
+		if ft1>0 then loc=loc+LOCATION_HAND end
+		if ft2>0 then loc=loc+LOCATION_EXTRA end
+		local g=tg:Filter(Card.IsLocation,sg,loc)
+		if #g==0 or ft==0 then break end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local tc=Group.SelectUnselect(g,sg,tp,true,true)
+		if not tc then break end
+		if sg:IsContains(tc) then
+				sg:RemoveCard(tc)
+				if tc:IsLocation(LOCATION_HAND) then
+				ft1=ft1+1
+			else
+				ft2=ft2+1
+			end
+			ft=ft+1
+		else
+			sg:AddCard(tc)
+			if tc:IsLocation(LOCATION_HAND) then
+				ft1=ft1-1
+			else
+				ft2=ft2-1
+			end
+			ft=ft-1
+		end
+	end
+	if #sg>0 then
+		Duel.Hint(HINT_CARD,0,id)
+		Duel.RegisterFlagEffect(tp,29432356,RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
+		Duel.HintSelection(Group.FromCards(c))
+		Duel.HintSelection(Group.FromCards(rpz))
 	Duel.SpecialSummonStep(tc,SUMMON_TYPE_PENDULUM,tp,tp,true,true,POS_FACEUP)
 		local e1=Effect.CreateEffect(c)
 			e1:SetCode(EFFECT_ADD_TYPE)
@@ -186,11 +164,6 @@ function Trampula.Operation()
 			e2:SetValue(TYPE_TRAP)
 			tc:RegisterEffect(e2)
 	end
-	Duel.SpecialSummonComplete()
-	if lp>0 then
-		Duel.BreakEffect()
-	end
-end
 end
 function Trampula.SetOp(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
