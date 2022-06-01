eb5738eeadbc533d1f4ca5b53759ded7e0ae84a5
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-01 10:38:22 -0600

    Update proc_trampula.lua

--- script/proc_trampula.lua
+++ script/proc_trampula.lua
@@ -140,6 +140,7 @@ function Trampula.Operation(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
 				ft2=ft2-1
 			end
 			ft=ft-1
+		end
 	Duel.SpecialSummonStep(tc,SUMMON_TYPE_PENDULUM,tp,tp,true,true,POS_FACEUP)
 		local e1=Effect.CreateEffect(c)
 			e1:SetCode(EFFECT_ADD_TYPE)
@@ -155,10 +156,8 @@ function Trampula.Operation(e,tp,eg,ep,ev,re,r,rp,c,sg,og)
 			e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
 			e2:SetValue(TYPE_TRAP)
 			tc:RegisterEffect(e2)
-		end
 	end
 	if #sg>0 then
-		Duel.Hint(HINT_CARD,0,46986414)
 		Duel.RegisterFlagEffect(tp,29432356,RESET_PHASE+PHASE_END+RESET_SELF_TURN,0,1)
 		Duel.HintSelection(Group.FromCards(c))
 		Duel.HintSelection(Group.FromCards(rpz))
