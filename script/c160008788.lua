8126d45b81a5161b0b8b37f08efd9668a73df64f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-03 16:16:18 -0600

    Update c160008788.lua

--- script/c160008788.lua
+++ script/c160008788.lua
@@ -1,6 +1,6 @@
 --Medivatale Onibear
 function c160008788.initial_effect(c)
-c160008788.IsEvolute=true
+c160008788.Is_Evolute=true
 if not EVOLUTE_IMPORTED then Duel.LoadScript("proc_evolute.lua") end
 	--c:EnableCounterPermit(0x88)
 	c:EnableReviveLimit()
