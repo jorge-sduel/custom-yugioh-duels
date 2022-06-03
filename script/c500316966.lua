7a2a7d3b8e32d73d180d7594914ead6a7acd9166
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-03 16:26:48 -0600

    Update c500316966.lua

--- script/c500316966.lua
+++ script/c500316966.lua
@@ -1,7 +1,7 @@
 --Fairy of Breeze
 	local cid,id=GetID()
 function cid.initial_effect(c)
-cid.IsEvolute=true
+cid.Is_Evolute=true
 if not EVOLUTE_IMPORTED then Duel.LoadScript("proc_evolute.lua") end
 	--c:EnableCounterPermit(0x88)
 	c:EnableReviveLimit()
