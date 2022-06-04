ee58910e073ab7d1852a480751b74f8248455448
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-03 18:11:31 -0600

    Update c160008888.lua

--- script/c160008888.lua
+++ script/c160008888.lua
@@ -65,8 +65,9 @@ function c160008888.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=g:GetFirst()
 	while tc do
-	 Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
-		tc=g:GetNext()
-			end
+			Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+			tc=g:GetNext()
+		end
+		Duel.SpecialSummonComplete()
 			
 end
