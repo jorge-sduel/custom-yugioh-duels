86019e97f27fea15700a0f8c1a6f4e61bd3bd9b0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-12 21:21:01 -0600

    Update c160008802.lua

--- script/c160008802.lua
+++ script/c160008802.lua
@@ -1,7 +1,50 @@
 --Medivatale Flora
 function c160008802.initial_effect(c)
    c:EnableReviveLimit()
-	aux.AddOrigEvoluteType(c)
-   aux.AddEvoluteProc(c,nil,10,aux.TRUE,aux.TRUE,3,3)
+c160008802.Is_Evolute=true
+if not EVOLUTE_IMPORTED then Duel.LoadScript("proc_evolute.lua") end
+	--c:EnableCounterPermit(0x88)
+	c:EnableReviveLimit()
+	Evolute.AddProcedure(c,nil,2,99)
+	--destroy
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(511000609,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_DESTROYED)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e1:SetCondition(c160008802.con)
+	e1:SetTarget(c160008802.tg)
+	e1:SetOperation(c160008802.op)
+	c:RegisterEffect(e1)
 end
+function c160008802.con(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetLP(tp)<Duel.GetLP(1-tp)
+end
+function c160008802.tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local dg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,0,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,#dg,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,Duel.GetLocationCount(tp,LOCATION_MZONE),0,0)
+end
+function c160008802.spfilter(c,e,tp)
+	return c:IsAnti() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c160008802.op(e,tp,eg,ep,ev,re,r,rp)
+	local dg=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,0,nil)
+	local spg=Duel.GetMatchingGroup(c160008802.spfilter,tp,LOCATION_DECK,0,nil,e,tp)
+	if Duel.Destroy(dg,REASON_EFFECT)>0 or #dg==0 then
+		Duel.BreakEffect()
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		if ft<=0 then return end
+		if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sp=spg:Select(tp,ft,ft,nil)
+		if #sp>0 then
+			ft=ft-#sp
+			Duel.SpecialSummon(sp,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+end
+
 
