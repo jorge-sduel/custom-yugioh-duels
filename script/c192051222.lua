49d1e3f385735779f4aea7e371c8990c2ab3356e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-17 09:59:05 -0600

    Update c192051222.lua

--- script/c192051222.lua
+++ script/c192051222.lua
@@ -1,22 +1,24 @@
 --coded by Lyris
 --Steelus Toxicatem Ultima
 function c192051222.initial_effect(c)
+c192051221.Is_Evolute=true
+if not EVOLUTE_IMPORTED then Duel.LoadScript("proc_evolute.lua") end
+	--c:EnableCounterPermit(0x88)
 	c:EnableReviveLimit()
-	--2: Level/Rank 3 EARTH, Level/Rank 6 Dragon
-	aux.AddEvoluteProc(c,9,c192051222.mfilter1,c192051222.mfilter2)
+	Evolute.AddProcedure(c,c192051222.mfilter1,2,99,c192051222.rcheck)
 	--If this card is Evolute Summoned using "Steelus Colarium", put 3 more E-Counters on it.
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_MATERIAL_CHECK)
-	e1:SetValue(c192051222.matcheck)
-	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetCondition(c192051222.tgcon)
-	e2:SetOperation(c192051222.tgop)
-	e2:SetLabelObject(e1)
-	c:RegisterEffect(e2)
+	--local e1=Effect.CreateEffect(c)
+	--e1:SetType(EFFECT_TYPE_SINGLE)
+	--e1:SetCode(EFFECT_MATERIAL_CHECK)
+	--e1:SetValue(c192051222.matcheck)
+	--c:RegisterEffect(e1)
+	--local e2=Effect.CreateEffect(c)
+	--e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	--e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	--e2:SetCondition(c192051222.tgcon)
+	--e2:SetOperation(c192051222.tgop)
+	--e2:SetLabelObject(e1)
+	--c:RegisterEffect(e2)
 	--Once per turn: You can remove 3 E-Counters from this card; Place 1 Toxic Counter on all monsters your opponent controls.
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_IGNITION)
@@ -33,8 +35,12 @@ function c192051222.initial_effect(c)
 	e4:SetOperation(c192051222.desop)
 	c:RegisterEffect(e4)
 end
+function c192051222.rcheck(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsAttribute,1,nil,ATTRIBUTE_EARTH)
+		and g:IsExists(Card.IsRace,1,nil,RACE_DRAGON)
+end
 function c192051222.mfilter1(c)
-	return c:IsAttribute(ATTRIBUTE_EARTH) and aux.EvoluteValue(c)==3
+	return  c:IsLevel(3) or c:IsRank(3) or c:IsLevel(6) or c:IsRank(6)
 end
 function c192051222.mfilter2(c)
 	return c:IsRace(RACE_DRAGON) and aux.EvoluteValue(c)==6
@@ -42,17 +48,17 @@ end
 function c192051222.matcheck(e,c)
 	e:SetLabel(0)
 	local g=c:GetMaterial()
-	if g:IsExists(Card.IsCode,1,nil,192051209) then e:SetLabel(3) end
+	if g:IsExists(Card.IsRace,1,nil,RACE_DRAGON) then e:SetLabel(3) end
 end
 function c192051222.tgcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL+388
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_EVOLUTE
 end
 function c192051222.tgop(e,tp,eg,ep,ev,re,r,rp)
 	e:GetHandler():RegisterFlagEffect(388,RESET_EVENT+0x1ff0000,0,1,e:GetLabelObject():GetLabel())
 end
 function c192051222.ctcost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return e:GetHandler():GetCounter(0x1088)>=3 end
-	e:GetHandler():RemoveCounter(tp,0x1088,3,REASON_COST)
+	if chk==0 then return e:GetHandler():GetCounter(0x111f)>=3 end
+	e:GetHandler():RemoveCounter(tp,0x111f,3,REASON_COST)
 end
 function c192051222.cttg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
