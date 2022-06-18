c80588252e8775be5a312e864b7c34350159c7c8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-18 16:44:45 -0600

    Update c160002222.lua

--- script/c160002222.lua
+++ script/c160002222.lua
@@ -1,9 +1,11 @@
 --MagneT.V
 local cid,id=GetID()
 function cid.initial_effect(c)
-		   aux.AddOrigEvoluteType(c)
+cid.Is_Evolute=true
+if not EVOLUTE_IMPORTED then Duel.LoadScript("proc_evolute.lua") end
+	--c:EnableCounterPermit(0x88)
 	c:EnableReviveLimit()
-  aux.AddEvoluteProc(c,nil,1,cid.filter2,1,1)  
+	Evolute.AddProcedure(c,nil,2,99)  
  --atkup
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
@@ -20,8 +22,8 @@ function cid.filter2(c,ec,tp)
 end
 function cid.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	if chk==0 then return c:IsCanRemoveEC(tp,1,REASON_COST) end
-	c:RemoveEC(tp,1,REASON_COST)
+	if chk==0 then return c:IsCanRemoveCounter(tp,0x111f,1,REASON_COST) end
+	c:RemoveCounter(tp,0x111f,1,REASON_COST)
 end
 function cid.atkop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
@@ -32,16 +34,6 @@ function cid.atkop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetValue(c:GetBaseAttack()*2)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_DISABLE)
 	c:RegisterEffect(e1)
-	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,1))
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e2:SetCode(EVENT_PHASE+PHASE_END)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCountLimit(1)
-	e2:SetTarget(cid.destg)
-	e2:SetOperation(cid.desop)
-	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	c:RegisterEffect(e2)
 end
 function cid.destg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
