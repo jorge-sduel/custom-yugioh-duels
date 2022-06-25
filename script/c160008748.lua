d9c6418f77225651e92268fbce843431b79ddb01
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-25 10:32:03 -0600

    Create c160008748.lua

--- script/c160008748.lua
+++ script/c160008748.lua
@@ -12,7 +12,7 @@ if not EVOLUTE_IMPORTED then Duel.LoadScript("proc_evolute.lua") end
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetCountLimit(1,id)
-	e1:SetCondition(cid.drcon)
+	e1:SetCondition(Evolute.sumcon)
 	--e1:SetCost(cid.drcost)
 	--e1:SetTarget(cid.drtg)
 	e1:SetOperation(cid.drop)
