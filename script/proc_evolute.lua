0852fb7f1bb62872e0a26829f61bdfa7678afa37
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-06-29 11:16:10 -0600

    Update proc_evolute.lua

--- script/proc_evolute.lua
+++ script/proc_evolute.lua
@@ -62,7 +62,7 @@ end
 function Evolute.IsLocation(c,loc)
 	if loc==nil then loc=LOCATION_MZONE end
 	if loc==LOCATION_HAND then loc=LOCATION_MZONE+LOCATION_HAND end
-	return c:IsLocation(loc) or (c:IsHasEffect(16000820) and c:IsLocation(LOCATION_HAND)) 
+	return c:IsLocation(loc) or c:IsHasEffect(16000820) 
 end
 function Evolute.ConditionFilter(c,f,lc,tp)
 	return (not f or f(c,lc,SUMMON_TYPE_SPECIAL,tp)) and not c:IsHasEffect(50031787,tp)
