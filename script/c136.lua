0002455d1bf8320b1d1c528260f33063d5a6e76b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-07-07 10:45:10 -0600

    Update c136.lua

--- script/c136.lua
+++ script/c136.lua
@@ -21,12 +21,12 @@ function c136.spfilter(c,e,tp)
 	return c:IsCode(137) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c136.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
 		and Duel.IsExistingMatchingCard(c136.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function c136.spop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=-1 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c136.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
