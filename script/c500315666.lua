9da48354ae85152f518f362bd8d14c4527cc45fa
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-07-10 13:16:07 -0600

    Add files via upload

---
+++ script/c500315666.lua
@@ -0,0 +1,72 @@
+--Dragon's Charge
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_BATTLE_DESTROYED)
+	e1:SetCondition(s.condition)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--add 2
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+ e1:SetRange(LOCATION_SZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target2)
+	e1:SetOperation(s.activate2)
+	c:RegisterEffect(e1)
+end
+function s.cfilter(c,tp)
+	local rc=c:GetReasonCard()
+	return c:IsReason(REASON_BATTLE) and rc.Is_Evolute and rc:IsControler(tp) and rc:IsRelateToBattle()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	if Duel.Draw(p,d,REASON_EFFECT)>0 then
+		Duel.BreakEffect()
+		local tc=Duel.GetOperatedGroup():GetFirst()
+		Duel.ConfirmCards(tp,tc)
+		if tc:IsLevelBelow(4) and tc:IsSummonable(true,nil) then
+			if Duel.SelectYesNo(tp,aux.Stringid(id,REASON_EFFECT)) then
+				Duel.Summon(tp,tc,true,nil)
+			else
+				Duel.ShuffleHand(tp)
+			end
+		else
+			Duel.ShuffleHand(tp)
+		end
+	end
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.thfilter(c)
+	return c:IsLevelBelow(2) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,2,2,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,2,2,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
