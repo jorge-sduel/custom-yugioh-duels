bd0f74da014b860c427039554d659d072609db67
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-07-10 13:20:55 -0600

    Update c500315666.lua

--- script/c500315666.lua
+++ script/c500315666.lua
@@ -2,6 +2,11 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e0)
+	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_DRAW)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
