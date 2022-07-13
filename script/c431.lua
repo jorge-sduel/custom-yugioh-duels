ef2f55f6e66ee5175c5572cb65c90ee6a85a406f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-07-13 17:33:25 -0600

    Update c431.lua

--- script/c431.lua
+++ script/c431.lua
@@ -4,8 +4,34 @@ function s.initial_effect(c)
 	--xyz summon
 	c:EnableReviveLimit()
 	Xyz.AddProcedure(c,s.xyzfilter,nil,2,nil,nil,nil,nil,false)
-
+	--opponent's turn xyz
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(302,1))
+	e1:SetHintTiming(0,TIMING_END_PHASE+TIMING_MAIN_END)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.syntg)
+	e1:SetOperation(s.synop)
+	c:RegisterEffect(e1)
 end
 function s.xyzfilter(c,xyz,sumtype,tp)
 	return (c:IsType(TYPE_LINK,xyz,sumtype,tp) and c:IsAttribute(ATTRIBUTE_LIGHT,xyz,sumtype,tp)) or (c:IsType(TYPE_XYZ,xyz,sumtype,tp) and c:IsAttribute(ATTRIBUTE_DARK,xyz,sumtype,tp))
+end
+function s.syntg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsXyzSummonable(nil) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_EXTRA)
+end
+function s.synop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetTargetRange(LOCATION_GRAVE,0)
+	e3:SetCode(id)
+	c:RegisterEffect(e3)
+		Duel.XyzSummon(tp,c,nil)
+	end
+	e3:Reset()
 end
