6e5f914641ad48710dcb42a09534850cc0c15b42
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-07-16 07:35:15 -0600

    Update proc_evolute.lua

--- script/proc_evolute.lua
+++ script/proc_evolute.lua
@@ -412,7 +412,7 @@ function Auxiliary.AddEcProcedure(c,cd)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e2:SetCondition(function(e) return e:GetHandler():GetSummonType()==cd
+	e2:SetCondition(function(e) return e:GetHandler():GetSummonType()==CD end)
 	--e2:SetTarget(Evolute.addct)
 	e2:SetOperation(Auxiliary.addEc)
 	c:RegisterEffect(e2)
