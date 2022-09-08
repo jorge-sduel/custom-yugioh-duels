e613364178bef6bac1bc89ec257b5cbc3b3eea29
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-09-08 07:42:49 -0600

    Update proc_evolute.lua

--- script/proc_evolute.lua
+++ script/proc_evolute.lua
@@ -368,7 +368,7 @@ function Auxiliary.ConvergentEvolSummonCondition(cd,loc,excon)
 				if c==nil then return true end
 				return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 --GetLocationCount
-					and (Duel.IsExistingMatchingCard(Auxiliary.ConvergentEvolSummonFilter,c:GetControler(),loc,0,1,nil,cd)
+					and (Duel.IsExistingMatchingCard(Auxiliary.ConvergentEvolSummonFilter,c:GetControler(),loc,0,2,nil,cd)
 					or Duel.IsExistingMatchingCard(Auxiliary.ConvergentEvolSummonSubstitute,c:GetControler(),LOCATION_ONFIELD+LOCATION_GRAVE,0,1,nil,cd,c:GetControler()))
 			end
 end
