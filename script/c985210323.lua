1a5710e96ea568133d562af9af159c753212193d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-09-20 07:36:19 -0600

    Add files via upload

---
+++ script/c985210323.lua
@@ -0,0 +1,72 @@
+--Advanced Rainbow Dragon
+function c985210323.initial_effect(c)
+	--Rune Summon
+	c:EnableReviveLimit()
+	aux.AddRuneProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x1034),1,1,aux.FilterBoolFunction(Card.IsSetCard,0x34),4,99)
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.runlimit)
+	c:RegisterEffect(e1)
+	--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(c985210323.gainval)
+	c:RegisterEffect(e2)
+	--
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e3:SetOperation(c985210323.regop)
+	c:RegisterEffect(e3)
+	--actlimit
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e4:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e4:SetOperation(c985210323.atkop)
+	c:RegisterEffect(e4)
+	--Multiple Attack
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+	e5:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e5:SetValue(c985210323.atkval)
+	c:RegisterEffect(e5)
+end
+function c985210323.regop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:GetSummonType()==SUMMON_TYPE_RUNE then
+		local ct=c:GetMaterialCount()
+		c:RegisterFlagEffect(985210323,RESET_EVENT+0x1fe0000,0,0,ct*200)
+	end
+end
+function c985210323.gainval(e,c)
+	local ct=e:GetHandler():GetFlagEffectLabel(985210323)
+	if not ct then return 0 end
+	return ct
+end
+function c985210323.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local e5=Effect.CreateEffect(e:GetHandler())
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e5:SetTargetRange(0,1)
+	e5:SetValue(c985210323.aclimit)
+	e5:SetReset(RESET_PHASE+PHASE_DAMAGE)
+	Duel.RegisterEffect(e5,tp)
+end
+function c985210323.aclimit(e,re,tp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function c985210323.atkfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSetCard(0x34)
+end
+function c985210323.atkval(e,c)
+	return Duel.GetMatchingGroupCount(c985210323.atkfilter,c:GetControler(),LOCATION_ONFIELD,0,nil)-1
+end
