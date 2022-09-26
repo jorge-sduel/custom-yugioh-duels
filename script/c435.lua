cd6be76c7444f0cc36242cfee161853687e1110f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-09-25 21:35:41 -0600

    Add files via upload

---
+++ script/c435.lua
@@ -0,0 +1,28 @@
+--宝玉獣 コバルト・イーグル
+local s,id=GetID()
+function s.initial_effect(c)
+	--send replace
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_TO_GRAVE_REDIRECT_CB)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCondition(s.repcon)
+	e1:SetOperation(s.repop)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x1034}
+function s.repcon(e)
+	local c=e:GetHandler()
+	return c:IsFaceup() and c:IsLocation(LOCATION_MZONE) and c:IsReason(REASON_DESTROY)
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetCode(EFFECT_CHANGE_TYPE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TURN_SET)
+	e1:SetValue(TYPE_SPELL+TYPE_CONTINUOUS)
+	c:RegisterEffect(e1)
+	Duel.RaiseEvent(c,EVENT_CUSTOM+47408488,e,0,tp,0,0)
+end
