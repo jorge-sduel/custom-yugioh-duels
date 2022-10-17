f339e4c06993c891420f40b5ad16b73bd81ba4b3
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-17 15:19:59 -0600

    Update c950231423.lua

--- script/c950231423.lua
+++ script/c950231423.lua
@@ -1,18 +1,21 @@
 --Abyss Actor - Splendid Heroine
+c950231423.Is_Runic=true
+if not RUNIC_IMPORTED then Duel.LoadScript("proc_runic.lua") end
 function c950231423.initial_effect(c)
 	--Rune Summon
 	c:EnableReviveLimit()
-	aux.AddRuneProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0x10ec),1,1,c950231423.STMatFilter,1,1)
-	--Destroy
+	Runic.AddProcedure(c,aux.FilterBoolFunction(Card.IsCode,24907044),c950231423.STMatFilter,1,1)
+--atk down
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(950231423,0))
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetCountLimit(1)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetTarget(c950231423.destg)
-	e1:SetOperation(c950231423.desop)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BATTLE_DAMAGE)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1,0,EFFECT_COUNT_CODE_SINGLE)
+	e1:SetCondition(c950231423.atkcon1)
+	e1:SetTarget(c950231423.atktg1)
+	e1:SetOperation(c950231423.atkop1)
 	c:RegisterEffect(e1)
 	--actlimit
 	local e3=Effect.CreateEffect(c)
@@ -22,24 +25,45 @@ function c950231423.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function c950231423.STMatFilter(c)
-	return c:IsType(TYPE_SPELL) and (c:IsSetCard(0x20ec) or c:IsSetCard(0x10ec))
+	return c:IsType(TYPE_SPELL) or c:IsType(TYPE_TRAP)
 end
-function c950231423.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return false end
-	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,0,1,nil)
-		and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g1=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,0,1,1,nil)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g2=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
-	g1:Merge(g2)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,2,0,0)
+function c950231423.atkcon1(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	return ep==tp and a:IsControler(1-tp) and a:IsFaceup() and a:IsRelateToBattle()
 end
-function c950231423.desop(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
-	if tg:GetCount()>0 then
-		Duel.Destroy(tg,REASON_EFFECT)
+function c950231423.atktg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsFaceup,tp,0,LOCATION_MZONE,1,nil) end
+end
+function c950231423.atkop1(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	if #g>0 then
+		local tc=g:GetFirst()
+		for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(-ev)
+		tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_FIELD)
+			e2:SetCode(EFFECT_UPDATE_ATTACK)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			e2:SetValue(ev)
+			tc:RegisterEffect(e2)
+			local e3=e2:Clone()
+			e3:SetCode(EFFECT_UPDATE_DEFENSE)
+			tc:RegisterEffect(e3)
+			local e4=Effect.CreateEffect(e:GetHandler())
+			e4:SetType(EFFECT_TYPE_SINGLE)
+			e4:SetCode(EFFECT_UPDATE_ATTACK)
+			e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			e4:SetValue(-ev)
+			tc:RegisterEffect(e4)
+			local e5=e4:Clone()
+			e5:SetCode(EFFECT_UPDATE_DEFENSE)
+			tc:RegisterEffect(e5)
+		end
 	end
 end
 function c950231423.atkop(e,tp,eg,ep,ev,re,r,rp)
