dd8cd3ffe0692877ae3f296528a1ab0901d7538c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-18 16:37:07 -0600

    Add files via upload

---
+++ script/c438.lua
@@ -0,0 +1,11 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),s.matfilter)
+end
+s.listed_names={CARD_GAIA_CHAMPION}
+function s.matfilter(c,fc,sumtype,tp)
+	return c:IsRace(RACE_BEAST,fc,sumtype,tp) and c:IsLevelAbove(6)
+end
