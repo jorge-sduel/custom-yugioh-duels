2e152547a6c37acfff051f48eb38fdec3f15c048
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-18 15:40:42 -0600

    Update c53790734.lua

--- script/c53790734.lua
+++ script/c53790734.lua
@@ -31,7 +31,7 @@ function c53790734.initial_effect(c)
 end
 function c53790734.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsControler(1-tp) and chkc:IsOnField() and aux.disfilter1(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(aux.disfilter1,tp,0,LOCATION_ONFIELD,1,nil) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsNegatableMonster,tp,0,LOCATION_ONFIELD,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	local g=Duel.SelectTarget(tp,aux.disfilter1,tp,0,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,1,0,0)
