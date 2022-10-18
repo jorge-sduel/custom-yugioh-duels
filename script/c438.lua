8c56bac8b84543c787a86a169a4502a9b50c8a7f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-18 16:56:09 -0600

    Update c438.lua

--- script/c438.lua
+++ script/c438.lua
@@ -1,11 +1,38 @@
---
+--gaia fire Wing rider
 local s,id=GetID()
 function s.initial_effect(c)
 	--Fusion Material
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_WARRIOR),s.matfilter)
+	--recover
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_RECOVER)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EVENT_BATTLE_DAMAGE)
+	e3:SetCondition(s.reccon)
+	e3:SetTarget(s.rectg)
+	e3:SetOperation(s.recop)
+	c:RegisterEffect(e3)
 end
 s.listed_names={CARD_GAIA_CHAMPION}
 function s.matfilter(c,fc,sumtype,tp)
 	return c:IsRace(RACE_BEAST,fc,sumtype,tp) and c:IsLevelAbove(6)
 end
+function s.reccon(e,tp,eg,ep,ev,re,r,rp)
+	return ep~=tp and eg:GetFirst():IsControler(tp)
+end
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(ev)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,ev)
+end
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end
+
