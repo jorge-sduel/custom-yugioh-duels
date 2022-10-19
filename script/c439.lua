bfae41bc30fc824632a371f6b582f3faa37cb19f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-19 10:38:16 -0600

    Update c439.lua

--- script/c439.lua
+++ script/c439.lua
@@ -27,7 +27,7 @@ function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return c:IsAttackAbove(0)
 end
 function s.filter(c)
-	return c:IsAttackBelow(2300) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
+	return c:IsAttackBelow(2300) and c:IsSpecialSummonable()
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
