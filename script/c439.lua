f10ead3006d615e9d3eecbf1257f78123c51623c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-19 09:46:08 -0600

    Update c439.lua

--- script/c439.lua
+++ script/c439.lua
@@ -1,16 +1,13 @@
---ブルル＠イグニスター (Anime)
---Bururu @Ignister (Anime)
---Scripted by Larry126
+--@Ignister (Anime)
 local s,id=GetID()
 function s.initial_effect(c)
 	--Special Summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,1))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_BE_MATERIAL)
+	e1:SetType(EFFECT_TYPE_IGNITION)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
-	--e1:SetCondition(s.condition)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.operation)
 	c:RegisterEffect(e1)
@@ -26,7 +23,7 @@ function s.initial_effect(c)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return c:IsLocation(LOCATION_GRAVE)
+	return c:IsAttackAbove(0)
 end
 function s.filter(c,e,tp)
 	return c:IsLocation(LOCATION_GRAVE) and c:IsControler(tp) and c:IsCanBeEffectTarget(e)
