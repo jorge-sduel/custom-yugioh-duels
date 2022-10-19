17c7eb29a40330f6d60291285b26d03f25da0fe6
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-19 09:56:41 -0600

    Update c439.lua

--- script/c439.lua
+++ script/c439.lua
@@ -34,7 +34,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
 	if chkc then return s.filter(chkc,e,tp) and chkc~=c end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExists(s.filter,1,c,e,tp) end
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=mg:FilterSelect(tp,s.filter,1,1,c,e,tp)
 	Duel.SetTargetCard(g)
