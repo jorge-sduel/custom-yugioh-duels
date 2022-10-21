fcca1a1bde3e1235b6ae0cc71ee4549052416c1e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-21 08:56:45 -0600

    Update c950231423.lua

--- script/c950231423.lua
+++ script/c950231423.lua
@@ -97,12 +97,16 @@ function c950231423.atkop2(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	local th=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	if #th>0 then
+		local tc1=th:GetFirst()
+		for tc1 in aux.Next(th) do
 		local e2=Effect.CreateEffect(e:GetHandler())
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_UPDATE_ATTACK)
 		e2:SetValue(ev)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		th:RegisterEffect(e2)
+		tc1:RegisterEffect(e2)
+end
 		end
 	end
 end
