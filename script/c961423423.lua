154eb8f4d03dbca347dd88258b4c1c95c8fa7356
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-23 14:28:03 -0600

    Add files via upload

---
+++ script/c961423423.lua
@@ -0,0 +1,83 @@
+--Blue Fairy in White
+function c961423423.initial_effect(c)
+	--Rune Summon
+	c:EnableReviveLimit()
+	aux.AddRuneProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_FAIRY),1,1,aux.FilterBoolFunction(Card.IsType,TYPE_SPELL),2,2)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(961423423,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(c961423423.setcon)
+	e1:SetTarget(c961423423.settg)
+	e1:SetOperation(c961423423.setop)
+	c:RegisterEffect(e1)
+	--Special Summon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(961423423,2))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetCountLimit(1,961423423+EFFECT_COUNT_CODE_OATH)
+	e3:SetRange(LOCATION_REMOVED)
+	e3:SetTarget(c961423423.sptg)
+	e3:SetOperation(c961423423.spop)
+	c:RegisterEffect(e3)
+	--remove
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(961423423,1))
+	e4:SetCategory(CATEGORY_REMOVE)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e4:SetCountLimit(1,961423423+EFFECT_COUNT_CODE_OATH)
+	e4:SetCost(c961423423.rmcost)
+	e4:SetTarget(c961423423.rmtg)
+	e4:SetOperation(c961423423.rmop)
+	c:RegisterEffect(e4)
+end
+function c961423423.setcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_RUNE
+end
+function c961423423.mgfilter(c)
+	return c:IsType(TYPE_SPELL) and c:IsSSetable()
+end
+function c961423423.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local mg=e:GetHandler():GetMaterial()
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0 and mg:FilterCount(c961423423.mgfilter,nil,e,tp,tc)>0 end
+end
+function c961423423.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local mg=c:GetMaterial()
+	if mg:FilterCount(c961423423.mgfilter,nil)>0 then
+		local tc=mg:FilterSelect(c:GetControler(),c961423423.mgfilter,1,1,nil)
+		Duel.SSet(tp,tc)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+function c961423423.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function c961423423.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+end
+function c961423423.rmcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemoveAsCost() end
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_COST)
+end
+function c961423423.rmtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(1-tp) and chkc:IsAbleToRemove() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectTarget(tp,Card.IsAbleToRemove,tp,0,LOCATION_GRAVE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,1,0,0)
+end
+function c961423423.rmop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)
+	end
+end
