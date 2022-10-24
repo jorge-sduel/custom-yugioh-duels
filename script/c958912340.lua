56d36f40dbbec049872d8f06e6972cd0eade4daf
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-23 22:01:26 -0600

    Update c958912340.lua

--- script/c958912340.lua
+++ script/c958912340.lua
@@ -5,6 +5,19 @@ function c958912340.initial_effect(c)
 	--Rune Summon
 	c:EnableReviveLimit()
 	Runic.AddProcedure(c,nil,aux.FilterBoolFunction(Card.IsCode,97077563),1,1)
+	--spsummon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(958912340,2))
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	--e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetCountLimit(1)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	--e4:SetCost(.cost)
+	e4:SetTarget(c958912340.sptg)
+	e4:SetOperation(c958912340.spop)
+	c:RegisterEffect(e4)
 	--set
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(958912340,1))
@@ -29,5 +42,21 @@ function c958912340.setop(e,tp,eg,ep,ev,re,r,rp)
 	if g:GetCount()>0 then
 		Duel.SSet(tp,g:GetFirst())
 		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function c958912340.filter(c,e,tp)
+	return c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function c958912340.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(c958912340.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function c958912340.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(c958912340.filter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
