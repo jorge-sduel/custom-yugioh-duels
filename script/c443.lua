268f3145097c2c9ca72b178fb9763893dd20a1db
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-31 09:08:25 -0600

    Update c443.lua

--- script/c443.lua
+++ script/c443.lua
@@ -88,7 +88,7 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.DiscardDeck(tp,1,REASON_EFFECT)~=0 then
 		local oc=Duel.GetOperatedGroup():GetFirst()
 		local c=e:GetHandler()
-		if oc:IsLocation(LOCATION_GRAVE) and c:IsRelateToEffect(e) then
+		if c:IsRelateToEffect(e) then
 			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
 		end
 	end
