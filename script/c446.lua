da054380dc598e1e995dd2923b3cbcdb4a2919bb
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-10-31 15:04:40 -0600

    Add files via upload

---
+++ script/c446.lua
@@ -0,0 +1,59 @@
+--Ceremonial Spiritual Art - Gishiki
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=s.ritualfil,location=LOCATION_HAND+LOCATION_GRAVE})
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+	--Inflict damage and Special Summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DAMAGE+CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0xbf}
+function s.ritualfil(c)
+	return (c:IsAttribute(ATTRIBUTE_WATER) c:IsLocation(LOCATION_HAND)) or c:IsCode(442)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK)
+end
+function s.filter(c,e,tp)
+	return c:IsDefenseBelow(1500) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.Damage(1-tp,0,REASON_EFFECT)==0 then return end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	if ft>2 then ft=2 end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil,e,tp)
+	if #g==0 then return end
+	if Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:Select(tp,1,ft,nil)
+		for tc in aux.Next(sg) do
+			if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+			end
+		end
+		Duel.SpecialSummonComplete()
+	end
+end
+function s.refilter(c)
+	return c:IsType(TYPE_RITUAL) or c:IsSetCard(0x10c0)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	e:SetLabel(1)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if chk==0 then return ft>-1 and Duel.CheckReleaseGroupCost(tp,s.refilter,1,false,aux.ReleaseCheckMMZ,nil) end
+	local g=Duel.SelectReleaseGroupCost(tp,s.refilter,1,1,false,aux.ReleaseCheckMMZ,nil)
+	Duel.Release(g,REASON_COST)
+	Duel.Remove(c,POS_FACEUP,REASON_COST)
+end
