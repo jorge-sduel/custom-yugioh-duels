c8f62c0a70473e1b3af983d132776f3a9be1e62e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-01 14:09:43 -0600

    Update c12340205.lua

--- script/c12340205.lua
+++ script/c12340205.lua
@@ -78,7 +78,7 @@ function c12340205.sptg2(e,tp,eg,ep,ev,re,r,rp,chk)
         if ft<=1 then return false end
         local g=Duel.GetMatchingGroup(c12340205.spfilter2,tp,LOCATION_HAND+LOCATION_DECK,0,nil,e,tp)
         local lv=e:GetHandler():GetLevel()
-        return g:CheckWithSumEqual(Card.GetLevel,lv,2,ct)
+        return g:CheckWithSumEqual(Card.GetLevel,lv,2,ft)
     end
 end
 function c12340205.spop2(e,tp,eg,ep,ev,re,r,rp)
