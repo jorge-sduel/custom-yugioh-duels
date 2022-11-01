25e5937f1cfdc926a30661d8913856bd90c00665
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-01 09:27:55 -0600

    Add files via upload

---
+++ script/c447.lua
@@ -0,0 +1,87 @@
+--TG サイバー・マジシャン
+local s,id=GetID()
+function s.initial_effect(c)
+	--If sent to GY as synchro material, draw 1
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DRAW)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EVENT_BE_MATERIAL)
+	e1:SetCondition(s.drcon)
+	e1:SetTarget(s.drtg)
+	e1:SetOperation(s.drop)
+	c:RegisterEffect(e1)
+	--Add counter
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_COUNTER)
+	e3:SetType(EFFECT_TYPE_TRIGGER_F+EFFECT_TYPE_FIELD)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetOperation(s.ctop)
+	c:RegisterEffect(e3)
+	--hand synchro
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetCode(EFFECT_HAND_SYNCHRO)
+	e3:SetLabel(id)
+	e3:SetValue(s.synval)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x27}
+function s.synval(e,c,sc)
+	if sc:IsRace(RACE_WARRIOR) or sc:IsRace(RACE_MACHINE) and --c:IsNotTuner() 
+		(not c:IsType(TYPE_TUNER) or c:IsHasEffect(EFFECT_NONTUNER)) and c:IsRace(RACE_WARRIOR) or c:IsRace(RACE_MACHINE) and c:IsLocation(LOCATION_HAND) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK)
+		e1:SetLabel(id)
+		e1:SetTarget(s.synchktg)
+		c:RegisterEffect(e1)
+		return true
+	else return false end
+end
+function s.chk2(c)
+	if not c:IsHasEffect(EFFECT_HAND_SYNCHRO) or c:IsHasEffect(EFFECT_HAND_SYNCHRO+EFFECT_SYNCHRO_CHECK) then return false end
+	local te={c:GetCardEffect(EFFECT_HAND_SYNCHRO)}
+	for i=1,#te do
+		local e=te[i]
+		if e:GetLabel()==id then return true end
+	end
+	return false
+end
+function s.synchktg(e,c,sg,tg,ntg,tsg,ntsg)
+	if c then
+		local res=tg:IsExists(s.chk2,1,c) or ntg:IsExists(s.chk2,1,c) or sg:IsExists(s.chk2,1,c)
+		return res,Group.CreateGroup(),Group.CreateGroup()
+	else
+		return true
+	end
+end
+function s.ctop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetFieldGroup(tp,LOCATION_FZONE,LOCATION_FZONE)
+	local g2=Duel.SelectMatchingCard(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
+	if c:IsRelateToEffect(e) then g:AddCard(g2) end
+	for tc in aux.Next(g) do
+		if tc:IsCanAddCounter(0x1148,1) then
+			tc:AddCounter(0x1148,1)
+		end
+	end
+end
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsLocation(LOCATION_GRAVE) and r==REASON_SYNCHRO
+end
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(1)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+end
