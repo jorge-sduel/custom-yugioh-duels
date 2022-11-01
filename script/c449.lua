6110a19d211003204a29e863eb3e782034e9a105
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-01 16:26:08 -0600

    Add files via upload

---
+++ script/c449.lua
@@ -0,0 +1,64 @@
+--ランサー・デーモン
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Material
+	c:EnableReviveLimit()
+
+Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_FIEND),s.matfilter)
+	--pierce
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_FIELD)
+	e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.piercecon)
+	e1:SetTarget(s.piercetg)
+	e1:SetOperation(s.pierceop)
+	c:RegisterEffect(e1)
+	--Change name
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_CHANGE_CODE)
+	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetValue(CARD_SUMMONED_SKULL)
+	c:RegisterEffect(e2)
+end
+function s.matfilter(c,fc,sumtype,tp)
+	return (c:IsRace(RACE_WARRIOR,fc,sumtype,tp) or c:IsRace(RACE_FIEND,fc,sumtype,tp)) and c:IsLevelAbove(6)
+end
+function s.piercecon(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return d and a:IsControler(tp)
+end
+function s.piercetg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.GetAttacker():CreateEffectRelation(e)
+end
+function s.pierceop(e,tp,eg,ep,ev,re,r,rp)
+	local a=Duel.GetAttacker()
+	if a:IsRelateToEffect(e) and a:IsFaceup() then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_PIERCE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		a:RegisterEffect(e1)
+	--double pierce
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PRE_BATTLE_DAMAGE)
+	e2:SetCondition(s.damcon)
+	e2:SetOperation(s.damop)
+e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	a:RegisterEffect(e2)
+	end
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return ep~=tp and c==Duel.GetAttacker() and Duel.GetAttackTarget()
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.ChangeBattleDamage(ep,ev*2)
+end
