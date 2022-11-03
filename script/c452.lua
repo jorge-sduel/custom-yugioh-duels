c9e38e24e89887fdd48035271b96f3cd80deea12
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-03 17:29:47 -0600

    Add files via upload

---
+++ script/c452.lua
@@ -0,0 +1,90 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--Atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,0x52))
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+	--cannot be battle target
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e3:SetRange(LOCATION_FZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetCondition(s.con)
+	e3:SetValue(s.atlimit)
+	c:RegisterEffect(e3)
+	--cannot be target
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e4:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTargetRange(LOCATION_MZONE,0)
+	e4:SetCondition(s.con)
+	e4:SetTarget(s.tfilter)
+	e4:SetValue(aux.tgoval)
+	c:RegisterEffect(e4)
+--Activate
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_TODECK+CATEGORY_DRAW)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e5:SetRange(LOCATION_FZONE)
+	e5:SetTarget(s.target2)
+	e5:SetOperation(s.activate2)
+	c:RegisterEffect(e5)
+end
+s.listed_series={0x52}
+function s.thfilter(c)
+	return c:IsSetCard(0x52) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+function s.con(e)
+	return Duel.IsExistingMatchingCard(Card.IsPosition,e:GetHandlerPlayer(),LOCATION_MZONE,0,2,nil,POS_FACEUP)
+end
+function s.tfilter(c,atk)
+	return c:IsFaceup() and c:IsSetCard(0x52) and c:GetAttack()<atk
+end
+function s.atlimit(e,c)
+	return c:IsFaceup() and not Duel.IsExistingMatchingCard(s.tfilter,c:GetControler(),LOCATION_MZONE,0,1,c,c:GetAttack())
+end
+
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp)
+		and Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_HAND)
+end
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Hint(HINT_SELECTMSG,p,HINTMSG_TODECK)
+	local g=Duel.SelectMatchingCard(p,Card.IsMonster,p,LOCATION_GRAVE,0,1,2,nil)
+	if #g==0 then return end
+	Duel.SendtoDeck(g,nil,1,REASON_EFFECT)
+	Duel.ShuffleDeck(p)
+	Duel.BreakEffect()
+	Duel.Draw(p,#g,REASON_EFFECT)
+end
