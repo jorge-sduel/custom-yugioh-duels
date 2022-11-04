81fce5aa2048b4a99c15f240aca5bb7715d708e4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-04 13:40:31 -0600

    Update c943627890.lua

--- script/c943627890.lua
+++ script/c943627890.lua
@@ -14,16 +14,6 @@ function c943627890.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
-	--destroy
-	local e5=Effect.CreateEffect(c)
-	e5:SetCategory(CATEGORY_DESTROY)
-	e5:SetDescription(aux.Stringid(952312343,0))
-	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e5:SetCode(EVENT_BE_MATERIAL)
-	e5:SetCondition(c943627890.descon)
-	e5:SetTarget(c943627890.destg)
-	e5:SetOperation(c943627890.desop)
-	c:RegisterEffect(e5)
 end
 function c943627890.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_REMOVED) and chkc:IsControler(tp) and chkc:IsAbleToHand() end
@@ -60,25 +50,4 @@ function c943627890.selfdescon(e,tp,eg,ep,ev,re,r,rp)
 end
 function c943627890.selfdesop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Destroy(e:GetLabelObject(),REASON_EFFECT)
-end
-function c943627890.descon(e,tp,eg,ep,ev,re,r,rp)
-	local rc=e:GetHandler():GetReasonCard()
-	return  rc:IsSetCard(0xfe9) and e:GetHandler():IsPreviousLocation(LOCATION_MZONE) and rc:IsPreviousLocation(LOCATION_HAND) and e:GetHandler():GetTurnID()==Duel.GetTurnCount() and rc.Is_Runic
-end
-function c943627890.desfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP)
-end
-function c943627890.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsOnField() and c943627890.desfilter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c943627890.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,c943627890.desfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
-end
-function c943627890.desop(e,tp,eg,ep,ev,re,r,rp)
-	if not e:GetHandler():IsRelateToEffect(e) then return end
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.Destroy(tc,REASON_EFFECT)
-	end
 end
