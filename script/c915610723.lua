b01633d41d17010f3a02038626c657c5012f0393
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-05 16:11:32 -0600

    Add files via upload

---
+++ script/c915610723.lua
@@ -0,0 +1,70 @@
+--Immortal Serpent of Everunic Forests
+function c915610723.initial_effect(c)
+	--Rune Summon
+	c:EnableReviveLimit()
+	aux.AddRuneProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_EARTH),2,2,c915610723.STMatFilter,1,1)
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.runlimit)
+	c:RegisterEffect(e1)
+	--cannot be target
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_ONFIELD,LOCATION_ONFIELD)
+	e2:SetTarget(c915610723.tglimit)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+	--atk
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetValue(c915610723.atkval)
+	c:RegisterEffect(e3)
+	--destroy replace
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_DESTROY_REPLACE)
+	e4:SetTarget(c915610723.reptg)
+	e4:SetOperation(c915610723.repop)
+	c:RegisterEffect(e4)
+end
+function c915610723.STMatFilter(c)
+	return bit.band(c:GetType(),0x20004)==0x20004
+end
+function c915610723.tglimit(e,c)
+	return c==e:GetHandler() or c:IsType(TYPE_TRAP)
+end
+function c915610723.atkfilter(c)
+	return c:IsFaceup() and c:IsType(TYPE_SPELL+TYPE_TRAP)
+end
+function c915610723.atkval(e,c)
+	return Duel.GetMatchingGroupCount(c915610723.atkfilter,c:GetControler(),LOCATION_ONFIELD,LOCATION_ONFIELD,nil)*200
+end
+function c915610723.rmfilter(c)
+	return c:IsCode(962438790) and c:IsAbleToRemove()
+end
+function c915610723.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsOnField() and c:IsFaceup()
+		and Duel.IsExistingMatchingCard(c915610723.rmfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if Duel.SelectYesNo(tp,aux.Stringid(59843383,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectMatchingCard(tp,c915610723.rmfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		Duel.SetTargetCard(g)
+		return true
+	end
+	return false
+end
+function c915610723.repop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	g:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,false)
+	Duel.Remove(g,POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
+end
