b74937ccc3e43b2b27a1cdca400f27bd1c8f49cd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-05 10:12:37 -0600

    Add files via upload

---
+++ script/c906821092.lua
@@ -0,0 +1,63 @@
+--Immortal Majesty of Everunic Waters
+function c906821092.initial_effect(c)
+	--Rune Summon
+	c:EnableReviveLimit()
+	aux.AddRuneProcedure(c,aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_WATER),2,2,aux.FilterBoolFunction(Card.IsType,TYPE_FIELD),1,1)
+	--cannot special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.runlimit)
+	c:RegisterEffect(e1)
+	--activate limit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetCondition(c906821092.condition)
+	e2:SetValue(c906821092.aclimit)
+	c:RegisterEffect(e2)
+	--defense attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_DEFENSE_ATTACK)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--destroy replace
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_DESTROY_REPLACE)
+	e4:SetTarget(c906821092.reptg)
+	e4:SetOperation(c906821092.repop)
+	c:RegisterEffect(e4)
+end
+function c906821092.condition(e)
+	local ph=Duel.GetCurrentPhase()
+	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+end
+function c906821092.aclimit(e,re,tp)
+	return not re:GetHandler():IsImmuneToEffect(e)
+end
+function c906821092.rmfilter(c)
+	return c:IsCode(958625123) and c:IsAbleToRemove()
+end
+function c906821092.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsOnField() and c:IsFaceup()
+		and Duel.IsExistingMatchingCard(c906821092.rmfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	if Duel.SelectYesNo(tp,aux.Stringid(59843383,2)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectMatchingCard(tp,c906821092.rmfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		Duel.SetTargetCard(g)
+		return true
+	end
+	return false
+end
+function c906821092.repop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	g:GetFirst():SetStatus(STATUS_DESTROY_CONFIRMED,false)
+	Duel.Remove(g,POS_FACEUP,REASON_EFFECT+REASON_REPLACE)
+end
