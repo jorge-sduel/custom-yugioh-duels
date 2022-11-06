502a3f7c4df4f107dbc80f3396a0fdfcb3d5d780
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-05 20:36:18 -0600

    Update c963567212.lua

--- script/c963567212.lua
+++ script/c963567212.lua
@@ -44,11 +44,11 @@ function c963567212.pcop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_DISABLE)
 		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		ec:RegisterEffect(e1)
+		tc:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(e:GetHandler())
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_DISABLE_EFFECT)
 		e2:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		ec:RegisterEffect(e2)
+		tc:RegisterEffect(e2)
 	end
 end
