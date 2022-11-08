a69cd940a3f0dcadbd17767e3e5e40eab52f5b9e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-08 08:44:35 -0600

    Update proc_runic.lua

--- script/proc_runic.lua
+++ script/proc_runic.lua
@@ -374,8 +374,8 @@ function Auxiliary.RunicTarget(f1,min1,max1,f2,min,max)
 				sg:Merge(mustg)
 				while #sg<(max+1) do
 					local cg=Group.CreateGroup()
-                    if not sg:IsExists(Runic.FilterEx,max,nil,f1,c,tp,mg2,LOCATION_MZONE) then
-                        cg=mg1:Filter(Runic.FilterEx,nil,f1,c,tp,mg2,LOCATION_MZONE)
+                    if not sg:IsExists(Runic.Filter,max1,nil,f1,c,tp,mg2,LOCATION_MZONE) then
+                        cg=mg1:Filter(Runic.Filter,nil,f1,c,tp,mg2,LOCATION_MZONE)
                     elseif not sg:IsExists(Runic.FilterEx,max,nil,f2,c,tp,mg1,LOCATION_ONFIELD) then
                         cg=mg2:Filter(Runic.FilterEx,nil,f2,c,tp,mg1,LOCATION_ONFIELD)
                     end
