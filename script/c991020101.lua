a91c959ea9c78698b351a6a39c4eab2489710f22
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-08 21:30:17 -0600

    Update c991020101.lua

--- script/c991020101.lua
+++ script/c991020101.lua
@@ -15,13 +15,6 @@ function s.initial_effect(c)
 	r1:SetOperation(s.runop)
 	r1:SetValue(SUMMON_TYPE_RUNIC)
 	c:RegisterEffect(r1)
-	--cannot special summon
-	local e1=Effect.CreateEffect(c)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.runlimit)
-	c:RegisterEffect(e1)
 	--cannot be target
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
