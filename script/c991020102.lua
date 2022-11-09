9cfdc786b3c9b5d5b1ceaa9888ca75e6cbcb74d1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-09 10:04:01 -0600

    Update c991020102.lua

--- script/c991020102.lua
+++ script/c991020102.lua
@@ -6,6 +6,15 @@ function s.initial_effect(c)
 	--rune procedure
 	c:EnableReviveLimit()
 	aux.AddRunicState(c)
+	local r1=Effect.CreateEffect(c)
+	r1:SetType(EFFECT_TYPE_FIELD)
+	r1:SetCode(EFFECT_SPSUMMON_PROC)
+	r1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	r1:SetRange(LOCATION_HAND)
+	r1:SetCondition(s.runcon)
+	r1:SetOperation(s.runop)
+	r1:SetValue(SUMMON_TYPE_RUNIC)
+	c:RegisterEffect(r1)
 	--atkup
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
@@ -24,6 +33,31 @@ function s.initial_effect(c)
 	e3:SetTarget(s.destg)
 	e3:SetOperation(s.desop)
 	c:RegisterEffect(e3)
+end
+function s.matfilter1(c)
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and c:IsLocation(LOCATION_MZONE)
+end
+function s.matfilter2(c)
+	return c:IsType(TYPE_TRAP)
+end
+function s.runfilter1(c)
+	return s.matfilter2(c) and Duel.IsExistingMatchingCard(s.matfilter1,c:GetControler(),LOCATION_MZONE,0,1,c)
+end
+function s.runcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>-1 and Duel.IsExistingMatchingCard(s.runfilter1,c:GetControler(),LOCATION_ONFIELD,0,2,nil)
+end
+function s.runop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Group.CreateGroup()
+	local g2=Duel.GetMatchingGroup(s.matfilter1,tp,LOCATION_MZONE,0,nil,c)
+	local mt1=Duel.SelectMatchingCard(tp,s.runfilter1,c:GetControler(),LOCATION_ONFIELD,0,1,99,nil,c)
+	g:Merge(mt1)
+	g2:Sub(mt1)
+	local mt2=g2:Select(tp,1,1,nil)
+	g:Merge(mt2)
+	c:SetMaterial(g)
+	Duel.SendtoGrave(g,REASON_MATERIAL+REASON_RUNIC)
 end
 function s.atkval(e,c)
 	return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),LOCATION_GRAVE,0,nil,TYPE_TRAP)*1000
