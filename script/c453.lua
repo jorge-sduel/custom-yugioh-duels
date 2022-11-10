d10213c349bd7644a884c45529c6b1a4da07dab9
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-10 08:27:56 -0600

    Update c453.lua

--- script/c453.lua
+++ script/c453.lua
@@ -28,7 +28,7 @@ function s.initial_effect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e3:SetCode(EFFECT_CHANGE_CODE)
-	e3:SetRange(LOCATION_MZONE)
+	e3:SetRange(LOCATION_ONFIELD)
 	e3:SetValue(95515060)
 	c:RegisterEffect(e3)
 end
