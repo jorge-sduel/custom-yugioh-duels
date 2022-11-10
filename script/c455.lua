08c16745d530d4d1c608ba2817a016ac28a18cc3
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-10 15:07:16 -0600

    Add files via upload

--- script/c455.lua
+++ script/c455.lua
@@ -11,14 +11,23 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1)
-	--chain attack
-	--local e3=Effect.CreateEffect(c)
-	--e3:SetDescription(aux.Stringid(id,0))
-	--e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	--e3:SetRange(LOCATION_MZONE)
-	--e3:SetCode(EVENT_BATTLE_DESTROYING)
-	--e3:SetOperation(s.atop)
-	--c:RegisterEffect(e3)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	--special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_TO_GRAVE)
+	e4:SetRange(LOCATION_GRAVE)
+	e4:SetCondition(s.spcon1)
+	e4:SetTarget(s.sptg1)
+	e4:SetOperation(s.spop1)
+	c:RegisterEffect(e4)
 end
 function s.spcon(e,c)
 	if c==nil then return true end
@@ -44,4 +53,23 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
 	local g2=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
 		Duel.SendtoHand(g2,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g2)
+end
+function s.cfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp)
+		and c:IsReason(REASON_BATTLE+REASON_EFFECT) and c:IsReason(REASON_DESTROY) and c:IsType(TYPE_FUSION)
+end
+function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.cfilter,1,nil,tp)
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,true,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	if Duel.SpecialSummon(c,0,tp,tp,true,false,POS_FACEUP)~=0 then
+		c:CompleteProcedure()
+	end
 end
