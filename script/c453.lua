8991f4aacf5e445a497a9408772b2eb8432bc487
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-10 07:16:54 -0600

    Add files via upload

---
+++ script/c453.lua
@@ -0,0 +1,58 @@
+--DNA改造手術
+--DNA Surgery
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_EQUIP)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCost(aux.RemainFieldCost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+	--race
+	--local e2=Effect.CreateEffect(c)
+	--e2:SetType(EFFECT_TYPE_FIELD)
+	--e2:SetRange(LOCATION_SZONE)
+	--e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	--e2:SetCode(EFFECT_CHANGE_RACE)
+	--e2:SetValue(s.value)
+	--c:RegisterEffect(e2)
+	--e1:SetLabelObject(e2)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) 
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+	local rc=Duel.SelectOption(tp,70,71,72)
+	if e:GetLabelObject() then e:GetLabelObject():SetLabel(rc) end
+	--e:GetHandler():SetHint(CHINT_RACE,rc)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsLocation(LOCATION_SZONE) or not c:IsRelateToEffect(e) or c:IsStatus(STATUS_LEAVE_CONFIRMED) then return end
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,c,tc)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(s.eqlimit)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		c:RegisterEffect(e1)
+	else
+		c:CancelToGrave(false)
+	end
+end
+function s.eqlimit(e,c)
+	return c:GetControler()==e:GetHandlerPlayer() or e:GetHandler():GetEquipTarget()==c
+end
+function s.value(e,c)
+	return e:GetLabel()
+end
