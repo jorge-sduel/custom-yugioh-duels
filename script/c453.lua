98806c38ea4fe3c354de78a26aa0785beeed717a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-10 07:59:44 -0600

    Update c453.lua

--- script/c453.lua
+++ script/c453.lua
@@ -17,14 +17,12 @@ function s.initial_effect(c)
 	e2:SetValue(500)
 	c:RegisterEffect(e2)
 	--race
-	--local e2=Effect.CreateEffect(c)
-	--e2:SetType(EFFECT_TYPE_FIELD)
-	--e2:SetRange(LOCATION_SZONE)
-	--e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
-	--e2:SetCode(EFFECT_CHANGE_RACE)
-	--e2:SetValue(s.value)
-	--c:RegisterEffect(e2)
-	--e1:SetLabelObject(e2)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetValue(s.efilter)
+	c:RegisterEffect(e2)
+	e1:SetLabelObject(e2)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
@@ -34,11 +32,13 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
 	local rc=Duel.SelectOption(tp,70,71,72)
+	--if (op~=0 and tc:IsMonster()) or (op~=1 and tc:IsSpell()) or (op~=2 and tc:IsTrap())
 	if e:GetLabelObject() then e:GetLabelObject():SetLabel(rc) end
 	--e:GetHandler():SetHint(CHINT_RACE,rc)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
+	--if (op~=0 and tc:IsMonster()) or (op~=1 and tc:IsSpell()) or (op~=2 and tc:IsTrap())
 	if not c:IsLocation(LOCATION_SZONE) or not c:IsRelateToEffect(e) or c:IsStatus(STATUS_LEAVE_CONFIRMED) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
@@ -57,6 +57,9 @@ end
 function s.eqlimit(e,c)
 	return c:GetControler()==e:GetHandlerPlayer() or e:GetHandler():GetEquipTarget()==c
 end
-function s.value(e,c)
-	return e:GetLabel()
+function s.value(e,te)
+	if e:GetLabel()==0 then g=IsMonster()
+	elseif e:GetLabel()==1 then g=IsSpell()
+	else g=IsTrap end
+	return te:IsActiveType(g)
 end
