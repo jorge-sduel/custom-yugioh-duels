03287f34d94a7df29db35914b0f124e4cc68db53
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-10 07:44:49 -0600

    Update c453.lua

--- script/c453.lua
+++ script/c453.lua
@@ -1,5 +1,4 @@
---DNA改造手術
---DNA Surgery
+--Guardian Rod
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
@@ -12,6 +11,11 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
 	--race
 	--local e2=Effect.CreateEffect(c)
 	--e2:SetType(EFFECT_TYPE_FIELD)
