abca6d68e767f550cd7a99bc8b85df8bb809e0fd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-10 22:22:59 -0600

    Update c457.lua

--- script/c457.lua
+++ script/c457.lua
@@ -53,7 +53,7 @@ function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsPreviousLocation(LOCATION_MZONE)
 end
 function s.spfilter(c,e,tp)
-	return c.Is_Neutrino or c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c.Is_Neutrino or c:IsRace(RACE_DRAGON) and not c:IsCode(id) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
