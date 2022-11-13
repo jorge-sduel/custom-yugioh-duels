0328db5e98458aef23d939eba53dac0e8f23e147
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-13 11:47:38 -0600

    Update c957801023.lua

--- script/c957801023.lua
+++ script/c957801023.lua
@@ -6,7 +6,8 @@ function c957801023.initial_effect(c)
 	Pendulum.AddProcedure(c)
 	--Rune Summon
 	c:EnableReviveLimit()
-	aux.AddRunicProcedure2(c,aux.FilterBoolFunction(Card.IsRace,RACE_SPELLCASTER),c957801023.matfilter,1,1,LOCATION_EXTRA+LOCATION_HAND)
+	Runic.AddProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_SPELLCASTER),c957801023.matfilter,1,1)
+	aux.AddRunicProcedure1(c,aux.FilterBoolFunction(Card.IsRace,RACE_SPELLCASTER),c957801023.matfilter,1,1)
 	--tohand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(957801023,0))
