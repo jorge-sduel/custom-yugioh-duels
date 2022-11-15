8e5649ea1d740220a2766d123f642a726e05680d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-15 13:54:53 -0600

    Update c900000130.lua

--- script/c900000130.lua
+++ script/c900000130.lua
@@ -37,7 +37,7 @@ end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+		--Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 		local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_MZONE,0,1,1,nil,tc)
 		local tc2=g:GetFirst()
 		if tc2 and aux.CheckUnionEquip(tc,tc2) and Duel.Equip(tp,tc,tc2) then
