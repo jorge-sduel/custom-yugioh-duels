40df6aa73f7db999889ed794fd83ae5a8160e98a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-16 11:01:04 -0600

    Update c462.lua

--- script/c462.lua
+++ script/c462.lua
@@ -84,7 +84,7 @@ function s.pcop(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e1)
+	Duel.RegisterEffect(e1,tp)
 	end
 end
 function s.splimit(e,c,tp,sumtp,sumpos)
