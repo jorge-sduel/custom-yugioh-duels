bf2cc2a4fab30a648195f8686a4dd5688c72d9b7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-16 10:44:53 -0600

    Add files via upload

---
+++ script/c462.lua
@@ -0,0 +1,107 @@
+--覇王黒竜オッドアイズ・リベリオン・ドラゴン
+--Odd-Eyes Rebellion Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--pendulum set
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_PZONE)
+	e1:SetCountLimit(1)
+	e1:SetTarget(s.pctg)
+	e1:SetOperation(s.pcop)
+	c:RegisterEffect(e1)
+	--pendulum set
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND+LOCATION_GRAVE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.pctg2)
+	e2:SetOperation(s.pcop2)
+	c:RegisterEffect(e2)
+	--tohand
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_SUMMON_SUCCESS)
+	e3:SetCountLimit(1,144)
+	e3:SetTarget(c144.thtg)
+	e3:SetOperation(c144.thop)
+	c:RegisterEffect(e3)
+	--tohand
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e4:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e4:SetCountLimit(1,144)
+	e4:SetTarget(c144.thtg)
+	e4:SetOperation(c144.thop)
+	c:RegisterEffect(e4)
+end
+function s.pcfilter2(c)
+	return c:IsType(TYPE_PENDULUM) and (c:IsSetCard(0x20f8) or c:IsSetCard(0x10f8)) and not c:IsForbidden()
+end
+function s.pctg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return  Duel.IsExistingMatchingCard(s.pcfilter2,tp,LOCATION_ONFIELD,0,1,nil) and Duel.IsPlayerCanDraw(tp,2) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+end
+function s.pcop2(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if then return end
+	local g=Duel.SelectMatchingCard(tp,s.pcfilter2,tp,LOCATION_ONFIELD,0,1,1,nil)
+	if #g>0 then
+			Duel.SendtoExtraP(g:GetFirst(),tp,REASON_EFFECT)
+			Duel.SendtoExtraP(c,tp,REASON_EFFECT)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Draw(p,d,REASON_EFFECT)
+	end
+end
+function s.pcfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsSetCard(0x20f8) and not (c:IsForbidden() and c:IsCode(id))
+end
+function s.pctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckPendulumZones(tp)
+		and Duel.IsExistingMatchingCard(s.pcfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.pcop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if not Duel.CheckPendulumZones(tp) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.pcfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+				Duel.SendtoExtraP(g:GetFirst(),tp,REASON_EFFECT)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1)
+	end
+end
+function s.splimit(e,c,tp,sumtp,sumpos)
+	return not c:IsAttribute(ATTRIBUTE_DARK) and (sumtp&SUMMON_TYPE_PENDULUM)==SUMMON_TYPE_PENDULUM
+end
+function s.afilter(c)
+	return c:ListsCode(13331639) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.afilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.afilter,tp,LOCATION_DECK,0,1,1,nil)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
