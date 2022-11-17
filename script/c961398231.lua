e01ea6c4574a483fa4ea19fca677dfe9d83738ad
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-17 12:36:14 -0600

    Update c961398231.lua

--- script/c961398231.lua
+++ script/c961398231.lua
@@ -4,6 +4,7 @@ if not RUNIC_IMPORTED then Duel.LoadScript("proc_runic.lua") end
 function c961398231.initial_effect(c)
 	--Rune Summon
 	c:EnableReviveLimit()
+  aux.AddRunicState(c)
 	local r1=Effect.CreateEffect(c)
 	r1:SetType(EFFECT_TYPE_FIELD)
 	r1:SetCode(EFFECT_SPSUMMON_PROC)
@@ -58,25 +59,6 @@ function c961398231.initial_effect(c)
 	e5:SetTarget(c961398231.sptg)
 	e5:SetOperation(c961398231.spop)
 	c:RegisterEffect(e5)
---
-       local e8=Effect.CreateEffect(c)
-	e8:SetType(EFFECT_TYPE_SINGLE)
-	e8:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e8:SetCode(EFFECT_LEVEL_RANK)
-	c:RegisterEffect(e8)
---
-        local e9=Effect.CreateEffect(c)
-	e9:SetType(EFFECT_TYPE_SINGLE)
-	e9:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e9:SetCode(EFFECT_CHANGE_LEVEL)
-	e9:SetValue(0)
-	c:RegisterEffect(e9)
---
-	local e10=Effect.CreateEffect(c)
-	e10:SetType(EFFECT_TYPE_SINGLE)
-	e10:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e10:SetCode(EFFECT_ALLOW_NEGATIVE)
-	c:RegisterEffect(e10)
 end
 function c961398231.matfilter1(c)
 	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and (c:GetAttack()==0 or c:GetDefense()==0)
