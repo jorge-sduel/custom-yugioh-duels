48a809511a85d9724025e8d993993c49721d51db
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-20 07:36:08 -0600

    Update c90252097.lua

--- script/c90252097.lua
+++ script/c90252097.lua
@@ -36,7 +36,7 @@ end
 function cid.evalue(e,re,rp)
 	return re:IsActiveType(TYPE_TRAP) and rp~=e:GetHandlerPlayer()
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function cid.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return false end
 	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_PZONE,0,1,nil)
 		and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_MZONE,1,nil) end
