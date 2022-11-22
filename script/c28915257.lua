cf1ffa61607eaa313a84ab43c737536a8238076b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-22 08:58:42 -0600

    Update c28915257.lua

--- script/c28915257.lua
+++ script/c28915257.lua
@@ -64,7 +64,7 @@ function ref.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_UPDATE_ATTACK)
 		e1:SetValue(ev)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
 		c:RegisterEffect(e1)
 	end
 end
