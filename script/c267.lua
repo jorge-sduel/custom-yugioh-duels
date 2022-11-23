a957ded321461e32457a9929856370b82bb15292
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-23 10:43:41 -0600

    Update c267.lua

--- script/c267.lua
+++ script/c267.lua
@@ -1,16 +1,6 @@
 --cekestial sword eatos
 function c267.initial_effect(c)
-	--Activate 1
-	local e1=Effect.CreateEffect(c)
-e1:SetDescription(aux.Stringid(267,0))
-
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_ACTIVATE)
-	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetTarget(c267.target2)
-	e1:SetOperation(c267.operation2)
-	c:RegisterEffect(e1)
+	aux.AddEquipProcedure(c)
 	--Atk Change
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
@@ -28,17 +18,6 @@ e1:SetDescription(aux.Stringid(267,0))
 	e3:SetTarget(c267.tgtg)
 	e3:SetOperation(c267.tgop)
 	c:RegisterEffect(e3)
-	--Activate
-	local e4=Effect.CreateEffect(c)
-e4:SetDescription(aux.Stringid(267,2))
-
-	e4:SetCategory(CATEGORY_EQUIP)
-	e4:SetType(EFFECT_TYPE_ACTIVATE)
-	e4:SetCode(EVENT_FREE_CHAIN)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e4:SetTarget(c267.target)
-	e4:SetOperation(c267.operation)
-	c:RegisterEffect(e4)
 	--to grave2
 	local e5=Effect.CreateEffect(c)
 	e5:SetDescription(aux.Stringid(69303178,1))
@@ -58,62 +37,8 @@ e4:SetDescription(aux.Stringid(267,2))
 	e6:SetValue(1)
 	c:RegisterEffect(e6)
 end
-function c267.spcon(e,c)
-	return not Duel.IsExistingMatchingCard(Card.IsType,c:GetControler(),0,LOCATION_GRAVE,1,nil,TYPE_MONSTER)
-end
-function c267.spfilter(c,e,tp)
-	return c:IsCode(34022290) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-end
-function c267.Afilter(c,e,tp)
-	return c:IsType(TYPE_MONSTER)
-end
-function c267.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_HAND) and chkc:IsControler(tp) and c267.spfilter(chkc,e,tp) end
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingTarget(c267.spfilter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectTarget(tp,c267.spfilter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
-end
-function c267.eqlimit(e,c)
-	return e:GetLabelObject()==c
-end
-function c267.operation(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
-		if Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP_ATTACK)==0 then return end
-		Duel.Equip(tp,c,tc)
-		--Add Equip limit
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_EQUIP_LIMIT)
-		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(c267.eqlimit)
-		e1:SetLabelObject(tc)
-		c:RegisterEffect(e1)
-	end
-end
-function c267.filter2(c)
-	return c:IsFaceup()
-end
-function c267.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and c267.filter2(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(c267.filter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	Duel.SelectTarget(tp,c267.filter2,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
-end
-function c267.operation2(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.Equip(tp,e:GetHandler(),tc)
-	end
-end
 function c267.value(e,c)
-	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,0,LOCATION_GRAVE,nil)
+	local g=Duel.GetMatchingGroup(Card.IsMonster,c:GetControler(),0,LOCATION_GRAVE,nil)
 		return g:GetSum(Card.GetAttack)
 end
 function c267.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
