0916e90b2f8c0cfaacfe3e08adb88296e2eb5f4e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-23 10:07:48 -0600

    Update c464.lua

--- script/c464.lua
+++ script/c464.lua
@@ -91,7 +91,7 @@ function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		local ct=Duel.GetMatchingGroupCount(s.cfilter,tp,LOCATION_MZONE,0,nil)
 		e:SetLabel(ct)
-		return Duel.IsExistingMatchingCard(aux.FaceupFilter(aux.TRUE),tp,LOCATION_GRAVE,0,ct,c)
+		return Duel.IsExistingMatchingCard(aux.FaceupFilter(aux.TRUE),tp,LOCATION_GRAVE,0,1,c)
 	end
 	local ct=e:GetLabel()
 	local sg=Duel.GetMatchingGroup(aux.FaceupFilter(aux.TRUE),tp,LOCATION_GRAVE,0,c)
