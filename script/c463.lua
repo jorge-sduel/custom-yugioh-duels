77711b185158be13577b7970bc5b54ec5fa85db4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-23 07:53:19 -0600

    Update c463.lua

--- script/c463.lua
+++ script/c463.lua
@@ -85,7 +85,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function s.activate2(e,tp,eg,ep,ev,re,r,rp)
-	local g=Duel.SelectMatchingCard(tp,s.revfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil,class,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.revfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
 	local e1=Effect.CreateEffect(e:GetHandler())
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_REVERSE_UPDATE)
