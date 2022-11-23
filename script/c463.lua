91bf4f35231e981acd084a0e43414246741e4225
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-23 08:26:33 -0600

    Update c463.lua

--- script/c463.lua
+++ script/c463.lua
@@ -99,7 +99,7 @@ function s.activate2(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_REVERSE_UPDATE)
 		--e1:SetValue(1500)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		tc:RegisterEffect(e1)
 	end
 end
