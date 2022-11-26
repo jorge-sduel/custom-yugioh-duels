fe7d36b7f73a7eed54b9594694f2279644665bb4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-25 21:50:25 -0600

    Update c461.lua

--- script/c461.lua
+++ script/c461.lua
@@ -105,7 +105,9 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
-	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
-	if #sg==2 and Duel.Destroy(sg,REASON_EFFECT)==2 then
-	end
+	--local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	--if #sg==2 and
+ Duel.Destroy(sg,REASON_EFFECT)
+--==2 then
+--	end
 end
