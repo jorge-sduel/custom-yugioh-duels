fe8e0e3ace17ed3500011950dace3f3812468519
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-11-27 09:57:12 -0600

    Add files via upload

---
+++ script/c470.lua
@@ -0,0 +1,77 @@
+--正義の味方 カイバーマン
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.spcost)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Increase its own ATK
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_REPEAT+EFFECT_FLAG_DELAY)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(s.adval)
+	c:RegisterEffect(e4)
+	local e5=e4:Clone()
+	e5:SetCode(EFFECT_SET_DEFENSE_FINAL)
+	c:RegisterEffect(e5)
+	--ATK check
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(id)
+	c:RegisterEffect(e7)
+end
+s.listed_names={40591390}
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function s.filter(c,e,tp,spchk)
+	return c:IsSetCard(0xc008) and c:IsMonster() and c:IsLevel(8) and (c:IsAbleToHand() or (spchk and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,1,tp,false,false)))
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	if #g>0 then
+		aux.ToHandOrElse(g,tp,
+			function(c)
+				return g:IsCode(40591390)
+					and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+					and tc:IsCanBeSpecialSummoned(e,1,tp,false,false)
+			end,
+			function(c)
+				Duel.SpecialSummon(g,1,tp,tp,false,false,POS_FACEUP)
+			end,
+		1152)
+	end
+end
+function s.afilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xc008) and not c:IsHasEffect(id)
+end
+function s.adval(e,c)
+	local g=Duel.GetMatchingGroup(s.afilter,0,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if #g==0 then 
+		return 0
+	else
+		local tg,val=g:GetMaxGroup(Card.GetAttack)
+		if not tg:IsExists(aux.TRUE,1,e:GetHandler()) then
+			g:RemoveCard(e:GetHandler())
+			tg,val=g:GetMaxGroup(Card.GetAttack)
+		end
+		return val
+	end
+end
