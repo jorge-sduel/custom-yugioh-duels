a758278721933193238d547a28b079472153375a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-02 08:37:45 -0600

    Add files via upload

---
+++ script/c472.lua
@@ -0,0 +1,71 @@
+--破邪の大剣－バオウ
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddEquipProcedure(c,nil,nil,nil,s.cost)
+	--Atk Change
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_SET_ATTACK)
+	e2:SetCondition(s.condition)
+	e2:SetValue(s.value)
+	c:RegisterEffect(e2)
+	--Atk Change
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_EQUIP)
+	e3:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e3:SetCondition(s.condition)
+	c:RegisterEffect(e3)
+	--Atk Change
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetCode(EFFECT_CANNOT_ATTACK)
+	e4:SetCondition(s.condition2)
+	c:RegisterEffect(e4)
+	--Atk Change
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_EQUIP)
+	e5:SetCode(EFFECT_DISABLE)
+	e5:SetCondition(s.condition2)
+	c:RegisterEffect(e5)
+	--spsummon
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,0))
+	e6:SetCategory(CATEGORY_EQUIP)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e6:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e6:SetCode(EVENT_TO_GRAVE)
+	e6:SetTarget(s.tg)
+	e6:SetOperation(s.op)
+	c:RegisterEffect(e6)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToGraveAsCost,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsAbleToGraveAsCost,1,1,REASON_COST)
+end
+function s.condition(e)
+	local ec=e:GetHandler():GetEquipTarget()
+	return ec:GetSetCard()=0x52
+end
+function s.value(e,c)
+		return c:GetBaseAttack()*2
+end
+function s.eqfilter(e,c)
+		return c:IsSetCard(0x52)
+end
+function s.condition2(e)
+	local ec=e:GetHandler():GetEquipTarget()
+	return not ec:GetSetCard()=0x52
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if e:GetHandler():IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+		Duel.Equip(tp,e:GetHandler(),tc)
+	end
+end
