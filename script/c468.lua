3d5a95e32b4ea50e44f51b9ba0967a5ee609612a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-03 09:42:27 -0600

    Update c468.lua

--- script/c468.lua
+++ script/c468.lua
@@ -20,7 +20,7 @@ function c468.filter2(c,e,tp,mc)
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_LINK,tp,false,false)
 end
 function c468.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c90800.filter1(chkc,e,tp) end
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c468.filter1(chkc,e,tp) end
 	if chk==0 then return Duel.IsExistingTarget(c468.filter1,tp,LOCATION_MZONE,0,1,nil,e,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	local g=Duel.SelectTarget(tp,c468.filter1,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
@@ -32,7 +32,7 @@ function c468.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCountFromEx(tp,tp,tc)<=0 then return end
 	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c90800.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc)
+	local g=Duel.SelectMatchingCard(tp,c468.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc)
 	local sc=g:GetFirst()
 	if sc then
 		sc:SetMaterial(Group.FromCards(tc))
