2699484722ec6d1d61f0b5a480190b1002a75ba7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-03 08:33:03 -0600

    Update c16018.lua

--- script/c16018.lua
+++ script/c16018.lua
@@ -17,7 +17,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
-	return c:IsRace(RACE_INSECT,fc,sumtype,tp) and c:IsLevelAbove(8) and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetCode(fc,sumtype,tp),fc,sumtype,tp))
+
+	return c:IsRace(RACE_INSECT,fc,sumtype,tp) and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetCode(fc,sumtype,tp),fc,sumtype,tp))
+end
+function s.fusfilter(c,code,fc,sumtype,tp)
+	return c:IsSummonCode(fc,sumtype,tp,code) and not c:IsHasEffect(511002961)
 end
 function s.splimit(e,se,sp,st)
 	return (st&SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
