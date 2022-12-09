abd6d38d73dce16be9116886c02f3174f14615b4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-08 21:35:03 -0600

    Update c350.lua

--- script/c350.lua
+++ script/c350.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e1:SetCondition(aux.NOT(s.eqcon))
+	e1:SetCondition(s.eqcon)
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
 	-- Cannot be destroyed by battle
