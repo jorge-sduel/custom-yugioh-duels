2366bb0a41ef9b33fc6555793a00d671c870d70e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-10 11:15:34 -0600

    Update c210220004.lua

--- script/c210220004.lua
+++ script/c210220004.lua
@@ -7,21 +7,22 @@ function card.initial_effect(c)
 	e1:SetCode(EVENT_TO_GRAVE)
 	--e1:SetProperty(EFFECT_FLAG_DELAY)
 	--e1:SetCondition(card.descon)
-	--e1:SetTarget(card.destg)
-	--e1:SetOperation(card.desop)
+	e1:SetTarget(card.destg)
+	e1:SetOperation(card.desop)
 	c:RegisterEffect(e1)
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCode(EVENT_TO_GRAVE)
 	--e2:SetProperty(EFFECT_FLAG_DELAY)
-	--e2:SetCondition(card.descon2)
+	e2:SetCondition(card.descon2)
 	e2:SetTarget(card.destg2)
 	e2:SetOperation(card.desop2)
 	c:RegisterEffect(e2)
 end
 function card.descon2(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_DECK)	and bit.band(r,REASON_EFFECT)~=0
+	return e:GetHandler():IsPreviousLocation(LOCATION_DECK)
+--	and bit.band(r,REASON_EFFECT)~=0
 end
 function card.destg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_SZONE) and chkc:IsControler(1-tp) end
