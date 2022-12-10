bec4ff74a5d3099a36911998922f1befbfe2c5a7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-09 20:49:19 -0600

    Update c475.lua

--- script/c475.lua
+++ script/c475.lua
@@ -1,6 +1,9 @@
 --Inzektor rango 6
 local s,id=GetID()
 function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,4,2)
+	c:EnableReviveLimit()
 	--equip
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_IGNITION)
