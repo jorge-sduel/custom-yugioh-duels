05e6a16cb7f3b0e757615561d12464bd8e573eb0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-10 17:03:38 -0600

    Add files via upload

---
+++ script/c473.lua
@@ -0,0 +1,96 @@
+--Purity of the Cemetery
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	--e1:SetCondition(s.condition)
+	c:RegisterEffect(e1)
+	--counter
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e2:SetProperty(EFFECT_FLAG_REPEAT)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.damcon)
+	e2:SetOperation(s.damop)
+	c:RegisterEffect(e2)
+	--counter
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetCode(EVENT_PHASE+PHASE_STANDBY)
+	e3:SetProperty(EFFECT_FLAG_REPEAT)
+	e3:SetCountLimit(1,id)
+	e3:SetCondition(s.damcon2)
+	e3:SetOperation(s.damop2)
+	c:RegisterEffect(e3)
+	--selfdes
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCode(EFFECT_SELF_DESTROY)
+	e4:SetCondition(s.sdcon2)
+	c:RegisterEffect(e4)
+	--spsummon
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(302,2))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetCountLimit(1)
+	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e5:SetTarget(c302.sptg)
+	e5:SetOperation(c302.spop)
+	c:RegisterEffect(e5)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return not Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,nil)
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and not Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,nil)
+end
+function s.damcon2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and Duel.IsExistingMatchingCard(Card.IsMonster,tp,LOCATION_GRAVE,0,1,nil)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,0,LOCATION_GRAVE,nil)
+	local dam=#g*400
+	Duel.Damage(1-tp,dam,REASON_EFFECT)
+end
+function s.damop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(Card.IsMonster,tp,0,LOCATION_GRAVE,nil)
+	local dam=#g*200
+	Duel.Damage(1-tp,dam,REASON_EFFECT)
+end
+function s.con(e,c)
+	--if c==nil then return true end
+	return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),LOCATION_GRAVE+LOCATION_REMOVED,0,nil,TYPE_MONSTER)>=3
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x52) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE+LOCATION_HAND,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE+LOCATION_HAND,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
+	end
+end
