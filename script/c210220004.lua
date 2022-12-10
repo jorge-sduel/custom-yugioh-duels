25475f9168290ab5a9d2027077a7410fc8b74d41
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-10 11:09:09 -0600

    Update c210220004.lua

--- script/c210220004.lua
+++ script/c210220004.lua
@@ -3,7 +3,7 @@ local card = c210220004
 function card.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_TO_GRAVE)
 	--e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCondition(card.descon)
@@ -37,7 +37,7 @@ function card.desop2(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 function card.descon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsPreviousLocation(LOCATION_HAND)
+	return e:GetHandler():IsPreviousLocation(LOCATION_HAND) and (r&0x4040)==0x4040
 end
 function card.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) end
