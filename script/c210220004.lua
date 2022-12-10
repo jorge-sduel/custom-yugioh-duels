173c91dfff7bfb7131a432bf44ea64e853cd5e3a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-10 10:56:38 -0600

    Update c210220004.lua

--- script/c210220004.lua
+++ script/c210220004.lua
@@ -3,7 +3,7 @@ local card = c210220004
 function card.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetCondition(card.descon)
