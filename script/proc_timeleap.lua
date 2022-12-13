1b62cfd64fcdbca203997fff494bcfa31e976abc
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-12 21:12:38 -0600

    Update proc_timeleap.lua

--- script/proc_timeleap.lua
+++ script/proc_timeleap.lua
@@ -316,7 +316,7 @@ end
 function Timeleap.Removecon(e,tp,eg,ep,ev,re,r,rp)
 	return not c:IsHasEffect(395)
 end
-function Timeleap.spfilter(c,code,lc,tp)
+function Timeleap.spfilter(e,c,code,lc,tp)
 	return (not code or code(c,lc,SUMMON_TYPE_SPECIAL,tp)) and c:IsLevel(e:GetHandler():GetLevel()-1) and c:IsAbleToRemoveAsCost()
 end
 function Timeleap.rescon(sg,e,tp,mg)
