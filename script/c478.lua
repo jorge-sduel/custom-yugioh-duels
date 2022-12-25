ef70722f609072bb018af54c64d69444a1132b6e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-25 13:35:48 -0600

    Add files via upload

---
+++ script/c478.lua
@@ -0,0 +1,38 @@
+--Trap
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	Fusion.RegisterSummonEff(c)
+	--Set 1 Spell/Trap from the Deck
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1,id)
+	e2:SetHintTiming(TIMING_END_PHASE)
+	e2:SetCondition(s.setcond)
+	e2:SetCost(aux.bfgcost)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+end
+function s.setcond(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCurrentPhase()==PHASE_END
+end
+function s.setfilter(c)
+	return c:IsSSetable() and not c:IsForbidden()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<1 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+	end
+end
