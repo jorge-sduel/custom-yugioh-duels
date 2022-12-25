1d96de4a35f79479e684ec59e62b4bca807717e4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-25 12:46:47 -0600

    Add files via upload

---
+++ script/c476.lua
@@ -0,0 +1,98 @@
+--ツイン・フォトン・リザード
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_DRAGON),3)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--set
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(24294108,0))
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetRange(LOCATION_MZONE)
+	--e2:SetCountLimit(1)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_SSET)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetOperation(s.aclimset)
+	c:RegisterEffect(e3)
+	--disable
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetCode(EVENT_CHAIN_ACTIVATING)
+	e4:SetOperation(s.disop)
+	c:RegisterEffect(e4)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function s.mgfilter(c,e,tp,fusc,mg)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_GRAVE)
+		and (c:GetReason()&0x40008)==0x40008 and c:GetReasonCard()==fusc
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and fusc:CheckFusionMaterial(mg,c,PLAYER_NONE+65536)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=e:GetHandler():GetMaterial()
+	if chk==0 then
+		local ct=#g
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+		return ct>0 and ft>=ct and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+			and e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+			and g:FilterCount(s.mgfilter,nil,e,tp,e:GetHandler(),g)==ct
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,#g,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	local g=e:GetHandler():GetMaterial()
+	local ct=#g
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)>=ct
+		and g:FilterCount(s.mgfilter,nil,e,tp,e:GetHandler(),g)==ct then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.setfilter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsFaceup() and c:IsCanTurnSet()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,e:GetHandler():GetControler(),LOCATION_ONFIELD,LOCATION_ONFIELD,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.setfilter,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,nil)
+	if c:IsRelateToEffect(e) and g:GetCount()>0 then
+		Duel.ChangePosition(g,POS_FACEDOWN)
+	end
+end
+function s.aclimset(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	for tc in aux.Next(eg) do
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_OPPO_TURN,0,1)
+	end
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local loc=Duel.GetChainInfo(ev,CHAININFO_TRIGGERING_LOCATION)
+	if re:IsActiveType(TYPE_MONSTER) and (loc==LOCATION_MZONE or loc==LOCATION_GRAVE)
+		and not re:GetHandler():IsAttribute(ATTRIBUTE_DARK) then
+		Duel.NegateEffect(ev)
+	end
+end
