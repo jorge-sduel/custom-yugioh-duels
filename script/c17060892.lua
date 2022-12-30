2887c1aac8878879b67203b9f26b8ba5e78e7f4f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2022-12-29 21:09:52 -0600

    Update c17060892.lua

--- script/c17060892.lua
+++ script/c17060892.lua
@@ -1,7 +1,7 @@
 --ダ·ヴィンチ
 function c17060892.initial_effect(c)
     --pendulum summon
-    aux.EnablePendulumAttribute(c)
+    Pendulum.AddProcedure(c)
     --to hand
     local e1=Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(17060892,0))
