a714cc9f212a28b8dd424e906489d79caffaf6a5
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-01 21:06:41 -0600

    Update c110600013.lua

--- script/c110600013.lua
+++ script/c110600013.lua
@@ -18,7 +18,7 @@ function cm.chainfilter(re,tp,cid)
 	return not (re:GetHandler():IsType(TYPE_PENDULUM) and not re:GetHandler():IsSetCard(0x303))
 end
 function cm.aclimit(e,re,tp)
-	return re:GetHandler():IsType(TYPE_PENDULUM) and not re:GetHandler():IsSetCard(0x303)
+	return not re:GetHandler():IsType(TYPE_PENDULUM) and not re:GetHandler():IsSetCard(0x303)
 end
 function cm.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetCustomActivityCount(m,tp,ACTIVITY_CHAIN)==0 end
