29e6e756c31fae27901247f3fbc03b489c60232e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-03 10:14:22 -0600

    Update c110600011.lua

--- script/c110600011.lua
+++ script/c110600011.lua
@@ -1,7 +1,7 @@
 --
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=s.ritualfil,location=LOCATION_HAND+LOCATION_PZONE})
+	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,location=LOCATION_HAND+LOCATION_PZONE})
 end
 function s.ritualfil(c)
 	return c:IsType(TYPE_RITUAL)
