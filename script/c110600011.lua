2999e4f96476ab8d6a222d1560d9c21ff4e43a01
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-03 10:10:49 -0600

    Update c110600011.lua

--- script/c110600011.lua
+++ script/c110600011.lua
@@ -4,5 +4,5 @@ function s.initial_effect(c)
 	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,filter=s.ritualfil,location=LOCATION_HAND+LOCATION_PZONE})
 end
 function s.ritualfil(c)
-	return c:IsType(TYPE_MONSTER+TYPE_RITUAL)
+	return c:IsType(TYPE_RITUAL)
 end
