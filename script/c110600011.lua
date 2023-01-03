2fc1c1c9369107689d049f1a262c3cec5008ab65
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-03 10:29:33 -0600

    Update c110600011.lua

--- script/c110600011.lua
+++ script/c110600011.lua
@@ -6,14 +6,3 @@ end
 function s.ritualfil(c)
 	return c:IsRitualMonster()
 end
-function s.mfilter(c)
-	return not Duel.IsPlayerAffectedByEffect(c:GetControler(),69832741) and c:HasLevel() and c:IsRace(RACE_ZOMBIE)
-		and c:IsMonster() and c:IsAbleToRemove()
-end
-function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
-	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE,0,nil)
-end
-function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
-end
