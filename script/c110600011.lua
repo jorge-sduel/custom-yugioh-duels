d496695d5a99d2a203f03bb395eaedd3b3218cbd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-03 10:23:02 -0600

    Update c110600011.lua

--- script/c110600011.lua
+++ script/c110600011.lua
@@ -1,8 +1,19 @@
 --
 local s,id=GetID()
 function s.initial_effect(c)
-	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_GREATER,location=LOCATION_HAND+LOCATION_PZONE})
+	local e1=Ritual.AddProcGreater({handler=c,filter=s.ritualfil,extrafil=s.extrafil,location=LOCATION_HAND|LOCATION_GRAVE,extratg=s.extratg})
 end
 function s.ritualfil(c)
-	return c:IsType(TYPE_RITUAL)
+	return c:IsRitualMonster()
+end
+function s.mfilter(c)
+	return not Duel.IsPlayerAffectedByEffect(c:GetControler(),69832741) and c:HasLevel() and c:IsRace(RACE_ZOMBIE)
+		and c:IsMonster() and c:IsAbleToRemove()
+end
+function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
+	return Duel.GetMatchingGroup(s.mfilter,tp,LOCATION_GRAVE,0,nil)
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
 end
