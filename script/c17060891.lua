2d43a9dbc7b790182f736cc5e948709f63ae330b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-04 09:16:05 -0600

    Update c17060891.lua

--- script/c17060891.lua
+++ script/c17060891.lua
@@ -16,6 +16,14 @@ function c17060891.initial_effect(c)
 	local e2=e1:Clone()
 	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
 	c:RegisterEffect(e2)
+	--Double a pendulum scale
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e3:SetRange(LOCATION_PZONE)
+	e3:SetTarget(c17060891.sctg)
+	e3:SetOperation(c17060891.scop)
+	c:RegisterEffect(e3)
 end
 c17060891.is_named_with_Dark_Degenerate=1
 function c17060891.IsDark_Degenerate(c)
@@ -34,5 +42,26 @@ function c17060891.op(e,tp,eg,ep,ev,re,r,rp)
 	if tc then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+function c17060891.sctg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_PZONE) and chkc:IsControler(tp) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_PZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_PZONE,0,1,1,nil)
+end
+function c17060891.scop(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LSCALE)
+		e1:SetValue(tc:GetLeftScale()*2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=e1:Clone()
+		e2:SetCode(EFFECT_CHANGE_RSCALE)
+		e2:SetValue(tc:GetRightScale()*2)
+		tc:RegisterEffect(e2)
 	end
 end
