0cf16e96a439027fbb340f6125c555e75b61fa1b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-04 09:03:07 -0600

    Add files via upload

---
+++ script/c17060860.lua
@@ -0,0 +1,83 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(65518099,0))
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)	
+	--Back to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(65518099,1))
+	e2:SetCategory(CATEGORY_TOHAND)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_PZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)	
+end
+s.is_named_with_Dark_Degenerate=1
+function s.IsDark_Degenerate(c)
+	local m=_G["c"..c:GetCode()]
+	return m and m.is_named_with_Dark_Degenerate
+end
+function s.ffilter(c)
+	return s.IsDark_Degenerate(c)
+end
+function s.thfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,0)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,tp,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local hg=Duel.GetFieldGroup(tp,LOCATION_HAND+LOCATION_ONFIELD,0):Filter(Card.IsLevelAbove,nil,1)
+	local tc=hg:GetFirst()
+	for tc in aux.Next(hg) do
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(tc:GetLevel()*2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_TO_HAND)
+	e2:SetReset(RESET_PHASE+PHASE_END)
+	e2:SetOperation(s.hlvop)
+	Duel.RegisterEffect(e2,tp)
+end
+function s.hlvfilter(c,tp)
+	return c:IsLevelAbove(1) and c:IsControler(tp)
+end
+function s.hlvop(e,tp,eg,ep,ev,re,r,rp)
+	local hg=eg:Filter(s.hlvfilter,nil,tp)
+	local tc=hg:GetFirst()
+	for tc in aux.Next(hg) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(tc:GetLevel()*2)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+	end
+end
