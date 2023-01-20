a7f1b31543d27499170d7cd996ef97779fe7c372
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-20 10:24:05 -0600

    Update c31881000.lua

--- script/c31881000.lua
+++ script/c31881000.lua
@@ -37,9 +37,9 @@ function c31881000.initial_effect(c)
 	c:RegisterEffect(e5)
 end
 function c31881000.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_HAND+LOCATION_EXTRA) and chkc:IsControler(tp) and chkc:IsSetCard(0xdd) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsSetCard,tp,LOCATION_GRAVE+LOCATION_EXTRA,0,1,nil,0xdd) end
-	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(31881000,1))
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_HAND+LOCATION_EXTRA) and chkc:IsSetCard(0xdd) end
+	if chk==0 then return true end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 	Duel.SelectTarget(tp,Card.IsSetCard,tp,LOCATION_HAND+LOCATION_EXTRA,0,1,1,nil,0xdd)
 end
 function c31881000.operation(e,tp,eg,ep,ev,re,r,rp)
