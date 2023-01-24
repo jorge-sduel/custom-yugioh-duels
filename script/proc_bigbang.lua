95c9d00196789610b9e8851cf6a12ba8c73ce9f3
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-24 11:27:04 -0600

    Update proc_bigbang.lua

--- script/proc_bigbang.lua
+++ script/proc_bigbang.lua
@@ -281,6 +281,7 @@ end
 function Auxiliary.BigbangSummonSubstitute(c,cd,tp)
 	return c:IsHasEffect(52401238,tp) and c:IsAbleToGraveAsCost()
 end
+--Space-Time summon
 function Auxiliary.AddBigbangProcedure(c,f,min,max,specialchk,opp,loc,send)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -288,9 +289,9 @@ function Auxiliary.AddBigbangProcedure(c,f,min,max,specialchk,opp,loc,send)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetRange(LOCATION_EXTRA)
-	e1:SetCondition(Bigbang.Condition(f,min,max,specialchk,opp,loc,send))
-	e1:SetTarget(Bigbang.Target(f,min,max,specialchk,opp,loc,send))
-	e1:SetOperation(Bigbang.Operation(f,min,max,specialchk,opp,loc,send))
+	e1:SetCondition(Auxiliary.BigbangCondition(f,min,max,specialchk,opp,loc,send))
+	e1:SetTarget(Auxiliary.BigbangTarget(f,min,max,specialchk,opp,loc,send))
+	e1:SetOperation(Auxiliary.BigbangOperation(f,min,max,specialchk,opp,loc,send))
     e1:SetValue(SUMMON_TYPE_BIGBANG)
 	c:RegisterEffect(e1)
 	--scale
@@ -299,4 +300,171 @@ function Auxiliary.AddBigbangProcedure(c,f,min,max,specialchk,opp,loc,send)
 	e3:SetCode(EFFECT_REMOVE_TYPE)
 	e3:SetValue(TYPE_SYNCHRO)
 	c:RegisterEffect(e3)
+end
+function Auxiliary.BigbangConditionFilter(c,f,lc,tp)
+	return not f or f(c,lc,SUMMON_TYPE_SPECIAL,tp)
+end
+function Auxiliary.BigbangGetBigbangCount(c)
+    if c:GetAttack()>1 then return c:GetAttack() end
+    return 1
+end
+function Auxiliary.BigbangCheckRecursive2(c,tp,sg,sg2,secondg,mg,lc,minc,maxc,f,specialchk,og,emt,filt)
+	if #sg>maxc then return false end
+	sg:AddCard(c)
+	for _,filt in ipairs(filt) do
+		if not filt[2](c,filt[3],tp,sg,mg,lc,filt[1],1) then
+			sg:RemoveCard(c)
+			return false
+		end
+	end
+	if not og:IsContains(c) then
+		res=aux.CheckValidExtra(c,tp,sg,mg,lc,emt,filt)
+		if not res then
+			sg:RemoveCard(c)
+			return false
+		end
+	end
+	if #(sg2-sg)==0 then
+		if secondg and #secondg>0 then
+			local res=secondg:IsExists(Auxiliary.BigbangCheckRecursive,1,sg,tp,sg,mg,lc,minc,maxc,f,specialchk,og,emt,{table.unpack(filt)})
+			sg:RemoveCard(c)
+			return res
+		else
+			local res=Auxiliary.BigbangCheckGoal(tp,sg,lc,minc,f,specialchk,{table.unpack(filt)})
+			sg:RemoveCard(c)
+			return res
+		end
+	end
+	local res=Auxiliary.BigbangCheckRecursive2((sg2-sg):GetFirst(),tp,sg,sg2,secondg,mg,lc,minc,maxc,f,specialchk,og,emt,filt)
+	sg:RemoveCard(c)
+	return res
+end
+function Auxiliary.BigbangCheckGoal(tp,sg,lc,minc,f,specialchk,filt)
+	for _,filt in ipairs(filt) do
+		if not sg:IsExists(filt[2],1,nil,filt[3],tp,sg,Group.CreateGroup(),lc,filt[1],1) then
+			return false
+		end
+	end
+	return #sg>=minc and sg:CheckWithSumEqual(Auxiliary.BigbangGetBigbangCount,lc:GetAttack(),#sg,#sg)
+		and (not specialchk or specialchk(sg,lc,SUMMON_TYPE_SPECIAL,tp)) and Duel.GetLocationCountFromEx(tp,tp,sg,lc)>0
+end
+function Auxiliary.BigbangCondition(f,minc,maxc,specialchk,opp,loc,send)
+	return	function(e,c,must,g,min,max)
+				if c==nil then return true end
+				if c:IsType(TYPE_PENDULUM) and c:IsFaceup() then return false end
+				local tp=c:GetControler()
+				local loc2=0
+				if opp then loc2=loc end
+				if not g then
+					g=Duel.GetMatchingGroup(Card.IsFaceup,tp,loc,loc2,nil)
+g:Merge(Duel.GetMatchingGroup(Auxiliary.BigbangSummonSubstitute,tp,LOCATION_HAND+LOCATION_EXTRA+LOCATION_GRAVE,0,nil,c:GetControler()))
+				end
+				local mg=g:Filter(Auxiliary.BigbangConditionFilter,nil,f,c,tp)
+				local mustg=Auxiliary.GetMustBeMaterialGroup(tp,g,tp,c,mg,REASON_BIGBANG)
+				if must then mustg:Merge(must) end
+				if min and min < minc then return false end
+				if max and max > maxc then return false end
+				min = min or minc
+				max = max or maxc
+				if mustg:IsExists(aux.NOT(Auxiliary.BigbangConditionFilter),1,nil,f,c,tp) or #mustg>max then return false end
+				local emt,tg=aux.GetExtraMaterials(tp,mustg+mg,c,SUMMON_TYPE_BIGBANG)
+				tg=tg:Filter(Auxiliary.BigbangConditionFilter,nil,f,c,tp)
+				local res=(mg+tg):Includes(mustg) and #mustg<=max
+				if res then
+					if #mustg==max then
+						local sg=Group.CreateGroup()
+						res=mustg:IsExists(Auxiliary.BigbangCheckRecursive,1,sg,tp,sg,(mg+tg),c,min,max,f,specialchk,mg,emt)
+					elseif #mustg<max then
+						local sg=mustg
+						res=(mg+tg):IsExists(Auxiliary.BigbangCheckRecursive,1,sg,tp,sg,(mg+tg),c,min,max,f,specialchk,mg,emt)
+					end
+				end
+				aux.DeleteExtraMaterialGroups(emt)
+				return res
+			end
+end
+function Auxiliary.BigbangTarget(f,minc,maxc,specialchk,opp,loc,send)
+	return	function(e,tp,eg,ep,ev,re,r,rp,chk,c,must,g,min,max)
+				local loc2=0
+				if opp then loc2=loc end
+				if not g then
+					g=Duel.GetMatchingGroup(Card.IsFaceup,tp,loc,loc2,nil)
+g:Merge(Duel.GetMatchingGroup(Auxiliary.BigbangSummonSubstitute,tp,LOCATION_HAND+LOCATION_EXTRA+LOCATION_GRAVE,0,nil,c:GetControler()))
+				end
+				if min and min < minc then return false end
+				if max and max > maxc then return false end
+				min = min or minc
+				max = max or maxc
+				local mg=g:Filter(Auxiliary.BigbangConditionFilter,nil,f,c,tp)
+				local mustg=Auxiliary.GetMustBeMaterialGroup(tp,g,tp,c,mg,REASON_BIGBANG)
+				if must then mustg:Merge(must) end
+				local emt,tg=aux.GetExtraMaterials(tp,mustg+mg,c,SUMMON_TYPE_BIGBANG)
+				tg=tg:Filter(Auxiliary.BigbangConditionFilter,nil,f,c,tp)
+				local sg=Group.CreateGroup()
+				local finish=false
+				local cancel=false
+				sg:Merge(mustg)
+				while #sg<max do
+					local filters={}
+					if #sg>0 then
+						Auxiliary.BigbangCheckRecursive2(sg:GetFirst(),tp,Group.CreateGroup(),sg,mg+tg,mg+tg,c,min,max,f,specialchk,mg,emt,filters)
+					end
+					local cg=(mg+tg):Filter(Auxiliary.BigbangCheckRecursive,sg,tp,sg,(mg+tg),c,min,max,f,specialchk,mg,emt,{table.unpack(filters)})
+					if #cg==0 then break end
+					finish=#sg>=min and #sg<=max and Auxiliary.BigbangCheckGoal(tp,sg,c,min,f,specialchk,filters)
+					cancel=not og and Duel.IsSummonCancelable() and #sg==0
+					Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_LMATERIAL)
+					local tc=Group.SelectUnselect(cg,sg,tp,finish,cancel,1,1)
+					if not tc then break end
+					if #mustg==0 or not mustg:IsContains(tc) then
+						if not sg:IsContains(tc) then
+							sg:AddCard(tc)
+						else
+							sg:RemoveCard(tc)
+						end
+					end
+				end
+				if #sg>0 then
+					local filters={}
+					Auxiliary.BigbangCheckRecursive2(sg:GetFirst(),tp,Group.CreateGroup(),sg,mg+tg,mg+tg,c,min,max,f,specialchk,mg,emt,filters)
+					sg:KeepAlive()
+					local reteff=Effect.GlobalEffect()
+					reteff:SetTarget(function()return sg,filters,emt end)
+					e:SetLabelObject(reteff)
+					return true
+				else 
+					aux.DeleteExtraMaterialGroups(emt)
+					return false
+				end
+			end
+end
+function Auxiliary.BigbangOperation(f,minc,maxc,specialchk,opp,loc,send)
+	return	function(e,tp,eg,ep,ev,re,r,rp,c,must,g,min,max)
+				local g,filt,emt=e:GetLabelObject():GetTarget()()
+				e:GetLabelObject():Reset()
+				for _,ex in ipairs(filt) do
+					if ex[3]:GetValue() then
+						ex[3]:GetValue()(1,SUMMON_TYPE_SPECIAL,ex[3],ex[1]&g,c,tp)
+					end
+				end
+				c:SetMaterial(g)
+				if send==1 then
+					Duel.SendtoGrave(g,REASON_MATERIAL+REASON_BIGBANG+REASON_RETURN)
+				elseif send==2 then
+					Duel.Remove(g,POS_FACEUP,REASON_MATERIAL+REASON_BIGBANG)
+				elseif send==3 then
+					Duel.Remove(g,POS_FACEDOWN,REASON_MATERIAL+REASON_BIGBANG)
+				elseif send==4 then
+					Duel.SendtoHand(g,nil,REASON_MATERIAL+REASON_BIGBANG)
+				elseif send==5 then
+					Duel.SendtoDeck(g,nil,2,REASON_MATERIAL+REASON_BIGBANG)
+				elseif send==6 then
+					Duel.Destroy(g,REASON_MATERIAL+REASON_BIGBANG)
+				else
+					Duel.SendtoGrave(g,REASON_MATERIAL+REASON_BIGBANG)
+				end
+				g:DeleteGroup()
+				aux.DeleteExtraMaterialGroups(emt)
+		--e:GetHandler():SetTurnCounter(e:GetHandler():GetOriginalLevel())
+			end
 end
