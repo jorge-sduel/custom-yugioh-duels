69032e706ded89390eea9e33481014b9ced07400
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-01-24 10:53:01 -0600

    Update c52401238.lua

--- script/c52401238.lua
+++ script/c52401238.lua
@@ -6,7 +6,7 @@ function cid.initial_effect(c)
 c:AddSetcodesRule(id,false,0xbb109)
 	c:EnableReviveLimit()
 	--.AddProcedure(c,cid.excon2,LOCATION_MZONE)
-	Bigbang2.AddProcedure(c,cid.excon2,2,99)
+	Auxiliary.AddBigbangProcedure(c,cid.excon2,2,99)
 	--local e1=Effect.CreateEffect(c)
 	--e1:SetType(EFFECT_TYPE_SINGLE)
 	--e1:SetCode(52401238)
@@ -19,5 +19,5 @@ function cid.excon(e)
 	return Duel.GetLP(e:GetHandlerPlayer())<=6000
 end
 function cid.excon2(e,c,tp)
-	return c:IsAttackAbove(Duel.GetLP(tp)-e:GetAttack())
+	return c:IsAttackAbove(Duel.GetLP(tp)-e:GetAttack()) and c:IsCode(52401237) 
 end
