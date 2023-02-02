16354af715c2a67fb3dbc1abff630666a0e7b070
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-02-01 20:16:20 -0600

    Update c18917010.lua

--- script/c18917010.lua
+++ script/c18917010.lua
@@ -1,6 +1,11 @@
 --Lyrica (WIND)
 local ref=_G['c'..18917010]
+ref.Is_Runic=true
+if not RUNIC_IMPORTED then Duel.LoadScript("proc_runic.lua") end
 function ref.initial_effect(c)
+	--Rune Summon
+	c:EnableReviveLimit()
+	Runic.AddProcedure(c,aux.FilterBoolFunction(Card.IsRace,RACE_SPELLCASTER),ref.matfilter,1,1)
 	--Cannot Target
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOHAND)
@@ -21,31 +26,15 @@ function ref.initial_effect(c)
 	e2:SetTarget(ref.thtg)
 	e2:SetOperation(ref.thop)
 	c:RegisterEffect(e2)
-	
-	if not ref.global_check then
-		ref.global_check=true
-		local ge2=Effect.CreateEffect(c)
-		ge2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-		ge2:SetCode(EVENT_ADJUST)
-		ge2:SetCountLimit(1)
-		ge2:SetProperty(EFFECT_FLAG_NO_TURN_RESET)
-		ge2:SetOperation(ref.chk)
-		Duel.RegisterEffect(ge2,0)
-	end
 end
-ref.transform=true
-ref.material1=function(mc) return mc:IsRace(RACE_SPELLCASTER) and mc:GetLevel()==3 end
-ref.material2=function(mc2) return mc2:IsAttribute(ATTRIBUTE_WIND) end
-function ref.chk(e,tp,eg,ep,ev,re,r,rp)
-	Duel.CreateToken(tp,666)
-	Duel.CreateToken(1-tp,666)
+function ref.matfilter(c)
+	return c:IsType(TYPE_TRAP) or c:IsType(TYPE_SPELL)
 end
-
 function ref.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and chkc:IsCode(18917005) end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsCode,tp,LOCATION_GRAVE,0,1,nil,18917005) end
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and chkc:IsRace(RACE_SPELLCASTER) end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsRace,tp,LOCATION_GRAVE,0,1,nil,RACE_SPELLCASTER) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectTarget(tp,Card.IsCode,tp,LOCATION_GRAVE,0,1,1,nil,18917005)
+	local g=Duel.SelectTarget(tp,Card.IsRace,tp,LOCATION_GRAVE,0,1,1,nil,RACE_SPELLCASTER)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
 end
 function ref.thop(e,tp,eg,ep,ev,re,r,rp)
