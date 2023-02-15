7ed82fd252af8c38b551e262b6f5e1f714e44163
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-02-15 17:03:43 -0600

    Add files via upload

---
+++ script/c492.lua
@@ -0,0 +1,38 @@
+--Red Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro summon
+	c:EnableReviveLimit()
+	Synchro.AddProcedure(c,nil,5,5,aux.FilterBoolFunction(Card.IsCode,70902743),1,1)
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(s.atkval)
+	c:RegisterEffect(e2)
+	--indestructable
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetValue(s.indval)
+	c:RegisterEffect(e3)
+	--double tuner
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e6:SetCode(21142671)
+	c:RegisterEffect(e6)
+end
+s.material={70902743}
+s.listed_names={70902743}
+s.synchro_nt_required=1
+function s.atkval(e,c)
+	return Duel.GetMatchingGroupCount(Card.IsType,c:GetControler(),LOCATION_GRAVE,0,nil,TYPE_TUNER)*1000
+end
+function s.indval(e,re,tp)
+	return e:GetHandler():GetControler()~=tp
+end
