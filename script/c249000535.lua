14e4a2db59cae7755d6afed4cfe40dfcc3589353
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-02-20 09:42:07 -0600

    Update c249000535.lua

--- script/c249000535.lua
+++ script/c249000535.lua
@@ -40,9 +40,9 @@ function c249000535.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	--if g1:GetFirst():IsLocation(LOCATION_HAND) then 
 	--	g2=Duel.SelectMatchingCard(tp,c249000535.costfilter,tp,LOCATION_GRAVE,0,1,1,g1:GetFirst(),1)
 	--else
-	--	g2=Duel.SelectMatchingCard(tp,c249000535.costfilter,tp,LOCATION_HAND,0,1,1,g1:GetFirst(),1)
+		g2=Duel.SelectMatchingCard(tp,c249000535.costfilter,tp,LOCATION_HAND,0,1,1,g1:GetFirst(),1)
 	--end
-	--g1:Merge(g2)
+	g1:Merge(g2)
 	Duel.Remove(g1,POS_FACEUP,REASON_COST)
 	local lv=g1:GetSum(Card.GetLevel)
 	e:SetLabel(lv)
