99b84cc0ac69252e8bfe3743f23aea75a56b6fdd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-02-24 16:48:20 -0600

    Update c210216007.lua

--- script/c210216007.lua
+++ script/c210216007.lua
@@ -13,10 +13,8 @@ function c210216007.initial_effect(c)
 end
 function c210216007.filter1(c,e,tp)
 	local rk=c:GetRank()
-	return c:IsFaceup() and c:IsType(TYPE_XYZ) and c:IsSetCard(0x216)
-		and Duel.IsExistingMatchingCard(c210216007.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,rk+1)
+	return c:IsFaceup() and c:IsType(TYPE_XYZ) and Duel.IsExistingMatchingCard(c210216007.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c,rk+1)
 		and Duel.GetLocationCountFromEx(tp,tp,c)>0
-		and aux.MustMaterialCheck(c,tp,EFFECT_MUST_BE_XMATERIAL)
 end
 function c210216007.filter2(c,e,tp,mc,rk)
 	return c:IsRank(rk) and c:IsSetCard(0x216) and mc:IsCanBeXyzMaterial(c)
@@ -32,7 +30,7 @@ function c210216007.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 end
 function c210216007.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if Duel.GetLocationCountFromEx(tp,tp,tc)<=0 or not aux.MustMaterialCheck(tc,tp,EFFECT_MUST_BE_XMATERIAL) then return end
+	if Duel.GetLocationCountFromEx(tp,tp,tc)<=0 then return end
 	if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsControler(1-tp) or tc:IsImmuneToEffect(e) then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,c210216007.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetRank()+1)
