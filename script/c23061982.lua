32e45b6f95044bd22c37b43340b8ba6fe5e4c667
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-03-17 11:00:39 -0600

    Update c23061982.lua

--- script/c23061982.lua
+++ script/c23061982.lua
@@ -7,7 +7,7 @@ function c23061982.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_CHANGE_CODE)
-	e1:SetCondition(aux.IsDualState)
+	e1:SetCondition(Gemini.EffectStatusCondition)
 	e1:SetValue(74677422)
 	c:RegisterEffect(e1)
 end
