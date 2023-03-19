59c1e4b34ce75f1e0cdde2080880fbe2365767f4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-03-19 09:51:44 -0600

    Update c23061993.lua

--- script/c23061993.lua
+++ script/c23061993.lua
@@ -3,24 +3,13 @@ function c23061993.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	Fusion.AddProcCode2(c,5405694,23061998,true,true)
-	--spsummon condition
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(c23061993.splimit)
+local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetValue(c23061993.atkval)
 	c:RegisterEffect(e1)
-	--atk
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetValue(c23061993.atkval)
-	c:RegisterEffect(e2)
-end
-function c23061993.splimit(e,se,sp,st)
-	return bit.band(st,SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
 end
 function c23061993.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_DRAGON)
