ed64ecb43c143e94117d620187d6c3dc1c23dfe1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-03-20 13:34:03 -0600

    Update c66.lua

--- script/c66.lua
+++ script/c66.lua
@@ -1,6 +1,6 @@
 --Red Chaos Unleashed
 function c66.initial_effect(c)
-	aux.AddRitualProcGreater(c,c66.ritual_filter)
+	Ritual.AddProcGreater(c,c66.ritual_filter)
 end
 function c66.ritual_filter(c)
 	return c:IsCode(19025379,70) or c:IsType(TYPE_RITUAL) and c:IsSetCard(0x3b)
