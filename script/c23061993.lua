cd193a1204cd13006bbe35efb53fcf2d6d3b7d58
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-03-20 09:09:26 -0600

    Update c23061993.lua

--- script/c23061993.lua
+++ script/c23061993.lua
@@ -10,10 +10,31 @@ local e1=Effect.CreateEffect(c)
 	e1:SetTargetRange(0,LOCATION_MZONE)
 	e1:SetValue(c23061993.atkval)
 	c:RegisterEffect(e1)
+	--atk down
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+	e2:SetCondition(c23061993.atkcon)
+	e2:SetOperation(c23061993.atkop)
+	c:RegisterEffect(e2)
 end
 function c23061993.filter(c)
 	return c:IsFaceup() and c:IsRace(RACE_DRAGON)
 end
 function c23061993.atkval(e,c)
 	return Duel.GetMatchingGroupCount(c23061993.filter,e:GetHandler():GetControler(),LOCATION_MZONE+LOCATION_GRAVE,0,nil)*-500
+end
+function c23061993.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local a=Duel.GetAttacker()
+	local d=Duel.GetAttackTarget()
+	return a==c and d and d:IsFaceup() and d:GetAttack()>e:GetHandler():GetAttack()
+end
+function c23061993.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+	e1:SetValue(Duel.GetAttackTarget():GetAttack())
+	e1:SetReset(RESET_PHASE+PHASE_DAMAGE)
+	e:GetHandler():RegisterEffect(e1)
 end
