ed0f7da1d1eb6c48a4250ae9fa182ec7d540948f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-03-22 09:02:38 -0600

    Add files via upload

--- script/c902763443.lua
+++ script/c902763443.lua
@@ -1,4 +1,4 @@
---Pandemoniumgraph of Supremacy
+--Pandemoniumgraph of Supermacy
 local function getID()
 	local str=string.match(debug.getinfo(2,'S')['source'],"c%d+%.lua")
 	str=string.sub(str,1,string.len(str)-4)
