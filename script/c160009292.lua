b77108deb54e8a3f14c8a0c8e2ba8b7c2b100d6f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-03-22 10:23:14 -0600

    Update c160009292.lua

--- script/c160009292.lua
+++ script/c160009292.lua
@@ -1,11 +1,10 @@
 function c160009292.initial_effect(c)
-    aux.AddOrigPandemoniumType(c)
-	
+    Pendulum.AddProcedure()	
 	  --Atk up
     local e2=Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetRange(LOCATION_SZONE)
-    e2:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+    e2:SetRange(LOCATION_PZONE+LOCATION_MZONE)
+    e2:SetTargetRange(LOCATION_MZONE,0)
     e2:SetCode(EFFECT_UPDATE_ATTACK)
    -- e2:SetTarget(aux.TargetBoolFunction(Card.IsAttribute,ATTRIBUTE_DARK))
     e2:SetValue(500)
