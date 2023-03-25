746ec98f14c15fe5be4b23382a62838212d2e9ab
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-03-24 20:24:12 -0600

    Update c307.lua

--- script/c307.lua
+++ script/c307.lua
@@ -4,7 +4,7 @@ if not TRAMPULA_IMPORTED then Duel.LoadScript("proc_trampula.lua") end
 function s.initial_effect(c)
 	--pendulum summon
 	Trampula.AddProcedure(c)
-aux.AddVTrampulaSkillProcedure(c,nil,s.flipop)
+aux.AddVTrampulaSkillProcedure(c,s.flipcon,s.flipop)
 	--synchro effect 
 	local e0=Effect.CreateEffect(c)
 	e0:SetDescription(aux.Stringid(id,0))
