d21473104ce0017a5236e977099adb214afe088e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-04-29 10:50:05 -0600

    Update c99998880.lua

--- script/c99998880.lua
+++ script/c99998880.lua
@@ -1,7 +1,7 @@
 --created & coded by Walrus
 local cid,id=GetID()
 function cid.initial_effect(c)
-	aux.CannotBeEDMaterial(c,nil,LOCATION_MZONE)
+	--aux.CannotBeEDMaterial(c,nil,LOCATION_MZONE)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
