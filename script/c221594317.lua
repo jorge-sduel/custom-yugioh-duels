4fd8de93d16c525877e3f4b5719d5ca3d250d022
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-04 10:48:07 -0600

    Update c221594317.lua

--- script/c221594317.lua
+++ script/c221594317.lua
@@ -29,13 +29,11 @@ function s.initial_effect(c)
 	e6:SetRange(LOCATION_SZONE)
 	e6:SetCountLimit(1)
 	e6:SetCategory(CATEGORY_RECOVER+CATEGORY_SEARCH+CATEGORY_TOHAND)
-	e6:SetCondition(aux.PandActCheck)
 	e6:SetCost(cid.cost)
 	e6:SetTarget(cid.target)
 	e6:SetOperation(cid.operation)
 	c:RegisterEffect(e6)
-	aux.EnablePandemoniumAttribute(c,e6)
-	aux.CannotBeEDMaterial(c,nil,LOCATION_MZONE)
+	--aux.CannotBeEDMaterial(c,nil,LOCATION_MZONE)
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_SINGLE)
 	e4:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
@@ -83,7 +81,7 @@ function cid.cfilter(c)
 end
 function cid.splimit(e,c,sump,sumtype,sumpos,targetp)
 	if c:IsSetCard(0xc97) then return false end
-	return bit.band(sumtype,SUMMON_TYPE_PANDEMONIUM)==SUMMON_TYPE_PANDEMONIUM
+	return bit.band(sumtype,SUMMON_TYPE_EQUILIBRIUM)==SUMMON_TYPE_EQUILIBRIUM
 end
 function cid.filter(c,sp)
 	return c:GetSummonPlayer()==sp and c:GetSummonLocation()==LOCATION_EXTRA
@@ -159,11 +157,8 @@ function cid.op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if c:IsForbidden() then return end
 	local b=Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and aux.PandSSetCon(c,nil,c:GetLocation(),c:GetLocation())(nil,e,tp,eg,ep,ev,re,r,rp)
 	if b and Duel.SelectOption(tp,1159,1105)==0 then
-		aux.PandSSet(c,REASON_EFFECT)(e,tp,eg,ep,ev,re,r,rp)
 	else
 		Duel.SelectOption(tp,1105)
-		aux.PandEnableFUInED(c,REASON_EFFECT)(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
