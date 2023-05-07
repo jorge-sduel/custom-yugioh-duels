e82b18cb0a49ca0749b61df608416c8d979c5c65
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-07 12:49:39 -0600

    Update c52401238.lua

--- script/c52401238.lua
+++ script/c52401238.lua
@@ -3,18 +3,21 @@ local cid,id=GetID()
 cid.IsSpacet=true
 if not TIMESPACE_IMPORTED then Duel.LoadScript("proc_timespace.lua") end
 function cid.initial_effect(c)
-	aux.AddConvergentTSSummonProcedure(c,cid.lcheck,1,1)
+	aux.AddConvergentTSSummonProcedure(c,cid.lcheck,1,1,cid.lcheck2)
 c:AddSetcodesRule(id,false,0xbb109)
 	c:EnableReviveLimit()
 	--.AddProcedure(c,cid.excon2,LOCATION_MZONE)
-	Timespace.AddProcedure(c,cid.excon2,1,1)
+	--Timespace.AddProcedure(c,cid.excon2,1,1)
 	--local e1=Effect.CreateEffect(c)
 	--e1:SetType(EFFECT_TYPE_SINGLE)
 	--e1:SetCode(52401238)
 	--c:RegisterEffect(e1)
 end
 function cid.lcheck(c)
-	return c:IsCode(52401237) or c.IsBigbang
+	return (c:IsCode(52401237) or c.IsBigbang)
+end
+function cid.lcheck2(g,lc,sumtype,tp)
+	return g:IsExists(Card.IsAttackAbove,1,nil,Duel.GetLP(tp))
 end
 function cid.excon(e)
 	return Duel.GetLP(e:GetHandlerPlayer())<=6000
