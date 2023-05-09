d50a38429b2715ce77f3f7861a2be92b9b5149ee
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-08 21:32:17 -0600

    Actualizar c442594300.lua

--- script/c442594300.lua
+++ script/c442594300.lua
@@ -3,7 +3,7 @@ local cid,id=GetID()
 function cid.initial_effect(c)
     c:SetUniqueOnField(1,0,id)
 	c:EnableReviveLimit()
-	
+	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0xc97),2)
     --Enviar un GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOGRAVE)
