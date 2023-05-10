bd36ff77b4a25a90828eb2e5bd4ae707bf2559c8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-10 10:58:09 -0600

    Actualizar c238.lua

--- script/c238.lua
+++ script/c238.lua
@@ -77,7 +77,7 @@ function c238.activate(e,tp,eg,ep,ev,re,r,rp)
  --[[and ct>0 and ct>=Duel.GetLocationCount(tp,LOCATION_MZONE) and mg:FilterCount(aux.NecroValleyFilter(c238.mgfilter),nil,e,tp,tc,mg)<=ct
 		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
 		and]]
-Duel.SelectYesNo(tp,aux.Stringid(238,0)) then
+Duel.SelectYesNo(tp,aux.Stringid(238,0)) - --then
 		Duel.BreakEffect()
 		Duel.SpecialSummonStep(mg,0,tc:GetControler(),tc:GetControler(),true,false,POS_FACEUP)
 if not mg:IsMonster() then
