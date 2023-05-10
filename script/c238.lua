2fffd1160b5ffbc48eb3a5239ff16262b09287b8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-10 10:44:54 -0600

    Actualizar c238.lua

--- script/c238.lua
+++ script/c238.lua
@@ -75,10 +75,22 @@ function c238.activate(e,tp,eg,ep,ev,re,r,rp)
 	--local ct=#mg
 --	local sumtype=tc:GetSummonType()
 	if Duel.SendtoDeck(tc,nil,0,REASON_EFFECT)~=0
- --[[and ct>0 and ct>=Duel.GetLocationCount(tp,LOCATION_MZONE) and mg:FilterCount(aux.NecroValleyFilter(c238.mgfilter),nil,e,tp,tc,mg)<=ct]]
-		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+ --[[and ct>0 and ct>=Duel.GetLocationCount(tp,LOCATION_MZONE) and mg:FilterCount(aux.NecroValleyFilter(c238.mgfilter),nil,e,tp,tc,mg)<=ct
+		and not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)]]
 		and Duel.SelectYesNo(tp,aux.Stringid(238,0)) then
 		Duel.BreakEffect()
-		Duel.SpecialSummon(mg,0,tc:GetControler(),tc:GetControler(),true,false,POS_FACEUP)
+		Duel.SpecialSummonStep(mg,0,tc:GetControler(),tc:GetControler(),true,false,POS_FACEUP)
+Duel.SpecialSummonComplete()
+if not tc:IsMonster() then
+	--local tc=e:GetHandler():GetMaterial()
+	local e3=Effect.CreateEffect(c)
+	e3:SetCode(EFFECT_ADD_TYPE)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+	e3:SetValue(TYPE_MONSTER)
+	tc:RegisterEffect(e3)
+			Duel.SpecialSummonComplete()
+  end
 	end
 end
