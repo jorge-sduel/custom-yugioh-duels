30780c8876a87e7463635c442a3ac7695cec4ee0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-10 09:47:57 -0600

    Actualizar c442594300.lua

--- script/c442594300.lua
+++ script/c442594300.lua
@@ -39,6 +39,10 @@ function cid.initial_effect(c)
 	e4:SetValue(cid.splimit)
 	c:RegisterEffect(e4)
 end
+cid.listed_names={221594307}
+cid.listed_series={0xc97}
+cid.material_setcode={0xc97}
+
 function cid.matfilter(c,fc,sumtype,tp)
 	return c:IsCode(221594307,fc,sumtype,tp)
 end
