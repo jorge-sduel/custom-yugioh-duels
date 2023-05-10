60914e7c2c4ccacfc0ac6041ceb6a7b059221d88
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-10 12:19:22 -0600

    Actualizar c999913.lua

--- script/c999913.lua
+++ script/c999913.lua
@@ -28,7 +28,7 @@ end
 function c999913.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) and chkc:IsAbleToDeck() end
 	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTODECK)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
 	local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,0,LOCATION_ONFIELD,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
 end
