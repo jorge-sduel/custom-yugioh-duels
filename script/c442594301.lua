80c6ed9012d2046d4468a0a45cbb26f5a300e961
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-13 12:15:45 -0600

    Actualizar c442594301.lua

--- script/c442594301.lua
+++ script/c442594301.lua
@@ -3,7 +3,7 @@ local cid,id=GetID()
 function cid.initial_effect(c)
     c:SetUniqueOnField(1,0,id)
 	c:EnableReviveLimit()
-	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xc97),1,1,Synchro.NonTuner(aux.FilterBoolFunctionEx(Card.IsCode,221594308),1,1)
+	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xc97),1,1,Synchro.NonTuner(aux.FilterBoolFunctionEx(Card.IsCode,221594308)),1,1)
     --Send to GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOGRAVE)
