dc226b2e9623ba900858cbb816e30c5b969016cc
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-16 09:43:49 -0600

    Actualizar c442594303.lua

--- script/c442594303.lua
+++ script/c442594303.lua
@@ -3,7 +3,7 @@ local cid,id=GetID()
 function cid.initial_effect(c)
     c:SetUniqueOnField(1,0,id)
 	c:EnableReviveLimit()
-	aux.AddXyzProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xc97),4,2)
+	Xyz.AddProcedure(c,nil,10,2, aux.FilterBoolFunctionEx(Card.IsCode,221594309))
     --Send to GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOGRAVE)
@@ -33,6 +33,21 @@ function cid.initial_effect(c)
 	e3:SetTarget(cid.sptg)
 	e3:SetOperation(cid.spop)
 	c:RegisterEffect(e3)
+ --splimit 
+ local e4=Effect.CreateEffect(c) 
+ e4:SetType(EFFECT_TYPE_SINGLE)  e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE) 
+ e4:SetCode(EFFECT_SPSUMMON_CONDITION) 
+ e4:SetValue(cid.splimit) 
+ c:RegisterEffect(e4) 
+end 
+cid.listed_names={221594309} 
+cid.listed_series={0xc97} 
+cid.material_setcode={0xc97} 
+function cid.splimit(e,se,sp,st)
+	return Duel.IsExistingMatchingCard(cid.ffilter2,e:GetHandlerPlayer(),LOCATION_GRAVE,LOCATION_GRAVE,3,nil) 
+end 
+function cid.ffilter2(c) 
+	return c:IsType(TYPE_XYZ) 
 end
 function cid.tgfilter(c)
 	return c:IsType(TYPE_XYZ) and c:IsAbleToGrave()
