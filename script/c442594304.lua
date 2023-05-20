dcaff36bc8793b7e6872bc53f8a0e7da321b42ad
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-20 09:50:57 -0600

    Actualizar c442594304.lua

--- script/c442594304.lua
+++ script/c442594304.lua
@@ -3,7 +3,7 @@ local cid,id=GetID()
 function cid.initial_effect(c)
     c:SetUniqueOnField(1,0,id)
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xc97),2,cid,lcheck)
+	Link.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0xc97),2,99,cid,lcheck)
     --Send to GY
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_TOGRAVE)
