b1b93bffcf277bb7c9559667b1f64db2fd9f0931
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-20 10:31:26 -0600

    Actualizar c221594306.lua

--- script/c221594306.lua
+++ script/c221594306.lua
@@ -2,7 +2,7 @@
 local cid,id=GetID()
 function cid.initial_effect(c)
 	c:EnableReviveLimit()
-	Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xc97),2)
+	Link.AddProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xc97),2,99)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetRange(LOCATION_MZONE)
