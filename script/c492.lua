b0d634f728fcf9546f31137169da05f156949ef4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-27 20:43:54 -0600

    Actualizar c492.lua

--- script/c492.lua
+++ script/c492.lua
@@ -3,8 +3,8 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--synchro summon
 	c:EnableReviveLimit()
-	--Synchro.AddProcedure(c,nil,5,5,aux.FilterBoolFunction(Card.IsType,TYPE_SYNCHRO),1,1)
-	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,1)
+	Synchro.AddProcedure(c,nil,5,5,aux.FilterBoolFunction(Card.IsType,TYPE_SYNCHRO),1,1)
+--	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,1)
 	--pierce
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
