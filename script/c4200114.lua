9dd7b4f7f90f9f69a05162dab46ff36f9651288b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-29 08:44:20 -0600

    Actualizar c4200114.lua

--- script/c4200114.lua
+++ script/c4200114.lua
@@ -79,7 +79,7 @@ function cid.ope(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SendtoDeck(c,nil,-2,REASON_RULE)
 	Duel.SendtoDeck(c,nil,0,REASON_RULE)
 	Duel.SendtoDeck(c,nil,-2,REASON_RULE)
-		Duel.MoveToField(token,tp,tp,LOCATION_MZONE,POS_FACEUP,true,seq)
+		Duel.MoveToField(token,tp,tp,LOCATION_MZONE,POS_FACEUP,true,c:GetPreviousSequence())
 		--tg:AddCard(token)
 			--token:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1)
 		--token type
