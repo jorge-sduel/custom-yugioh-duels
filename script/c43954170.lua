0a9b15b4fe3d9237e77bd047545de656d0ec9dd7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-05-29 16:01:00 -0600

    Actualizar c43954170.lua

--- script/c43954170.lua
+++ script/c43954170.lua
@@ -1,16 +1,9 @@
 --Felgrandrise Waterfall
 --Scripted by: XGlitchy30
-local function getID()
-	local str=string.match(debug.getinfo(2,'S')['source'],"c%d+%.lua")
-	str=string.sub(str,1,string.len(str)-4)
-	local cod=_G[str]
-	local id=tonumber(string.sub(str,2))
-	return id,cod
-end
-local id,cid=getID()
+local cid,id=GetID()
 function cid.initial_effect(c)
 	--synchro summon
-	aux.AddSynchroProcedure(c,nil,aux.NonTuner(cid.matfilter),1)
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(cid.matfilter),1,99)
 	c:EnableReviveLimit()
 	--special summon
 	local e1=Effect.CreateEffect(c)
