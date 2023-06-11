a7216739a7b574f21b08893a4ac8d7cf9eb6be28
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-06-10 21:54:17 -0600

    Actualizar c802807372.lua

--- script/c802807372.lua
+++ script/c802807372.lua
@@ -27,24 +27,23 @@ function c802807372.filter2(c,tcode,e,tp)
 	return c:IsSetCard(0x104f) and c.assault_mode and c.assault_mode==tcode and c:IsCanBeSpecialSummoned(e,0,tp,true,false)
 end
 function c802807372.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if chk==0 then
 		if e:GetLabel()~=1 then return false end
 		e:SetLabel(0)
-		return Duel.GetLocationCount(tp,LOCATION_MZONE)>-1
-			and Duel.CheckReleaseGroup(tp,c802807372.filter1,1,nil,e,tp)
+		return ft>-1 and Duel.CheckReleaseGroupCost(tp,c802807372.filter1,1,false,nil,nil,e,tp,ft)
 	end
-	local rg=Duel.SelectReleaseGroup(tp,c802807372.filter1,1,1,nil,e,tp)
-	e:SetLabel(rg:GetFirst():GetCode())
+	local rg=Duel.SelectReleaseGroupCost(tp,c802807372.filter1,1,1,false,nil,nil,e,tp,ft)
+	Duel.SetTargetParam(rg:GetFirst():GetCode())
 	Duel.Release(rg,REASON_COST)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function c802807372.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local code=e:GetLabel()
-	local tcode=c802807372.list[code]
-	local tc=Duel.SelectMatchingCard(tp,c802807372.filter2,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,tcode,e,tp):GetFirst()
+	local code=Duel.GetChainInfo(0,CHAININFO_TARGET_PARAM)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local tc=Duel.SelectMatchingCard(tp,c802807372.filter2,tp,LOCATION_DECK,0,1,1,nil,code,e,tp):GetFirst()
 	if tc and Duel.SpecialSummon(tc,0,tp,tp,true,false,POS_FACEUP_ATTACK)>0 then
 		tc:CompleteProcedure()
 	end
-end
 end
