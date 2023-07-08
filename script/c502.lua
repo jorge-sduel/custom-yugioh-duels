cb89821b18afb862c5307504d60b3257b39e1102
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-07-08 13:47:52 -0600

    Add files via upload

---
+++ script/c502.lua
@@ -0,0 +1,110 @@
+--E－HERO
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion material
+	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x8),5)
+	--lizard check
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(CARD_CLOCK_LIZARD)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCondition(s.lizcon)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+	--Special Summon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.EvilHeroLimit)
+	c:RegisterEffect(e1)
+	--summon success
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_MATERIAL_CHECK)
+	e3:SetValue(s.matcheck)
+	c:RegisterEffect(e3)
+end
+s.dark_calling=true
+s.listed_names={CARD_DARK_FUSION}
+function s.lizcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),EFFECT_SUPREME_CASTLE)
+end
+function s.ffilter(c,fc)
+	return c:IsSetCard(0x3008)
+end
+function s.matcheck(e,c)
+	local ct=c:GetMaterial():GetClassCount(Card.GetCode)
+	if ct>0 then
+			--chain material
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CHAIN_MATERIAL)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetRange(LOCATION_FZONE)
+	e4:SetTargetRange(1,0)
+	e4:SetCondition(s.chcon)
+	e4:SetTarget(s.chtg)
+	e4:SetOperation(s.chop)
+	e4:SetValue(aux.FilterBoolFunction(Card.IsType,TYPE_FUSION))
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetOperation(s.chk)
+	e4:SetLabelObject(e5)
+	end
+	if ct>=1 then
+		--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	end
+end
+function s.filter(c,e,tp)
+	return c:IsType(TYPE_FUSION) and c:IsSetCard(0x3008) and Duel.GetLocationCountFromEx(tp,tp,nil,c)>0 and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP_ATTACK)
+	end
+end
+function s.chcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.CheckLPCost(tp,1000)
+end
+function s.chfilter(c,e,tp)
+	return c:IsMonster() and (c:IsFaceup() or c:IsControler(tp)) and c:IsCanBeFusionMaterial() and not c:IsImmuneToEffect(e)
+end
+function s.chtg(e,te,tp,value)
+	if value&SUMMON_TYPE_FUSION==0 then return Group.CreateGroup() end
+	return Duel.GetMatchingGroup(s.chfilter,tp,LOCATION_MZONE+LOCATION_HAND,LOCATION_MZONE,nil,te,tp)
+end
+function s.chop(e,te,tp,tc,mat,sumtype,sg,sumpos)
+	if not sumtype then sumtype=SUMMON_TYPE_FUSION end
+	tc:SetMaterial(mat)
+	Duel.SendtoGrave(mat,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+	if mat:IsExists(Card.IsControler,1,nil,1-tp) then
+		Duel.PayLPCost(tp,2000)
+	end
+	Duel.BreakEffect()
+	if sg then
+		sg:AddCard(tc)
+	else
+		Duel.SpecialSummon(tc,sumtype,tp,tp,false,false,sumpos)
+	end
+end
+function s.chk(tp,sg,fc)
+	return sg:FilterCount(Card.IsControler,nil,1-tp)<=1
+end
