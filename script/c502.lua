57b60e92459d6bb58dcfdb8e420b8d0a2fe3c144
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-07-08 16:23:36 -0600

    Update c502.lua

--- script/c502.lua
+++ script/c502.lua
@@ -120,9 +120,9 @@ end
 function s.chain_target(e,te,tp,value)
 	if not value or value&SUMMON_TYPE_FUSION==0 then return Group.CreateGroup() end
 	if Duel.IsPlayerAffectedByEffect(tp,69832741) then
-		return Duel.GetMatchingGroup(s.filter1,tp,LOCATION_MZONE+LOCATION_HAND+LOCATION_DECK,0,nil,te)
+		return Duel.GetMatchingGroup(s.filter1,tp,0,LOCATION_ONFIELD,nil,te)
 	else
-		return Duel.GetMatchingGroup(s.filter1,tp,LOCATION_MZONE+LOCATION_GRAVE+LOCATION_HAND+LOCATION_DECK,0,nil,te)
+		return Duel.GetMatchingGroup(s.filter1,tp,0,LOCATION_ONFIELD,nil,te)
 	end
 end
 function s.chain_operation(e,te,tp,tc,mat,sumtype,sg,sumpos)
