734835142a7ff6b2043ce7012fdb00359c558fba
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-07-17 16:27:34 -0600

    Add files via upload

---
+++ script/c503.lua
@@ -0,0 +1,105 @@
+--超次元ロボ ギャラクシー・デストロイヤー
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+	--Unaffected by other cards' effects
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetCondition(s.imcon)
+	e2:SetOperation(s.imop)
+	c:RegisterEffect(e2)
+	--Unaffected by other cards' effects
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(3100)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
+	e3:SetCode(EFFECT_IMMUNE_EFFECT)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(s.efilter)
+	c:RegisterEffect(e3)
+--atk
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(1447,1))
+	e6:SetCategory(CATEGORY_DAMAGE)
+	e6:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e6:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e6:SetTarget(s.damtg1)
+	e6:SetOperation(s.damop1)
+	c:RegisterEffect(e6)
+	--atk
+	local e7=Effect.CreateEffect(c)
+	e7:SetDescription(aux.Stringid(1447,1))
+	--e7:SetCategory(CATEGORY_DAMAGE)
+	e7:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e7:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e7:SetCondition(s.damcon)
+	e7:SetTarget(s.damtg)
+	e7:SetOperation(s.damop)
+	c:RegisterEffect(e7)
+end
+function s.pmfilter(c,sc)
+	return c:IsCode(93717133)
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsSummonType(SUMMON_TYPE_SYNCHRO) and c:GetMaterial():IsExists(s.pmfilter,1,nil,c)
+end
+function s.filter(c)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP+TYPE_MONSTER)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_ONFIELD,1,nil) end
+	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_ONFIELD,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetChainLimit(s.climit)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_ONFIELD,nil)
+	Duel.Destroy(g,REASON_EFFECT)
+end
+function s.climit(e,lp,tp)
+	return lp==tp or not e:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.imcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+end
+function s.imop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	--Unaffected by other cards' effects
+		local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END+RESET_SELF_TURN,Duel.IsTurnPlayer(tp) and 2 or 1)
+	c:RegisterEffect(e1)
+	--Cannot be destroyed by battle
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	c:RegisterEffect(e2)
+end
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(1-tp)>1 end
+	Duel.SetTargetPlayer(1-tp)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.SetLP(p,1)
+end
+function s.damtg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(1-tp)>2000 end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetTargetParam(5000)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,5000)
+	Duel.SetChainLimit(s.climit)
+end
+function s.damop1(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Damage(e:GetHandler():GetAttack(),1-tp,REASON_EFFECT)
+end
