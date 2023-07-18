fb7aa980a32d9122c601a5a2b604e6eef9a6c8b4
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-07-18 13:59:03 -0600

    Update c350.lua

--- script/c350.lua
+++ script/c350.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-	e1:SetCondition(s.eqcon)
+	e1:SetCondition(aux.NOT(s.eqcon))
 	e1:SetValue(1)
 	c:RegisterEffect(e1)
 	-- Cannot be destroyed by battle
@@ -40,7 +40,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4,false,REGISTER_FLAG_DETACH_XMAT)
 end
 function s.eqcon(e)
-	return e:GetHandler():GetEquipCount()<0
+	return e:GetHandler():GetEquipCount()>0
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
 	return ep==tp
