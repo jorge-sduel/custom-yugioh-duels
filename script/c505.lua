80cbe4e95a5c6f2c09a798e15a5e07abe4e2da6e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-07-25 21:31:28 -0600

    Add files via upload

---
+++ script/c505.lua
@@ -0,0 +1,22 @@
+--影依融合
+--Shaddol Fusion
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsType,TYPE_FUSION),nil,s.fextra,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,s.extratg)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x9d}
+function s.fextra(e,tp,mg)
+	local g=Duel.GetMatchingGroup(s.spfilter,c:GetControler(),LOCATION_ONFIELD+LOCATION_GRAVE,0,nil)
+	local ct=g:GetClassCount(Card.GetCode)
+	if ct>6 then
+		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil)
+	end
+	return nil
+end
+function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,0,tp,LOCATION_DECK)
+end
