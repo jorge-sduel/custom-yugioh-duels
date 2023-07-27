8f4fb57a52c55624eb17fc13ce21ab1c70368abe
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-07-27 06:49:23 -0600

    Update c507.lua

--- script/c507.lua
+++ script/c507.lua
@@ -1,12 +1,14 @@
 --t.g asambled golem
 local s,id=GetID()
 function s.initial_effect(c)
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit() 
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
-	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
