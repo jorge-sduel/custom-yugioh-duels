89772406730041ee910ad06cdd096b307919f8e1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-08-03 17:13:49 -0600

    Update c509.lua

--- script/c509.lua
+++ script/c509.lua
@@ -5,7 +5,7 @@ function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_TO_GRAVE)
 	e1:SetCondition(s.spcon)
 	e1:SetTarget(s.sptg)
@@ -13,7 +13,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return (e:GetHandler():GetPreviousLocation()==LOCATION_HAND or e:GetHandler():GetPreviousLocation()==LOCATION_DECK) and (r&0x4040)==0x4040
+	return e:GetHandler():GetPreviousLocation()==LOCATION_HAND or e:GetHandler():GetPreviousLocation()==LOCATION_DECK
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
