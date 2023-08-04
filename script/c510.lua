21a3ef41d223c0f29539b7d636ca29a7b9c6b1b7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-08-04 09:42:01 -0600

    Add files via upload

---
+++ script/c510.lua
@@ -0,0 +1,36 @@
+--サイレント・ウォビー
+--Silent Wooby
+local s,id=GetID()
+function s.initial_effect(c)
+	--Special Sumon itself to the opponent's field
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsControlerCanBeChanged()
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+		and e:GetHandler():IsCanBeSpecialSummoned(e,1,tp,false,false,POS_FACEUP,1-tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,0,LOCATION_MZONE,1,1,nil)
+	local tc=g:GetFirst()
+	if tc and c:IsRelateToEffect(e) then
+		Duel.HintSelection(g)
+			Duel.GetControl(tc,tp)
+		Duel.SpecialSummon(c,1,tp,1-tp,false,false,POS_FACEUP)
+	end
+end
