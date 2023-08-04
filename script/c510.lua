ea65c54a19267a3f6b7c65dd954301c1764efb70
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-08-04 09:55:35 -0600

    Update c510.lua

--- script/c510.lua
+++ script/c510.lua
@@ -20,7 +20,6 @@ function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_CONTROL,g,1,0,0)
- end
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
