d2d72734fb09558f89c1ce26d2566c3276eccbcf
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-08-05 22:02:19 -0600

    Update c516.lua

--- script/c516.lua
+++ script/c516.lua
@@ -23,12 +23,12 @@ function s.initial_effect(c)
 end
 s.listed_series={0x52}
 function s.cfilter(c)
-	return c:IsFacedown() or not c:IsSetCard(0x52)
+	return c:IsFaceup() and c:IsSetCard(0x52)
 end
 function s.ntcon(e,c)
 	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE,tp)>0
-		and (Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 or not Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil))
+		and (Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil))
 end
 
