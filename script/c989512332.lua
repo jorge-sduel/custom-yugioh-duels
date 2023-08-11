626b48d5c18f564e172b5e5504497d0e15998844
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-08-10 21:46:45 -0600

    Update c989512332.lua

--- script/c989512332.lua
+++ script/c989512332.lua
@@ -22,6 +22,16 @@ function c989512332.initial_effect(c)
 	e1:SetCondition(c989512332.condtion)
 	e1:SetValue(1000)
 	c:RegisterEffect(e1)
+		--Destroy cards
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetCost(c989512332.descost)
+	e3:SetTarget(c989512332.destg)
+	e3:SetOperation(c989512332.desop)
+	c:RegisterEffect(e3)
 end
 function c989512332.MonMatFilter(c)
 	return c:GetLevel()==7 and c:IsRace(RACE_DRAGON)
@@ -48,4 +58,23 @@ function c989512332.condtion(e)
 	local ph=Duel.GetCurrentPhase()
 	return (ph==PHASE_DAMAGE or ph==PHASE_DAMAGE_CAL)
 		and Duel.GetAttacker()==e:GetHandler()
+end
+function c989512332.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckLPCost(tp,2000) end
+	Duel.PayLPCost(tp,2000)
+end
+function c989512332.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	local ct=g:FilterCount(Card.IsControler,nil,1-tp)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*200)
+end
+function c989512332.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,e:GetHandler())
+	Duel.Destroy(g,REASON_EFFECT)
+	local ct=Duel.GetOperatedGroup():FilterCount(function(c,tp) return c:IsPreviousControler(tp) end,nil,1-tp)
+	if ct>0 then
+		Duel.Damage(1-tp,ct*200,REASON_EFFECT)
+	end
 end
