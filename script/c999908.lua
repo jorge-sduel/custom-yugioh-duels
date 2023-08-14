f5f235db64fa0f01311df076e570c1da92f98d10
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-08-14 08:29:51 -0600

    Update c999908.lua

--- script/c999908.lua
+++ script/c999908.lua
@@ -25,6 +25,17 @@ function c999908.initial_effect(c)
 	e2:SetTarget(c999908.drtg1)
 	e2:SetOperation(c999908.drop1)
 	c:RegisterEffect(e2)
+	--tuner
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_DISABLE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetTarget(c999908.target)
+	e3:SetCondition(c999908.condition)
+	e3:SetOperation(c999908.operation)
+	c:RegisterEffect(e3)
 end
 function c999908.drcon(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SYNCHRO
@@ -65,5 +76,25 @@ function c999908.drop(e,tp,eg,ep,ev,re,r,rp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	Duel.Draw(p,d,REASON_EFFECT)
+	end
+end
+function c999908.filter(c)
+	return c:IsFaceup() and not c:IsType(TYPE_TUNER)
+end
+function c999908.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c999908.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c999908.filter,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	Duel.SelectTarget(tp,c999908.filter,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c999908.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_ADD_TYPE)
+		e1:SetValue(TYPE_TUNER)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
 	end
 end
