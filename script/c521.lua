ee9d56678838b073c2456b6a91b783946ff08cbb
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-10 14:50:21 -0600

    Update c521.lua

--- script/c521.lua
+++ script/c521.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
-	e2:SetRange(LOCATION_SZONE)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(LOCATION_MZONE,0)
 	e2:SetTarget(aux.TargetBoolFunction(Card.IsRace,RACE_DRAGON))
 	e2:SetValue(500)
@@ -28,9 +28,9 @@ function s.filter(c)
 end
 function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
-	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,e:GetHandler())
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1)
 end
 function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	local rc=Duel.AnnounceRace(tp,1,RACE_ALL)
