27fe10ed2f70fd77b8863fe639114297d8f01774
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-16 17:01:12 -0600

    Add files via upload

---
+++ script/c523.lua
@@ -0,0 +1,40 @@
+--Galaxy Antineutrino Dragon
+local s,id=GetID()
+s.Is_Neutrino=true
+function s.initial_effect(c)
+	--fusion summon
+	Fusion.AddProcMixN(c,true,true,s.matfilter,2)
+	c:EnableReviveLimit()
+		--atk
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.atkcon)
+	e1:SetOperation(s.atkop)
+	c:RegisterEffect(e1)
+	--pierce
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e2)
+end
+function s.matfilter(c)
+	return c.Is_Neutrino and c:IsSetCard(0x7b) and c:GetLevel()=8
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_FUSION)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetValue(1)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
