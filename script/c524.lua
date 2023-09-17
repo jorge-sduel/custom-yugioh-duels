381fe5625d53bf60d8aeb6b26a92ab5f0f8eceb0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-17 17:15:01 -0600

    Add files via upload

---
+++ script/c524.lua
@@ -0,0 +1,37 @@
+--Galaxy Antineutrino Dragon
+local s,id=GetID()
+s.Is_Neutrino=true
+function s.initial_effect(c)
+	--Fusion Material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_DRAGON),aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK))
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--end battle phase
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.condition2)
+	e2:SetCost(aux.bfgcost)
+	e2:SetOperation(s.operation2)
+	c:RegisterEffect(e2)
+--
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
+function s.condition2(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()~=tp and (Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<PHASE_BATTLE)
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	Duel.SkipPhase(1-tp,PHASE_BATTLE,RESET_PHASE+PHASE_BATTLE_STEP,1)
+end
