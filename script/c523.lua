8050b16c472d3916a571b5820b01281adb697270
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-16 21:41:03 -0600

    Update c523.lua

--- script/c523.lua
+++ script/c523.lua
@@ -19,6 +19,17 @@ function s.initial_effect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetCode(EFFECT_PIERCE)
 	c:RegisterEffect(e2)
+	--damage
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_DAMAGE)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetTarget(s.damtg)
+	e3:SetOperation(s.damop)
+	c:RegisterEffect(e3)
 end
 function s.matfilter(c)
 	return c.Is_Neutrino and c:IsSetCard(0x7b) and c:IsLevel(8)
@@ -37,4 +48,18 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e1)
 	end
+end
+function s.filter(e,c)
+	return c:IsFaceup() and c:IsLevelBelow(e:GetHandler():GetLevel()-1)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ct=Duel.GetMatchingGroupCount(s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	if chk==0 then return ct>0 end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*500)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local ct=Duel.GetMatchingGroupCount(c27.filter,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	Duel.Damage(p,ct*500,REASON_EFFECT)
 end
