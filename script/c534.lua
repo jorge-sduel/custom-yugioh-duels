c0853327d63f1d098d241420738539f2af9a56eb
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-25 16:17:31 -0600

    Update c534.lua

--- script/c534.lua
+++ script/c534.lua
@@ -32,7 +32,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL and e:GetHandler():GetPreviusLocation()==LOCATION_EXTRA
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL and e:GetHandler():GetPreviousLocation()==LOCATION_EXTRA
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
