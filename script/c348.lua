f2277e999362fac6163acf946b402dfd1007ca86
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-26 10:07:18 -0600

    Update c348.lua

--- script/c348.lua
+++ script/c348.lua
@@ -12,23 +12,24 @@ function s.initial_effect(c)
 end
 function s.sctg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
-		local mg=Duel.GetMatchingGroup(Card.IsCanBeSynchroMaterial,tp,LOCATION_GRAVE,0,nil)
-		return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,2,nil,nil,mg)
+		local mg=Duel.GetMatchingGroup(Card.IsCanBeSynchroMaterial,tp,LOCATION_MZONE,0,nil)
+		return Duel.IsExistingMatchingCard(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,1,nil,nil,mg)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
-function s.scop(e,tp,eg,ep,ev,re,r,rp)
-	local sc=Group.CreateGroup() 
-	local mg=Duel.GetMatchingGroup(Card.IsCanBeSynchroMaterial,tp,LOCATION_GRAVE,0,nil)
+function s.scop(e,tp,eg,ep,ev,re,r,rp) 
+	local mg=Duel.GetMatchingGroup(Card.IsCanBeSynchroMaterial,tp,LOCATION_MZONE,0,nil)
 	local g=Duel.GetMatchingGroup(Card.IsSynchroSummonable,tp,LOCATION_EXTRA,0,nil,nil,mg)
 	if #g>0 then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	sc:Merge(g)
-	local sc2=sc:Select(tp,1,1,nil)
-	--sc:Merge(mt2)
-		Duel.SynchroSummon(tp,sc2,nil,mg)
-		--local sg2=g:Select(tp,1,1,nil)
+		local sg=g:Select(tp,1,1,nil)
+		local sc=sg:GetFirst()
+		Synchro.Send=2
+		Duel.SynchroSummon(tp,sc,nil,mg)
+		local mg2=sc:GetMaterial()
+		local sg2=g:Select(tp,1,1,nil)
+		local sc2=sg:GetFirst()
 		--local sc2=sg2:GetFirst()
-		--Duel.SynchroSummon(tp,sc2,nil,mg)
+		Duel.SynchroSummon(tp,sc2,nil,mg2)
 	end
 end
