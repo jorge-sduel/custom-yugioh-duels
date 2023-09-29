9ab8f2be13d697f14ebbfd8b9d2f191adead73bc
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-29 16:26:18 -0600

    Update c527.lua

--- script/c527.lua
+++ script/c527.lua
@@ -5,6 +5,14 @@ function s.initial_effect(c)
 	--Fusion Material
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,s.matfilter1,s.matfilter2)
+	--lvup
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(81974607,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTarget(s.lvtg)
+	e1:SetOperation(s.lvop)
+	c:RegisterEffect(e1)
 	--damage
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
@@ -59,5 +67,19 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.lvfilter,tp,LOCATION_MZONE,0,1,e:GetHandler()) end
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local atk=c:GetAttack()
+	local g=Duel.GetMatchingGroup(s.lvfilter,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	while tc do
+	Duel.Recover(tp,atk,REASON_EFFECT)
+Duel.Release(e:GetHandler(),REASON_COST)
+		tc=g:GetNext()
 	end
 end
