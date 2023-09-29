f868be890a5daeac02cad28d8ef525c6ae98736e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-28 21:32:31 -0600

    Update c526.lua

--- script/c526.lua
+++ script/c526.lua
@@ -8,7 +8,7 @@ function s.initial_effect(c)
 	--special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCode(EVENT_BATTLE_DAMAGE)
+	e1:SetCode(EVENT_BATTLE_DESTROYING)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetCondition(aux.bdogcon)
 	e1:SetOperation(s.spop)
