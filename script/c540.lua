764322e73eaa09a6cdd71f55f5dfd95503485944
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-09-30 20:55:51 -0600

    Update c540.lua

--- script/c540.lua
+++ script/c540.lua
@@ -36,6 +36,12 @@ function s.initial_effect(c)
 	e4:SetTarget(s.atktg)
 	e4:SetOperation(s.atkop)
 	c:RegisterEffect(e4)
+	--pierce
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_ADD_RACE)
+	e5:SetValue(RACE_DRAGON)
+	c:RegisterEffect(e5)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():IsRelateToBattle()
