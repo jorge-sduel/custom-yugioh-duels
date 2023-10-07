259e876c8828648a80cfa4d01912b95b83645e33
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-10-06 22:21:01 -0600

    Add files via upload

---
+++ script/c547.lua
@@ -0,0 +1,46 @@
+--魔竜星－トウテツ
+local s,id=GetID()
+s.Is_Neutrino=true
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	--effect gain
+	local e3=Effect.CreateEffect(c)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(s.atkcon)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+function s.spfilter(c)
+	return c:IsFaceup() and (c:IsSetCard(0x55) or c:IsSetCard(0x7b))
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.spfilter,c:GetControler(),LOCATION_MZONE,0,1,nil)
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return (r&REASON_SYNCHRO)~=0
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	--Destroy monster
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetValue(1)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+	rc:RegisterEffect(e3)
+end
