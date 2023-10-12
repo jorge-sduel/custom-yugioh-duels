7f014844306e690f38c6d7b26ed1c36f9592a794
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-10-11 21:10:35 -0600

    Update c556.lua

--- script/c556.lua
+++ script/c556.lua
@@ -69,11 +69,11 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	for tc in sg:Iter() do
 		if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
+		end
 	local g2=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_EXTRA,0,1,1,nil)
 	local tc1=g2:GetFirst()
 	--if tc1 then]] 
 	   Duel.SpecialSummonRule(tp,tc1,0)
-		end
 	end
 end
 function s.filter2(c,e,tp)
