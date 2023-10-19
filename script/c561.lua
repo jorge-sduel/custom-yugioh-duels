d87f98bf6cad0ab7628308d34a4554c650b89ae1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-10-19 17:19:59 -0600

    Add files via upload

---
+++ script/c561.lua
@@ -0,0 +1,168 @@
+--
+local s,id=GetID()
+s.Is_Neutrino=true
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,s.xyzfilter,nil,3,nil,nil,nil,nil,false)
+	c:EnableReviveLimit()
+	--cannot negate summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	c:RegisterEffect(e1)
+	--damage
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_DAMAGE)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCondition(s.damcon)
+	e2:SetTarget(s.damtg)
+	e2:SetOperation(s.damop)
+	--reduce
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CHANGE_DAMAGE)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(1,0)
+	e3:SetCondition(s.damcon2)
+	e3:SetValue(s.damval)
+	c:RegisterEffect(e3)
+	local e4=e3:Clone()
+	e4:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+	c:RegisterEffect(e4)
+	--special summon
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_FREE_CHAIN)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCondition(s.spcon)
+	e5:SetCost(s.spcost)
+	e5:SetTarget(s.sptg)
+	e5:SetOperation(s.spop)
+	c:RegisterEffect(e5)
+ --
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,1))
+	e6:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+	c:RegisterEffect(e6)
+--unaffected by no equip card
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetCode(EFFECT_IMMUNE_EFFECT)
+	e7:SetValue(s.efilter)
+	c:RegisterEffect(e7)
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetCode(555)
+	c:RegisterEffect(e8)
+	local e9=Effect.CreateEffect(c)
+	e9:SetDescription(aux.Stringid(id,1))
+	e9:SetCategory(CATEGORY_RECOVER)
+	e9:SetType(EFFECT_TYPE_QUICK_O)
+	e9:SetRange(LOCATION_SZONE)
+	e9:SetCode(EVENT_FREE_CHAIN)
+	e9:SetHintTiming(0,TIMING_DAMAGE+TIMING_BATTLE_END+TIMING_BATTLE_START)
+	e1:SetCost(aux.dxmcostgen(1,1,nil))
+	e9:SetCondition(s.condition)
+	e9:SetOperation(s.operation)
+	c:RegisterEffect(e9)
+	--Protect itself from destruction
+	local e10=Effect.CreateEffect(c)
+	e10:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e10:SetCode(EFFECT_DESTROY_REPLACE)
+	e10:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e10:SetRange(LOCATION_MZONE)
+	e10:SetTarget(s.reptg)
+	c:RegisterEffect(e10)
+end
+function s.xyzfilter(c,xyz,sumtype,tp)
+	return c:IsType(TYPE FUSION) and c:IsLevelAbove(10) and c:IsAttribute(ATTRIBUTE_DARK)
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetOverlayGroup():IsExists(Card.IsRace,1,nil,RACE_DRAGON)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	local dam=math.abs(Duel.GetLP(0)-Duel.GetLP(1))
+	Duel.SetTargetParam(dam)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,dam)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local dam=math.abs(Duel.GetLP(0)-Duel.GetLP(1))
+	Duel.Damage(p,dam,REASON_EFFECT)
+end
+function s.damcon2(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetOverlayGroup():IsExists(Card.IsRace,1,nil,RACE_WARRIOR)
+end
+function s.damval(e,re,val,r,rp,rc)
+	if bit.band(r,REASON_EFFECT)~=0 then 
+	return 0
+	else return val
+	end
+end
+function s.spcon(e)
+	return e:GetHandler():GetOverlayCount()==0
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToExtraAsCost() end
+	Duel.SendtoDeck(c,nil,0,REASON_COST)
+end
+function s.filter(c,e,tp)
+	return not c:IsCode(id) and c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,121,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+		if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+		return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) and ft>1 
+			and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,3,nil,e,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,3,tp,LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<3 then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil,e,tp)
+	if #g>=3 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=g:Select(tp,3,3,nil)
+		local tc=sg:GetFirst()
+		Duel.SpecialSummonStep(tc,121,tp,tp,false,false,POS_FACEUP)
+		tc:RegisterFlagEffect(tc:GetOriginalCode(),RESET_EVENT+RESETS_STANDARD_DISABLE,0,0)
+		tc=sg:GetNext()
+		Duel.SpecialSummonStep(tc,121,tp,tp,false,false,POS_FACEUP)
+		tc:RegisterFlagEffect(tc:GetOriginalCode(),RESET_EVENT+RESETS_STANDARD_DISABLE,0,0)
+		Duel.SpecialSummonComplete()
+	end
+end
+function s.efilter(e,te)
+	return not ((te:IsCode(467) or te:IsCode(493) or te:IsCode(12632096) or te:IsCode(68396121) or te:IsCode(39272762) or te:IsCode(48348921) or te:IsCode(1497) or te:IsCode(249001043)) or te:IsHasEffect(555))
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetLP(tp)<8000 and not e:GetHandler():IsStatus(STATUS_CHAINING)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	Duel.SetLP(tp,8000,REASON_EFFECT)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:CheckRemoveOverlayCard(tp,1,REASON_EFFECT) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		c:RemoveOverlayCard(tp,1,1,REASON_EFFECT)
+		return true
+	else return false end
+end
