3b3eaad00fd16a2e1cd618dab8fa2695024db3eb
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-10-27 13:44:59 -0600

    Update c102400155.lua

--- script/c102400155.lua
+++ script/c102400155.lua
@@ -22,8 +22,9 @@ function cid.filter3(c,e,tp)
 	--and (not ect or ect>1)
 		and Duel.IsExistingMatchingCard(cid.filter2,tp,LOCATION_EXTRA,0,1,nil,e,tp,c)
 end
-function cid.filter2(c,e,tp,mc)
-	return  c:IsRank(mc:GetRank()+1)
+function cid.filter2(c,e,tp,mc,rk)
+	if c.rum_limit and not c.rum_limit(mc,e) then return false end
+	return c:IsType(TYPE_XYZ) and mc:IsType(TYPE_XYZ,c,SUMMON_TYPE_XYZ,tp) and c:IsRank(rk+1) and mc:IsCanBeXyzMaterial(c,tp) 
 		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_XYZ,tp,false,false)
 end
 function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -34,27 +35,28 @@ function cid.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function cid.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_XYZ)<=0 then return end
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 or Duel.GetLocationCountFromEx(tp)<=0 then return end
+	local tc=Duel.GetFirstTarget()
+	if not tc or not tc:IsRelateToEffect(e) or tc:IsImmuneToEffect(e) 
+		or not Duel.XyzSummon(tp,tc,nil) then return end 
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g1=Duel.SelectMatchingCard(tp,cid.filter1,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
-	local tc=g1:GetFirst()
-	if tc and Duel.XyzSummon(tp,tc,nil) and Duel.SelectYesNo(tp,aux.Stringid(58988903,0)) then
-	local g3=Duel.SelectMatchingCard(tp,cid.filter3,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
-	local tc1=g3:GetFirst() 
-		local smg=tc1:Select(tp,1,1,nil)
-		local g2=Duel.SelectMatchingCard(tp,cid.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc1,smg:GetRace(),smg:GetAttribute())
-		local tc2=g2:GetFirst()
-		if tc2 then
-			Duel.BreakEffect()
-			--local xmg=tc1:GetOverlayGroup()
-			--Duel.Overlay(tc2,xmg)
-			tc2:SetMaterial(smg)
-			Duel.Overlay(tc2,smg)
-			Duel.SpecialSummon(tc2,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)
-			tc2:CompleteProcedure()
+	local g=Duel.SelectMatchingCard(tp,cid.filter2,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc,tc:GetRank())
+	local sc=g:GetFirst()
+	if sc then
+		Duel.BreakEffect()
+		sc:SetMaterial(Group.FromCards(tc))
+		Duel.Overlay(sc,Group.FromCards(tc))
+		if Duel.SpecialSummon(sc,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)>0 then
+			sc:CompleteProcedure()
+			if c:IsRelateToEffect(e) and e:IsHasType(EFFECT_TYPE_ACTIVATE) then
+				c:CancelToGrave()
+				Duel.Overlay(sc,Group.FromCards(c))
+			end
 		end
 	end
 end
+
 
 
 
