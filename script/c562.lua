fa0135491eae549ce7098420bc3b143aef1a0a90
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-10-29 10:55:49 -0600

    Update c562.lua

--- script/c562.lua
+++ script/c562.lua
@@ -18,7 +18,8 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0)
+		local tc=g:Select(tp,1,1,nil)
 	--if tc and tc:IsRelateToEffect(e) then 	
 	Duel.SendtoHand(tc,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,tc)
