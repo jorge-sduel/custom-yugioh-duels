3c43420ba75d6761b45f0ffdfe31ae37e88ee369
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-10-29 10:22:07 -0600

    Update c562.lua

--- script/c562.lua
+++ script/c562.lua
@@ -21,7 +21,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if #g>0 then
-		local atk=g:GetAttack()
+		--local atk=g:GetAttack()
 		local lv=g:GetLevel()
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
