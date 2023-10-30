39df8f882d99008de1ed0ae1f75383287f4fb675
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-10-29 21:33:40 -0600

    Update c210210504.lua

--- script/c210210504.lua
+++ script/c210210504.lua
@@ -2,13 +2,13 @@
 --AlphaKretin
 function c210210504.initial_effect(c)
 	--pendulum summon
-	--cannot special summon
+	--[[cannot special summon
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
 	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
 	e1:SetValue(c210210504.splimit)
-	c:RegisterEffect(e1)
+	c:RegisterEffect(e1)]]
 	--draw
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(66729231,0))
