f0f9425d23fcbebb3fcdf2bf77ba727a758837f8
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-01 21:40:09 -0600

    Update c210220067.lua

--- script/c210220067.lua
+++ script/c210220067.lua
@@ -75,6 +75,11 @@ end
 function s.regop2(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local mg=c:GetMaterial()
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_BECOME_LINKED_ZONE)
+	e1:SetValue(0xffffff)
+	Duel.RegisterEffect(e1,tp)
 	local lv=mg:Select(tp,1,1,nil)
 	for tc in mg:Iter() do
 	if Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP) then
@@ -85,7 +90,8 @@ function s.regop2(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 	tc:RegisterEffect(e2,true)
   end
---e1:Reset()
+Duel.SpecialSummonComplete() 
+e1:Reset()
 end
 end
 function s.regop3(e,tp,eg,ep,ev,re,r,rp)
