275e7aa0e8624c9a3fb10fd1ce7390df2364200e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-02 21:33:26 -0600

    Update c210220097.lua

--- script/c210220097.lua
+++ script/c210220097.lua
@@ -99,8 +99,8 @@ function s.splimit(rc,attr,ct)
 	end
 end
 function s.ope(rc,attr)
-	return function(e,c,tp)
-	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,LOCATION_MZONE,nil)
+	return function(e,c)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,e:GetHandlerPlayer(),LOCATION_MZONE,LOCATION_MZONE,nil)
 for tc in aux.Next(g) do 
 local e1=Effect.CreateEffect(c)
 e1:SetType(EFFECT_TYPE_SINGLE)
