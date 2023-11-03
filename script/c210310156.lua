4b7dcaac6aae507541d5c1dcee8f072fc630d6ca
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-03 12:27:49 -0600

    Update c210310156.lua

--- script/c210310156.lua
+++ script/c210310156.lua
@@ -1,6 +1,6 @@
 --CXyz Gaia Master, the Charging Storm
 --AlphaKretin
-function c210310156.initial_effect(c)
+function s.initial_effect(c)
 	--xyz summon
 	Xyz.AddProcedure(c,nil,8,4)
 	c:EnableReviveLimit()
@@ -10,32 +10,61 @@ function c210310156.initial_effect(c)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCode(EFFECT_IMMUNE_EFFECT)
-	e1:SetValue(c210310156.efilter)
+	e1:SetValue(s.efilter)
 	c:RegisterEffect(e1)
-	--pierce
+		--multi attack
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCondition(c210310156.condition)
-	e2:SetCode(EFFECT_PIERCE)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetHintTiming(0,TIMING_MAIN_END)
+	e2:SetCondition(s.mtcon)
+	e2:SetCost(s.mtcost)
+	e2:SetTarget(s.mttg)
+	e2:SetOperation(s.mtop)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e3:SetCode(EVENT_PRE_BATTLE_DAMAGE)
-	e3:SetCondition(c210310156.damcon)
-	e3:SetOperation(c210310156.damop)
-	c:RegisterEffect(e3)
+	--remove att
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e4:SetValue(ATTRIBUTE_LIGHT)
+	c:RegisterEffect(e4)
 end
-function c210310156.efilter(e,te)
+function s.efilter(e,te)
 	return te:IsActiveType(TYPE_TRAP) and te:GetOwner()~=e:GetOwner()
 end
-function c210310156.condition(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode,1,nil,91949988)
+function s.mtcon(e,tp,eg,ep,ev,re,r,rp)
+	return ((Duel.GetCurrentPhase()>=PHASE_BATTLE_START and Duel.GetCurrentPhase()<=PHASE_BATTLE) or Duel.IsAbleToEnterBP()) and e:GetHandler():GetOverlayGroup():IsExists(Card.IsCode,1,nil,365)
 end
-function c210310156.damcon(e,tp,eg,ep,ev,re,r,rp)
+function s.mtcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.mtfilter(c)
+	return c:IsFaceup() and not c:IsHasEffect(EFFECT_EXTRA_ATTACK) and c:GetFlagEffect(id)==0
+end
+function s.mttg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and s.mtfilter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.mtfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local tc=Duel.SelectTarget(tp,s.mtfilter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,nil):GetFirst()
+	tc:RegisterFlagEffect(id,RESET_CHAIN,0,1)
+end
+function s.mtop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
-	return ep~=tp and c==Duel.GetAttacker() and Duel.GetAttackTarget() and Duel.GetAttackTarget():IsDefensePos() and c:GetOverlayGroup():IsExists(Card.IsCode,1,nil,91949988)
+	local tc=Duel.GetFirstTarget()
+	local fid=c:GetFieldID()
+	if tc and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+		e1:SetCode(EVENT_BATTLE_START)
+		e1:SetTarget(s.atktg)
+		e1:SetOperation(s.atkop)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		--tc:RegisterFlagEffect(51102591,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1,fid)
+	end
 end
-function c210310156.damop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.ChangeBattleDamage(ep,ev*2)
-end
-
