742ea4f68f0ff1cd8d742192e26e5b04d291de68
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-09 10:48:08 -0600

    Add files via upload

---
+++ script/c569.lua
@@ -0,0 +1,62 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,s.matfilter1,s.matfilter2)
+Fusion.AddContactProc(c,s.contactfil,s.contactop)
+	--cannot attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,LOCATION_MZONE)
+	e2:SetCondition(s.atkcon)
+	e2:SetTarget(s.atktg)
+	c:RegisterEffect(e2)
+	--check
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetOperation(s.checkop)
+	e3:SetLabelObject(e2)
+	c:RegisterEffect(e3)
+--cannot release
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EFFECT_CANNOT_RELEASE)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetTargetRange(0,1)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x52}
+s.listed_names={34022290}
+s.material_setcode=0x52
+function s.matfilter1(c)
+	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x52)
+end
+function s.matfilter2(c)
+	return (c:IsType(TYPE_MONSTER) and c:IsType(TYPE_EFFECT)) or (c:IsCode(511000453) and c:IsLocation(LOCATION_ONFIELD))
+end
+function s.atkcon(e)
+	return e:GetHandler():GetFlagEffect(id)>0
+end
+function s.contactfil(tp)
+	return Duel.GetReleaseGroup(tp)
+end
+function s.contactop(g,tp)
+	Duel.Release(g,REASON_COST+REASON_MATERIAL)
+end
+function s.atktg(e,c)
+	return c:GetFieldID()~=e:GetLabel()
+end
+function s.checkop(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():GetFlagEffect(id)>0 then return end
+	local fid=eg:GetFirst():GetFieldID()
+	e:GetHandler():RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,0,1)
+	e:GetLabelObject():SetLabel(fid)
+end
