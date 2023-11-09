389fa5e6cd86955095bdd752cb6a7bfd32085e5c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-09 11:14:24 -0600

    Update c569.lua

--- script/c569.lua
+++ script/c569.lua
@@ -1,16 +1,26 @@
 --
-local s,id=GetID()
+local s,id=GetID()
+
 function s.initial_effect(c)
 	--Fusion Material
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,s.matfilter1,s.matfilter2)
-Fusion.AddContactProc(c,s.contactfil,s.contactop)
+	--spsummon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetRange(LOCATION_EXTRA)
+	e1:SetCondition(s.hspcon)
+	e1:SetTarget(s.hsptg)
+	e1:SetOperation(s.hspop)
+	c:RegisterEffect(e1)
 	--cannot attack
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD)
 	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
-	e2:SetRange(LOCATION_SZONE)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetTargetRange(0,LOCATION_MZONE)
 	e2:SetCondition(s.atkcon)
 	e2:SetTarget(s.atktg)
@@ -20,7 +30,7 @@ Fusion.AddContactProc(c,s.contactfil,s.contactop)
 	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e3:SetCode(EVENT_ATTACK_ANNOUNCE)
-	e3:SetRange(LOCATION_SZONE)
+	e3:SetRange(LOCATION_MZONE)
 	e3:SetOperation(s.checkop)
 	e3:SetLabelObject(e2)
 	c:RegisterEffect(e3)
@@ -36,6 +46,36 @@ end
 s.listed_series={0x52}
 s.listed_names={34022290}
 s.material_setcode=0x52
+function s.spfilter(c)
+	return c:IsAbleToGraveAsCost() and ((c:IsLocation(LOCATION_ONFIELD) and c:IsCode(34022290)) or (c:IsSetCard(0x52) and c:IsType(TYPE_MONSTER)))
+end
+function s.rescon(sg,e,tp,mg)
+	return Duel.GetLocationCountFromEx(tp,tp,sg,e:GetHandler())>0 and sg:FilterCount(Card.IsLocation,nil,LOCATION_ONFIELD)~=#sg
+end
+function s.hspcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_SZONE|LOCATION_MZONE,0,nil)
+	if #g==g:FilterCount(Card.IsLocation,nil,LOCATION_ONFIELD) then return false end
+	return aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,0)
+end
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk,c)
+	local g=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_SZONE|LOCATION_MZONE,0,nil)
+	local sg=aux.SelectUnselectGroup(g,e,tp,2,2,s.rescon,1,tp,HINTMSG_REMOVE,nil,nil,true)
+	if #sg > 0 then
+		sg:KeepAlive()
+		e:SetLabelObject(sg)
+		return true
+	else
+		return false
+	end
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	local sg=e:GetLabelObject()
+	Duel.SentToGrave(sg,nil,POS_FACEUP,REASON_COST)
+	c:SetMaterial(sg)
+	sg:DeleteGroup()
+end
 function s.matfilter1(c)
 	return c:IsType(TYPE_MONSTER) and c:IsSetCard(0x52)
 end
@@ -44,12 +84,6 @@ function s.matfilter2(c)
 end
 function s.atkcon(e)
 	return e:GetHandler():GetFlagEffect(id)>0
-end
-function s.contactfil(tp)
-	return Duel.GetReleaseGroup(tp)
-end
-function s.contactop(g,tp)
-	Duel.Release(g,REASON_COST+REASON_MATERIAL)
 end
 function s.atktg(e,c)
 	return c:GetFieldID()~=e:GetLabel()
