f6785c7c2bdc45f33b2eb2021ea520662b19be2b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-12 11:26:04 -0600

    Update c51615306.lua

--- script/c51615306.lua
+++ script/c51615306.lua
@@ -15,6 +15,18 @@ Timeleap.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsCode,id-6),1,1,cid.timec
 	e1:SetTarget(cid.destg)
 	e1:SetOperation(cid.desop)
 	c:RegisterEffect(e1)
+		--negate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_QUICK_F)
+	e2:SetCode(EVENT_CHAINING)
+	e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(cid.negcon)
+	e2:SetTarget(cid.negtg)
+	e2:SetOperation(cid.negop)
+	c:RegisterEffect(e2)
 	local e4=Effect.CreateEffect(c)
 	e4:SetType(EFFECT_TYPE_FIELD)
 	e4:SetRange(LOCATION_MZONE)
@@ -43,4 +55,23 @@ function cid.tglim(e,c)
 end
 function cid.efilter(e,re,rp)
 	return re:IsActiveType(TYPE_MONSTER) and rp~=e:GetHandlerPlayer()
+end
+function cid.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	if not g or not g:IsContains(c) then return false end
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE) and c:IsSummonType(SUMMON_TYPE_TIMELEAP2)
+end
+function cid.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) and re:GetHandler():IsDestructable() then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function cid.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(re:GetHandler(),REASON_EFFECT)
+	end
 end
