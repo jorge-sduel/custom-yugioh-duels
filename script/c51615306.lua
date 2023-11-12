c7230c5c2b69ebed02e4a1a539a76d53c3c6af10
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-12 10:56:40 -0600

    Update c51615306.lua

--- script/c51615306.lua
+++ script/c51615306.lua
@@ -5,10 +5,7 @@ if not TIMELEAP_IMPORTED then Duel.LoadScript("proc_timeleap.lua") end
 function cid.initial_effect(c)
 	c:EnableReviveLimit()
 	--time leap procedure
-Timeleap.AddProcedure(c,cid.material,1,1,cid.TimeCon)
-	c:EnableReviveLimit()
-	--time leap procedure
-Timeleap.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsCode,id-6),function(e,tc) return not Duel.IsExistingMatchingCard(aux.AND(Card.IsFaceup,Card.IsCode),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil,id) end )
+Timeleap.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsCode,id-6),1,1,function(e,tc) return not Duel.IsExistingMatchingCard(aux.AND(Card.IsFaceup,Card.IsCode),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil,id) end )
 function cid.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
