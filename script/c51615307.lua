5648149a9f2dd59dbd7a6020efd45a535dbddec7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-12 12:26:51 -0600

    Update c51615307.lua

--- script/c51615307.lua
+++ script/c51615307.lua
@@ -1,10 +1,11 @@
 --created by LeonDuvall, coded by Lyris
 local cid,id=GetID()
+cid.IsTimeleap=true
+if not TIMELEAP_IMPORTED then Duel.LoadScript("proc_timeleap.lua") end
 function cid.initial_effect(c)
 	c:EnableReviveLimit()
-	aux.AddOrigTimeleapType(c)
-	aux.AddTimeleapProc(c,5,function(e,tc) return not Duel.IsExistingMatchingCard(aux.AND(Card.IsFaceup,Card.IsCode),tp,LOCATION_MZONE,0,1,nil,id) end,aux.FilterBoolFunction(Card.IsCode,id-6))
-	aux.AddCodeList(c,id-6)
+	--time leap procedure
+Timeleap.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsCode,id-6),1,1,cid.timecon)
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
@@ -22,6 +23,10 @@ function cid.initial_effect(c)
 	e4:SetTarget(cid.tglim)
 	e4:SetValue(cid.efilter)
 	c:RegisterEffect(e4)
+end
+cid.material={id-6}
+function cid.timecon(e)
+	return not Duel.IsExistingMatchingCard(aux.AND(Card.IsFaceup,Card.IsCode),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil,id) 
 end
 function cid.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(1-tp) and chkc:IsType(TYPE_SPELL+TYPE_TRAP) end
