83e60ce40538f5125c5a16eda01108eebd0d2a51
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-16 14:17:36 -0600

    Add files via upload

---
+++ script/c573.lua
@@ -0,0 +1,79 @@
+--ハンター・アウル
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,4,2,99)
+	c:EnableReviveLimit()
+	--token
+	local e0=Effect.CreateEffect(c)
+	e0:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_TOKEN)
+	e0:SetDescription(aux.Stringid(10389142,0))
+	e0:SetType(EFFECT_TYPE_IGNITION)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetCost(s.spcost1)
+	e0:SetTarget(s.sptg1)
+	e0:SetOperation(s.spop1)
+	c:RegisterEffect(e0,false,REGISTER_FLAG_DETACH_XMAT)
+	--cannot be attacked
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_BATTLE_TARGET)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.atcon)
+	e1:SetValue(aux.imval1)
+	c:RegisterEffect(e1)
+	--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.upval)
+	c:RegisterEffect(e2)
+	--atkup
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+
+end
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_DINOSAUR)
+end
+function s.atcon(e)
+	return Duel.IsExistingMatchingCard(s.cfilter,e:GetHandlerPlayer(),LOCATION_MZONE,0,1,e:GetHandler())
+end
+function s.upval(e,c)
+	return Duel.GetMatchingGroupCount(s.upfilter,c:GetControler(),LOCATION_MZONE,0,nil)*300
+end
+function s.upfilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_DINOSAUR)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,572,0,TYPES_TOKEN,1000,1000,3,RACE_DINOSAUR,ATTRIBUTE_EARTH) end
+	local ft=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,ft,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,ft,tp,0)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetFieldGroupCount(tp,0,LOCATION_MZONE)
+	if ft<=0 or not Duel.IsPlayerCanSpecialSummonMonster(tp,572,0,TYPES_TOKEN,1000,1000,3,RACE_DINOSAUR,ATTRIBUTE_EARTH) then return end
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then ft=1 end
+	local fid=e:GetHandler():GetFieldID()
+	local g=Group.CreateGroup()
+	for i=1,ft do
+		local token=Duel.CreateToken(tp,10389143)
+		Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+		g:AddCard(token)
+	end
+	Duel.SpecialSummonComplete()
+	g:KeepAlive()
+end
