ad1dd50365ec83113c222911d27c810cde85781f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-16 11:07:05 -0600

    Add files via upload

---
+++ script/c571.lua
@@ -0,0 +1,66 @@
+--
+local s,id=GetID()
+s.Is_Neutrino=true
+function s.initial_effect(c)
+--
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(511001225)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+--attack up
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_BATTLE_START)
+	e3:SetCondition(s.condition)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsAbleToDeck()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_REMOVED,0,e:GetHandler())
+	return aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),0,c)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
+	--Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local rg=Duel.GetMatchingGroup(s.filter,tp,LOCATION_REMOVED,0,e:GetHandler())
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,aux.ChkfMMZ(1),1,tp,HINTMSG_TODECK,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoDeck(g,nil,0,REASON_COST)
+	Duel.ShuffleDeck(tp)
+	g:DeleteGroup()
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsRelateToBattle()
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		Duel.Draw(tp,1,REASON_EFFECT) 
+	end
+end
