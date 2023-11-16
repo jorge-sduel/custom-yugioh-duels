68720f270edc89e4d3d4609b2b5dcdd927acff9f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-16 10:27:51 -0600

    Update c570.lua

--- script/c570.lua
+++ script/c570.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsType,TYPE_FUSION),s.ffilter2,2)
+	Fusion.AddProcMixRep(c,true,true,s.ffilter,2,2,aux.FilterBoolFunctionEx(Card.IsType,TYPE_FUSION))
 	--act limit
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
