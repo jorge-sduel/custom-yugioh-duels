5fb76eb584f77d76c37a26d4978bf7f31e79162f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-17 12:07:59 -0600

    Update c888000010.lua

--- script/c888000010.lua
+++ script/c888000010.lua
@@ -20,7 +20,7 @@ function c888000010.initial_effect(c)
 	c:RegisterEffect(e1)
 		--draw
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(888000010,0))
 	e2:SetCategory(CATEGORY_DRAW)
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
