7986fb463bc2959775c4300456f8dfef8c1e9634
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-16 21:00:30 -0600

    Update c566.lua

--- script/c566.lua
+++ script/c566.lua
@@ -43,15 +43,15 @@ function s.operation(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
-local ogatk=2100
+--local ogatk=2100
 	if #g>0 then Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_ATTACK) 
 	local e2=Effect.CreateEffect(e:GetHandler())
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_PHASE+PHASE_END)
 	e2:SetCountLimit(1)
-	e2:SetLabel(ogatk)
+	--e2:SetLabel(ogatk)
 	e2:SetLabelObject(g)
-	e2:SetCondition(s.damcon)
+	--e2:SetCondition(s.damcon)
 	e2:SetOperation(s.damop)
 	Duel.RegisterEffect(e2,tp)
 end
@@ -61,6 +61,6 @@ function s.damcon(e,tp,eg,ep,ev,re,r,rp)
 	return Duel.GetTurnPlayer()==tp
 end
 function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Recover(e:GetLabelObject():GetControler(),e:GetLabel(),REASON_EFFECT)
+	Duel.Recover(2100,REASON_EFFECT)
 end
 
