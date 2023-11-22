eca63b0aca5e597ce500865cb520912294227161
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-22 12:47:57 -0600

    Add files via upload

---
+++ script/c575.lua
@@ -0,0 +1,71 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsAttribute,ATTRIBUTE_DARK),s.ffilter2)
+--lizard check
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(CARD_CLOCK_LIZARD)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCondition(s.lizcon)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+	--Special Summon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.EvilHeroLimit)
+	c:RegisterEffect(e1)
+	--atkup
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.val)
+	c:RegisterEffect(e2)
+	--direct attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_DIRECT_ATTACK)
+	c:RegisterEffect(e3)
+	-- Inflict Damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	--e4:SetCategory(CATEGORY_DAMAGE)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTarget(s.damtg)
+	e4:SetOperation(s.damop)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x6008}
+s.material_setcode={0x8,0x6008}
+s.dark_calling=true
+s.listed_names={CARD_DARK_FUSION}
+function s.lizcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),EFFECT_SUPREME_CASTLE)
+end
+function s.ffilter2(c,fc,sumtype,tp)
+	return c:IsSetCard(0x6008,fc,sumtype,tp) and c:IsType(TYPE_FUSION,fc,sumtype,tp)
+end
+function s.val(e,c)
+	return Duel.GetFieldGroupCount(c:GetControler(),0,LOCATION_SZONE)*500
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local ct=Duel.GetFieldGroupCount(tp,LOCATION_GRAVE,0)
+	Duel.SetTargetPlayer(tp)
+	--Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,ct*300)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local ct=Duel.GetFieldGroupCount(tp,LOCATION_GRAVE,0)
+	Duel.SetLp(p,ct*300,REASON_EFFECT)
+end
