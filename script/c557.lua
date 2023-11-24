2f3bfb693143bdc4883e595a9996c85ed20828e5
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-24 11:55:22 -0600

    Update c557.lua

--- script/c557.lua
+++ script/c557.lua
@@ -8,7 +8,17 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetValue(700)
 	c:RegisterEffect(e2)
-
+	--draw
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_DRAW)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetType(EFFECT_TYPE_EQUIP)
+	e4:SetCode(EVENT_BATTLE_DESTROYING)
+	e4:SetCondition(s.descon)
+	e4:SetTarget(s.drtg)
+	e4:SetOperation(s.drop)
+	c:RegisterEffect(e4)
 end
 function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local ec=eg:GetFirst()
@@ -19,14 +29,14 @@ end
 function s.desfilter(c,lv)
 	return c:IsFaceup() and c:IsLevelBelow(lv)
 end
-function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.drtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsPlayerCanDraw(tp,1) end
 	Duel.SetTargetPlayer(tp)
 	Duel.SetTargetParam(1)
 	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,500)
 end
-function s.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
 	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
 	Duel.Draw(p,d,REASON_EFFECT) 
 			Duel.Damage(1-tp,500,REASON_EFFECT)
