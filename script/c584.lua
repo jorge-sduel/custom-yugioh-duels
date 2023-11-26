08e1c8a2c17dc5fcb3fe2d6bb352cd7cbcbbb849
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-26 10:49:23 -0600

    Add files via upload

---
+++ script/c584.lua
@@ -0,0 +1,91 @@
+--Evil HERO Brightness
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcFun2(c,aux.FilterBoolFunction(Card.IsSetCard,0x8),aux.FilterBoolFunction(Card.IsAttribute,ATTRIBUTE_LIGHT),true)
+	--lizard check
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(CARD_CLOCK_LIZARD)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCondition(s.lizcon)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+	--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.atkup)
+	c:RegisterEffect(e1)
+	--Special Summon condition
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e2:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e2:SetValue(aux.EvilHeroLimit)
+	c:RegisterEffect(e2)
+	--tohand
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(888000016,0))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetLabel(0)
+	e3:SetCondition(s.thcon)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
+	--damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(6602300,0))
+	e4:SetCategory(CATEGORY_DAMAGE)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.damtg)
+	e4:SetOperation(s.damop)
+	c:RegisterEffect(e4)
+end
+s.material_setcode={0x8,0x6008}
+s.dark_calling=true
+s.listed_names={CARD_DARK_FUSION,58932615,21844576}
+function s.lizcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),EFFECT_SUPREME_CASTLE)
+end
+
+function s.atkup(e,c)
+	return Duel.GetMatchingGroupCount(nil,c:GetControler(),LOCATION_REMOVED,0,nil)*300
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return bit.band(e:GetHandler():GetPreviousLocation(),LOCATION_ONFIELD)>0
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then 
+		return   Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_REMOVED,0,1,nil)
+	end
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_REMOVED,0,1,nil)
+local sg=g:GetFirst()
+local mat=sg:Select(tp,1,2,nil)
+		Duel.SendtoHand(mat,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,mat)
+end
+function s.damtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetPlayer(1-tp)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,math.abs(e:GetHandler():GetAttack()-e:GetHandler():GetbaseAttack()))
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.Damage(p,e:GetHandler():GetAttack()-e:GetHandler():GetbaseAttack()),REASON_EFFECT)
+end
