0b2f0206cfff58a2578ee9935d11f99c91a2b4a9
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-26 17:00:57 -0600

    Add files via upload

---
+++ script/c587.lua
@@ -0,0 +1,59 @@
+--Ev・HERO トリニティー
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x8),3)
+--lizard check
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(CARD_CLOCK_LIZARD)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCondition(s.lizcon)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+
+	--Special Summon condition
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.EvilHeroLimit)
+	c:RegisterEffect(e1)
+	--extra attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_EXTRA_ATTACK)
+	e2:SetValue(2)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,1)
+	e3:SetValue(s.aclimit)
+	c:RegisterEffect(e3)
+	--Cannot be destroyed once per turn
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+	e4:SetCountLimit(1)
+	e4:SetValue(s.indct)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x8}
+s.listed_names={CARD_DARK_FUSION}
+s.material_setcode={0x8}
+function s.lizcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),EFFECT_SUPREME_CASTLE)
+end
+function s.aclimit(e,re,tp)
+	return re:IsActiveType(TYPE_MONSTER)
+end
+function s.indct(e,re,r,rp)
+	return (r&REASON_BATTLE+REASON_EFFECT)~=0
+end
