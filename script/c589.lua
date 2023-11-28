0b727c662dbd1f9cbead75ccb76046b4c2137a95
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-28 10:16:38 -0600

    Update c589.lua

--- script/c589.lua
+++ script/c589.lua
@@ -2,11 +2,14 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsType,TYPE_FUSION),nil,s.fextra,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,s.extratg)
+	local e1=Fusion.CreateSummonEff(c,s.fusfilter,nil,s.fextra,nil,nil,nil,nil,nil,nil,nil,nil,nil,nil,s.extratg)
 	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	c:RegisterEffect(e1)
 end
 s.listed_names={CARD_DARK_FUSION}
+function s.fusfilter(c)
+	return c.dark_calling
+end
 function s.fextra(e,tp,mg)
 	if Duel.IsExistingMatchingCard(Card.IsSummonLocation,tp,0,LOCATION_MZONE,1,nil,LOCATION_EXTRA) then
 		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToGrave),tp,LOCATION_DECK,0,nil)
