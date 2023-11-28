9376256362d3fdf532e38987e60129f513b5f176
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-28 14:40:03 -0600

    Add files via upload

---
+++ script/c596.lua
@@ -0,0 +1,45 @@
+--バックアップ・セクレタリー
+--Backup Secretary
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+--Name
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_CHANGE_CODE)
+	e2:SetRange(LOCATION_GRAVE+LOCATION_MZONE+LOCATION_ONFIELD)
+	e2:SetValue(21844576)
+	c:RegisterEffect(e2)
+end
+s.listed_names={CARD_DARK_FUSION,SUPREME_CASTLE,Avian}
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x6008),c:GetControler(),LOCATION_MZONE,0,1,nil)
+end
+function s.filter(c,tp)
+	return c:IsCode(SUPREME_CASTLE) c:GetActivateEffect():IsActivatable(tp,true,true))
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,tp) end
+	if not Duel.CheckPhaseActivity() then Duel.RegisterFlagEffect(tp,CARD_MAGICAL_MIDBREAKER,RESET_CHAIN,0,1) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+								local te=tc:GetActivateEffect()
+								return te:IsActivatable(tp,true,true)
+								Duel.ActivateFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
+end
