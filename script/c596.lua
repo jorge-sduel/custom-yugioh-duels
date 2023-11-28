5cc5f43280b54ad04bd8a0a5c6f9bf0e3db0d731
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-28 16:59:00 -0600

    Update c596.lua

--- script/c596.lua
+++ script/c596.lua
@@ -8,8 +8,8 @@ function s.initial_effect(c)
 	e1:SetCode(EFFECT_SPSUMMON_PROC)
 	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e1:SetRange(LOCATION_HAND)
-	e1:SetTarget(s.target)
-	e1:SetOperation(s.activate)	e1:SetCountLimit(1,id)
+	--e1:SetTarget(s.target)
+	e1:SetOperation(s.activate2)	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.spcon)
 	c:RegisterEffect(e1)
 --Name
@@ -27,8 +27,20 @@ function s.spcon(e,c)
 	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x6008),c:GetControler(),LOCATION_MZONE,0,1,nil)
 end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler() 
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
 function s.filter(c,tp)
-	return c:IsCode(72043279) and c:GetActivateEffect():IsActivatable(tp,true,true)
+	return c:IsType(TYPE_FIELD) and (c:IsAbleToHand() or c:GetActivateEffect():IsActivatable(tp,true,true))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,tp) end
@@ -37,9 +49,13 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()				
-	local te=tc:GetActivateEffect()
-	return te:IsActivatable(tp,true,true)							
-	Duel.ActivateFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
- end
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,tp):GetFirst()
+	aux.ToHandOrElse(tc,tp,function(c)
+								local te=tc:GetActivateEffect()
+								return te:IsActivatable(tp,true,true)
+							end,
+							function(c)
+								Duel.ActivateFieldSpell(tc,e,tp,eg,ep,ev,re,r,rp)
+							end,
+							aux.Stringid(id,2))
 end
