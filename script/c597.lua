2938a2d3a46ee74e3165b0d5c394fb4e6df64117
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-28 19:53:44 -0600

    Add files via upload

---
+++ script/c597.lua
@@ -0,0 +1,37 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+--Name
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_CHANGE_CODE)
+	e2:SetRange(LOCATION_GRAVE+LOCATION_MZONE+LOCATION_ONFIELD)
+	e2:SetValue(58932615)
+	c:RegisterEffect(e2)
+	--effect gain
+	local e3=Effect.CreateEffect(c)
+	e3:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EVENT_BE_MATERIAL)
+	e3:SetCondition(s.atkcon)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+s.listed_names={58932615}
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	return (r&REASON_FUSION)~=0 and rc:IsRace(RACE_FIEND)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local rc=c:GetReasonCard()
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,2))
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(800)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+	rc:RegisterEffect(e2)
+end
