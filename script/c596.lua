4efe0211857e8f992815eb0e24de9c3449680819
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-28 19:58:24 -0600

    Update c596.lua

--- script/c596.lua
+++ script/c596.lua
@@ -15,9 +15,8 @@ function s.initial_effect(c)
 --Name
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetCode(EFFECT_CHANGE_CODE)
-	e2:SetRange(LOCATION_GRAVE+LOCATION_MZONE+LOCATION_ONFIELD)
+	e2:SetRange(LOCATION_GRAVE+LOCATION_HAND+LOCATION_ONFIELD)
 	e2:SetValue(21844576)
 	c:RegisterEffect(e2)
 end
