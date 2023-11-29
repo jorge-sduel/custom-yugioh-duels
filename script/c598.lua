e3746f6bdb9cac412ed5d07b53c4a4975fd17a8b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-11-28 20:50:39 -0600

    Update c598.lua

--- script/c598.lua
+++ script/c598.lua
@@ -6,22 +6,6 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 Fusion.AddProcMixN(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x6008),2)
 Fusion.AddContactProc(c,s.contactfil,s.contactop,nil,nil,nil,nil,false)
---lizard check
-	local e0=Effect.CreateEffect(c)
-	e0:SetType(EFFECT_TYPE_SINGLE)
-	e0:SetCode(CARD_CLOCK_LIZARD)
-	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e0:SetCondition(s.lizcon)
-	e0:SetValue(1)
-	c:RegisterEffect(e0)
-
-	--Special Summon condition
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e1:SetValue(aux.EvilHeroLimit)
-	c:RegisterEffect(e1)
 	--attack up
 	local e2=Effect.CreateEffect(c)
 	e2:SetDescription(aux.Stringid(id,0))
@@ -30,17 +14,23 @@ Fusion.AddContactProc(c,s.contactfil,s.contactop,nil,nil,nil,nil,false)
 	e2:SetCode(EVENT_BATTLE_START)
 	e2:SetCondition(s.condition)
 	e2:SetCost(s.cost)
-	e2:SetTarget(s.target)
+	--e2:SetTarget(s.target)
 	e2:SetOperation(s.operation)
 	c:RegisterEffect(e2)
+		--spsummon
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
 end
 s.listed_series={0x6008}
 s.material_setcode={0x8,0x6008}
-s.listed_names={CARD_DARK_FUSION}
-function s.lizcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(),EFFECT_SUPREME_CASTLE)
-end
 function s.contactfil(tp)
 	return Duel.GetReleaseGroup(tp)
 end
@@ -59,7 +49,6 @@ function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(g:GetFirst():GetAttack())
 	Duel.Release(g,REASON_COST)
 end
-
 function s.operation(e,tp,eg,ep,ev,re,r,rp)
 local c=e:GetHandler()
 		--attack
@@ -68,4 +57,20 @@ local c=e:GetHandler()
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetValue(e:GetLabel())
 	c:RegisterEffect(e1)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x6008) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(s.filter),tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if g:GetCount()>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
 end
