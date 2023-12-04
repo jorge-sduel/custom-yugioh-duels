971d48b6f09ceac8c03857b56e0c569ced510529
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-04 13:27:51 -0600

    Update c999911.lua

--- script/c999911.lua
+++ script/c999911.lua
@@ -36,7 +36,7 @@ function c999911.initial_effect(c)
 	c:RegisterEffect(e4)
 end
 function c999911.filter(c,e,tp)
-	return c:IsSetCard(0x344) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsRed() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function c999911.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c999911.filter(chkc,e,tp) end
