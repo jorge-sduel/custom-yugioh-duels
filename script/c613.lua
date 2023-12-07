6b254786d033010a278434627d294285a7c0b06e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-07 12:27:35 -0600

    Add files via upload

---
+++ script/c613.lua
@@ -0,0 +1,37 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	c:EnableReviveLimit()
+	--Set
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+e1:SetCondition(function(e,c) return not Duel.IsExistingMatchingCard(Card.IsSpellTrap,e:GetHandlerPlayer(),LOCATION_HAND,0,1,nil) end)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.settg)
+	e1:SetOperation(s.setop)
+	c:RegisterEffect(e1)
+end
+function s.filter(c)
+	return c:IsSpellTrap() and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>=2
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,2,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+local conf=Duel.GetFieldGroup(tp,LOCATION_HAND,0)
+	if #conf>0 then
+		Duel.ConfirmCards(tp,conf)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<2 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,2,2,nil)
+	if #g>0 then
+		Duel.SSet(tp,g)
+  end
+	end
+end
