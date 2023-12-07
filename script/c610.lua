c5074aa96f8ee62b91ff5f45c07365d7aedbb76d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-07 09:50:54 -0600

    Add files via upload

---
+++ script/c610.lua
@@ -0,0 +1,69 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	Link.AddProcedure(c,s.matfilter,2,2)
+	--Add 1 "Salamangreat Sanctuary" to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.matfilter(c,scard,sumtype,tp)
+	return c:IsLevelBelow(4)
+end
+function s.thfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsAbleToHand()
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return  Duel.GetLocationCount(tp,LOCATION_PZONE)>2
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,130,0,0,0,0,1,RACE_FAIRY,ATTRIBUTE_FIRE) end
+	Duel.SetOperationInfo(0,CATEGORY_TOKEN,nil,2,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_PZONE)>2
+		and Duel.IsPlayerCanSpecialSummonMonster(tp,130,0,0,0,0,1,RACE_FAIRY,ATTRIBUTE_FIRE) then
+		for i=1,2 do
+			local token=Duel.CreateToken(tp,129+i)
+Duel.MoveToField(token,tp,tp,LOCATION_PZONE,POS_FACEUP,true) 
+			--Duel.SpecialSummonStep(token,0,tp,tp,false,false,POS_FACEUP)
+		end
+		--Duel.SpecialSummonComplete()
+	end
+end
