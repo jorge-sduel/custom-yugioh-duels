513cfd4633d63d881cb5447916374a82c2e8478f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-12 13:52:10 -0600

    Add files via upload

---
+++ script/c624.lua
@@ -0,0 +1,65 @@
+ -- スカーレッド・デーモン
+-- Scar-Red Dragon Archfiend
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	-- Synchro Summon procedure
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTunerEx(Card.IsAttribute,ATTRIBUTE_DARK),1,99)
+	-- Name becomes "Red Dragon Archfiend" while on the field on in the GY
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGEh)
+	e0:SetCode(EFFECT_CHANGE_CODE)
+	e0:SetValue(CARD_RED_DRAGON_ARCHFIEND)
+	c:RegisterEffect(e0)
+	--destroy1
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BATTLED)
+	e1:SetCondition(s.condition1)
+	e1:SetTarget(s.destg)
+	e1:SetOperation(s.desop)
+	c:RegisterEffect(e1)
+	--recover
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_RECOVER)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e3:SetCode(EVENT_PHASE+PHASE_END)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1)
+	e3:SetOperation(s.operation)
+	c:RegisterEffect(e3)
+end
+function s.condition1(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetAttacker()==e:GetHandler() and Duel.GetAttackTarget() 
+end
+function s.filter(c,atk)
+	return c:IsMonster()
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,c,c:GetAttack()) end
+	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,c,c:GetAttack())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,1-tp,#g*500)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,c,c:GetAttack())
+	local ct=Duel.Destroy(g,REASON_EFFECT)
+	if ct>0 then
+		Duel.BreakEffect()
+		--(1-tp,ct*500,REASON_EFFECT)
+	end
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.Damage(1-tp,c:GetAttack(),REASON_EFFECT)
+	end
+end
