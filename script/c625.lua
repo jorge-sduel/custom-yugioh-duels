8322aad61091a90f5e3d26dc8b87abae9542145b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-13 13:56:03 -0600

    Update c625.lua

--- script/c625.lua
+++ script/c625.lua
@@ -6,9 +6,17 @@ function s.initial_effect(c)
 	e1:SetCategory(CATEGORY_DESTROY+CATEGORY_DAMAGE)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCondition(s.condition)
 	e1:SetTarget(s.destg)
 	e1:SetOperation(s.desop)
 	c:RegisterEffect(e1)
+end
+s.listed_names={70902743}
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsCode(70902743)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsFaceup() end
