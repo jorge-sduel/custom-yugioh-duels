47d0f83f429156af893719bb02b852542c7047bb
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-18 14:41:49 -0600

    Update proc_rune.lua

--- script/proc_rune.lua
+++ script/proc_rune.lua
@@ -43,11 +43,6 @@ function Rune.AddProcedure(c,monf,mmin,mmax,stf,smin,smax,loc,group,condition,ex
 		--mt.rune_monster_filter=function(c) end
 		mt.rune_parameters={}
 		table.insert(mt.rune_parameters,{monf,mmin,mmax,stf,smin,smax,loc+LOCATION_HAND,group,condition,excondition,specialchk,customoperation,stage2})
-	end
-	
-	local e1=Rune.CreateProcedure(c,monf,mmin,mmax,stf,smin,smax,loc,group,condition,specialchk,customoperation,stage2)
-	c:RegisterEffect(e1)
-	--synchro custom
 	local e3=Effect.CreateEffect(c)
 	e3:SetType(EFFECT_TYPE_SINGLE)
 	e3:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
@@ -75,10 +70,42 @@ function Rune.AddProcedure(c,monf,mmin,mmax,stf,smin,smax,loc,group,condition,ex
 	e6:SetCode(EFFECT_ALLOW_NEGATIVE)
 	e6:SetCondition(Rune.Levelcon)
 	c:RegisterEffect(e6)
+	end
+	
+	local e1=Rune.CreateProcedure(c,monf,mmin,mmax,stf,smin,smax,loc,group,condition,specialchk,customoperation,stage2)
+	c:RegisterEffect(e1)
+	--[[synchro custom
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e3:SetValue(Rune.synlimit)
+	c:RegisterEffect(e3)
+--
+       local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e4:SetCode(EFFECT_LEVEL_RANK)
+	c:RegisterEffect(e4)
+--
+        local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e5:SetCode(EFFECT_CHANGE_LEVEL)
+	e5:SetCondition(Rune.Levelcon)
+	e5:SetValue(0)
+	c:RegisterEffect(e5)
+--
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e6:SetCode(EFFECT_ALLOW_NEGATIVE)
+	e6:SetCondition(Rune.Levelcon)
+	c:RegisterEffect(e6)]]
 	if loc then
 		local e2=Rune.CreateSecondProcedure(c,monf,mmin,mmax,stf,smin,smax,loc,group,condition,excondition,specialchk,customoperation,stage2)
 		c:RegisterEffect(e2)
-		--synchro custom
+		--[[synchro custom
 	local e7=Effect.CreateEffect(c)
 	e7:SetType(EFFECT_TYPE_SINGLE)
 	e7:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
@@ -105,7 +132,7 @@ function Rune.AddProcedure(c,monf,mmin,mmax,stf,smin,smax,loc,group,condition,ex
 	e10:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 	e10:SetCode(EFFECT_ALLOW_NEGATIVE)
 	e10:SetCondition(Rune.Levelcon)
-	c:RegisterEffect(e10)
+	c:RegisterEffect(e10)]]
 	end
 end
 function Rune.AddSecondProcedure(c,monf,mmin,mmax,stf,smin,smax,loc,group,condition,excondition,specialchk,customoperation,stage2)
@@ -711,8 +738,8 @@ function Card.IsCanBeRuneGroup(c,chain)
 	if not chain then chain=Duel.GetCurrentChain() end
 	return c:IsFaceup() and (chain~=1 or not c:IsStatus(STATUS_LEAVE_CONFIRMED))
 end
-function Rune.Levelcon(c,e)
-	return  not e:GetHandler():IsHasEffect(999381000,tp)
+function Rune.Levelcon(e,c)
+	return  not c:IsHasEffect(999381000)
 end
 function Rune.synlimit(e,c)
 	if not c then return false end
