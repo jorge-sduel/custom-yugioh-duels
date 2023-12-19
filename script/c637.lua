beb021d85df05d610d64a3a1fa36b72e92b1c924
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-19 08:27:05 -0600

    Update c637.lua

--- script/c637.lua
+++ script/c637.lua
@@ -37,6 +37,16 @@ function s.initial_effect(c)
 	e5:SetTarget(s.sptg)
 	e5:SetOperation(s.spop)
 	c:RegisterEffect(e5)
+		--Special Summon procedure
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetCode(EFFECT_SPSUMMON_PROC)
+	e6:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e6:SetRange(LOCATION_HAND)
+	e6:SetCondition(s.spcon1)
+	e6:SetTarget(s.sptg1)
+	e6:SetOperation(s.spop1)
+	c:RegisterEffect(e6)
 end
 s.listed_series={0xe3}
 s.listed_names={CARD_VIJAM,id}
@@ -115,4 +125,31 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if tc and tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
+end
+function s.tgfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0xe3) and c:IsAbleToGraveAsCost()
+end
+function s.spcon1(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local rg=Duel.GetMatchingGroup(s.tgfilter,tp,LOCATION_MZONE,0,nil)
+	return #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,99,aux.ChkfMMZ(1),0)
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	local g=nil
+	local rg=Duel.GetMatchingGroup(s.tgfilter,tp,LOCATION_MZONE,0,nil)
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,99,aux.ChkfMMZ(1),1,tp,HINTMSG_TOGRAVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.SendtoGrave(g,REASON_COST)
+	g:DeleteGroup()
 end
