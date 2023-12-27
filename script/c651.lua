dc8af4027d7197b73f366ca16306e3a21105ce9b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-27 10:08:22 -0600

    Update c651.lua

--- script/c651.lua
+++ script/c651.lua
@@ -28,7 +28,7 @@ function s.initial_effect(c)
 end
 s.listed_series={0xb4}
 function s.filter(c)
-	return (c:IsSetCard(0xb4) or c:IsAttribute(ATTRIBUTE_WATER)) and c:IsAbleToDeck()
+	return (c:IsSetCard(0xb4) or (c:IsAttribute(ATTRIBUTE_WATER) and c:IsType(TYPE_RITUAL))) and c:IsAbleToDeck()
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil) end
