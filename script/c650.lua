4cf68f3455cc0e3d38a850797ea81ca0d7c95736
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-26 21:35:10 -0600

    Update c650.lua

--- script/c650.lua
+++ script/c650.lua
@@ -31,7 +31,7 @@ function s.cfilter(c,e,tp,g,maxc)
 end
 function s.costfilter(c,lv)
 	local clv=c:GetLevel()
-	return c:IsAbleToGraveAsCost()
+	return c:IsHasLevel() and c:IsAbleToGraveAsCost()
 end
 function s.lvcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	local lv=e:GetHandler():GetLevel()
