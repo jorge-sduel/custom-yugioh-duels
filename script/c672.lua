a58ea5af1886907acdc162350d90ad5d544afdb6
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-28 17:14:41 -0600

    Add files via upload

---
+++ script/c672.lua
@@ -0,0 +1,69 @@
+--失楽の聖女
+--Condemned Maiden
+local s,id=GetID()
+function s.initial_effect(c)
+	--act qp in hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(LOCATION_HAND,0)
+	e1:SetCountLimit(1,id)
+	e1:SetDescription(aux.Stringid(id,0))
+	c:RegisterEffect(e1)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_PREDRAW)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCondition(s.condition)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0xb4}
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return tp==Duel.GetTurnPlayer() and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0
+		and Duel.GetDrawCount(tp)>0
+end
+function s.filter(c)
+	return c:IsSetCard(0xb4) and c:IsRitualMonster() and c:IsAbleToHand()
+end
+function s.filter2(c)
+	return c:IsSetCard(0xb4) and c:IsRitualSpell() and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) end
+	local dt=Duel.GetDrawCount(tp)
+	if dt~=0 then
+		_replace_count=0
+		_replace_max=dt
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_FIELD)
+		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+		e1:SetCode(EFFECT_DRAW_COUNT)
+		e1:SetTargetRange(1,0)
+		e1:SetReset(RESET_PHASE+PHASE_DRAW)
+		e1:SetValue(0)
+		Duel.RegisterEffect(e1,tp)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,0,LOCATION_DECK)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	_replace_count=_replace_count+1
+	if _replace_count>_replace_max or not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK,0,nil)
+	local g2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil)
+	if #g>=1 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		local sg2=g2:Select(tp,1,1,nil)
+		Duel.ConfirmCards(1-tp,sg)
+		Duel.ConfirmCards(1-tp,sg2)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.SendtoHand(sg2,nil,REASON_EFFECT) 
+		Duel.ShuffleDeck(tp)
+	end
+end
