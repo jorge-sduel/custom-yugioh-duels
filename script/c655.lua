2d53c38140d705291bd4881678db2308b65b1450
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-29 15:59:54 -0600

    Update c655.lua

--- script/c655.lua
+++ script/c655.lua
@@ -16,7 +16,7 @@ function s.exfilter01(c)
 	return c:IsSummonLocation(LOCATION_EXTRA)
 end
 function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
-	if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 and Duel.GetMatchingGroup(s.exfilter01,tp,0,LOCATION_MZONE,nil) then
+	if Duel.GetMatchingGroup(s.exfilter01,tp,0,LOCATION_MZONE,nil) then
 		return Duel.GetMatchingGroup(s.exfilter0,tp,LOCATION_DECK,0,nil)
 	end
 end
