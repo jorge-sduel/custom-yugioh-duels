7d06cb1d53d008c3100a14188be4dca9761f2032
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2023-12-29 15:50:36 -0600

    Add files via upload

---
+++ script/c655.lua
@@ -0,0 +1,31 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	local e1=Ritual.CreateProc({handler=c,lvtype=RITPROC_EQUAL,filter=s.ritualfil,extrafil=s.extrafil,extraop=s.extraop,matfilter=s.forcedgroup,location=LOCATION_REMOVED+LOCATION_DECK})
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0xb4}
+function s.ritualfil(c)
+	return c:IsSetCard(0xb4)
+end
+function s.exfilter0(c)
+	return c:IsLevelAbove(1) and c:IsAbleToGrave()
+end
+function s.exfilter01(c)
+	return c:IsSummonLocation(LOCATION_EXTRA)
+end
+function s.extrafil(e,tp,eg,ep,ev,re,r,rp,chk)
+	if Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 then
+		return Duel.GetMatchingGroup(s.exfilter0,tp,LOCATION_DECK,0,nil) and Duel.GetMatchingGroup(s.exfilter01,tp,0,LOCATION_MZONE,nil)
+	end
+end
+function s.extraop(mg,e,tp,eg,ep,ev,re,r,rp)
+	local mat2=mg:Filter(Card.IsLocation,nil,LOCATION_DECK)
+	mg:Sub(mat2)
+	Duel.ReleaseRitualMaterial(mg)
+	Duel.SendtoGrave(mat2,REASON_EFFECT+REASON_MATERIAL+REASON_RITUAL)
+end
+function s.forcedgroup(c,e,tp)
+	return (c:IsLocation(LOCATION_HAND+LOCATION_ONFIELD)) or (c:IsLocation(LOCATION_DECK))
+end
