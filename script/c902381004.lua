67aa0c910e95108cd2e03906080f8c1300ffb05a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-01 20:38:03 -0600

    Update c902381004.lua

--- script/c902381004.lua
+++ script/c902381004.lua
@@ -1,7 +1,7 @@
 --Shimzu Kujira
+local s,id=GetID()
 s.Is_Runic=true
 if not Rune then Duel.LoadScript("proc_rune.lua") end
-local s,id=GetID()
 function s.initial_effect(c)
 	--rune
 	c:EnableReviveLimit()
