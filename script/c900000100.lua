83445dc1f0a660c08792f6065ad9ddbdc542dd73
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-02 09:21:01 -0600

    Add files via upload

--- script/c900000100.lua
+++ script/c900000100.lua
@@ -5,22 +5,20 @@ function s.initial_effect(c)
 	--direct attack
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_EQUIP)
-	e2:SetCode(EFFECT_ATTACK_ALL)
-	e2:SetValue(s.atkfilter)
+	e2:SetCode(EFFECT_EXTRA_ATTACK_MONSTER)
+	e2:SetValue(1)
 	c:RegisterEffect(e2)
 	--set
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(2521011,0))
+	e3:SetDescription(aux.Stringid(id,1))
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_SUMMON_SUCCESS)
-	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
 	e3:SetTarget(s.settg)
 	e3:SetOperation(s.setop)
 	c:RegisterEffect(e3)
 end
-function s.atkfilter(e,c)
-	return c:IsFaceup()
-end
+s.listed_series={0xff6}
 function s.filter(c)
 	return c:IsSetCard(0xff6) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsSSetable()
 end
@@ -33,6 +31,5 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
 	if g:GetCount()>0 then
 		Duel.SSet(tp,g:GetFirst())
-		Duel.ConfirmCards(1-tp,g)
 	end
 end
--- script/c900000110.lua
+++ script/c900000110.lua
@@ -1,21 +1,24 @@
 --Grandmaster Osignis Ignile
+if not Rune then Duel.LoadScript("proc_rune.lua") end
 local s,id=GetID()
 function s.initial_effect(c)
-	Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsType,TYPE_UNION),s.ffilter)
-	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit,nil,nil,nil,false)
-	--activate limit
+	--fusion procedure
+	c:EnableReviveLimit()
+	Fusion.AddProcMixRep(c,true,true,aux.FilterBoolFunctionEx(Card.IsType,TYPE_UNION),1,99,s.ffilter)
+	Fusion.AddContactProc(c,s.contactfil,s.contactop,s.splimit)
+	--cannot be target
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetTargetRange(0,1)
-	e1:SetCondition(s.condition)
-	e1:SetValue(s.aclimit)
+	e1:SetTargetRange(LOCATION_ONFIELD,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_SPELL))
+	e1:SetValue(aux.tgoval)
 	c:RegisterEffect(e1)
 	--equip
 	local e2=Effect.CreateEffect(c)
-	e2:SetDescription(aux.Stringid(95090813,3))
+	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetCategory(CATEGORY_LEAVE_GRAVE+CATEGORY_EQUIP)
 	e2:SetType(EFFECT_TYPE_IGNITION)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
@@ -24,32 +27,22 @@ function s.initial_effect(c)
 	e2:SetTarget(s.eqtg)
 	e2:SetOperation(s.eqop)
 	c:RegisterEffect(e2)
-	aux.AddEREquipLimit(c,nil,s.eqval,s.equipop,e2)
-end
-function s.splimit(e,se,sp,st)
-	return (st&SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION or e:GetHandler():GetLocation()~=LOCATION_EXTRA 
-end
-function s.contactfil(tp)
-	return Duel.GetReleaseGroup(tp)
-end
-function s.contactop(g)
-	Duel.Release(g,REASON_COST+REASON_MATERIAL)
+	aux.AddEREquipLimit(c,nil,s.eqval,s.eqop,e2)
 end
 function s.ffilter(c,fc,sumtype,tp)
-	return c:IsAttribute(ATTRIBUTE_FIRE,nil,sumtype,tp) and c.Is_Runic
+	return c:IsAttribute(ATTRIBUTE_FIRE,nil,sumtype,tp) and c:IsType(TYPE_RUNE,fc,sumtype,tp)
 end
 function s.contactfil(tp)
-	return Duel.GetReleaseGroup(tp)
+	return Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,nil)
 end
-function s.condition(e)
-	local ph=Duel.GetCurrentPhase()
-	return ph>=PHASE_BATTLE_START and ph<=PHASE_BATTLE
+function s.contactop(g)
+	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
 end
-function s.aclimit(e,re,tp)
-	return re:IsActiveType(TYPE_SPELL+TYPE_TRAP) and not re:GetHandler():IsImmuneToEffect(e)
+function s.splimit(e,se,sp,st)
+	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
 end
 function s.eqfilter(c)
-	return c:IsSetCard(0xff6) and not c:IsForbidden()
+	return c:IsSetCard(0xff6) and c:IsType(TYPE_UNION) and not c:IsForbidden()
 end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.eqfilter(chkc) end
@@ -57,7 +50,7 @@ function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 		and Duel.IsExistingTarget(s.eqfilter,tp,LOCATION_GRAVE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
 	local g=Duel.SelectTarget(tp,s.eqfilter,tp,LOCATION_GRAVE,0,1,1,nil)
-	--Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
@@ -65,13 +58,10 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
 	if c:IsFaceup() and c:IsRelateToEffect(e) and tc and tc:IsRelateToEffect(e) then
-		Card.EquipByEffectAndLimitRegister(c,e,tp,tc)
-		aux.SetUnionState(c)
+		c:EquipByEffectAndLimitRegister(e,tp,tc)
+		aux.SetUnionState(tc)
 	end
 end
 function s.eqval(ec,c,tp)
-	return ec:IsControler(tp) and ec:IsSetCard(0xff6)
-end
-function s.equipop(c,e,tp,tc)
-	Card.EquipByEffectAndLimitRegister(c,e,tp,tc,nil,true)
+	return ec:IsType(TYPE_UNION) and ec:IsSetCard(0xff6) and ec:IsControler(tp)
 end
