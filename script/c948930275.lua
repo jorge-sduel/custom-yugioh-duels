ad37399c93313ace153212312c5e49798d83113f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-03 09:07:47 -0600

    Update c948930275.lua

--- script/c948930275.lua
+++ script/c948930275.lua
@@ -1,58 +1,59 @@
 --Lost Runic Circle
-if not RUNIC_IMPORTED then Duel.LoadScript("proc_runic.lua") end
-function c948930275.initial_effect(c)
+if not Rune then Duel.LoadScript("proc_rune.lua") end
+local s,id=GetID()
+function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetTarget(c948930275.target)
-	e1:SetOperation(c948930275.activate)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
 end
-function c948930275.filter(c,e,tp)
-	return c.Is_Runic and c:IsRankBelow(6)
-		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RUNIC,tp,false,true)
+function s.filter(c,e,tp)
+	return c:IsType(TYPE_RUNE) and c:IsLevelBelow(6)
+		and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_RUNE,tp,false,true)
 end
-function c948930275.target(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(c948930275.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
 end
-function c948930275.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,c948930275.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	local tc=g:GetFirst()
 	if not tc then return end
 	local mg=Group.FromCards(e:GetHandler())
 	tc:SetMaterial(mg)
-	if Duel.SpecialSummon(tc,SUMMON_TYPE_RUNIC,tp,tp,false,true,POS_FACEUP)~=0 then
+	if Duel.SpecialSummon(tc,SUMMON_TYPE_RUNE,tp,tp,false,true,POS_FACEUP)~=0 then
 		local fid=e:GetHandler():GetFieldID()
 		tc:CompleteProcedure()
-		tc:RegisterFlagEffect(948930275,RESET_EVENT+0x1fe0000,0,1,fid)
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,1,fid)
 		local e2=Effect.CreateEffect(e:GetHandler())
 		e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 		e2:SetCode(EVENT_PHASE+PHASE_END)
 		e2:SetCountLimit(1)
 		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 		e2:SetLabelObject(tc)
-		e2:SetCondition(c948930275.descon)
-		e2:SetOperation(c948930275.desop)
+		e2:SetCondition(s.descon)
+		e2:SetOperation(s.desop)
 		Duel.RegisterEffect(e2,tp)
 	end
 	mg:DeleteGroup()
 end
-function c948930275.descon(e,tp,eg,ep,ev,re,r,rp)
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
-	if tc:GetFlagEffect(948930275)~=0 then
+	if tc:GetFlagEffect(id)~=0 then
 		return true
 	else
 		e:Reset()
 		return false
 	end
 end
-function c948930275.desop(e,tp,eg,ep,ev,re,r,rp)
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=e:GetLabelObject()
 	Duel.Destroy(tc,REASON_EFFECT)
 end
