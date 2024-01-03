4192575cb745356fed936fb28dd048a14cdd3b53
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-02 18:55:11 -0600

    Update c999381000.lua

--- script/c999381000.lua
+++ script/c999381000.lua
@@ -1,13 +1,14 @@
 --Greenwood Dragon
-if not RUNIC_IMPORTED then Duel.LoadScript("proc_runic.lua") end
+if not Rune then Duel.LoadScript("proc_rune.lua") end
 local s,id=GetID()
 function s.initial_effect(c)
 	--effect
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(95492061,0))
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e1:SetType(EFFECT_TYPE_TRIGGER_O+EFFECT_TYPE_SINGLE)
 	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCost(s.cost)
 	e1:SetTarget(s.tg)
 	e1:SetOperation(s.op)
 	c:RegisterEffect(e1)
@@ -29,7 +30,7 @@ function s.initial_effect(c)
 	e4:SetCode(id)
 	e4:SetRange(LOCATION_MZONE)
 	e4:SetTargetRange(LOCATION_HAND+LOCATION_MZONE,0)
-	e4:SetTarget(aux.TargetBoolFunction(Card.IsRunic))
+	e4:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_RUNE))
 	c:RegisterEffect(e4)
 	--level
 	local e5=Effect.CreateEffect(c)
@@ -41,39 +42,32 @@ function s.initial_effect(c)
 	--e5:SetValue(0x5sr)
 	c:RegisterEffect(e5)
 end
-function s.Level(e,c)
-	return c:GetRank()
+function s.rvfilter(c,tp)
+	return c:IsType(TYPE_SPELL+TYPE_TRAP) and not c:IsPublic()
+		and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,c,tp)
 end
-function s.rvfilter(c)
-	return c:IsType(TYPE_SPELL+TYPE_TRAP)
--- and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil,c)
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.rvfilter,tp,LOCATION_HAND,0,1,nil,tp) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+    local g=Duel.SelectMatchingCard(tp,s.rvfilter,tp,LOCATION_HAND,0,1,1,nil,tp)
+	e:SetLabelObject(g:GetFirst())
+    Duel.ConfirmCards(1-tp,g)
+    Duel.ShuffleHand(tp)
 end
-function s.thfilter(c)
-	--if not c.runic_parameters or not c.Is_Runic then return false end
-	--local f1=c.runic_parameters[6]
-	--Owns Extra Runic Parameters
-	--if c.ex_runic_parameters then
-		--local f2=c.ex_runic_parameters[6]
-		return c:IsAbleToHand() and c.Is_Runic
---and (not f1 or f1(mc) or not f2 or f2(mc))
-	--else return c:IsAbleToHand() and (not f1 or f1(mc)) end
+function s.thfilter(c,mc,tp)
+	return c:IsType(TYPE_RUNE) and c:IsAbleToHand()
+		and mc:IsCanBeRuneMaterial(c,tp)
 end
 function s.tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.rvfilter,tp,LOCATION_HAND,0,1,nil) end
+	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.op(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local g=Duel.SelectMatchingCard(tp,s.rvfilter,tp,LOCATION_HAND,0,1,1,nil)
-	Duel.ConfirmCards(1-tp,g)
-	Duel.ShuffleHand(tp)
-	local mc=g:GetFirst()
-	if mc then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-		local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,mc)
-		if g:GetCount()>0 then
-			Duel.SendtoHand(g,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,g)
-		end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local mc=e:GetLabelObject()
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil,mc,tp)
+	if g:GetCount()>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
