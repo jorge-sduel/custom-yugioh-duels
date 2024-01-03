bbb2501b15befd26aa6e88c1043c753e45f70f2e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-03 16:46:40 -0600

    Update c982391022.lua

--- script/c982391022.lua
+++ script/c982391022.lua
@@ -12,7 +12,7 @@ function c982391022.initial_effect(c)
 	c:RegisterEffect(e1)
 end
 function c982391022.filter(c,e,tp)
-	return c.Is_Runic and c:IsSpecialSummonable() or (c:IsLocation(LOCATION_GRAVE) and c:IsAbleToHand())
+	return (c.Is_Runic or c:IsType(TYPE_RUNE)) and c:IsSpecialSummonable() or (c:IsLocation(LOCATION_GRAVE) and c:IsAbleToHand())
 end
 function c982391022.xyztg(e,tp,eg,ep,ev,re,r,rp,chk)
  if chk==0 then return Duel.IsExistingMatchingCard(c982391022.filter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil) end
@@ -24,6 +24,6 @@ function c982391022.xyzop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=g:GetFirst()
 	if tc then
 		Duel.SendtoHand(tc,nil,REASON_EFFECT)
-	   Duel.SpecialSummonRule(tp,tc,SUMMON_TYPE_RUNIC)
+	   Duel.SpecialSummonRule(tp,tc,SUMMON_TYPE_RUNE)
 	end
 end
