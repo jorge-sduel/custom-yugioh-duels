c51a8dcdd5a9f756fc7dd43a117345f860ae2a99
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-05 09:48:04 -0600

    Add files via upload

---
+++ script/c660.lua
@@ -0,0 +1,167 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Cannot be special summoned
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e1:SetValue(aux.ritlimit)
+	c:RegisterEffect(e1)
+	--Add to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetCountLimit(1,id)
+	e2:SetCost(s.thcost)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+	--Special summon procedure
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_SPSUMMON_PROC)
+	e3:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e3:SetRange(LOCATION_HAND)
+	e3:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e3:SetValue(SUMMON_TYPE_RITUAL)
+	e3:SetCondition(s.spcon1)
+	e3:SetTarget(s.sptg1)
+	e3:SetOperation(s.spop1)
+	c:RegisterEffect(e3)
+	--Summon Thunder Bullet
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,0))
+	e6:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE+CATEGORY_DESTROY)
+	e6:SetType(EFFECT_TYPE_QUICK_O)
+	e6:SetCode(EVENT_FREE_CHAIN)
+	e6:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e6:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	e6:SetCost(s.atkcost)
+	e6:SetCondition(s.atkcon)
+	e6:SetTarget(s.atktg)
+	e6:SetOperation(s.atkop)
+	c:RegisterEffect(e6)
+--X000
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetProperty(EFFECT_FLAG_UNCOPYABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e8:SetCode(513000136)
+	c:RegisterEffect(e8)
+	--Gain ATK/DEF
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_SINGLE)
+	e9:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_UNCOPYABLE)
+	e9:SetRange(LOCATION_MZONE)
+	e9:SetCode(EFFECT_SET_BASE_ATTACK)
+	e9:SetCondition(function(e) return e:GetHandler():IsHasEffect(513000136) end)
+	e9:SetValue(s.adval)
+	c:RegisterEffect(e9)
+	local e10=e9:Clone()
+	e10:SetCode(EFFECT_SET_BASE_DEFENSE)
+	c:RegisterEffect(e10)
+end
+s.listed_series={0xb4}
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+function s.filter(c,e,tp,spchk)
+	return c:IsSetCard(0xb4) and c:IsMonster() and (c:IsAbleToHand() or Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false))
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(1-tp) and chkc:IsLocation(LOCATION_REMOVED) and s.filter(c,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_REMOVED,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_REMOVED,0,1,1,nil)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_TOHAND,g,1,tp,LOCATION_REMOVED)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,tp,LOCATION_REMOVED)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		aux.ToHandOrElse(tc,tp,
+			function(c)
+				return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+					and tc:IsCanBeSpecialSummoned(e,0,tp,false,false)
+			end,
+			function(c)
+				Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+			end,
+		1152)
+	end
+end
+function s.spfilter(c,ft)
+	return c:IsFaceup() and c:IsLevel(10) and c:IsAbleToRemoveAsCost() and (ft>0 or c:GetSequence()<5)
+end
+function s.spcon1(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil,ft)
+	return ft>-1 and #rg>0 and aux.SelectUnselectGroup(rg,e,tp,1,1,nil,0)
+end
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,c)
+	local c=e:GetHandler()
+	local g=nil
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_MZONE,0,nil,Duel.GetLocationCount(tp,LOCATION_MZONE))
+	local g=aux.SelectUnselectGroup(rg,e,tp,1,1,nil,1,tp,HINTMSG_REMOVE,nil,nil,true)
+	if #g>0 then
+		g:KeepAlive()
+		e:SetLabelObject(g)
+		return true
+	end
+	return false
+end
+function s.spop1(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=e:GetLabelObject()
+	if not g then return end
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	g:DeleteGroup()
+end
+function s.adval(e,c)
+	return Duel.GetFieldGroupCount(c:GetControler(),LOCATION_HAND,0)*1000
+end
+function s.atkcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsMonster,1,false,nil,nil,1,tp) end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsMonster,1,1,false,nil,nil,1,tp)
+	Duel.Release(g,REASON_COST)
+end
+function s.atkfilter(c,p,e)
+	return c:IsControler(p) and c:IsFaceup() and c:IsCanBeEffectTarget(e)
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	--if chkc then return end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.atkfilter,tp,0,LOCATION_MZONE,1,nil) end		local g=Duel.GetMatchingGroup(s.atkfilter,tp,0,LOCATION_MZONE,nil)
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.atkfilter,tp,0,LOCATION_MZONE,nil)
+	if #g==0 then return end
+	local dg=Group.CreateGroup()
+	local c=e:GetHandler()
+	for tc in aux.Next(g) do
+			local e1=Effect.CreateEffect(c)
+			e1:SetType(EFFECT_TYPE_SINGLE)
+			e1:SetCode(EFFECT_UPDATE_ATTACK)
+			e1:SetValue(-2000)
+			e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+			tc:RegisterEffect(e1)
+			local e2=Effect.CreateEffect(c)
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_UPDATE_DEFENSE)
+			e2:SetValue(-2000)
+			e2:SetReset(RESET_EVENT|RESETS_STANDARD)
+			tc:RegisterEffect(e2)
+			if (tc:GetAttack()==0 or tc:GetDefense()==0) and c:GetMaterialCount()>=3 then dg:AddCard(tc)
+		end
+	end
+	if #dg==0 then return end
+	Duel.BreakEffect()
+	local ct=Duel.Destroy(dg,REASON_EFFECT)
+	Duel.Draw(tp,ct,REASON_EFFECT)
+end
