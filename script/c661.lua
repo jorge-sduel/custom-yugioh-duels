2570910e95b40aa5eed19e884ae9e30f5000926d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-06 09:36:01 -0600

    Add files via upload

---
+++ script/c661.lua
@@ -0,0 +1,118 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	-- material
+	c:EnableReviveLimit()
+	--spsummon condition
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_CONDITION)
+	e0:SetValue(aux.ritlimit)
+	c:RegisterEffect(e0)
+	--Cannot be destroyed
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_MATERIAL_CHECK)
+	e1:SetValue(s.valcheck)
+	c:RegisterEffect(e1)
+	--Your opponent takes all battle damage from battles involving this monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_REFLECT_BATTLE_DAMAGE)
+	e2:SetValue(1)
+	c:RegisterEffect(e2)
+	--atk
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DELAY)
+	e3:SetCode(EVENT_DAMAGE)
+	e3:SetCondition(s.atkcon)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+	-- Inflict Damage
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,1))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e4:SetCode(EVENT_LEAVE_FIELD)
+	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCost(s.lpcost)
+	e4:SetTarget(s.lptg)
+	e4:SetOperation(s.lpop)
+	c:RegisterEffect(e4)
+end
+s.listed_names={21082832}
+function s.valcheck(e,c)
+	if c:GetMaterial():GetType()==TYPE_RITUAL  then
+local e2=Effect.CreateEffect(c)
+    	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+    	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    	e2:SetRange(LOCATION_MZONE)
+    	e2:SetValue(s.efilter)
+e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+    	c:RegisterEffect(e2,true)
+	--immune
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetValue(aux.tgoval)
+e3:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+	c:RegisterEffect(e3,true)
+	end
+end
+function s.efilter(e,te)
+	return te:GetOwnerPlayer()~=e:GetHandlerPlayer()
+end
+function s.filter2(c)
+	return c:IsFaceup()
+end
+function s.atkcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFaceup() and c:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(ev)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+		local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_MZONE,LOCATION_MZONE,c)
+		local lc=g:GetFirst()
+		for lc in aux.Next(g) do
+			local e2=Effect.CreateEffect(e:GetHandler())
+			e2:SetType(EFFECT_TYPE_SINGLE)
+			e2:SetCode(EFFECT_UPDATE_ATTACK)
+			e2:SetValue(-ev)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			lc:RegisterEffect(e2)
+		end
+	end
+end
+function s.lpcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsAbleToEnterBP() end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	e1:SetTargetRange(1,1)
+	e1:SetValue(0)
+	c:RegisterEffect(e1)
+end
+function s.lptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLP(1-tp)>1 and Duel.GetLP(tp)>1 end
+	Duel.SetTargetPlayer(1-tp)
+end
+function s.lpop(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	Duel.SetLP(p,1)
+	Duel.SetLP(tp,1)
+end
