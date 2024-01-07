c6aa651d43ce4501f7e8c8df16bccde17d8df68d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-07 17:07:03 -0600

    Update c663.lua

--- script/c663.lua
+++ script/c663.lua
@@ -29,7 +29,7 @@ function s.initial_effect(c)
 	--spsummon
 	local e2=Effect.CreateEffect(c)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetDescription(aux.Stringid(id,2))
 	e2:SetType(EFFECT_TYPE_QUICK_O)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetRange(LOCATION_MZONE)
