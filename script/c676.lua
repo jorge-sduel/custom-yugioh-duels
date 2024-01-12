b4fbaa4906ba31af167cdc5ba45aa525b2f7a75a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-12 09:15:59 -0600

    Update c676.lua

--- script/c676.lua
+++ script/c676.lua
@@ -3,31 +3,34 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
-	local e1=Fusion.CreateSummonEff(c,aux.FilterBoolFunction(Card.IsType,TYPE_MONSTER),nil,s.fextra,nil,nil,s.stage2,nil,nil,nil,nil,nil,nil,nil,s.extratg)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsRace,RACE_CYBERSE),
+		extrafil=s.fextra,extraop=s.extraop,extratg=s.extratg})
 	c:RegisterEffect(e1)
 end
 s.listed_names={15259703}
+function s.fcheck(tp,sg,fc)
+	return sg:FilterCount(Card.IsLocation,nil,LOCATION_GRAVE)>1
+end
+function s.exfilter0(c)
+	return c:IsAbleToRemove()
+end
 function s.fextra(e,tp,mg)
-	if Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_ONFIELD,0,1,nil,15259703) then
-		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,LOCATION_GRAVE,nil)
+	if Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_ONFIELD,0,1,nil,15259703) and not Duel.IsPlayerAffectedByEffect(tp,CARD_SPIRIT_ELIMINATION) then
+		local eg=Duel.GetMatchingGroup(s.exfilter0,tp,LOCATION_MZONE|LOCATION_GRAVE,LOCATION_GRAVE,nil)
+		if #eg>0 then
+			return nil
+		end
 	end
 	return nil
 end
+function s.extraop(e,tc,tp,sg)
+	local rg=sg:Filter(Card.IsLocation,nil,LOCATION_GRAVE)
+	if #rg>0 then
+		Duel.Remove(rg,POS_FACEUP,REASON_EFFECT|REASON_MATERIAL|REASON_FUSION)
+		sg:Sub(rg)
+	end
+end
 function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	Duel.SetPossibleOperationInfo(0,CATEGORY_TOGRAVE,nil,0,tp,LOCATION_DECK)
-end
-function s.stage2(e,tc,tp,sg,chk)
-	if chk==1 then
-		--Cannot be targeted by opponent's card effects
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetDescription(3061)
-		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetCode(EFFECT_ADD_TYPE)
-		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(TYPE_TOON)
-		tc:RegisterEffect(e1)
-	end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_GRAVE)
 end
