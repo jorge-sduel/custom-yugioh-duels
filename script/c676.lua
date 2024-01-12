bbed8fe8d4522231e0225957b318ef4d3bb7d83f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-12 09:24:07 -0600

    Update c676.lua

--- script/c676.lua
+++ script/c676.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Fusion.CreateSummonEff({handler=c,fusfilter=aux.FilterBoolFunction(Card.IsType,TYPE_MONSTER),
-		extrafil=s.fextra,extraop=s.extraop,extratg=s.extratg})
+		extrafil=s.fextra,extraop=s.extraop,extratg=s.extratg,stage2=s.stage2})
 	c:RegisterEffect(e1)
 end
 s.listed_names={15259703}
@@ -33,3 +33,15 @@ function s.extratg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,0,tp,LOCATION_GRAVE)
 end
+function s.stage2(e,tc,tp,sg,chk)
+	if chk==1 then
+		--Cannot be targeted by opponent's card effects
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetDescription(3061)
+		e1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_ADD_TYPE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(TYPE_TOON)
+		tc:RegisterEffect(e1)
+	end
