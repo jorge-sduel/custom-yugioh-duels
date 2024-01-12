9aa70c80ab7c8ed0f4dd0cd3024548fe1498cd8a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-11 20:56:06 -0600

    Update c675.lua

--- script/c675.lua
+++ script/c675.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,false,false,90960358,aux.FilterBoolFunctionEx(Card.IsRace,RACE_DRAGON))
+	Fusion.AddProcMix(c,false,false,90960358,aux.FilterBoolFunctionEx(Card.IsType,TYPE_TOON))
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
