269f490aa992d1d6adccb43fad9acdd61ba64a31
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-12 09:54:05 -0600

    Add files via upload

---
+++ script/c678.lua
@@ -0,0 +1,131 @@
+--Blue-Eyes Ultimate Toon Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+	--Must be properly summoned before reviving
+	c:EnableReviveLimit()
+	--Fusion summon procedure
+	Fusion.AddProcMixN(c,true,true,53183600,3)
+	--Cannot be targeted by opponent's card effects
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(aux.tgoval)
+	c:RegisterEffect(e1)
+	--Cannot be destroyed by opponent's card effects
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e2:SetValue(s.indval)
+	c:RegisterEffect(e2)
+	--Destroy up to 3 of opponent's cards
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1)
+	e3:SetCost(s.descost)
+	e3:SetTarget(s.destg)
+	e3:SetOperation(s.desop)
+	c:RegisterEffect(e3)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_MATERIAL_CHECK)
+	e4:SetValue(s.valcheck)
+	c:RegisterEffect(e4)
+	--destroy
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCode(EVENT_LEAVE_FIELD)
+	e5:SetCondition(s.sdescon)
+	e5:SetOperation(s.sdesop)
+	c:RegisterEffect(e5)
+	--direct attack
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_SINGLE)
+	e6:SetCode(EFFECT_DIRECT_ATTACK)
+	e6:SetCondition(s.dircon)
+	c:RegisterEffect(e6)
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+	e7:SetCondition(s.atcon)
+	e7:SetValue(s.atlimit)
+	c:RegisterEffect(e7)
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_SINGLE)
+	e8:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e8:SetCondition(s.atcon)
+	c:RegisterEffect(e8)
+	--cannot attack
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e9:SetCode(EVENT_SUMMON_SUCCESS)
+	e9:SetOperation(s.atklimit)
+	c:RegisterEffect(e9)
+end
+s.material_setcode=0xdd
+s.listed_names={15259703,CARD_BLUEEYES_W_DRAGON,53183600}
+
+function s.indval(e,re,tp)
+	return tp~=e:GetHandlerPlayer()
+end
+function s.valcheck(e,c)
+	if c:GetMaterial():IsExists(Card.IsOriginalCode,1,nil,53183600) then
+		c:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD&~(RESET_TOFIELD|RESET_TEMP_REMOVE|RESET_LEAVE),EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,1))
+	end
+end
+function s.descost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():GetAttackAnnouncedCount()==0 end
+	--Cannot attack
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetDescription(3206)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_OATH+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e:GetHandler():RegisterEffect(e1)
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsOnField() and chkc:IsControler(1-tp) end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	local mc=1
+	if e:GetHandler():GetFlagEffect(id) ~= 0 then mc=3 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,mc,nil)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetTargetCards(e)
+	if #g>0 then
+		Duel.Destroy(g,REASON_EFFECT)
+	end
+end
+function s.sfilter(c)
+	return c:IsReason(REASON_DESTROY) and c:IsPreviousPosition(POS_FACEUP) and c:GetPreviousCodeOnField()==15259703 and c:IsPreviousLocation(LOCATION_ONFIELD)
+end
+function s.sdescon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(s.sfilter,1,nil)
+end
+function s.sdesop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Destroy(e:GetHandler(),REASON_EFFECT)
+end
+function s.dircon(e)
+	return not Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_TOON),e:GetHandlerPlayer(),0,LOCATION_MZONE,1,nil)
+end
+function s.atcon(e)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsType,TYPE_TOON),e:GetHandlerPlayer(),0,LOCATION_MZONE,1,nil)
+end
+function s.atlimit(e,c)
+	return not c:IsType(TYPE_TOON) or c:IsFacedown()
+end
+function s.atklimit(e,tp,eg,ep,ev,re,r,rp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_ATTACK)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+	e:GetHandler():RegisterEffect(e1)
+end
