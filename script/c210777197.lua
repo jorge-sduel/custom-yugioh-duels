00f878c0f95f7fa6c665144349675514a8e092b2
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-12 21:17:24 -0600

    Update c210777197.lua

--- script/c210777197.lua
+++ script/c210777197.lua
@@ -36,7 +36,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,true)
+	local g=Duel.SelectMatchingCard(tp,s.tgfilter,tp,LOCATION_DECK,0,1,1,nil,true)
 	if #g>0 and Duel.SendtoHand(g,nil,thfilter)>0 then
 		Duel.ConfirmCards(1-tp,g)
 		if Duel.IsExistingMatchingCard(s.tgfilter,tp,LOCATION_DECK,0,1,nil,g:GetFirst():GetCode()) and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
