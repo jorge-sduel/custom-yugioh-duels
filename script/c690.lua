23009346806ce6adcd4210f665688cb82be6c762
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-19 20:50:18 -0600

    Update c690.lua

--- script/c690.lua
+++ script/c690.lua
@@ -40,7 +40,7 @@ end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE+LOCATION_ONFIELD+LOCATION_HAND,LOCATION_GRAVE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE+LOCATION_ONFIELD+LOCATION_HAND,LOCATION_GRAVE,1,1,nil)
 	Duel.Remove(g,POS_FACEUP,REASON_COST)
 end
 function s.negcon(e,tp,eg,ep,ev,re,r,rp)
