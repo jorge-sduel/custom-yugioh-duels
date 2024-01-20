76b98369b74b8491d035b05e505e526d6ccb2b28
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-19 21:26:10 -0600

    Add files via upload

---
+++ script/c691.lua
@@ -0,0 +1,97 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,4,2)
+	c:EnableReviveLimit()
+	--lv change
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetCode(EFFECT_XYZ_LEVEL)
+	e0:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
+	e0:SetRange(LOCATION_EXTRA)
+	e0:SetTargetRange(LOCATION_MZONE,0)
+	e0:SetTarget(s.lvtg)
+	e0:SetValue(s.lvval)
+	c:RegisterEffect(e0)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+ e1:SetCost(aux.AND(aux.dxmcostgen(1,1,nil),s.cacost))
+	e1:SetTarget(s.lvtg2)
+	e1:SetOperation(s.lvop2)
+	c:RegisterEffect(e1)
+	--attach
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(id,1))
+	e5:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e5:SetCode(EVENT_PHASE+PHASE_END)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1,id)
+	e5:SetCondition(s.damcon)
+	e5:SetTarget(s.xyztg)
+	e5:SetOperation(s.xyzop)
+	c:RegisterEffect(e5)
+end
+function s.lvtg(e,c)
+	return c:IsLevelAbove(1) and c:IsSetCard(0xbf)
+end
+function s.lvval(e,c,rc)
+	local lv=c:GetLevel()
+	if rc:IsCode(id) then return 4
+	else return lv end
+end
+function s.spfilter(c)
+	return c:IsType(TYPE_MONSTER) and c:IsAbleToRemoveAsCost()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.Remove(g,POS_FACEUP,REASON_COST)
+	e:SetLabel(g:GetFirst():GetRace())
+end
+function s.filter2(c,att)
+	return c:IsFaceup() and c:IsAttribute(att)
+end
+function s.lvtg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,c,e:GetLabel()) end
+end
+function s.lvop2(e,tp,eg,ep,ev,re,r,rp)
+ local c=e:GetHandler()
+ local g=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_MZONE,0,c,e:GetLabel())
+		local lc=g:GetFirst()
+		for lc in aux.Next(g) do
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		lc:RegisterEffect(e2)
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_DISABLE_EFFECT)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
+		lc:RegisterEffect(e3)
+		end
+	end
+end
+function s.damcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetTurnPlayer()==tp
+end
+function s.xyzfilter(c)
+	return c:IsMonster()
+end
+function s.xyztg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.xyzop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.xyzfilter,tp,LOCATION_DECK,0,nil)
+	if #g>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+		local og=g:Select(tp,1,1,nil)
+		Duel.Overlay(c,og)
+	end
+end
