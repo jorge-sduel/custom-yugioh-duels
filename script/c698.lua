f1157511e218dd8e3646cf197008cc04a1bb0e00
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-25 08:27:32 -0600

    Add files via upload

---
+++ script/c698.lua
@@ -0,0 +1,49 @@
+--
+local s,id=GetID()
+s.Is_Cosmic=true
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	--Attach
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(12744567,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_REMOVED)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(c12340417.xyztg)
+	e2:SetOperation(c12340417.xyzop)
+	c:RegisterEffect(e2)
+end
+function s.filter(c)
+	return c.Is_Cosmic
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,c:GetControler(),LOCATION_MZONE,0,1,nil)
+end
+function c12340417.xyzfil(c)
+	return c:IsType(TYPE_XYZ)
+end
+function c12340417.xyztg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) and c12340417.xyzfil(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(c12340417.xyzfil,tp,LOCATION_MZONE,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,c12340417.xyzfil,tp,LOCATION_MZONE,0,1,1,nil)
+end
+function c12340417.xyzop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and not tc:IsImmuneToEffect(e) then
+		Duel.Overlay(tc,Group.FromCards(c))
+	end
+end
