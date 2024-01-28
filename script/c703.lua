69bc28b5312e4462f3ffd26b9066d6cc37768375
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-27 20:19:58 -0600

    Update c703.lua

--- script/c703.lua
+++ script/c703.lua
@@ -1,7 +1,5 @@
 --
---
 local s,id=GetID()
-s.Is_Cosmic=true
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
@@ -9,7 +7,6 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id)
 	e1:SetCondition(s.condition)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
@@ -20,14 +17,12 @@ function s.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e2:SetCode(EVENT_FREE_CHAIN)
 	e2:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-		e2:SetCondition(aux.exccon)
+	e2:SetCondition(aux.exccon)
 	e2:SetCost(aux.bfgcost)
 	e2:SetRange(LOCATION_GRAVE)
 	e2:SetOperation(s.atkop)
 	c:RegisterEffect(e2)
 end
-s.listed_series={0x55,0x7b}
-s.listed_names={CARD_GALAXYEYES_P_DRAGON}
 function s.cfilter(c)
 	return c:IsFaceup() and c.Is_Cosmic
 end
@@ -43,7 +38,7 @@ end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter1(chkc) end
 	if chk==0 then return Duel.IsExistingTarget(s.filter1,tp,0, LOCATION_MZONE,1,nil)
-		and Duel.IsExistingMatchingCard(s.filter2,tp,0,LOCATION_MZONE,2,nil) end
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_MZONE,0,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
 	Duel.SelectTarget(tp,s.filter1,tp,0,LOCATION_MZONE,1,1,nil)
 end
