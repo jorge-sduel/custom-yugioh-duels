26e2f2ee568a2b28629bd4ed3f74ce52c520d3f5
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-01-28 13:30:09 -0600

    Update c394.lua

--- script/c394.lua
+++ script/c394.lua
@@ -52,7 +52,7 @@ function print_hand(e,tp,eg,ep,ev,re,r,rp)
 		--Duel.SendtoHand(c,1-tp,REASON_RULE)
 		--Duel.RegisterFlagEffect(tp,4392470,0,99,99)
 	--else
-		Duel.SendtoHand(e:GetHandler(),nil,REASON_RULE)
+		--Duel.SendtoHand(e:GetHandler(),nil,REASON_RULE)
 		--Duel.Remove(c,POS_FACEUP,REASON_RULE)
 	local token01=Duel.CreateToken(tp,210220076)
 	local token02=Duel.CreateToken(tp,210220077)
