a67eb0188bbc75fb252ca0deaad3b7c19f702c79
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-02-16 11:47:46 -0600

    Add files via upload

---
+++ script/c727.lua
@@ -0,0 +1,217 @@
+--SS K ritual
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--pendulum summon
+	Pendulum.AddProcedure(c)
+	--disable
+	local e1=Effect.CreateEffect(c)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetCondition(s.condition)
+	e1:SetCost(s.cost)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	--Activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DESTROYED)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTarget(s.target1)
+	e2:SetOperation(s.activate1)
+	c:RegisterEffect(e2)
+	--Activate
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_REMOVE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.target1)
+	e3:SetOperation(s.activate1)
+	c:RegisterEffect(e3)
+	--Your s/t card cannot be destroyed by card effects
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(LOCATION_SZONE,0)
+	e4:SetCondition(s.immcon1)
+	e4:SetValue(1)
+	c:RegisterEffect(e4)
+	--Cannot be targeted by opponent's effects
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_SINGLE)
+	e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCondition(s.immcon)
+	e5:SetValue(aux.tgoval)
+	c:RegisterEffect(e5)
+--unaffected by card
+	local e7=Effect.CreateEffect(c)
+	e7:SetType(EFFECT_TYPE_SINGLE)
+	e7:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e7:SetRange(LOCATION_MZONE)
+	e7:SetCode(EFFECT_IMMUNE_EFFECT)
+	e7:SetCondition(s.immcon3)
+	e7:SetValue(s.efilter)
+	c:RegisterEffect(e7)
+	--replace
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e8:SetCode(EFFECT_DESTROY_REPLACE)
+	e8:SetRange(LOCATION_PZONE)
+	e8:SetCountLimit(1)
+	e8:SetTarget(s.indtg)
+	e8:SetValue(s.indval)
+	c:RegisterEffect(e8)
+
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_FIELD)
+	e9:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e9:SetRange(LOCATION_PZONE)
+	e9:SetTargetRange(LOCATION_MZONE,0)
+	e9:SetTarget(s.indtg)
+	e9:SetValue(1)
+	c:RegisterEffect(e9)
+	local e10=Effect.CreateEffect(c)
+	e10:SetType(EFFECT_TYPE_FIELD)
+	e10:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+	e10:SetRange(LOCATION_MZONE)
+	e10:SetTargetRange(LOCATION_MZONE,0)
+	e10:SetTarget(s.indtg)
+	e10:SetValue(1)
+	c:RegisterEffect(e10)
+	local e11=Effect.CreateEffect(c)
+	e11:SetType(EFFECT_TYPE_FIELD)
+	e11:SetCode(EFFECT_UPDATE_ATTACK)
+	e11:SetRange(LOCATION_MZONE)
+	e11:SetTargetRange(LOCATION_MZONE,0)
+	e11:SetTarget(s.indtg)
+	e11:SetValue(500)
+	c:RegisterEffect(e11)
+end
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType(id)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,1,e:GetHandler()) end
+	local g=Duel.GetMatchingGroup(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DISABLE,g,#g,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsNegatableMonster,tp,0,LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		tc:RegisterEffect(e2)
+		if tc:IsType(TYPE_TRAPMONSTER) then
+			local e3=Effect.CreateEffect(e:GetHandler())
+			e3:SetType(EFFECT_TYPE_SINGLE)
+			e3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+			e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+			tc:RegisterEffect(e3)
+		end
+	end
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=eg:GetFirst()
+	if chk==0 then return Duel.GetLocationCount(tc:GetPreviousControler(),LOCATION_MZONE)>0 and #eg==1
+		and tc:IsCanBeSpecialSummoned(e,0,tp,false,false,tc:GetPreviousPosition(),tc:GetPreviousControler()) end
+	tc:CreateEffectRelation(e)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,eg,1,0,0)
+end
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	if tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tc:GetPreviousControler(),false,false,tc:GetPreviousPosition())
+if not Duel.CheckPendulumZones(tp) then return end
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.MoveToField(c,tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	local ft1=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	local ft2=Duel.GetLocationCountFromEx(tp)
+	local ft3=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_FUSION+TYPE_SYNCHRO+TYPE_XYZ)
+	local ft4=Duel.GetLocationCountFromEx(tp,tp,nil,TYPE_PENDULUM+TYPE_LINK)
+	local ft=math.min(Duel.GetUsableMZoneCount(tp),2)
+	if Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then
+		if ft1>0 then ft1=1 end
+		if ft2>0 then ft2=1 end
+		if ft3>0 then ft3=1 end
+		if ft4>0 then ft4=1 end
+		ft=1
+	end
+	local ect=aux.CheckSummonGate(tp)
+	if ect then
+		ft1=math.min(ect,ft1)
+		ft2=math.min(ect,ft2)
+		ft3=math.min(ect,ft3)
+		ft4=math.min(ect,ft4)
+	end
+	local loc=0
+	if ft1>0 then loc=loc+LOCATION_DECK+LOCATION_GRAVE end
+	if ft2>0 or ft3>0 or ft4>0 then loc=loc+LOCATION_EXTRA end
+	if loc==0 then return end
+	local sg=Duel.GetMatchingGroup(aux.NecroValleyFilter(s.spfilter),tp,loc,0,nil,e,tp)
+	if #sg==0 then return end
+	local rg=aux.SelectUnselectGroup(sg,e,tp,1,ft,s.rescon(ft1,ft2,ft3,ft4,ft),1,tp,HINTMSG_SPSUMMON)
+	Duel.SpecialSummon(rg,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+end
+	 end
+	end
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(SET_SUPREME_KING_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function s.exfilter1(c)
+	return c:IsLocation(LOCATION_EXTRA) and c:IsFacedown() and c:IsType(TYPE_FUSION|TYPE_SYNCHRO|TYPE_XYZ)
+end
+function s.exfilter2(c)
+	return c:IsLocation(LOCATION_EXTRA) and (c:IsType(TYPE_LINK) or (c:IsFaceup() and c:IsType(TYPE_PENDULUM)))
+end
+function s.rescon(ft1,ft2,ft3,ft4,ft)
+	return function(sg,e,tp,mg)
+			local exnpct=sg:FilterCount(s.exfilter1,nil,LOCATION_EXTRA)
+			local expct=sg:FilterCount(s.exfilter2,nil,LOCATION_EXTRA)
+			local mct=sg:FilterCount(aux.NOT(Card.IsLocation),nil,LOCATION_EXTRA)
+			local exct=sg:FilterCount(Card.IsLocation,nil,LOCATION_EXTRA)
+			local res=ft3>=exnpct and ft4>=expct and ft1>=mct
+			return res,not res
+		end
+end
+function s.immcon1(e)
+	return Duel.IsExistingMatchingCard(Card.IsType,e:GetHandlerPlayer(),LOCATION_GRAVE,0,1,nil,TYPE_XYZ)
+end
+function s.immcon(e)
+	return Duel.IsExistingMatchingCard(Card.IsType,e:GetHandlerPlayer(),LOCATION_GRAVE,0,1,nil,TYPE_FUSION)
+end
+function s.immcon3(e)
+	return Duel.IsExistingMatchingCard(Card.IsType,e:GetHandlerPlayer(),LOCATION_GRAVE,0,1,nil,TYPE_SYNCHRO)
+end
+function s.efilter(e,te)
+	return te:GetOwner()~=e:GetOwner()
+end
+function s.indfilter(c)
+	return c:IsFaceup() and c:IsOnField() and c:IsSetCard(SET_SUPREME_KING_DRAGON)
+end
+function s.indtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.indfilter,1,nil) end
+	return Duel.SelectEffectYesNo(tp,c,96)
+end
+function s.indval(e,c)
+	return s.indfilter(c)
+end
+function s.indtg(e,c)
+	return c:IsFaceup() and c:IsSetCard(SET_SUPREME_KING_DRAGON)
+end
