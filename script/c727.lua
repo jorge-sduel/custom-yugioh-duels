b576fd0e42ff361b6ed2a42bc7dc40b557c2e6e7
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-02-16 12:11:26 -0600

    Update c727.lua

--- script/c727.lua
+++ script/c727.lua
@@ -4,6 +4,16 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	--pendulum summon
 	Pendulum.AddProcedure(c)
+	--special summon
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_FIELD)
+	e0:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e0:SetCode(EFFECT_SPSUMMON_PROC)
+	e0:SetRange(LOCATION_HAND)
+	e0:SetCondition(s.hspcon)
+	e0:SetOperation(s.hspop)
+	e0:SetValue(id)
+	c:RegisterEffect(e0)
 	--disable
 	local e1=Effect.CreateEffect(c)
 	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
@@ -92,6 +102,15 @@ function s.initial_effect(c)
 	e11:SetTarget(s.indtg)
 	e11:SetValue(500)
 	c:RegisterEffect(e11)
+end
+function s.hspcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.CheckLPCost(tp,4000)
+end
+function s.hspop(e,tp,eg,ep,ev,re,r,rp,c)
+	Duel.PayLPCost(tp,4000)
 end
 function s.condition(e,tp,eg,ep,ev,re,r,rp)
 	return e:GetHandler():GetSummonType(id)
