c3dc2baef81c719d41bde80f49da267c1be785e2
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-02-20 10:20:23 -0600

    Add files via upload

---
+++ script/c732.lua
@@ -0,0 +1,111 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableCounterPermit(0xdark)
+	--summon success
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_COUNTER)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e1:SetCode(EVENT_SUMMON_SUCCESS)
+	e1:SetTarget(s.addct)
+	e1:SetOperation(s.addc)
+	c:RegisterEffect(e1)
+	local e2=e1:Clone()
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	c:RegisterEffect(e2)
+	--attackup
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+	--Banish up to 1 Spells and increase level
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetCategory(CATEGORY_REMOVE+CATEGORY_ATKCHANGE)
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCountLimit(1)
+	e4:SetTarget(s.rmtg)
+	e4:SetOperation(s.rmop)
+	c:RegisterEffect(e4)
+end
+function s.addct(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_COUNTER,nil,1,0,0xdark)
+end
+function s.addc(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetHandler():IsRelateToEffect(e) then
+		e:GetHandler():AddCounter(0xdark,3)
+	end
+end
+function s.filter(c,cc,e,tp)
+	return c:IsFaceup() and c:HasLevel() and cc:IsCanRemoveCounter(tp,0xdark, 1,REASON_COST)
+end
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,nil,e:GetHandler(),e,tp) end
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_MZONE,0,nil,e:GetHandler(),e,tp)
+	local lvt={}
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local tlv=tc:GetLevel()-1
+		lvt[tlv]=tlv
+	end
+	local pc=1
+	for i=1,12 do
+		if lvt[i] then lvt[i]=nil lvt[pc]=i pc=pc+1 end
+	end
+	lvt[pc]=nil
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	local lv=Duel.AnnounceNumber(tp,table.unpack(lvt))
+	e:GetHandler():RemoveCounter(tp,0xdark,lv,REASON_COST)
+	e:SetLabel(lv)
+	Duel.SetOperationInfo(0,CATEGORY_LVCHANGE,nil,1,tp,LOCATION_MZONE)
+end
+function s.sfilter(c,lv,e,tp)
+	return c:IsFaceup() and c:HasLevel()
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	local lv=e:GetLabel()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.sfilter,tp,LOCATION_MZONE,0,1,1,nil,lv,e,tp)
+	if #g>0 then
+				--Increase lv
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(ct)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		g:RegisterEffect(e1)
+	end
+end
+function s.rmfilter(c)
+	return c:IsSpell() and c:IsAbleToRemove()
+end
+function s.rmtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.rmfilter,tp,LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,nil,1,1-tp,LOCATION_GRAVE)
+end
+function s.rmop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+	local g=Duel.SelectMatchingCard(tp,s.rmfilter,tp,LOCATION_GRAVE,0,1,99,nil)
+	if #g==0 then return end
+	Duel.HintSelection(g,true)
+	local ct=Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	local c=e:GetHandler()
+	if ct>0 and c:IsFaceup() and c:IsRelateToEffect(e) then
+		--Increase ATK
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_LEVEL)
+		e1:SetValue(ct)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END+RESET_OPPO_TURN)
+		c:RegisterEffect(e1)
+	end
+end
