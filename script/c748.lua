43876d65c351e7e2af5bfce274a49de38954dcc0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-05 16:04:34 -0600

    Add files via upload

---
+++ script/c748.lua
@@ -0,0 +1,69 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--synchro summon
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTunerEx(Card.IsType,TYPE_SYNCHRO),1,99,c217.sssmatfilter)
+	c:EnableReviveLimit()
+	--summon success
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_MATERIAL_CHECK)
+	e2:SetValue(s.matcheck)
+	c:RegisterEffect(e2)
+end
+function c217.sssmatfilter(c)
+	return c:IsType(TYPE_XYZ)
+end
+function s.matcheck(e,c)
+	local ct=c:GetMaterial()
+	if ct:IsExists(Card.IsType,1,nil,TYPE_XYZ) then
+	--actlimit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_ACTIVATE)
+	e1:SetRange(LOCATION_MZONE)
+e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(s.aclimit)
+	e1:SetCondition(s.actcon)
+	c:RegisterEffect(e1)
+	--PIERCE
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_PIERCE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	c:RegisterEffect(e3)
+	end
+	if ct:IsExists(Card.IsType,1,nil,TYPE_SYNCHRO) then
+	--ATK Up
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	e2:SetValue(s.val)
+	c:RegisterEffect(e2)
+--TWO ATTACK
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+	end
+end
+function s.aclimit(e,re,tp)
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.actcon(e)
+	return Duel.GetAttacker()==e:GetHandler()
+end
+
+function s.val(e,c)
+	return Duel.GetMatchingGroup(Card.IsReason,c:GetControler(),LOCATION_GRAVE+LOCATION_REMOVED,0,nil,REASON_MATERIAL+REASON_SYNCHRO)*c:GetAttack()
+end
