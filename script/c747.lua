485d002b9078128e0d7f8f00609ff44dabc198b9
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-05 11:15:02 -0600

    Add files via upload

---
+++ script/c747.lua
@@ -0,0 +1,69 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	c:EnableReviveLimit()
+	--Fusion materials
+	Fusion.AddProcMix(c,false,false,aux.FilterBoolFunctionEx(Card.IsRace,RACE_DRAGON),s.ffilter2)
+	--summon success
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_MATERIAL_CHECK)
+	e2:SetValue(s.matcheck)
+	c:RegisterEffect(e2)
+end
+function s.ffilter2(c)
+	return c:IsRace(RACE_DRAGON) and (c:IsType(TYPE_FUSION) or c:IsType(TYPE_XYZ))
+end
+function s.matcheck(e,c)
+	local ct=c:GetMaterial()
+	if ct:IsExists(Card.IsType,1,nil,TYPE_XYZ) then
+	--atk up
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetRange(LOCATION_MZONE)
+e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	e1:SetCondition(s.atkcon)
+	e1:SetValue(s.atkval)
+	c:RegisterEffect(e1)
+	end
+	if ct:IsExists(Card.IsType,1,nil,TYPE_FUSION) then
+	--chain attack
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(5041348,1))
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_BATTLE_DESTROYING)
+e2:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE-RESET_TOFIELD)
+	e2:SetCondition(s.atkcon2)
+	e2:SetTarget(s.atktg2)
+	e2:SetOperation(s.atkop2)
+	c:RegisterEffect(e2)
+	end
+end
+function s.atkcon(e)
+	return Duel.GetCurrentPhase()==PHASE_DAMAGE_CAL
+		and e:GetHandler()==Duel.GetAttacker() and Duel.GetAttackTarget()~=nil
+end
+function s.atkval(e,c)
+	return Duel.GetAttackTarget():GetAttack()/2
+end
+function s.atkcon2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local bc=c:GetBattleTarget()
+	return Duel.GetAttacker()==c and aux.bdgcon(e,tp,eg,ep,ev,re,r,rp) and bc:IsType(TYPE_MONSTER)
+end
+function s.atktg2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsRelateToBattle() end
+	local bc=e:GetHandler():GetBattleTarget()
+	Duel.SetTargetCard(bc)
+end
+function s.atkop2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	local code=tc:GetOriginalCode()
+	if c:IsRelateToBattle() and tc:IsRelateToEffect(e) and c:IsFaceup() then
+				c:CopyEffect(code,RESET_EVENT+RESETS_STANDARD,1)
+	end
+end
+
