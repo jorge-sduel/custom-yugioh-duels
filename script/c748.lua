f509b7da55b83f43e2b00fd7fc548f22fa48e81b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-06 11:21:20 -0600

    Update c748.lua

--- script/c748.lua
+++ script/c748.lua
@@ -123,6 +123,7 @@ function s.desop(e,tp,eg,ep,ev,re,r,rp)
 		Duel.AdjustInstantly(c)
 	end
 	Duel.Destroy(tc,REASON_EFFECT)
+	Duel.Damage(atk,REASON_EFFECT)
 	e:SetProperty(e:GetProperty()&~EFFECT_FLAG_IGNORE_IMMUNE)
 end
 function s.val(e,c)
