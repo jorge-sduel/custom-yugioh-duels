47df912e2352f00f3f01d9cab9763481bf76eb99
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-06 11:20:19 -0600

    Update c748.lua

--- script/c748.lua
+++ script/c748.lua
@@ -101,6 +101,7 @@ end
 function s.desop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=e:GetLabelObject()
+	local atk=tc:GetAttack()
 	if tc:IsRelateToBattle() then
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
