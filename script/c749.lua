712f7794a846c93a483a7e77cfe3a511913e0d96
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-07 09:56:14 -0600

    Add files via upload

---
+++ script/c749.lua
@@ -0,0 +1,101 @@
+--フォース
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_ATKCHANGE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsFaceup() end
+	if chk==0 then return Duel.IsExistingTarget(Card.IsFaceup,tp,LOCATION_MZONE,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
+	local g1=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,nil)
+	e:SetLabelObject(g1:GetFirst())
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	local g2=Duel.SelectTarget(tp,Card.IsFaceup,tp,LOCATION_MZONE,0,1,1,g1:GetFirst())
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local hc=e:GetLabelObject()
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tc=g:GetFirst()
+	if tc==hc then tc=g:GetNext() end
+	if hc:IsFaceup() and hc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsRelateToEffect(e) then
+		if tc:IsType(TYPE_SYNCHRO) then
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_CHANGE_LEVEL)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetValue(4)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(e:GetHandler())
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_SYNCHRO_LEVEL)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e2:SetValue(s.synval)
+		tc:RegisterEffect(e2)
+		if tc:IsType(TYPE_XYZ) then
+		local e3=Effect.CreateEffect(e:GetHandler())
+		e3:SetType(EFFECT_TYPE_SINGLE)
+		e3:SetCode(EFFECT_RANK_LEVEL_S)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e3:SetValue(7)
+		tc:RegisterEffect(e3)
+		if tc:IsType(TYPE_FUSION) then
+		local e4=Effect.CreateEffect(e:GetHandler())
+		e4:SetType(EFFECT_TYPE_SINGLE)
+		e4:SetCode(EFFECT_CHANGE_LEVEL)
+		e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e4:SetValue(8)
+		tc:RegisterEffect(e4)
+		local e5=Effect.CreateEffect(e:GetHandler())
+		e5:SetType(EFFECT_TYPE_SINGLE)
+		e5:SetCode(EFFECT_ADD_TYPE)
+		e5:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e5:SetValue(TYPE_TUNER)
+		tc:RegisterEffect(e5)
+		if hc:IsType(TYPE_SYNCHRO) then
+		local e6=Effect.CreateEffect(e:GetHandler())
+		e6:SetType(EFFECT_TYPE_SINGLE)
+		e6:SetCode(EFFECT_CHANGE_LEVEL)
+		e6:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e6:SetValue(4)
+		hc:RegisterEffect(e6)
+		local e7=Effect.CreateEffect(e:GetHandler())
+		e7:SetType(EFFECT_TYPE_SINGLE)
+		e7:SetCode(EFFECT_SYNCHRO_LEVEL)
+		e7:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e7:SetValue(s.synval)
+		hc:RegisterEffect(e7)
+		if hc:IsType(TYPE_XYZ) then
+		local e8=Effect.CreateEffect(e:GetHandler())
+		e8:SetType(EFFECT_TYPE_SINGLE)
+		e8:SetCode(EFFECT_RANK_LEVEL_S)
+		e8:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e8:SetValue(7)
+		hc:RegisterEffect(e8)
+		if tc:IsType(TYPE_FUSION) then
+		local e9=Effect.CreateEffect(e:GetHandler())
+		e9:SetType(EFFECT_TYPE_SINGLE)
+		e9:SetCode(EFFECT_CHANGE_LEVEL)
+		e9:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e9:SetValue(8)
+		tc:RegisterEffect(e9)
+		local e10=Effect.CreateEffect(e:GetHandler())
+		e10:SetType(EFFECT_TYPE_SINGLE)
+		e10:SetCode(EFFECT_ADD_TYPE)
+		e10:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e10:SetValue(TYPE_TUNER)
+		tc:RegisterEffect(e10)
+		end
+	end
+end
+function s.synval(e,c)
+	local lv=e:GetHandler():GetLevel()
+	return 2*65536+lv
+end
