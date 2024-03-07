8a5958e83558ce695d4cf159635ec99f47432900
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-07 11:30:19 -0600

    Update c749.lua

--- script/c749.lua
+++ script/c749.lua
@@ -28,79 +28,101 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
 		local tc2=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,LOCATION_MZONE,1,1,tc):GetFirst()
-		if tc:IsType(TYPE_SYNCHRO) then
+		--if tc:IsType(TYPE_SYNCHRO) then
 		local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_CHANGE_LEVEL)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e1:SetCondition(s.syncond)
 		e1:SetValue(4)
 		tc:RegisterEffect(e1)
 		local e2=Effect.CreateEffect(e:GetHandler())
 		e2:SetType(EFFECT_TYPE_SINGLE)
 		e2:SetCode(EFFECT_SYNCHRO_LEVEL)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e2:SetCondition(s.cond)
 		e2:SetValue(s.synval)
 		tc:RegisterEffect(e2)
-		if tc:IsType(TYPE_XYZ) then
+		--if tc:IsType(TYPE_XYZ) then
 		local e3=Effect.CreateEffect(e:GetHandler())
 		e3:SetType(EFFECT_TYPE_SINGLE)
 		e3:SetCode(EFFECT_RANK_LEVEL_S)
 		e3:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e3:SetCondition(s.xyzcond)
 		e3:SetValue(7)
 		tc:RegisterEffect(e3)
-		if tc:IsType(TYPE_FUSION) then
+		local e12=Effect.CreateEffect(e:GetHandler())
+		e12:SetType(EFFECT_TYPE_SINGLE)
+		e12:SetCode(EFFECT_CHANGE_LEVEL_FINAL)
+		e12:SetValue(7)
+		e12:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e12)
+		--if tc:IsType(TYPE_FUSION) then
 		local e4=Effect.CreateEffect(e:GetHandler())
 		e4:SetType(EFFECT_TYPE_SINGLE)
 		e4:SetCode(EFFECT_CHANGE_LEVEL)
 		e4:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e4:SetCondition(s.fuscond)
 		e4:SetValue(8)
 		tc:RegisterEffect(e4)
 		local e5=Effect.CreateEffect(e:GetHandler())
 		e5:SetType(EFFECT_TYPE_SINGLE)
 		e5:SetCode(EFFECT_ADD_TYPE)
 		e5:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e5:SetCondition(s.fuscond)
 		e5:SetValue(TYPE_TUNER)
 		tc:RegisterEffect(e5)
 		local tc3=tc2:GetFirst()
-		if tc3:IsType(TYPE_SYNCHRO) then
+		--if tc3:IsType(TYPE_SYNCHRO) then
 		local e6=Effect.CreateEffect(e:GetHandler())
 		e6:SetType(EFFECT_TYPE_SINGLE)
 		e6:SetCode(EFFECT_CHANGE_LEVEL)
 		e6:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e6:SetCondition(s.syncond)
 		e6:SetValue(4)
 		tc3:RegisterEffect(e6)
 		local e7=Effect.CreateEffect(e:GetHandler())
 		e7:SetType(EFFECT_TYPE_SINGLE)
 		e7:SetCode(EFFECT_SYNCHRO_LEVEL)
 		e7:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e7:SetCondition(s.syncond)
 		e7:SetValue(s.synval)
 		tc3:RegisterEffect(e7)
-		if tc3:IsType(TYPE_XYZ) then
+		--if tc3:IsType(TYPE_XYZ) then
 		local e8=Effect.CreateEffect(e:GetHandler())
 		e8:SetType(EFFECT_TYPE_SINGLE)
-		e8:SetCode(EFFECT_RANK_LEVEL_S)
+		e8:SetCode(EFFECT_RANK_LEVEL)
 		e8:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-		e8:SetValue(7)
+		e8:SetCondition(s.xyzcond)
 		tc3:RegisterEffect(e8)
-		if tc3:IsType(TYPE_FUSION) then
+		local e11=Effect.CreateEffect(e:GetHandler())
+		e11:SetType(EFFECT_TYPE_SINGLE)
+		e11:SetCode(EFFECT_CHANGE_LEVEL_FINAL)
+		e11:SetCondition(s.xyzcond)
+		e11:SetValue(7)
+		e11:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc3:RegisterEffect(e11)
+		--if tc3:IsType(TYPE_FUSION) then
 		local e9=Effect.CreateEffect(e:GetHandler())
 		e9:SetType(EFFECT_TYPE_SINGLE)
 		e9:SetCode(EFFECT_CHANGE_LEVEL)
 		e9:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e9:SetCondition(s.fuscond)
 		e9:SetValue(8)
 		tc3:RegisterEffect(e9)
 		local e10=Effect.CreateEffect(e:GetHandler())
 		e10:SetType(EFFECT_TYPE_SINGLE)
 		e10:SetCode(EFFECT_ADD_TYPE)
 		e10:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
+		e10:SetCondition(s.fuscond)
 		e10:SetValue(TYPE_TUNER)
 		tc3:RegisterEffect(e10)
-							end
-						end
-					end
-				end
-			end
-		end
+							--end
+						--end
+					--end
+				--end
+			--end
+		--end
 	end
 end
 function s.synval(e,c)
