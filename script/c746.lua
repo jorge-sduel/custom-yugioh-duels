5871d97259697f05765078ddace9dfe223739bdd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-14 15:25:12 -0600

    Update c746.lua

--- script/c746.lua
+++ script/c746.lua
@@ -63,7 +63,7 @@ function s.atkop(e,tp,eg,ep,ev,re,r,rp)
 			e2:SetType(EFFECT_TYPE_SINGLE)
 			e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
 			e2:SetCode(EFFECT_UPDATE_ATTACK)
-			e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+			e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 			e2:SetValue(s.val)
 			c:RegisterEffect(e2)
 end
