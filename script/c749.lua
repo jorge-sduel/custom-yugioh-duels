5d0f2291586c244fbd9c1dc63d663e2ba40031fc
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-14 15:14:01 -0600

    Update c749.lua

--- script/c749.lua
+++ script/c749.lua
@@ -3,32 +3,40 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+		--Activate
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_ACTIVATE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetOperation(s.activate2)
+	c:RegisterEffect(e2)
 end
 function s.filter(c)
 	return c:IsFaceup() and (c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_FUSION))
 end
 function s.xyzfilter(c,tp)
-	return c:IsFaceup() and (c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_FUSION))
-	--and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,c)
+	return c:IsFaceup() and (c:IsType(TYPE_SYNCHRO) or c:IsType(TYPE_XYZ) or c:IsType(TYPE_FUSION)) and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_MZONE,0,1,c)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.xyzfilter(chkc,tp) end
 	if chk==0 then return Duel.IsExistingTarget(s.xyzfilter,tp,LOCATION_MZONE,0,1,nil,tp) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
-	Duel.SelectTarget(tp,s.xyzfilter,tp,LOCATION_MZONE,0,0,1,nil,tp)
+	Duel.SelectTarget(tp,s.xyzfilter,tp,LOCATION_MZONE,0,1,1,nil,tp)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	if not e:GetHandler():IsRelateToEffect(e) then return end
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
 		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
-		local tc3=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,0,1,tc):GetFirst()
+		local tc3=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_MZONE,0,1,1,tc):GetFirst()
 		local ac=Duel.AnnounceLevel(tp,1,4)
 		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
 		tc3:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD,0,0)
@@ -116,4 +124,14 @@ function s.con(e,c)
 end
 function s.scfilter(c,mg)
 	return c:IsSynchroSummonable(nil,mg)
+end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local mg=Duel.GetMatchingGroup(aux.FaceupFilter(Card.IsType,TYPE_MONSTER),tp,LOCATION_MZONE,0,nil)
+		local eg=Duel.GetMatchingGroup(s.scfilter,tp,LOCATION_EXTRA,0,nil,mg)
+		if #mg>0 and #eg>0 and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+			local sg=eg:Select(tp,1,1,nil)
+			Duel.SynchroSummon(tp,sg:GetFirst(),nil,mg)
+	end
 end
