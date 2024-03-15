281976d8e6a4da884edc68ed7f2c6c3fabb2901d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-15 16:07:55 -0600

    Update c754.lua

--- script/c754.lua
+++ script/c754.lua
@@ -26,7 +26,7 @@ function s.initial_effect(c)
 	e3:SetCategory(CATEGORY_TOGRAVE)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_TO_GRAVE)
-	e3:SetCondition(s.condition)
+	--e3:SetCondition(s.condition)
 	e3:SetTarget(s.target)
 	e3:SetOperation(s.operation)
 	c:RegisterEffect(e3)
@@ -56,7 +56,7 @@ function s.spcon(e,c)
 		and Duel.IsExistingMatchingCard(s.ffilter,c:GetControler(),LOCATION_MZONE,0,1,nil)
 end
 function s.ffilter(c,e,tp)
-	return (c:ListsCode(CARD_GAIA_CHAMPION) or c:IsCode(CARD_GAIA_CHAMPION)) and c:IsFaceup()
+	return (c:ListsCode(CARD_GAIA_CHAMPION) or c:IsCode(CARD_GAIA_CHAMPION) or c:IsSetCard(0xbd)) and c:IsFaceup()
 end
 function s.filter(c,e,tp)
 	return (c:IsSetCard(0xbd) or (c:IsRace(RACE_DRAGON) and c:IsLevel(5))) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
@@ -64,13 +64,13 @@ end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
 	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
-	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND,0,1,nil,e,tp) end
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND)
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
 end
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND,0,1,1,nil,e,tp)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g>0 then
 		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
 	end
