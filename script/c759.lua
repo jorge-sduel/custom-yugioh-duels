e62943368e38e5941ae45a7af7e27ad4180e18e1
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-17 20:15:55 -0600

    Add files via upload

---
+++ script/c759.lua
@@ -0,0 +1,99 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion summon procedure
+	c:EnableReviveLimit()
+	Fusion.AddProcMixRep(c,true,true,s.matfilter2,2,2,aux.FilterBoolFunctionEx(Card.IsSetCard,0xbd))
+	--Change name
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_CHANGE_CODE)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(CARD_GAIA_CHAMPION)
+	c:RegisterEffect(e1)
+	--Decrease ATK and destroy target
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DESTROY+CATEGORY_DAMAGE)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_MAIN_END)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.descon)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
+	c:RegisterEffect(e2)
+	--ATK Up
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_ATKCHANGE)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BATTLE_DESTROYING)
+	e3:SetCountLimit(1,{id,1})
+	e3:SetCondition(aux.bdocon)
+	e3:SetTarget(s.atktg)
+	e3:SetOperation(s.atkop)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0xbd}
+s.listed_names={CARD_GAIA_CHAMPION}
+function s.matfilter(c,fc,sumtype,tp)
+	return c:IsRace(RACE_DRAGON,fc,sumtype,tp) and c:IsLevel(5)
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsMainPhase() and e:GetHandler():GetAttack()>=2600
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	local c=e:GetHandler()
+	if chkc then return chkc:IsOnField() and chkc~=c end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,c) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,LOCATION_ONFIELD,1,1,c)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and c:UpdateAttack(-2600,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,c)==-2600 then
+		if tc and tc:IsRelateToEffect(e) then
+			Duel.Destroy(tc,REASON_EFFECT)
+			Duel.Damage(1-tp,2600,REASON_EFFECT)
+		end
+	end
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetTargetCard(e:GetHandler():GetBattleTarget())
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsFacedown() or not c:IsRelateToEffect(e) or not c:IsRelateToBattle() then return end
+	local tc=Duel.GetFirstTarget()
+	local value=tc:GetBaseAttack()
+	if value<0 then value=0 end
+	--Gains ATK equal to destroyed monster's original ATK
+	if tc and tc:IsRelateToEffect(e) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(value)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
+	--Can make a second attack on monsters
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(3202)
+	e2:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CANNOT_DIRECT_ATTACK)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_BATTLE)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_EXTRA_ATTACK)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+			Duel.Recover(tp,2800,REASON_EFFECT)
+end
