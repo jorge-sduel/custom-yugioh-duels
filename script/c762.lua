1c02970a6c6cdca8ef1e00bdec6a21e9b20476a0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-21 20:44:48 -0600

    Add files via upload

---
+++ script/c762.lua
@@ -0,0 +1,47 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,6368038,s.ffilter)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_PIERCE)
+	c:RegisterEffect(e1)
+	--change levels
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCost(s.lvcost)
+	e2:SetOperation(s.lvop)
+	c:RegisterEffect(e2)
+end
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsRace(RACE_DRAGON,fc,sumtype,tp)
+end
+function s.costfilter(c,lv)
+	local clv=c:GetLevel()
+	return c:IsLevelAbove(7) and c:IsAbleToRemove()
+end
+function s.lvcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local lv=e:GetHandler():GetLevel()
+	if chk==0 then return Duel.IsExistingMatchingCard(s.costfilter,tp,LOCATION_DECK,0,1,nil,lv) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.costfilter,tp,LOCATION_DECK,0,1,1,nil,lv)
+	Duel.Remove(g,POS_FACEUP,REASON_EFFECT)
+	e:SetLabel(g:GetFirst():GetAttack())
+end
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local lv=e:GetLabel()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(lv)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE)
+		c:RegisterEffect(e1)
+	end
+end
