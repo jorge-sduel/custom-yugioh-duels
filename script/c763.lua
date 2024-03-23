ee922a1eaa14f1f3fa675bfa79f1c9e62c2e5e04
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-23 08:58:07 -0600

    Update c763.lua

--- script/c763.lua
+++ script/c763.lua
@@ -77,7 +77,7 @@ function s.damop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Draw(p,d,REASON_EFFECT)
 end
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0x27) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return c:IsSetCard(0x27) and c:IsLevelBelow(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
