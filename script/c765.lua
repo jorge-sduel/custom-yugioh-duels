8df2a735788ea759b7fb77e20a63896114ca8112
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-03-24 12:26:22 -0600

    Update c765.lua

--- script/c765.lua
+++ script/c765.lua
@@ -71,6 +71,7 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 		c:RegisterFlagEffect(id,0,0,1)
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
 		e1:SetCode(EFFECT_SYNCHRO_MATERIAL)
 		tc:RegisterEffect(e1,true)
 	end
