dd7efdd3fd29fba7c37d242b1c1a0ded58aadaf0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-04-14 09:08:18 -0600

    Add files via upload

---
+++ script/c778.lua
@@ -0,0 +1,42 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	--act limit
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetTarget(s.settg)
+	e2:SetOperation(s.setop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x76,0x92}
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and (Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x76),c:GetControler(),LOCATION_MZONE,0,1,nil) or Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x92),c:GetControler(),LOCATION_MZONE,0,1,nil))
+end
+function s.setfilter(c)
+	return c:IsSpellTrap() and c:IsSSetable()
+end
+function s.settg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.setfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.setop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SET)
+	local g=Duel.SelectMatchingCard(tp,s.setfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		local c=e:GetHandler()
+		local tc=g:GetFirst()
+		Duel.SSet(tp,tc)
+	end
+end
