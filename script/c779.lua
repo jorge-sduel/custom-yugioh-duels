ff7364588f42a6805af117975ea98972cb28ba4d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-04-14 10:39:30 -0600

    Update c779.lua

--- script/c779.lua
+++ script/c779.lua
@@ -64,7 +64,7 @@ function s.effop(e,tp,eg,ep,ev,re,r,rp)
 	c:CopyEffect(code,RESETS_STANDARD_PHASE_END)
 		--disable
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetTargetRange(0,LOCATION_MZONE)
 	e1:SetCode(EFFECT_DISABLE)
 	e1:SetReset(RESET_PHASE+PHASE_END)
