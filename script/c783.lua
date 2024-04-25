f3c00ce4caa14e578eeb5dfb0654b2ac94d2264c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-04-25 10:16:27 -0600

    Update c783.lua

--- script/c783.lua
+++ script/c783.lua
@@ -14,96 +14,17 @@ function s.initial_effect(c)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
-		--Adjust
-	--adjust
+	--Destroy
 	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-	e2:SetCode(EVENT_ADJUST)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCountLimit(1)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetOperation(s.adjustop)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetTarget(s.destg)
+	e2:SetOperation(s.desop)
 	c:RegisterEffect(e2)
-	--cannot summon,spsummon,flipsummon
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCode(EFFECT_FORCE_SPSUMMON_POSITION)
-	e4:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-	e4:SetTargetRange(0,1)
-	e4:SetTarget(s.sumlimit)
-	e4:SetValue(POS_FACEDOWN)
-	c:RegisterEffect(e4)
-	local e5=e4:Clone()
-	e5:SetCode(EFFECT_CANNOT_SUMMON)
-	c:RegisterEffect(e5)
-	local e6=e4:Clone()
-	e6:SetCode(EFFECT_CANNOT_FLIP_SUMMON)
-	c:RegisterEffect(e6)
-end
-s[0]=0
-s[1]=0
-function s.acttg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	s[0]=0
-	s[1]=0
-end
-function s.sumlimit(e,c,sump,sumtype,sumpos,targetp)
-	local rc=s.getrace(Duel.GetMatchingGroup(Card.IsFaceup,targetp or sump,LOCATION_MZONE,0,nil))
-	if rc==0 then return false end
-	return c:GetCode()~=rc
-end
-function s.getrace(g)
-	local arc=0
-	for tc in g:Iter() do
-		arc~=(arc|tc:GetCode())
-	end
-	return arc
-end
-function s.rmfilter(c,rc)
-	return c:GetCode()~=rc
-end
-function s.adjustop(e,tp,eg,ep,ev,re,r,rp)
-	local phase=Duel.GetCurrentPhase()
-	if (phase==PHASE_DAMAGE and not Duel.IsDamageCalculated()) or phase==PHASE_DAMAGE_CAL then return end
-	local g1=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
-	local g2=Duel.GetMatchingGroup(Card.IsFaceup,tp,0,LOCATION_MZONE,nil)
-	local c=e:GetHandler()
-	if #g1==0 then
-		s[tp]=0
-	else
-		local rac=s.getrace(g1)
-		if (rac&rac-1)~=0 then
-			if s[tp]==0 or (s[tp]&rac)==0 then
-				Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,0))
-				--rac=Duel.AnnounceRace(tp,1,rac)
-			else rac=s[tp] end
-		end
-		g1:Remove(s.rmfilter,nil,rac)
-		s[tp]=rac
-	end
-	if #g2==0 then
-		s[1-tp]=0
-	else
-		local rac=s.getrace(g2)
-		if (rac&rac-1)~=0 then
-			if s[1-tp]==0 or (s[1-tp]&rac)==0 then
-				Duel.Hint(HINT_SELECTMSG,1-tp,aux.Stringid(id,0))
-				--rac=Duel.AnnounceRace(1-tp,1,rac)
-			else rac=s[1-tp] end
-		end
-		g2:Remove(s.rmfilter,nil,rac)
-		s[1-tp]=rac
-	end
-	local readjust=false
-	if #g1>0 then
-		Duel.SendtoGrave(g1,REASON_RULE,PLAYER_NONE,tp)
-		readjust=true
-	end
-	if #g2>0 then
-		Duel.SendtoGrave(g2,REASON_RULE,PLAYER_NONE,1-tp)
-		readjust=true
-	end
-	if readjust then Duel.Readjust() end
 end
 function s.filter1(c)
 	return c:IsFaceup()
@@ -135,5 +56,23 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 			lc:RegisterEffect(e1)
 		end
+	end
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,0,1,nil)
+		and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g2=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	g1:Merge(g2)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,2,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #tg>0 then
+		Duel.Destroy(tg,REASON_EFFECT)
 	end
 end
