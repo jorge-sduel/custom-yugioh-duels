6dd9425e734a46fdb54c9ede364eedaaa4331d5d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-05-06 11:23:09 -0600

    Add files via upload

---
+++ script/c789.lua
@@ -0,0 +1,78 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x76),4,2,nil,nil,99)
+	c:EnableReviveLimit()
+	--
+local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.tg)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1)
+	--Atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(aux.TargetBoolFunction(Card.IsType,TYPE_XYZ))
+	e2:SetValue(500)
+	c:RegisterEffect(e2)
+	--handes
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_IGNITION)
+	e4:SetCategory(CATEGORY_TODECK)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCost(aux.dxmcostgen(1,1,nil))
+	e4:SetCountLimit(1)
+	e4:SetOperation(s.hdop)
+	c:RegisterEffect(e4,false,REGISTER_FLAG_DETACH_XMAT)
+end
+s.listed_series={0x76}
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x76) 
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP_DEFENSE)
+end
+function s.tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.filter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP_DEFENSE) then
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_DISABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_SINGLE)
+		e2:SetCode(EFFECT_DISABLE_EFFECT)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+	end
+	Duel.SpecialSummonComplete()
+end
+function s.hdop(e,tp,eg,ep,ev,re,r,rp)
+	--if e:GetHandler():IsRelateToEffect(e) then
+		local g=Duel.GetFieldGroup(tp,0,LOCATION_HAND)
+		if #g==0 then return end
+		local sg=g:RandomSelect(tp,1)
+		Duel.SendtoDeck(sg,nil,2,REASON_EFFECT)
+end
