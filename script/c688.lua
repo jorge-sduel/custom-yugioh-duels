3e00ebea8831700284b7ee10cb45a3542a9d6788
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-05-13 10:15:33 -0600

    Update c688.lua

--- script/c688.lua
+++ script/c688.lua
@@ -33,6 +33,18 @@ function s.initial_effect(c)
 	e3:SetTarget(s.lvtg2)
 	e3:SetOperation(s.lvop2)
 	c:RegisterEffect(e3)
+		--Add 1 "Salamangreat Sanctuary" to hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.thcon)
+	e1:SetTarget(s.thtg)
+	e1:SetOperation(s.thop)
+	c:RegisterEffect(e1)
 end
 function s.spfilter(c)
 	return c:IsRace(RACE_FIEND)
@@ -109,5 +121,23 @@ function s.lvop2(e,tp,eg,ep,ev,re,r,rp)
 		e3:SetValue(TYPE_TUNER)
 		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 		e:GetHandler():RegisterEffect(e3)
+	end
+end
+function s.thfilter(c)
+	return c:IsSetCard() and c:IsAbleToHand()
+end
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
 	end
 end
