859bc1b8269319779adf329af4ede8555a321e3b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-05-20 17:16:41 -0600

    Update c322.lua

--- script/c322.lua
+++ script/c322.lua
@@ -2,15 +2,15 @@
 function c322.initial_effect(c)
 	--Return
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetType(EFFECT_TYPE_QUICK_O+EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL+EFFECT_FLAG_DELAY)
-	e1:SetCode(511002521)
+	e1:SetCode(EVENT_LP0)
 	e1:SetRange(LOCATION_EXTRA)
 	e1:SetCountLimit(1)
 	e1:SetTarget(c322.tg)
 	e1:SetOperation(c322.op)
 	c:RegisterEffect(e1)
-	--aux.LP0ActivationValidity(e1)
+	aux.LP0ActivationValidity(e1)
 --remove
 local e2=Effect.CreateEffect(c)
 e2:SetType(EFFECT_TYPE_QUICK_O)
