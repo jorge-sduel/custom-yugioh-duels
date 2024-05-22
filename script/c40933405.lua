555d5bf492c2fae31c8474635a714ce775194c84
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-05-22 14:38:29 -0600

    Update c40933405.lua

--- script/c40933405.lua
+++ script/c40933405.lua
@@ -1,7 +1,7 @@
 --No.99 希望皇龍ホープドラグーン
 function c40933405.initial_effect(c)
 	--xyz summon
-	aux.AddFusionProcCodeFun(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x19),aux.FilterBoolFunction(c40933405.spfilter2),1,true,true)
+	--aux.AddFusionProcCodeFun(c,aux.FilterBoolFunction(Card.IsFusionSetCard,0x19),aux.FilterBoolFunction(c40933405.spfilter2),1,true,true)
 	c:EnableReviveLimit()
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
@@ -16,26 +16,21 @@ function c40933405.initial_effect(c)
 	e2:SetCode(EFFECT_SPSUMMON_PROC)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_EXTRA)
+	e2:SetValue(SUMMON_TYPE_FUSION)
 	e2:SetCondition(c40933405.sprcon)
 	e2:SetOperation(c40933405.sprop)
 	c:RegisterEffect(e2)
-	--Disable
+		--Destroy
 	local e3=Effect.CreateEffect(c)
-	e3:SetDescription(aux.Stringid(7841112,1))
+	e3:SetDescription(aux.Stringid(id,0))
 	e3:SetType(EFFECT_TYPE_IGNITION)
-	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e3:SetCountLimit(1)
 	e3:SetRange(LOCATION_MZONE)
-	e3:SetTarget(c40933405.distg)
-	e3:SetOperation(c40933405.disop)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCategory(CATEGORY_DESTROY)
+	e3:SetTarget(c40933405.destg)
+	e3:SetOperation(c40933405.desop)
 	c:RegisterEffect(e3)
-	--activate limit
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e4:SetCode(EVENT_CHAINING)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetOperation(c40933405.alop)
-	c:RegisterEffect(e4)
 end
 function c40933405.splimit(e,se,sp,st)
 	return e:GetHandler():GetLocation()~=LOCATION_EXTRA
@@ -66,39 +61,21 @@ function c40933405.sprop(e,tp,eg,ep,ev,re,r,rp,c)
 	Duel.SendtoDeck(g1,nil,1,REASON_EFFECT)
 	Duel.DiscardHand(tp,c40933405.spfilter2,1,1,REASON_EFFECT+REASON_DISCARD)
 end
-function c40933405.disfilter(c)
-	return c:IsType(TYPE_EFFECT) and c:IsSetCard(0x19) and not c:IsCode(40933405)
+function c40933405.destg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return false end
+	if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_ONFIELD,0,1,nil)
+		and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+	--Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g1=Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_ONFIELD,0,1,1,nil)
+	--Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g2=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,1,nil)
+	g1:Merge(g2)
+	--Duel.SetOperationInfo(0,CATEGORY_DESTROY,g1,2,0,0)
 end
-function c40933405.distg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chk==0 then return Duel.IsExistingTarget(c40933405.disfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
-	local g=Duel.SelectTarget(tp,c40933405.disfilter,tp,LOCATION_MZONE+LOCATION_GRAVE,0,1,1,nil)
-	Duel.SetOperationInfo(0,nil,g,1,0,0)
-end
-function c40933405.disop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	local c=e:GetHandler()
-	if tc:IsFaceup() and tc:IsRelateToEffect(e) then
-		local e1=Effect.CreateEffect(c)
-		e1:SetType(EFFECT_TYPE_SINGLE)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		tc:RegisterEffect(e1)
-		Duel.MajesticCopy(c,tc)
+function c40933405.desop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local tg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #tg>0 then
+		Duel.SendtoDeck(tg,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
 	end
-end
-function c40933405.alop(e,tp,eg,ep,ev,re,r,rp)
-	if re:GetOwner()==e:GetOwner() and not re:IsHasProperty(EFFECT_FLAG_INITIAL) then
-		local e1=Effect.CreateEffect(e:GetHandler())
-		e1:SetType(EFFECT_TYPE_FIELD)
-		e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-		e1:SetCode(EFFECT_CANNOT_ACTIVATE)
-		e1:SetRange(LOCATION_MZONE)
-		e1:SetTargetRange(1,1)
-		e1:SetValue(c40933405.aclimit)
-		e1:SetReset(RESET_EVENT+0x1fe0000+RESET_PHASE+PHASE_END)
-		e:GetHandler():RegisterEffect(e1)
-	end
-end
-function c40933405.aclimit(e,re,tp)
-	return re:GetOwner()==e:GetOwner() and not re:IsHasProperty(EFFECT_FLAG_INITIAL)
 end
