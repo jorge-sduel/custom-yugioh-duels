1a7aa9660fe299425e72b50dd6264f3567dbfb41
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-05-22 14:24:54 -0600

    Update c40933398.lua

--- script/c40933398.lua
+++ script/c40933398.lua
@@ -1,7 +1,7 @@
 function c40933398.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	aux.AddFusionProcCodeFun(c,57731460,aux.FilterBoolFunction(Card.IsFusionSetCard,0x19),1,true,true)
+	Fusion.AddProcMixRep(c,true,true,aux.FilterBoolFunctionEx(Card.IsSetCard,0x19),1,1,57731460)
 	--spsummon condition
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
