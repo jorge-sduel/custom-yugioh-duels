5c18f4aaccff1c80b6ac11d2d7eee0cd064e9836
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-05-27 17:17:59 -0600

    Update c901.lua

--- script/c901.lua
+++ script/c901.lua
@@ -86,7 +86,7 @@ function s.breakcon(sg,e,tp,mg)
 	return sg:GetSum(Card.GetScale)>=13
 end
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,c)
-	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_HAND+LOCATION_MZONE,0,e:GetHandler())
+	local rg=Duel.GetMatchingGroup(s.spfilter,tp,LOCATION_PZONE+LOCATION_SZONE+LOCATION_MZONE,0,e:GetHandler())
 	local mg=aux.SelectUnselectGroup(rg,e,tp,1,99,s.rescon,1,tp,HINTMSG_TOGRAVE,s.breakcon,s.breakcon,true)
 	if #mg>0 then
 		mg:KeepAlive()
