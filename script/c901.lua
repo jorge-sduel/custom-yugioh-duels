39ce72fa4f95e7b69cec19dd2d2b6856ca46348f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-05-28 11:52:43 -0600

    Update c901.lua

--- script/c901.lua
+++ script/c901.lua
@@ -167,5 +167,9 @@ function s.valop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(val)
 		o:RegisterEffect(e1) 
 	end
+		local tg=s:GetTarget()
+	if tg then tg(e,tp,eg,ep,ev,re,r,rp,1) end
+	local op=s:GetOperation()
+	if op then op(e,tp,eg,ep,ev,re,r,rp) end
 	Duel.Release(s,REASON_COST)
 end
