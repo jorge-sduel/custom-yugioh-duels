af5ff630dceb7c3eeddc6edc024e924dd509978f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-06-02 13:52:28 -0600

    Add files via upload

---
+++ script/c838.lua
@@ -0,0 +1,68 @@
+--Starve 
+local s,id=GetID()
+function s.initial_effect(c)
+		c:EnableReviveLimit()
+	--fusion material	
+Fusion.AddProcMix(c,true,true,aux.FilterBoolFunctionEx(Card.IsRace,RACE_ROCK),aux.FilterBoolFunctionEx(Card.IsRace,RACE_ROCK))
+	--indes
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e1:SetTargetRange(LOCATION_MZONE,0)
+	e1:SetTarget(s.target)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+	--indes
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.target)
+	e2:SetValue(300)
+	c:RegisterEffect(e2)
+	--spsummon
+	local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
+	e4:SetCondition(s.spcon2)
+	e4:SetTarget(s.sptg2)
+	e4:SetOperation(s.spop2)
+	c:RegisterEffect(e4)
+end
+function s.target(e,c)
+	return c:IsFaceup() and c:IsRace(RACE_ROCK)
+end
+function s.spcon2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsReason(REASON_BATTLE)
+		or (rp~=tp and c:IsReason(REASON_EFFECT) and c:IsPreviousControler(tp))
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0x2066) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:GetControler()==tp and s.filter(chkc,e,tp) end
+	if chk==0 then return not Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>1
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,3,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+function s.spop2(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local fg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #fg>1 and Duel.IsPlayerAffectedByEffect(tp,CARD_BLUEEYES_SPIRIT) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	if #fg>1 and ft==1 then fg=fg:Select(tp,1,1,nil) end
+	for tc in aux.Next(fg) do
+		Duel.SpecialSummonStep(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+	Duel.SpecialSummonComplete()
+end
