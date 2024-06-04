a4b214d92c0113fff4e7bc93cf4832a74ef87b91
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-06-03 21:34:24 -0600

    Update c841.lua

--- script/c841.lua
+++ script/c841.lua
@@ -19,7 +19,6 @@ function s.initial_effect(c)
 	--Draw 1 card and end the Battle Phase
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,0))
-	e3:SetCategory(CATEGORY_DRAW)
 	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e3:SetCode(EVENT_BE_BATTLE_TARGET)
 	e3:SetCost(s.spcost)
