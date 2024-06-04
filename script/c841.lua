281c7417c2a8d7dd6ab9e9703699afa64d77da98
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-06-03 21:20:44 -0600

    Update c841.lua

--- script/c841.lua
+++ script/c841.lua
@@ -16,8 +16,40 @@ function s.initial_effect(c)
 	e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
 	e2:SetValue(1)
 	c:RegisterEffect(e2)
+	--Draw 1 card and end the Battle Phase
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,0))
+	e3:SetCategory(CATEGORY_DRAW)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_BE_BATTLE_TARGET)
+	e3:SetCost(s.spcost)
+	e3:SetTarget(s.drawtg1)
+	e3:SetOperation(s.drawop1)
+	c:RegisterEffect(e1)
 end
-
+s.listed_series={0xe9}
 function s.ffilter(c)
 	return c:IsSetCard(0x2066)
+end
+function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	Duel.Release(e:GetHandler(),REASON_COST)
+end
+function s.filter(c,e,tp)
+	return c:IsSetCard(0xe9) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if e:GetHandler():GetSequence()<5 then ft=ft+1 end
+	if chk==0 then return ft>0 and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SkipPhase(1-tp,PHASE_BATTLE,RESET_PHASE|PHASE_BATTLE_STEP,1)
+		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
+	end
 end
