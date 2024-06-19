5f45cd139307b2d4fc21f74d578673cca0451115
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-06-19 13:53:25 -0600

    Update c69.lua

--- script/c69.lua
+++ script/c69.lua
@@ -1,7 +1,8 @@
 --Supremacy Synchro
+if not TRAMPULA_IMPORTED then Duel.LoadScript("proc_trampula.lua") end
 function c69.initial_effect(c)
 	--pendulum summon
-	Pendulum.AddProcedure(c)
+	Trampula.AddProcedure(c)
 	--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
