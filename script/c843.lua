b19c8087241099c121faeb8d5173bfe3f728de18
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-06-23 21:54:01 -0600

    Add files via upload

---
+++ script/c843.lua
@@ -0,0 +1,66 @@
+--No.s102
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,s.matfilter,4,3,s.ovfilter,aux.Stringid(id,0))
+	c:EnableReviveLimit()
+	--recover
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.tglimit)
+	e2:SetValue(aux.tgoval)
+	c:RegisterEffect(e2)
+end
+s.xyz_number=102
+function s.ovfilter(c,tp,lc)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_FAIRY)
+end
+function s.matfilter(c)
+	return c:IsRace(RACE_FAIRY)
+end
+function s.tglimit(e,c)
+	return c~=e:GetHandler()
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,2,2,REASON_COST)
+end
+function s.op(e,tp,eg,ep,ev,re,r,rp)
+	--destroy replace
+	local e3=Effect.CreateEffect(e:GetHandler())
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTarget(s.reptg)
+	e3:SetValue(s.repval)
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e3,tp)
+end
+function s.repfilter(c,tp)
+	return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+		and c:IsReason(REASON_BATTLE+REASON_EFFECT) and not c:IsReason(REASON_REPLACE)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	local g=c:GetLinkedGroup()
+	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp) and Duel.CheckLPCost(tp,700) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		Duel.PayLPCost(tp,700)
+		return true
+	else return false end
+end
+function s.repval(e,c)
+	return s.repfilter(c,e:GetHandlerPlayer())
+end
