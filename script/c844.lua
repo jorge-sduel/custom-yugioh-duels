cba5ea8e403988c1cf49c9cb7edefb57690cd00d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-06-24 13:03:02 -0600

    Add files via upload

---
+++ script/c844.lua
@@ -0,0 +1,153 @@
+--No.s103
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,s.matfilter,4,3,s.ovfilter,aux.Stringid(id,0))
+	c:EnableReviveLimit()
+	--recover
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1,id)
+	e1:SetOperation(s.op)
+	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+	--banish from the GY and special summon itself
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE+CATEGORY_SPECIAL_SUMMON)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetCondition(s.spcon)
+	e3:SetTarget(s.sptg)
+	e3:SetOperation(s.spop)
+	c:RegisterEffect(e3)
+--
+local e4=Effect.CreateEffect(c)
+	e4:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE)
+	e4:SetType(EFFECT_TYPE_QUICK_O)
+	e4:SetCode(EVENT_SUMMON_SUCCESS)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetCost(s.cost)
+	e4:SetTarget(s.target)
+	e4:SetOperation(s.activate)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE)
+	e5:SetType(EFFECT_TYPE_QUICK_O)
+	e5:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCost(s.cost) 
+	e5:SetTarget(s.target)
+	e5:SetOperation(s.activate)
+	c:RegisterEffect(e5)
+	local e6=Effect.CreateEffect(c)
+	e6:SetCategory(CATEGORY_DESTROY+CATEGORY_REMOVE)
+	e6:SetType(EFFECT_TYPE_QUICK_O)
+	e6:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCost(s.cost)
+	e6:SetTarget(s.target2)
+	e6:SetOperation(s.activate2)
+	c:RegisterEffect(e6)
+end
+s.xyz_number=103
+function s.ovfilter(c,tp,lc)
+	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_FAIRY)
+end
+function s.matfilter(c)
+	return c:IsAttribute(ATTRIBUTE_WATER)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,2,REASON_COST) end
+	e:GetHandler():RemoveOverlayCard(tp,2,2,REASON_COST)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsReason(REASON_DESTROY)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	local dg=Duel.GetFieldGroup(tp,LOCATION_MZONE,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,dg,#dg,0,0)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_REMOVE,nil,1,tp,LOCATION_GRAVE)
+	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,tp,LOCATION_GRAVE)
+end
+function s.rmfilter(c)
+	return c:IsAttribute(ATTRIBUTE_DARK) and c:IsLevelAbove(8) and c:IsAbleToRemove() and aux.SpElimFilter(c,true)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.rmfilter,tp,LOCATION_GRAVE,0,c)
+	local c=e:GetHandler()
+	if #g>0 and c:IsLocation(LOCATION_GRAVE) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,2)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local rg=g:Select(tp,1,1,nil)
+if Duel.Remove(rg,POS_FACEUP,REASON_EFFECT)==0 then return end
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP) 
+	end
+	local g2=Duel.GetMatchingGroup(Card.IsMonster,tp,LOCATION_GRAVE,LOCATION_GRAVE,c)
+	if #g2>0 and c:IsLocation(LOCATION_GRAVE)
+		and Duel.SelectYesNo(tp,aux.Stringid(id,3)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local rg2=g2:Select(tp,1,2,nil)
+			Duel.Overlay(c,rg2)
+ end
+end
+function s.filter(c,tp,ep)
+	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup()
+		and ep~=tp and c:IsAbleToRemove()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	local tc=eg:GetFirst()
+	if chk==0 then return s.filter(tc,tp,ep) end
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,tc,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,tc,1,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local tc=eg:GetFirst()
+	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 then
+		local dg=Duel.GetOperatedGroup()
+		local atk=0
+		local tc=dg:GetFirst()
+		while tc do
+			if tc:IsPreviousPosition(POS_FACEUP) then
+				atk=atk+tc:GetPreviousAttackOnField()
+			end
+			tc=dg:GetNext()
+		end
+		Duel.Damage(1-tp,atk,REASON_EFFECT)
+	end
+end
+function s.filter2(c,tp)
+	return c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and c:GetSummonPlayer()~=tp
+		and c:IsAbleToRemove()
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return eg:IsExists(s.filter2,1,nil,tp) end
+	local g=eg:Filter(s.filter2,nil,tp)
+	Duel.SetTargetCard(eg)
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_REMOVE,g,#g,0,0)
+end
+function s.filter3(c,e,tp)
+	return c:IsFaceup() and not c:IsSummonPlayer(tp)
+		and c:IsRelateToEffect(e) and c:IsLocation(LOCATION_MZONE)
+end
+function s.activate2(e,tp,eg,ep,ev,re,r,rp)
+	local g=eg:Filter(s.filter3,nil,e,tp)
+	if Duel.Remove(g,POS_FACEUP,REASON_EFFECT)>0 then
+		local dg=Duel.GetOperatedGroup()
+		local atk=0
+		local tc=dg:GetFirst()
+		while tc do
+			if tc:IsPreviousPosition(POS_FACEUP) then
+				atk=atk+tc:GetPreviousAttackOnField()
+			end
+			tc=dg:GetNext()
+		end
+		Duel.Damage(1-tp,atk,REASON_EFFECT)
+	end
+end
