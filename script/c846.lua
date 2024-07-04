fc51b2948e9a131e0eacb7bbb1fb4e2929accf4f
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-07-04 13:18:08 -0600

    Update c846.lua

--- script/c846.lua
+++ script/c846.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--xyz summon
-	Xyz.AddProcedure(c,s.matfilter,4,3,s.ovfilter,aux.Stringid(id,0))
+	Xyz.AddProcedure(c,s.matfilter,4,3,s.ovfilter,aux.Stringid(id,0),3,s.xyzop)
 	c:EnableReviveLimit()
 	-- Destroy 1 Spell/Trap on the field
 	local e0=Effect.CreateEffect(c)
@@ -45,6 +45,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)	
 end
 s.xyz_number=106
+function s.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	return true
+end
 function s.ovfilter(c,tp,lc)
 	return c:IsRace(RACE_WARRIOR) and c:IsType(TYPE_XYZ)
 end
