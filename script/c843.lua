9e780fe688b7eced792c9dbf94080829fe15cb39
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-07-04 13:21:44 -0600

    Update c843.lua

--- script/c843.lua
+++ script/c843.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--xyz summon
-	Xyz.AddProcedure(c,s.matfilter,4,3,s.ovfilter,aux.Stringid(id,0))
+	Xyz.AddProcedure(c,s.matfilter,4,3,s.ovfilter,aux.Stringid(id,0),3,s.xyzop)
 	c:EnableReviveLimit()
 	--recover
 	local e1=Effect.CreateEffect(c)
@@ -35,6 +35,11 @@ function s.initial_effect(c)
 	c:RegisterEffect(e3)
 end
 s.xyz_number=102
+function s.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	return true
+end
 function s.ovfilter(c,tp,lc)
 	return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_FAIRY) and c:IsType(TYPE_XYZ)
 end
