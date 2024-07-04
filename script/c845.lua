89d7cb251e4414c5167d8870166c82cad09c23fc
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-07-04 13:19:07 -0600

    Update c845.lua

--- script/c845.lua
+++ script/c845.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--xyz summon
-	Xyz.AddProcedure(c,s.matfilter,4,3,s.ovfilter,aux.Stringid(id,0))
+	Xyz.AddProcedure(c,s.matfilter,4,3,s.ovfilter,aux.Stringid(id,0),3,s.xyzop)
 	c:EnableReviveLimit()
 	--Attach top deck card during the Standby Phase
 	local e1=Effect.CreateEffect(c)
@@ -38,6 +38,11 @@ function s.initial_effect(c)
 	aux.DoubleSnareValidity(c,LOCATION_MZONE)
 end
 s.xyz_number=104
+function s.xyzop(e,tp,chk)
+	if chk==0 then return Duel.GetFlagEffect(tp,id)==0 end
+	Duel.RegisterFlagEffect(tp,id,RESET_PHASE|PHASE_END,0,1)
+	return true
+end
 function s.ovfilter(c,tp,lc)
 	return c:IsSetCard(SET_NUMBER)
 end
