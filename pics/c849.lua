64deeb640b1ea296f843fb5945f7a7e19b4366db
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-07-07 10:32:49 -0600

    Add files via upload

---
+++ pics/c849.lua
@@ -0,0 +1,41 @@
+--Rxyz
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	Xyz.AddProcedure(c,nil,7,3,nil,nil,99)
+	c:EnableReviveLimit()
+		--special summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SPSUMMON_PROC)
+	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCondition(s.spcon)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_NUMBER}
+function s.cfilter(c)
+	return c:IsSetCard(SET_NUMBER) and c:IsCanBeXyzMaterial()
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	local tp=c:GetControler()
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,3,nil)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,3,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	local g1=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,3,99,nil)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_XMATERIAL)
+	og=Group.CreateGroup()
+	og:Merge(g1)
+		if g1:GetCount()>0 then
+		local mg1=g1:GetOverlayGroup()
+		if mg1:GetCount()~=0 then
+			og:Merge(mg1)
+			Duel.Overlay(c,mg1)
+		end
+		c:SetMaterial(og)
+		Duel.Overlay(c,g1)
+end
