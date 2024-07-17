4bf6807f5120e94a6cc0f01024a423a11ff92201
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-07-17 09:51:38 -0600

    Add files via upload

---
+++ script/c885.lua
@@ -0,0 +1,66 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--synchro limit
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CANNOT_BE_XYZ_MATERIAL)
+	e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
+	e1:SetValue(s.synlimit)
+	c:RegisterEffect(e1)
+	--spsummon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(57421866,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+end
+s.listed_series={SET_NUMBER}
+function s.synlimit(e,c)
+local no=c.Number_xyz
+	if not c then return false end
+	return not (c:IsType(TYPE_XYZ) and c:IsSetCard(SET_NUMBER) and no>100 and no<108)
+end
+function s.filter(c)
+	return c:IsFaceup() and c:GetLevel()>=1
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.filter(chkc) end
+	local c=e:GetHandler()
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil)
+		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 and c:IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(57421866,1))
+	Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsFacedown() or not tc:GetLevel()<1 then return end
+	local c=e:GetHandler()
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_CHANGE_LEVEL)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+	e1:SetValue(tc:GetLevel())
+	c:RegisterEffect(e1)
+--
+local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_CHANGE_RACE)
+	e2:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+	e2:SetValue(tc:GetRace())
+	c:RegisterEffect(e2)
+local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+	e3:SetReset(RESET_EVENT+RESETS_STANDARD-RESET_TOFIELD)
+	e3:SetValue(tc:GetAttribute())
+	c:RegisterEffect(e3)
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
