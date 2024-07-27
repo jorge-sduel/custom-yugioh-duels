d66be35dbeef0696162bed4edcd04603e55e0648
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-07-27 13:55:44 -0600

    Update c886.lua

--- script/c886.lua
+++ script/c886.lua
@@ -54,7 +54,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	e1:SetCode(EFFECT_SET_ATTACK)
 	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
 	e1:SetValue(7000)
-	tc:GetFirst():RegisterEffect(e1)
+	tc:RegisterEffect(e1)
 	end 
 end
 function s.spfilter(c,e,tp)
