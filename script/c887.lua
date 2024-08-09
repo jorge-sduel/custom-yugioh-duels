eed063a0da411f82ac0a7908d505bf8a8b2fa0ff
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-08-09 14:24:19 -0600

    Update c887.lua

--- script/c887.lua
+++ script/c887.lua
@@ -36,6 +36,19 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 		Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 		Duel.Draw(tp,1,REASON_EFFECT)
 	end
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(s.splimit)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
+function s.splimit(e,c)
+	local no=c.Number_xyz 
+	return not (c:IsType(TYPE_XYZ) and no>100) and c:IsLocation(LOCATION_EXTRA)
 end
 function s.xmfil1(c,tp)
 	return c:IsType(TYPE_XYZ) and c:IsSetCard(SET_NUMBER) and Duel.IsExistingMatchingCard(s.xmfil2,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,c,c)
