baf03ba82e0baf2e7869820eab9d2a6090581002
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2024-11-17 11:42:04 -0600

    Update c976719209.lua

--- script/c976719209.lua
+++ script/c976719209.lua
@@ -3,7 +3,7 @@ if not Rune then Duel.LoadScript("proc_rune.lua") end
 local s,id=GetID()
 function s.initial_effect(c)
 	--xyz summon
-	Xyz.AddProcedure(c,nil,s.xyzfilter,2,nil,nil,99)
+	Xyz.AddProcedure(c,s.xyzfilter,nil,2,nil,nil,99)
 	c:EnableReviveLimit()
 	--Equip 1 face-up monster on field to this card
 	local e1=Effect.CreateEffect(c)
