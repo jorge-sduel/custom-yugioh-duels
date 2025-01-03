b08c86d6851d1ec9e0819487e70dc78aff9d523d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-01-03 15:27:45 -0600

    Update c890.lua

--- script/c890.lua
+++ script/c890.lua
@@ -29,10 +29,11 @@ function s.tgfilter(c,e,tp)
 	return c:IsFaceup() and c:IsCode(CARD_BUSTER_BLADER) and c:IsCanBeFusionMaterial()
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c)
 end
+
 function s.spfilter(c,e,tp,mc)
 	if Duel.GetLocationCountFromEx(tp,tp,mc,c)<=0 then return false end
 	local mustg=aux.GetMustBeMaterialGroup(tp,nil,tp,c,nil,REASON_FUSION)
-	return c:IsType(TYPE_FUSION) and c:ListsCodeAsMaterial(CARD_BUSTER_BLADER) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,true,false)
+	return c:IsType(TYPE_FUSION) and c:ListsCodeAsMaterial(mc:GetCode()) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,false,false)
 		and (#mustg==0 or (#mustg==1 and mustg:IsContains(mc)))
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
@@ -52,7 +53,7 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 			sc:SetMaterial(Group.FromCards(tc))
 			Duel.SendtoGrave(tc,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
 			Duel.BreakEffect()
-			Duel.SpecialSummon(sc,SUMMON_TYPE_FUSION,tp,tp,true,false,POS_FACEUP)
+			Duel.SpecialSummon(sc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
 			sc:CompleteProcedure()
 		end
 	end
