4767b4a926580d83d5dee997d3ba34bac9d0f711
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-01-24 20:07:57 -0600

    Update c43954164.lua

--- script/c43954164.lua
+++ script/c43954164.lua
@@ -72,7 +72,7 @@ function cid.atkop(e,tp,eg,ep,ev,re,r,rp)
 end
 --SEARCH
 function cid.thfilter(c)
-	return (c:IsSetCard(0xfe9) or c:IsCode(table.unpack(c43954163.FELGRAND))) and c:IsAbleToHand() and not c:IsCode(id)
+	return (c:IsSetCard(0xfe9) or c:IsCode(table.unpack(c43954163.FELGRAND)) or c:IsSetCard(0xf72)) and c:IsAbleToHand() and not c:IsCode(id)
 end
 --------
 function cid.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -93,7 +93,7 @@ function cid.thop(e,tp,eg,ep,ev,re,r,rp)
 end
 --EQUIP
 function cid.efilter(c)
-	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and (c:IsSetCard(0xfe9) or c:IsCode(table.unpack(c43954163.FELGRAND)))
+	return c:IsFaceup() and c:IsType(TYPE_MONSTER) and (c:IsSetCard(0xfe9) or c:IsCode(table.unpack(c43954163.FELGRAND)) or c:IsSetCard(0xf72))
 end
 function cid.eqfilter(c,tp)
 	return c:IsLevel(7,8) and c:CheckUniqueOnField(tp) and c:IsType(TYPE_MONSTER) and c:IsRace(RACE_DRAGON) and not c:IsForbidden()
