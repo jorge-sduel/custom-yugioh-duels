9cbc0c14dc2b6f75605c980fa3c74e1c531321b0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-03-02 21:27:36 -0600

    Add files via upload

---
+++ script/c914.lua
@@ -0,0 +1,67 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,74367458,s.ffilter)
+		--atkup
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetValue(s.val)
+	c:RegisterEffect(e1)
+	--equip
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_EQUIP)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	--e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCode(EVENT_FREE_CHAIN)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetTarget(s.eqtg)
+	e2:SetOperation(s.eqop)
+	c:RegisterEffect(e2)
+	--Your equip cannot be destroyed by card effects
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e3:SetRange(LOCATION_SZONE)
+	e3:SetTargetRange(LOCATION_MZONE,0)
+	e3:SetTarget(s.indtg)
+	e3:SetValue(1)
+	c:RegisterEffect(e3)
+end
+s.listed_series={0x52}
+s.listed_names={74367458}
+s.material_setcode=0x52
+function s.ffilter(c,fc,sumtype,tp)
+	return c:IsSetCard(0x52)
+end
+function s.val(e,c)
+	return c:GetEquipCount()*1000
+end
+function s.filter(c,ec)
+	return c:IsType(TYPE_EQUIP) and c:CheckEquipTarget(ec)
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_DECK) and chkc:IsControler(tp) and s.filter(chkc,e:GetHandler()) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,nil,e:GetHandler()) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
+	--Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,g,1,0,0)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.Equip(tp,tc,c)
+	end
+end
+function s.indtg(e,c)
+	return c:IsFaceup() and c:IsType(TYPE_EQUIP)
+end
