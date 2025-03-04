2bc799e14b5124b6affb2cf8bb1eeb3e5f2bfbfd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-03-04 08:48:28 -0600

    Update c916.lua

--- script/c916.lua
+++ script/c916.lua
@@ -17,7 +17,7 @@ function s.initial_effect(c)
 	--attack
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetCode(EFFECT_SET_ATTACK)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetValue(s.atkval)
