d45129ed21772a05e9a8578f98f126a62c25a3a0
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-03-05 21:01:22 -0600

    Add files via upload

---
+++ script/c917.lua
@@ -0,0 +1,108 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--xyz summon
+	c:EnableReviveLimit()
+	Xyz.AddProcedure(c,nil,8,2,nil,nil,nil,nil,nil,s.xyzcheck)
+	--Cannot be destroyed by battle
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e0:SetRange(LOCATION_MZONE)
+	e0:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e0:SetValue(1)
+	c:RegisterEffect(e0)
+	--Negate attack, change battle position and special summon 1 BEWD from hand, Deck or GY
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_POSITION+CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_BE_BATTLE_TARGET)
+	e1:SetCountLimit(1,id)
+	e1:SetTarget(s.distarget)
+	e1:SetOperation(s.disop)
+	c:RegisterEffect(e1)
+	--Special Summon 1 BEWD from hand, Deck or GY
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_QUICK_O)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCode(EVENT_BECOME_TARGET)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.spcon)
+	e2:SetTarget(s.distarget)
+	e2:SetOperation(s.disop)
+	c:RegisterEffect(e2)
+	--atk
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetCode(EFFECT_UPDATE_ATTACK)
+	e3:SetValue(s.value)
+	c:RegisterEffect(e3)
+	--Special summon
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e4:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
+	e4:SetCode(EVENT_DESTROYED)
+	e4:SetTarget(s.target2)
+	e4:SetOperation(s.operation2)
+	c:RegisterEffect(e4)
+end
+s.listed_series={0x52}
+function s.xyzcheck(g)
+	return not g:IsExists(Card.IsSummonableCard,1)
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsContains(e:GetHandler())
+end
+function s.distarget(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDiscardDeck(tp,5) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(5)
+	Duel.SetOperationInfo(0,CATEGORY_DECKDES,nil,0,tp,5)
+end
+function s.disop(e,tp,eg,ep,ev,re,r,rp)
+	local p,val=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.DiscardDeck(p,val,REASON_EFFECT)
+end
+function s.value(e,c)
+	return Duel.GetMatchingGroupCount(Card.IsMonster,0,LOCATION_GRAVE,LOCATION_GRAVE,nil)*500
+end
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x52) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+end
+function s.swfilter(c,e,tp)
+	return c:IsType(TYPE_EQUIP)
+end
+function s.target2(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spfilter(chkc,e,tp) end
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) and Duel.IsExistingTarget(s.swfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,e:GetHandler(),1,0,0)
+end
+function s.eqlimit(e,c)
+	return e:GetLabelObject()==c
+end
+function s.operation2(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) then
+		if Duel.SpecialSummon(tc,0,tp,tp,true,true,POS_FACEUP_ATTACK)==0 then return end
+	local ec=Duel.SelectMatchingCard(tp,s.swfilter,tp,LOCATION_GRAVE,0,1,1,nil):GetFirst()
+	Duel.Equip(tp,ec,tc)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetProperty(EFFECT_FLAG_OWNER_RELATE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(s.eqlimit)
+		e1:SetLabelObject(tc)
+		ec:RegisterEffect(e1)
+	end
+end
