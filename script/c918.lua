64ded46bcacee37b0554e9959b064b43a3e41f09
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-03-07 08:04:48 -0600

    Update c918.lua

--- script/c918.lua
+++ script/c918.lua
@@ -41,7 +41,7 @@ function s.initial_effect(c)
 	c:RegisterEffect(e4)
 	----Special Summon monster
 	local e5=Effect.CreateEffect(c)
-	e5:SetDescription(aux.Stringid(id,0))
+	e5:SetDescription(aux.Stringid(id,1))
 	e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e5:SetCode(EVENT_LEAVE_FIELD)
