7edd319a72ed961e242379cc3caca4850519a2c3
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-03-08 14:13:02 -0600

    Add files via upload

---
+++ script/c919.lua
@@ -0,0 +1,141 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion Materials: 5 "Guardian" monsters with different names
+	Fusion.AddProcMixN(c,true,true,s.ffilter,5)
+Fusion.AddContactProc(c,s.contactfil,s.contactop,true)
+	c:EnableReviveLimit()
+	--negate
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_NEGATE+CATEGORY_DESTROY)
+	e1:SetType(EFFECT_TYPE_QUICK_O)
+	e1:SetCode(EVENT_CHAINING)
+	e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP+EFFECT_FLAG_DAMAGE_CAL)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.negcon)
+	e1:SetTarget(s.negtg)
+	e1:SetOperation(s.negop)
+	c:RegisterEffect(e1)
+	--Atk Change
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_EQUIP)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
+	e2:SetValue(c267.value)
+	c:RegisterEffect(e2)
+	--equip
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(18175965,1))
+	e5:SetCategory(CATEGORY_EQUIP)
+	e5:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e5:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+	e5:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e5:SetCondition(s.eqcon)
+	e5:SetTarget(s.eqtg)
+	e5:SetOperation(s.eqop)
+	c:RegisterEffect(e5)
+	--disable summon
+	local e6=Effect.CreateEffect(c)
+	e6:SetType(EFFECT_TYPE_FIELD)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetCode(EFFECT_CANNOT_SUMMON)
+	e6:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e6:SetTargetRange(1,0)
+	c:RegisterEffect(e6)
+	local e7=e6:Clone()
+	e7:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	c:RegisterEffect(e7)
+	--destroy
+	local e8=Effect.CreateEffect(c)
+	e8:SetType(EFFECT_TYPE_IGNITION)
+	e8:SetCategory(CATEGORY_DESTROY)
+	e8:SetDescription(aux.Stringid(39765958,0))
+	e8:SetRange(LOCATION_MZONE)
+	e8:SetCondition(s.descon)
+	e8:SetTarget(s.destg)
+	e8:SetOperation(s.desop)
+	c:RegisterEffect(e8)
+	--Destroy replace
+	local e9=Effect.CreateEffect(c)
+	e9:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e9:SetCode(EFFECT_DESTROY_REPLACE)
+	e9:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e9:SetRange(LOCATION_MZONE)
+	e9:SetTarget(s.desreptg)
+	c:RegisterEffect(e9)
+end
+s.listed_names={34022290}
+function s.ffilter(c,fc,sumtype,tp,sub,mg,sg)
+	return c:IsSetCard(0x52,fc,0,tp) and (not sg or not sg:IsExists(s.fusfilter,1,c,c:GetCode(fc,0,tp),fc,0,tp))
+end
+function s.fusfilter(c,code,fc,sumtype,tp)
+	return c:IsSummonCode(fc,0,tp,code) and not c:IsHasEffect(511002961)
+end
+function s.contactfil(tp)
+	return Duel.GetMatchingGroup(Card.IsAbleToGraveAsCost,tp,LOCATION_ONFIELD,0,nil)
+end
+function s.contactop(g)
+	Duel.SendtoGrave(g,REASON_COST+REASON_MATERIAL)
+end
+function s.negcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+	local g=Duel.GetChainInfo(ev,CHAININFO_TARGET_CARDS)
+	if not g or not g:IsContains(c) then return false end
+	return re:IsHasType(EFFECT_TYPE_ACTIVATE)
+end
+function s.negtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return true end
+	Duel.SetOperationInfo(0,CATEGORY_NEGATE,eg,1,0,0)
+	if re:GetHandler():IsRelateToEffect(re) and re:GetHandler():IsDestructable() then
+		Duel.SetOperationInfo(0,CATEGORY_DESTROY,eg,1,0,0)
+	end
+end
+function s.negop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+		Duel.Destroy(re:GetHandler(),REASON_EFFECT)
+	end
+end
+function s.value(e,c)
+	local g=Duel.GetMatchingGroup(Card.IsMonster,c:GetControler(),LOCATION_GRAVE,LOCATION_GRAVE,nil)
+		return g:GetSum(Card.GetAttack)
+end
+function s.filter(c,ec)
+	return c:IsType(TYPE_EQUIP) and c:CheckEquipTarget(ec)
+end
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():GetSummonType()==SUMMON_TYPE_SPECIAL
+end
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil,e:GetHandler()) end
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,nil,1,tp,LOCATION_DECK)
+end
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 or c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(18175965,3))
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil,c)
+	if #g>0 then
+		Duel.Equip(tp,g:GetFirst(),c)
+	end
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsAttackPos()
+end
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,1,e:GetHandler()) end
+	local sg=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.SetOperationInfo(0,CATEGORY_DESTROY,sg,#sg,0,0)
+end
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+	local sg=Duel.GetMatchingGroup(Card.IsDestructable,tp,LOCATION_MZONE,LOCATION_MZONE,e:GetHandler())
+	Duel.Destroy(sg,REASON_EFFECT)
+end
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and Duel.GetFieldGroupCount(tp,LOCATION_DECK,0)>0 and Duel.GetFieldGroupCount(1-tp,LOCATION_DECK,0)>0 end
+	Duel.DiscardDeck(tp,5,REASON_EFFECT)
+Duel.DiscardDeck(1-tp,5,REASON_EFFECT)
+	return true
+end
