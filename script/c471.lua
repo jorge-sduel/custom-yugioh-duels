49368ae2f8b5e61442a84c557392865b5407be78
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-03-16 15:45:52 -0600

    Update c471.lua

--- script/c471.lua
+++ script/c471.lua
@@ -16,8 +16,8 @@ function s.initial_effect(c)
 	e2:SetDescription(aux.Stringid(id,0))
 	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
 	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e2:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
-	e2:SetCode(EVENT_DESTROYED)
+	e2:SetCode(EVENT_TO_GRAVE)
+	e2:SetCondition(s.spcon)
 	e2:SetTarget(s.sptg)
 	e2:SetOperation(s.spop)
 	c:RegisterEffect(e2)
@@ -82,4 +82,8 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.eqlimit(e,c)
 	return e:GetLabelObject()==c
+end
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	local ec=e:GetHandler():GetPreviousEquipTarget()
+	return (ec and ec:IsReason(REASON_DESTROY)) or e:GetHandler():IsReason(REASON_DESTROY)
 end
