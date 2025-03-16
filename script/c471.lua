0b536e131f3f45c96093e9e6748c797651eb2496
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-03-16 15:18:46 -0600

    Update c471.lua

--- script/c471.lua
+++ script/c471.lua
@@ -1,13 +1,12 @@
---トランスターン
+5 --トランスターン
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate
+		--Activate
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
 	e1:SetLabel(0)
-	e1:SetCountLimit(1,id)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
@@ -50,52 +49,35 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
 	e:SetLabel(100)
-	if chk==0 then return true end
+	return true
 end
-function s.cfilter(c,e,tp,ft)
-	local lv=c:GetOriginalLevel()
-	local rc=c:GetOriginalRace()
-	local att=c:GetOriginalAttribute()
-	local eff4064256={Duel.GetPlayerEffect(tp,4064256)}
-	for _,te in ipairs(eff4064256) do
-		local val=te:GetValue()
-		if val and val(te,c,e,0) then rc=val(te,c,e,1) end
-	end
-	local eff12644061={Duel.GetPlayerEffect(tp,12644061)}
-	for _,te in ipairs(eff12644061) do
-		local val=te:GetValue()
-		if val and val(te,c,e,0) then att=val(te,c,e,1) end
-	end
-	return c:GetOriginalType()&TYPE_MONSTER~=0 and lv>0 and c:IsFaceup() and c:IsAbleToGraveAsCost() and (ft>0 or c:GetSequence()<5)
-		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,lv,rc,att,e,tp) and c:IsSetCard(0x52)
+function s.filter1(c,e,tp)
+	local lv=c:GetLevel()
+	return  c:IsSetCard(0x52) and  Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil,lv,e,tp)
 end
-function s.spfilter(c,lv,rc,att,e,tp)
-	return c:IsLevel(lv) and c:IsSetCard(0x52) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
+function c72.filter2(c,lv,e,tp)
+	return c:IsSetCard(0x52) and c:IsLevel(lv) and c:IsCanBeSpecialSummoned(e,0,tp,true,true)
 end
-function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
-	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+function c72.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		if e:GetLabel()~=100 then return false end
 		e:SetLabel(0)
-		return ft>-1 and Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil,e,tp,ft)
+		return Duel.CheckReleaseGroup(tp,s.filter1,1,nil,e,tp)
 	end
-	e:SetLabel(0)
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
-	local g=Duel.SelectMatchingCard(tp,s.cfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp,ft)
-	local tc=g:GetFirst()
-	Duel.SendtoGrave(tc,REASON_COST)
-	Duel.SetTargetCard(tc)
-	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_DECK)
+	local rg=Duel.SelectReleaseGroup(tp,s.filter1,1,1,nil,e,tp)
+	e:SetLabel(rg:GetFirst():GetLevel())
+	Duel.Release(rg,REASON_COST)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	local tc=Duel.GetFirstTarget()
-	if not tc or not tc:IsRelateToEffect(e) then return end
+	if Duel.GetLocationCountFromEx(tp)<=0 then return end
+	local lv=e:GetLabel()
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetLevel()+1,tc:GetRace(),tc:GetAttribute(),e,tp)
-	if #g>0 then
+	local g=Duel.SelectMatchingCard(tp,s.filter2,tp,LOCATION_DECK,0,1,1,nil,lv,e,tp)
+	if g:GetCount()>0 then
 		Duel.SpecialSummon(g,0,tp,tp,true,true,POS_FACEUP)
-		--Duel.Equip(tp,c,g) 
+		Duel.Equip(tp,c,g)
+		g:GetFirst():CompleteProcedure()
 	end
 end
 function s.eqlimit(e,c)
