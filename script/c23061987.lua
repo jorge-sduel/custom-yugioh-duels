e08a720ba7b1bdb516e9f6118f72290a08b148eb
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-03-29 09:25:41 -0600

    Add files via upload

--- script/c23061987.lua
+++ script/c23061987.lua
@@ -1,33 +1,100 @@
---Heart of the True Duelists-Dragon
-function c23061987.initial_effect(c)
+--
+local s,id=GetID()
+function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcCode2(c,23995346,23061998,false,false)
-	--atkup
+	Fusion.AddProcMix(c,true,true,23061993,23995346)
+	--atk
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_SINGLE)
 	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
-	e1:SetValue(c23061987.val)
+	e1:SetCode(EFFECT_UPDATE_ATTACK)
+	e1:SetValue(s.atkval)
 	c:RegisterEffect(e1)
-	local e2=e1:Clone()
-	e2:SetCode(EFFECT_UPDATE_DEFENCE)
+	--Is also treated as LIGHT
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_ADD_ATTRIBUTE)
+	e2:SetRange(LOCATION_MZONE+LOCATION_GRAVE)
+	e2:SetValue(ATTRIBUTE_DARK)
 	c:RegisterEffect(e2)
-	--only be fusion-summoned
+	--Untargetable
 	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(3060)
 	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_UNCOPYABLE)
-	e3:SetCode(EFFECT_SPSUMMON_CONDITION)
-	e3:SetValue(c23061987.splimit)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetValue(aux.tgoval)
 	c:RegisterEffect(e3)
+	--Indestructible
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(s.indval)
+	c:RegisterEffect(e4)
+	--Sin's Curse
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_UPDATE_ATTACK)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetTargetRange(0,LOCATION_MZONE)
+	e5:SetValue(s.sincurseval)
+	c:RegisterEffect(e5)
+	--Special Summon
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,1))
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetCountLimit(1,id)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetTarget(s.sptg)
+	e6:SetOperation(s.spop)
+	c:RegisterEffect(e6)
 end
-function c23061987.splimit(e,se,sp,st)
-	return bit.band(st,SUMMON_TYPE_FUSION)==SUMMON_TYPE_FUSION
+s.material_setcode={0xdd,0x10cf,0xcf}
+s.listed_names={23061993,23995346}
+function s.filter(c)
+	return (c:IsLocation(LOCATION_GRAVE) or c:IsFaceup()) and c:IsRace(RACE_DRAGON+RACE_SPELLCASTER+RACE_WARRIOR) 
 end
-function c23061987.val(e,c)
-	return Duel.GetMatchingGroupCount(c23061987.filter,0,0x14,0x14,nil)*1000
+function s.atkval(e,c)
+	return Duel.GetMatchingGroupCount(s.filter,c:GetControler(),LOCATION_MZONE+LOCATION_GRAVE+LOCATION_EXTRA,LOCATION_MZONE+LOCATION_GRAVE+LOCATION_EXTRA,c)*800
 end
-function c23061987.filter(c)
-	return c:IsRace(RACE_DRAGON) and (c:IsLocation(LOCATION_GRAVE))
+function s.indval(e,re,tp)
+	return tp~=e:GetHandlerPlayer()
+end
+--special summon
+function s.filter(c,e,tp)
+	return (c:IsAttribute(ATTRIBUTE_LIGHT) or c:IsAttribute(ATTRIBUTE_DARK))and c:IsRace(RACE_DRAGON) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,nil,e,tp) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_HAND+LOCATION_GRAVE+LOCATION_DECK,0,1,1,nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+function s.sincursefilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_DRAGON)
+end
+function s.sincurseval(e,c)
+	local c=e:GetHandler()
+	local ATK=0
+	local g=Duel.GetMatchingGroup(s.sincursefilter,e:GetOwnerPlayer(),LOCATION_MZONE,0,c)
+	local tc=g:GetFirst()
+	while tc do
+	ATK=ATK+tc:GetAttack()
+	tc=g:GetNext()
+	end
+	return -ATK
 end
