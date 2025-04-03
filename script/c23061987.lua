e3915ae533283b66dbaf5b6d4b8a9e1fd749811d
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-04-02 19:48:07 -0600

    Update c23061987.lua

--- script/c23061987.lua
+++ script/c23061987.lua
@@ -3,7 +3,7 @@ local s,id=GetID()
 function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcMix(c,true,true,23061993,23995346)
+	Fusion.AddProcMix(c,true,true,23061998,23995346)
 	--atk
 	local e0=Effect.CreateEffect(c)
 	e0:SetType(EFFECT_TYPE_SINGLE)
