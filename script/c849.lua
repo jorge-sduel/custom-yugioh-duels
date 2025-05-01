e6b487848b76f4a6e517bc5f2e6a0b9eabb5848e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-05-01 15:02:09 -0600

    Update c849.lua

--- script/c849.lua
+++ script/c849.lua
@@ -2,7 +2,7 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	--xyz summon
-	Xyz.AddProcedure(c,nil,7,3,nil,nil,99)
+	Xyz.AddProcedure(c,nil,7,3,nil,nil,Xyz.InfiniteMats)
 	c:EnableReviveLimit()
 	--Detach Xyz material
 	local e1=Effect.CreateEffect(c)
