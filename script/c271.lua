1b231c9cbeca5b3e6be7d0f5cfd00a884771703b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-05-31 14:55:52 -0600

    Update c271.lua

--- script/c271.lua
+++ script/c271.lua
@@ -3,7 +3,7 @@ if not Chrono then Duel.LoadScript("proc_chrono.lua") end
 function c271.initial_effect(c)
 	c:EnableReviveLimit()
 	Chrono.AddProcedure(c,LOCATION_HAND)
-	--recover
+	--[[recover
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(87102774,0))
 	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
@@ -24,7 +24,7 @@ function c271.initial_effect(c)
 	e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
 	e2:SetRange(LOCATION_HAND)
 	e2:SetCondition(c271.spcon)
-	c:RegisterEffect(e2)
+	c:RegisterEffect(e2)]]
 	--to grave
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(13893596,0))
