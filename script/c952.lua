ef2f14e618cb1226e882d77f8706fe3c469c5977
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-02 14:52:06 -0600

    Add files via upload

---
+++ script/c952.lua
@@ -0,0 +1,29 @@
+--Quartz
+function s.initial_effect(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCondition(s.spcon)
+	e1:SetOperation(s.spop)
+	c:RegisterEffect(e1)
+	--Ritual Summon 1 LIGHT Warrior or Dragon Ritual Monster from your hand
+	local e2=Ritual.CreateProc(c,RITPROC_GREATER,
+		function(rit_c) return rit_c:IsAttribute(ATTRIBUTE_LIGHT) end,
+		nil,aux.Stringid(id,1))
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,{id,1})
+	--e2:SetCost(Cost.SelfBanish)
+	c:RegisterEffect(e2)
+end
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.CheckReleaseGroup(c:GetControler(),Card.IsAttribute,1,nil,ATTRIBUTE_LIGHT)
+end
+function s.spop(e,tp,eg,ep,ev,re,r,rp,c)
+	local g=Duel.SelectReleaseGroup(c:GetControler(),Card.IsAttribute,1,1,nil,ATTRIBUTE_LIGHT)
+	Duel.Release(g,REASON_COST)
+end
