c21d9518cee7a2679cc18d5468369cb3f28ed190
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-04 16:17:08 -0600

    Add files via upload

---
+++ script/c955.lua
@@ -0,0 +1,102 @@
+--Destruction Sword Styles
+local s,id=GetID()
+function s.initial_effect(c)
+	--copy trap
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_REMOVE)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetHintTiming(TIMING_DRAW_PHASE+TIMINGS_CHECK_MONSTER_E,TIMING_DRAW_PHASE+TIMINGS_CHECK_MONSTER_E)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_CANNOT_DISABLE)
+	e2:SetCode(511001408)
+	c:RegisterEffect(e2)
+	--Activate
+	local e3=Effect.CreateEffect(c)
+	e3:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+e3:SetRange(LOCATION_GRAVE)
+	e3:SetCost(Cost.SelfBanish)
+	e3:SetTarget(s.target1)
+	e3:SetOperation(s.activate1)
+	c:RegisterEffect(e3)
+	--Can be activated from the hand
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,0))
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+	e4:SetCondition(s.handcon)
+	c:RegisterEffect(e4)
+end
+s.listed_series={SET_DESTRUCTION_SWORD,SET_BUSTER_BLADER}
+function s.tgfilter(c,e,tp)
+	return c:IsFaceup() and c:IsSetCard(SET_BUSTER_BLADER) and c:IsCanBeFusionMaterial()
+		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp,c)
+end
+function s.spfilter(c,e,tp,mc)
+	if Duel.GetLocationCountFromEx(tp,tp,mc,c)<=0 then return false end
+	local mustg=aux.GetMustBeMaterialGroup(tp,nil,tp,c,nil,REASON_FUSION)
+	return c:IsSetCard(SET_BUSTER_BLADER) and c:ListsCodeAsMaterial(mc:GetCode()) and c:IsCanBeSpecialSummoned(e,SUMMON_TYPE_FUSION,tp,true,true)
+		and (#mustg==0 or (#mustg==1 and mustg:IsContains(mc)))
+end
+function s.target1(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc==0 then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(tp) and s.tgfilter(chkc,e,tp) end
+	if chk==0 then return Duel.IsExistingTarget(s.tgfilter,tp,LOCATION_MZONE,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+	Duel.SelectTarget(tp,s.tgfilter,tp,LOCATION_MZONE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_EXTRA)
+end
+function s.activate1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and tc:IsFaceup() and tc:IsCanBeFusionMaterial() and not tc:IsImmuneToEffect(e) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local sg=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp,tc)
+		local sc=sg:GetFirst()
+		if sc then
+			sc:SetMaterial(Group.FromCards(tc))
+			Duel.SendtoGrave(tc,REASON_EFFECT+REASON_MATERIAL+REASON_FUSION)
+			Duel.BreakEffect()
+			Duel.SpecialSummon(sc,SUMMON_TYPE_FUSION,tp,tp,false,false,POS_FACEUP)
+			sc:CompleteProcedure()
+		end
+	end
+end
+function s.cfilter(c)
+	return not c:IsHasEffect(511001408) and not c:IsHasEffect(511001283) and s.filter(c)
+end
+function s.filter(c)
+	return c:IsSpellTrap() and c:IsSetCard(SET_DESTRUCTION_SWORD) and c:IsAbleToDeck() and c:CheckActivateEffect(false,false,false)~=nil
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE+LOCATION_REMOVED) and chkc:IsControler(1-tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.cfilter,tp,LOCATION_GRAVE+LOCATION_REMOVED,0,1,nil) end
+	e:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,0,LOCATION_GRAVE+LOCATION_REMOVED)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsFaceup() and tc:IsRelateToEffect(e) and Duel.Remove(tc,POS_FACEUP,REASON_EFFECT)>0 then
+		local te,teg,tep,tev,tre,tr,trp=tc:CheckActivateEffect(false,false,true)
+		if not te then return end
+		e:SetCategory(te:GetCategory())
+		e:SetProperty(te:GetProperty())
+		local co=te:GetCost()
+		local tg=te:GetTarget()
+		local op=te:GetOperation()
+		if co then co(e,tp,teg,tep,tev,tre,tr,trp,1) end
+		if tg then tg(e,tp,teg,tep,tev,tre,tr,trp,1) end
+		if op then op(e,tp,teg,tep,tev,tre,tr,trp) end
+	end
+end
+function s.handcon(e)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,SET_BUSTER_BLADER),e:GetHandlerPlayer(),LOCATION_MZONE,0,1,nil)
+end
