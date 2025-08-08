3685de719353e65963cfed235623e7ca19c35cab
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-08 14:13:04 -0600

    Add files via upload

---
+++ script/c960.lua
@@ -0,0 +1,78 @@
+--Qli field
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetOperation(s.activate)
+	c:RegisterEffect(e0)
+		--extra summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,1))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+	e1:SetRange(LOCATION_FZONE)
+	e1:SetTargetRange(LOCATION_HAND,0)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard,SET_QLI))
+	e1:SetValue(0x1)
+	c:RegisterEffect(e1)
+	--summon
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(92001300,0))
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_SUMMON_PROC)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetTargetRange(LOCATION_HAND,LOCATION_HAND)
+	e2:SetCondition(s.sumcon)
+	e2:SetOperation(s.sumop)
+	c:RegisterEffect(e2)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_SET_PROC)
+	c:RegisterEffect(e3)
+end
+function s.thfilter(c)
+	return c:IsCode(27279764) and c:IsAbleToHand()
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	local g=Duel.GetMatchingGroup(s.thfilter,tp,LOCATION_DECK,0,nil)
+	if g:GetCount()>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg=g:Select(tp,1,1,nil)
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg)
+	end
+end
+
+
+function s.sumfilter(c,e,tp)
+	return c:IsAbleToGraveAsCost()
+		and c:IsFaceup() and c:IsSetCard(SET_QLI) and c:IsType(TYPE_PENDULUM)
+end
+function s.tdcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.tdcostfilter,tp,LOCATION_EXTRA,0,1,nil,e,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+	local g=Duel.SelectMatchingCard(tp,s.tdcostfilter,tp,LOCATION_EXTRA,0,1,1,nil,e,tp)
+	Duel.SendtoGrave(g,REASON_COST)
+	e:SetLabel(g:GetFirst():GetType()&(TYPE_EXTRA|TYPE_PENDULUM))
+end
+
+
+
+function s.sumcon(e,c,minc)
+	if c==nil then return true end
+	local mi,ma=c:GetTributeRequirement()
+	if mi<minc then mi=minc end
+	if ma<mi then return false end
+	e:SetLabel(mi)
+	return Duel.IsExistingMatchingCard(s.sumfilter,c:GetControler(),LOCATION_EXTRA,0,mi,nil) and Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+end
+function s.sumop(e,tp,eg,ep,ev,re,r,rp,c)
+	local mi,ma=c:GetTributeRequirement()
+		local g=Duel.GetMatchingGroup(s.sumfilter,tp,LOCATION_EXTRA,0,nil)
+	local g1=g:Select(tp,mi,mi,true,nil)
+	Duel.SendtoGrave(g1,REASON_COST) 
+end
