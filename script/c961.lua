c19545e141108a7be2403b9962dca6d7c73df123
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-09 10:48:42 -0600

    Update c961.lua

--- script/c961.lua
+++ script/c961.lua
@@ -6,9 +6,23 @@ function s.initial_effect(c)
 	e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
 	e0:SetType(EFFECT_TYPE_ACTIVATE)
 	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetCountLimit(1,{1,id})
 	e0:SetTarget(s.pctg)
 	e0:SetOperation(s.pcop)
 	c:RegisterEffect(e0)
+		--salvage
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,2))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_IGNITION)
+	e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCondition(aux.exccon)
+	e3:SetCountLimit(1,{2,id})
+	e3:SetCost(aux.bfgcost)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
 end
 s.listed_series={SET_QLI}
 function s.pcfilter(c)
@@ -65,3 +79,19 @@ end
 function s.splimit(e,c)
 	return not c:IsSetCard(SET_QLI)
 end
+function s.thfilter(c)
+	return  (c:IsSetCard(SET_QLI) and not c:IsCode(id)) and c:IsAbleToHand()
+end
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
+
