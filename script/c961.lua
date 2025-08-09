4df689afe1fb37a8d1b242f1cd6f88b5d385e90e
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-09 10:53:23 -0600

    Update c961.lua

--- script/c961.lua
+++ script/c961.lua
@@ -80,19 +80,20 @@ function s.splimit(e,c)
 	return not c:IsSetCard(SET_QLI)
 end
 function s.thfilter(c)
-	return  (c:IsSetCard(SET_QLI) and not c:IsCode(id)) and c:IsAbleToHand()
+	return  c:IsSpell() and (c:IsSetCard(SET_QLI) and not c:IsCode(id)) and c:IsAbleToHand()
 end
 function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 function s.thop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
 	end
 end
+
 
 
