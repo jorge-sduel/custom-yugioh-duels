31f3c72958c52f04148fb9b2c682c49a373a3459
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-09 09:12:40 -0600

    Add files via upload

---
+++ script/c961.lua
@@ -0,0 +1,50 @@
+--Qli field
+local s,id=GetID()
+function s.initial_effect(c)
+	c:SetUniqueOnField(1,0,id)
+	--activate
+	local e0=Effect.CreateEffect(c)
+	e0:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e0:SetType(EFFECT_TYPE_ACTIVATE)
+	e0:SetCode(EVENT_FREE_CHAIN)
+	e0:SetTarget(s.pctg)
+	e0:SetOperation(s.pcop)
+	c:RegisterEffect(e0)
+end
+s.listed_series={SET_QLI}
+function s.pcfilter(c)
+	return c:IsType(TYPE_PENDULUM) and c:IsSetCard(SET_QLI) and not c:IsForbidden()
+end
+function s.filter(c)
+	return c:IsType(TYPE_TRAP) and c:IsSetCard(SET_QLI) and c:IsSSetable()
+end
+function s.pctg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckPendulumZones(tp)
+		and Duel.IsExistingMatchingCard(s.pcfilter,tp,LOCATION_DECK,0,1,nil) end
+end
+function s.pcop(e,tp,eg,ep,ev,re,r,rp)
+	if not e:GetHandler():IsRelateToEffect(e) then return end
+	if not Duel.CheckPendulumZones(tp) then return end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+	local g=Duel.SelectMatchingCard(tp,s.pcfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.MoveToField(g:GetFirst(),tp,tp,LOCATION_PZONE,POS_FACEUP,true)
+	if Duel.CheckReleaseGroupCost(tp,nil,1,true,nil,nil) and Duel.GetLocationCount(tp,LOCATION_SZONE)>-1
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_DECK,0,1,nil) and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+local sg=Duel.SelectReleaseGroupCost(tp,nil,1,1,true,nil,nil)
+		Duel.Release(sg,REASON_COST)
+	local tc=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil):GetFirst()
+	if tc then
+		Duel.SSet(tp,tc)
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+		e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetDescription(aux.Stringid(id,0))
+		tc:RegisterEffect(e1)
+   end
+	 end
+	end
+end
