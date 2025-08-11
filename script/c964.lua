357fb2f84bc2da53af4273c6fd98ed79040d9333
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-11 12:10:03 -0600

    Add files via upload

---
+++ script/c964.lua
@@ -0,0 +1,67 @@
+--Data qlir
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--return
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.operation)
+	c:RegisterEffect(e2)
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetCode(EFFECT_CANNOT_DISEFFECT)
+	e4:SetRange(LOCATION_SZONE)
+	e4:SetValue(s.chainfilter)
+	c:RegisterEffect(e4)
+	local e5=Effect.CreateEffect(c)
+	e5:SetType(EFFECT_TYPE_FIELD)
+	e5:SetCode(EFFECT_CANNOT_DISABLE)
+	e5:SetRange(LOCATION_SZONE)
+	e5:SetTargetRange(LOCATION_ONFIELD,0)
+	e5:SetTarget(s.distarget)
+	c:RegisterEffect(e5)
+end
+s.listed_names={id}
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(SET_QLI) and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_ONFIELD) and chkc:IsControler(tp) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_ONFIELD,0,1,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_RTOHAND)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_ONFIELD,0,1,1,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,1,0,0)
+end
+function s.sumfilter(c)
+	return c:IsSetCard(SET_QLI) and c:IsSummonable(true,nil)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc:IsRelateToEffect(e) and Duel.SendtoHand(tc,nil,REASON_EFFECT)>0 and tc:IsLocation(LOCATION_HAND) then
+		if Duel.IsExistingMatchingCard(s.sumfilter,tp,LOCATION_HAND,0,1,nil)
+			and Duel.SelectYesNo(tp,aux.Stringid(id,1)) then
+			Duel.BreakEffect()
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SUMMON)
+			local g=Duel.SelectMatchingCard(tp,s.sumfilter,tp,LOCATION_HAND,0,1,1,nil)
+			Duel.Summon(tp,g:GetFirst(),true,nil)
+		end
+	end
+end
+function s.distarget(e,c)
+	return c:IsSetCard(SET_QLI) and c:IsType(TYPE_MONSTER)
+end
+function s.chainfilter(e,ct)
+	local p=e:GetHandlerPlayer()
+	local te,tp=Duel.GetChainInfo(ct,CHAININFO_TRIGGERING_EFFECT,CHAININFO_TRIGGERING_PLAYER)
+	return p==tp and te:GetActiveType()==TYPE_MONSTER and te:IsSetCard(SET_QLI) 
+end
