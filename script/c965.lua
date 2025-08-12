42d90fe4d467333c54dff0e8ce3100a01cc5781a
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-12 10:55:42 -0600

    Update c965.lua

--- script/c965.lua
+++ script/c965.lua
@@ -67,9 +67,9 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
-	if e:GetLabel()==1 then
-	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
-	if not tc or not tc:IsLocation(LOCATION_GRAVE) or not tc:IsRelateToEffect(e) then return end
+	if e:GetLabel()==0 then
+	--[[if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+	if not tc or not tc:IsLocation(LOCATION_GRAVE) or not tc:IsRelateToEffect(e) then return end]]
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,tc:GetLevel(),tc:GetCode(),tc:GetAttribute(),e,tp)
 		if #g>0 then
