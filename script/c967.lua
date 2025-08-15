11301f6a5e686f94439046fa1a53f9b73817c65c
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-15 16:03:02 -0600

    Add files via upload

---
+++ script/c967.lua
@@ -0,0 +1,112 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+	--Fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,21,aux.FilterBoolFunctionEx(Card.IsType,TYPE_PENDULUM))
+	--Halve damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCode(EFFECT_CHANGE_DAMAGE)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetTargetRange(0,1)
+	e1:SetValue(s.val)
+	c:RegisterEffect(e1)
+	--atk
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetCategory(CATEGORY_ATKCHANGE)
+	e2:SetDescription(aux.Stringid(900787,0))
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1)
+	e2:SetCondition(s.atkcon)
+	e2:SetTarget(s.atktg)
+	e2:SetOperation(s.atkop)
+	c:RegisterEffect(e2)
+	--cannot attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_ATTACK)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetTarget(s.atktarget)
+	c:RegisterEffect(e3)
+	--disable
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_FIELD)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetTargetRange(0,LOCATION_MZONE)
+	e4:SetTarget(s.disable)
+	e2:SetCode(EFFECT_DISABLE)
+	c:RegisterEffect(e2)
+	--salvage
+	local e5=Effect.CreateEffect(c)
+	e5:SetDescription(aux.Stringid(21,0))
+	e5:SetCategory(CATEGORY_TOHAND)
+	e5:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e5:SetType(EFFECT_TYPE_IGNITION)
+	e5:SetRange(LOCATION_MZONE)
+	e5:SetCountLimit(1)
+	e5:SetTarget(s.target)
+	e5:SetOperation(s.operation)
+	c:RegisterEffect(e5)
+end
+s.listed_names={21}
+function s.val(e,re,dam,r,rp,rc)
+	return math.floor(dam/2)
+end
+function s.filter(c)
+	return c:IsFaceup()
+end
+function s.atktg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter,tp,0,LOCATION_MZONE,1,nil) end
+end
+function s.atkop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local g=Duel.GetMatchingGroup(s.filter,tp,0,LOCATION_MZONE,nil)
+	if #g>0 then
+		local atk=0
+		local tc=g:GetFirst()
+		while tc do
+			atk=atk+tc:GetAttack()
+			tc=g:GetNext()
+		end
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetValue(atk)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD_DISABLE+RESET_PHASE+PHASE_END)
+		c:RegisterEffect(e1)
+	end
+end
+function s.atktarget(e,c)
+	return c:GetAttack()~=c:GetBaseAttack()
+end
+function s.disable(e,c)
+	return c:GetAttack()~=c:GetBaseAttack()
+end
+function s.spcfilter1(c,tp)
+	return c:IsType(TYPE_SPELL) and Duel.IsExistingMatchingCard(s.spcfilter2,tp,LOCATION_GRAVE,0,2,c,c:GetCode()) and c:IsAbleToHand()
+end
+function s.spcfilter2(c,code)
+	return c:IsType(TYPE_SPELL) and c:IsCode(code) and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and s.spcfilter1(chkc,tp) end
+	if chk==0 then return Duel.IsExistingMatchingCard(s.spcfilter1,tp,LOCATION_GRAVE,0,1,c,tp) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.spcfilter1,tp,LOCATION_GRAVE,0,1,1,c,tp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g2=Duel.SelectMatchingCard(tp,s.spcfilter2,tp,LOCATION_GRAVE,0,2,2,g:GetFirst(),g:GetFirst():GetCode())
+	g:Merge(g2)
+	Duel.SetTargetCard(g)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,g:GetCount(),0,0)
+end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if sg:GetCount()>0 then
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+	end
+end
