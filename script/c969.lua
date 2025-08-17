d13f736b6d7fd587252cbc686497acd8f060e022
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-17 12:39:55 -0600

    Update c969.lua

--- script/c969.lua
+++ script/c969.lua
@@ -1,6 +1,8 @@
 --
 local s,id=GetID()
 function s.initial_effect(c)
+		--pendulum summon
+	Pendulum.AddProcedure(c)
 	--spsummon
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
