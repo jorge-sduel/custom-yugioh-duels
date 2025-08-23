9f215ada864f983753c23806ca458fa538e7a4bb
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-23 09:17:12 -0600

    Add files via upload

---
+++ script/c973.lua
@@ -0,0 +1,88 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+		--pendulum summon
+	Pendulum.AddProcedure(c)
+	--special summon
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_SPSUMMON_PROC)
+	e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
+	e1:SetCondition(s.spcon)
+	c:RegisterEffect(e1)
+	--Draw
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_RECOVER)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCost(s.cost)
+	e2:SetTarget(s.target)
+	e2:SetOperation(s.activate)
+	c:RegisterEffect(e2)
+	--cannot attack
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetTargetRange(0,LOCATION_MZONE)
+	e3:SetTarget(s.atktarget)
+	c:RegisterEffect(e3)
+	--damage
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,2))
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCountLimit(1)
+	e1:SetCost(s.cost2)
+	e1:SetOperation(s.operation)
+	c:RegisterEffect(e1)
+end
+s.listed_series={0x572}
+function s.spcon(e,c)
+	if c==nil then return true end
+	return Duel.GetLocationCount(c:GetControler(),LOCATION_MZONE)>0
+		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsSetCard,0x572),c:GetControler(),LOCATION_ONFIELD,0,1,nil)
+end
+function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsReleasable() end
+	local c=e:GetHandler()
+	Duel.Release(c,REASON_COST)
+end
+function s.filter(c)
+	return c:IsMonster() and c:IsAbleToDeck()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsPlayerCanDraw(tp)
+		and Duel.IsExistingMatchingCard(s.filter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,1,e:GetHandler()) end
+	Duel.SetTargetPlayer(tp)
+	Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_EXTRA)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,1)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local p=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER)
+	local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_EXTRA+LOCATION_GRAVE,0,nil)
+	if g:GetCount()==0 then return end
+	Duel.SendtoDeck(g,nil,2,REASON_EFFECT)
+	Duel.ShuffleDeck(p)
+	Duel.BreakEffect()
+	Duel.Draw(p,2,REASON_EFFECT)
+end
+function s.atktarget(e,c)
+	return c:IsAttackAbove(2000)
+end
+function s.cost2(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,Card.IsType,1,false,nil,nil,TYPE_MONSTER)
+ end
+	local g=Duel.SelectReleaseGroupCost(tp,Card.IsType,1,10,false,nil,nil,TYPE_MONSTER)
+	local ct=Duel.Release(g,REASON_COST)
+	e:SetLabel(ct)
+end
+function s.damop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Damage(tp,e:GetLabel()*500,REASON_EFFECT)
+end
+
