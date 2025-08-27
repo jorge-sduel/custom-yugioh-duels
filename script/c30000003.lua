d885df0acfb6540ac537f2d9d40d7cb223731032
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-08-27 14:34:04 -0600

    Update c30000003.lua

--- script/c30000003.lua
+++ script/c30000003.lua
@@ -41,7 +41,7 @@ function c30000003.attop(e,tp,eg,ep,ev,re,r,rp)
 		if tc:IsFaceup() and tc:IsRelateToEffect(e) then
 			Duel.Hint(HINT_SELECTMSG,tp,562)
 			local catt=tc:GetAttribute()
-            if catt~=0x1 and catt~=0x2 and catt~=0x4 and catt~=0x8 and catt~=0x10 and catt~=0x20 and catt~=0x40 then catt=0x0 end
+         -- if catt~=0x1 and catt~=0x2 and catt~=0x4 and catt~=0x8 and catt~=0x10 and catt~=0x20 and catt~=0x40 then catt=0x0 end
 			local att=tc:AnnounceAnotherAttribute(tp,1,ATTRIBUTE_ALL)
 			local e1=Effect.CreateEffect(c)
 			e1:SetType(EFFECT_TYPE_SINGLE)
@@ -124,6 +124,7 @@ function c30000003.attcount(g)
 	if g:Filter(c30000003.attfilter,nil,ATTRIBUTE_DIVINE)~=nil then att=att+1 end
 	return att
 end
+
 
 
 
