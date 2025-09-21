8b4071c71c9456f61c00a8e9003d36ed366bec18
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-09-21 09:08:51 -0600

    Add files via upload

---
+++ script/c979.lua
@@ -0,0 +1,71 @@
+--
+local s,id=GetID()
+function s.initial_effect(c)
+s.Fallen_Angel=true
+	Synchro.AddProcedure(c,nil,1,1,Synchro.NonTuner(nil),1,99)
+	--Change a monster effect to "Destroy 1 Set Spell/Trap your opponent controls"
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_CHAIN_SOLVING)
+	e1:SetRange(LOCATION_MZONE)
+--	e1:SetCountLimit(1,{id,1})
+	e1:SetCondition(s.chcon1)
+	e1:SetOperation(s.chop1)
+	c:RegisterEffect(e1)
+--
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_SINGLE)
+	e2:SetCode(EFFECT_IMMUNE_EFFECT)
+	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetValue(s.immval)
+	c:RegisterEffect(e2)
+	--cannot target
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+	e3:SetValue(aux.tgoval)
+	c:RegisterEffect(e3)
+	--indes
+	local e4=Effect.CreateEffect(c)
+	e4:SetType(EFFECT_TYPE_SINGLE)
+	e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e4:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+	e4:SetRange(LOCATION_MZONE)
+	e4:SetValue(s.indval)
+	c:RegisterEffect(e4)
+end
+function s.indval(e,re,tp)
+	return tp~=e:GetHandlerPlayer()
+end
+function s.immval(e,te)
+	return te:IsActiveType(TYPE_MONSTER) and te:IsActivated()
+		and not te:IsActiveType(TYPE_EXTRA)
+end
+function s.chcon1(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp and re:IsActiveType(TYPE_SPELL+TYPE_TRAP)
+end
+function s.chop1(e,tp,eg,ep,ev,re,r,rp)
+	local g=Group.CreateGroup()
+	Duel.ChangeTargetCard(ev,g)
+	Duel.ChangeChainOperation(ev,s.repop)
+end
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(s.confilter,tp,0, LOCATION_MZONE,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(500)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+Duel.Damage(tp,500,REASON_EFFECT)
+	end
+end
+function s.confilter(c)
+	return c:IsFaceup() and c:IsRace(RACE_FAIRY) and c:IsAttribute(ATTRIBUTE_DARK)
+end
