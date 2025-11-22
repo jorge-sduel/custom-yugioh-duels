569be7c80167f424cb22562c73f37e98a9e6643b
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-11-22 12:00:34 -0600

    Update c252.lua

--- script/c252.lua
+++ script/c252.lua
@@ -1,15 +1,16 @@
 --SS fus water
-function c252.initial_effect(c)
+local s,id=GetID() 
+function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
-	Fusion.AddProcFun2(c,c252.ffilter,aux.FilterBoolFunction(Card.IsSetCard,0x1115),true)
+	Fusion.AddProcFun2(c,s.ffilter,aux.FilterBoolFunction(Card.IsSetCard,0x1115),true)
 	--atk down
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(0,LOCATION_MZONE)
-	e1:SetValue(c252.atkval)
+	e1:SetValue(s.atkval)
 	c:RegisterEffect(e1)
 	--damage
 	local e2=Effect.CreateEffect(c)
@@ -19,8 +20,8 @@ function c252.initial_effect(c)
 	e2:SetRange(LOCATION_MZONE)
 	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
 	e2:SetCountLimit(1)
-	e2:SetTarget(c252.rectg)
-	e2:SetOperation(c252.recop)
+	e2:SetTarget(s.rectg)
+	e2:SetOperation(s.recop)
 	c:RegisterEffect(e2)
 	--Special summon
 	local e7=Effect.CreateEffect(c)
@@ -29,34 +30,34 @@ function c252.initial_effect(c)
 	e7:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e7:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DAMAGE_STEP)
 	e7:SetCode(EVENT_DESTROYED)
-	e7:SetTarget(c252.sptg)
-	e7:SetOperation(c252.spop)
+	e7:SetTarget(s.sptg)
+	e7:SetOperation(s.spop)
 	c:RegisterEffect(e7)
 end
-function c252.ffilter(c)
+function s.ffilter(c)
 	return c:IsCode(90673288)
 end
-function c252.atkval(e)
+function s.atkval(e)
 	return Duel.GetMatchingGroupCount(Card.IsType,e:GetHandlerPlayer(),LOCATION_GRAVE,0,nil,TYPE_LINK)*-400
 end
-function c252.filter(c)
+function s.filter(c)
 	return c:IsFaceup() and c:IsType(TYPE_SPELL)
 end
-function c252.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
+function s.rectg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	local ct=Duel.GetMatchingGroupCount(c252.filter,tp,LOCATION_GRAVE,0,nil)
 	Duel.SetTargetPlayer(1-tp)
 	Duel.SetTargetParam(ct*100)
 	Duel.SetOperationInfo(0,CATEGORY_DAMAGE,nil,0,tp,ct*100)
 end
-function c252.recop(e,tp,eg,ep,ev,re,r,rp)
+function s.recop(e,tp,eg,ep,ev,re,r,rp)
 	local ct=Duel.GetMatchingGroupCount(c252.filter,tp,LOCATION_GRAVE,0,nil)
 	Duel.Recover(tp,ct*100,REASON_EFFECT)
 end
-function c252.spfilter(c,e,tp)
+function s.spfilter(c,e,tp)
 	return c:IsSetCard(0x1115) and not c:IsType(TYPE_FUSION) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
-function c252.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	if chkc then return chkc:IsLocation(LOCATION_GRAVE) and chkc:IsControler(tp) and c252.spfilter(chkc,e,tp) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingTarget(c252.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp) end
@@ -64,9 +65,10 @@ function c252.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local g=Duel.SelectTarget(tp,c252.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
-function c252.spop(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc:IsRelateToEffect(e) then
 		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
 	end
+
 end
