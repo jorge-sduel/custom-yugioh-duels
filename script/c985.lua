e203e1dd65200314a61e441e3f3a5fc21bcf2513
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2025-12-25 19:13:05 -0600

    Update c985.lua

--- script/c985.lua
+++ script/c985.lua
@@ -40,7 +40,7 @@ function s.repop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(s.confilter,tp,0, LOCATION_MZONE,nil)
 	local tc=g:GetFirst()
 	for tc in aux.Next(g) do
-			local e1=Effect.CreateEffect(c)
+			local e1=Effect.CreateEffect(e:GetHandler())
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_SET_ATTACK)
 		e1:SetReset(RESETS_STANDARD_DISABLE_PHASE_END)
