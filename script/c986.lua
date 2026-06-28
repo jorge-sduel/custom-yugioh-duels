1388636b28e5d52feb63c853ad54f0a6be17bc77
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2026-06-28 13:13:44 -0600

    Update c986.lua

--- script/c986.lua
+++ script/c986.lua
@@ -61,6 +61,14 @@ function s.initial_effect(c)
 	e7:SetTarget(s.tglimit)
 	e7:SetValue(1)
 	c:RegisterEffect(e7)
+		--ATK down
+	local e8=Effect.CreateEffect(c)
+	e8:SetDescription(aux.Stringid(id,0))
+	e8:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
+	e8:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
+	e8:SetCode(EVENT_BE_BATTLE_TARGET)
+	e8:SetOperation(s.operation)
+	c:RegisterEffect(e8)
 end
 s.listed_series={0x10ec}
 function s.matfilter(c,lc,sumtype,tp)
@@ -98,3 +106,36 @@ end
 function s.tglimit(e,c)
 	return c~=e:GetHandler()
 end
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) and c:IsFaceup() then
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(300)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+		end 
+	end
+end
+
+
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetMatchingGroup(Card.IsFaceup,tp,LOCATION_MZONE,0,nil)
+	local tc=g:GetFirst()
+	for tc in aux.Next(g) do
+		local e1=Effect.CreateEffect(e:GetHandler())
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_UPDATE_ATTACK)
+		e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+		e1:SetValue(300)
+		e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+		tc:RegisterEffect(e1)
+	end
+end
+
