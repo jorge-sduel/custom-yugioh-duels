39d48ef87a617a284b022a67796e5ab29362e9fd
jorge-sduel <82419268+jorge-sduel@users.noreply.github.com>
2026-06-28 11:48:36 -0600

    Update c986.lua

--- script/c986.lua
+++ script/c986.lua
@@ -1,4 +1,6 @@
---Abyss Actor
+
+--Abyss Actor
+
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
@@ -18,7 +20,7 @@ function s.initial_effect(c)
 		local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	e2:SetCode(EVENT_PREDRAW)
-	e2:SetRange(LOCATION_SZONE)
+	e2:SetRange(LOCATION_MZONE)
 	e2:SetOperation(s.drop)
 	c:RegisterEffect(e2)
 --
